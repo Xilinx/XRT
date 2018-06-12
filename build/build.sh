94a1355e4d529f0881e6b3d3f4325cd3c4073449
Soren Soe <soren.soe@gonsoe.com>
2018-06-11 19:43:53 -0700

    Add -j4 to make in build script

--- build/build.sh
+++ build/build.sh
@@ -4,12 +4,12 @@ here=$PWD
 mkdir -p Debug Release
 cd Debug
 cmake -DCMAKE_BUILD_TYPE=Debug ../../src
-make DESTDIR=$PWD install
+make -j4 DESTDIR=$PWD install
 cd $here
 
 cd Release
 cmake -DCMAKE_BUILD_TYPE=Release ../../src
-make DESTDIR=$PWD install
+make -j4 DESTDIR=$PWD install
 cd $here
 
 
