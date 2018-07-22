df7ad5a2eb15f3ea22178c8acd3382c261d69f37
Sonal Santan <sonal.santan@xilinx.com>
2018-07-21 17:00:21 -0700

    xrtdeps.sh should install EPEL automatically. First draft of cl_ext_xilinx.h to help completely move to Khronos headers obsoleting our copy in future.

--- src/runtime_src/tools/scripts/xrtdeps.sh
+++ src/runtime_src/tools/scripts/xrtdeps.sh
@@ -93,17 +93,34 @@ FLAVOR=`grep '^ID=' /etc/os-release | awk -F= '{print $2}'`
 FLAVOR=`echo $FLAVOR | tr -d '"'`
 
 if [ $FLAVOR == "ubuntu" ]; then
+    echo "Installing Ubuntu packages..."
     sudo apt install -y "${UB_LIST[@]}"
+fi
+
+# Enable EPEL on CentOS/RHEL
+if [ $FLAVOR == "centos" ]; then
+    echo "Enabling EPEL repository..."
+    sudo yum install epel-release
+elif [ $FLAVOR == "rhel" ]; then
+    echo "Enabling EPEL repository..."
+    rpm -q --quiet epel-release
+    if [ $? != 0 ]; then
+	sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+	sudo yum check-update
+    fi
 fi
 
 # Enable GCC 6 compiler set on RHEL/CentOS 7.X
 if [ $FLAVOR == "rhel" ]; then
+    echo "Enabling RHEL SCL repository..."
     sudo yum-config-manager --enable rhel-server-rhscl-7-rpms
 elif [ $FLAVOR == "centos" ]; then
+    echo "Enabling CentOS SCL repository..."
     sudo yum install centos-release-scl
 fi
 
 if [ $FLAVOR == "rhel" ] || [ $FLAVOR == "centos" ]; then
+    echo "Installing RHEL/CentOS packages..."
     sudo yum install -y "${RH_LIST[@]}"
     sudo yum install devtoolset-6
 fi
