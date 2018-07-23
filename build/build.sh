245d96c74d13c61fba31d1100b638e75446f3799
Soren Soe <soren.soe@xilinx.com>
2018-07-23 08:26:08 -0700

    Add support for building with Coverity

--- build/build.sh
+++ build/build.sh
@@ -10,11 +10,13 @@ usage()
     echo
     echo "[-help]                    List this help"
     echo "[clean|-clean]             Remove build directories"
+    echo "[-coverity]                Run a Coverity build, requires admin priviledges to Coverity"
 
     exit 1
 }
 
 clean=0
+covbuild=0
 while [ $# -gt 0 ]; do
     case "$1" in
         -help)
@@ -22,6 +24,20 @@ while [ $# -gt 0 ]; do
             ;;
         clean|-clean)
             clean=1
+            shift
+            ;;
+        coverity|-coverity)
+            covbuild=1
+            shift
+            ;;
+        -covuser)
+            shift
+            covuser=$1
+            shift
+            ;;
+        -covpw)
+            shift
+            covpw=$1
             shift
             ;;
         *)
@@ -34,10 +50,27 @@ done
 here=$PWD
 cd $BUILDDIR
 
-if [ $clean == 1 ]; then
+if [[ $clean == 1 ]]; then
     echo $PWD
     echo "/bin/rm -rf Release Debug"
-    /bin/rm -rf Release Debug
+    /bin/rm -rf Release Debug Coverity
+    exit 0
+fi
+
+if [[ $covbuild == 1 ]]; then
+    if [[ -z ${covuser+x} ]]; then
+        echo -n "Enter coverity user name: "
+        read covuser
+    fi
+    if [[ -z ${covpw+x} ]]; then
+    echo -n "Enter coverity password: "
+    read covpw
+    fi
+    mkdir -p Coverity
+    cd Coverity
+    cmake -DCMAKE_BUILD_TYPE=Release ../../src
+    make -j4 COVUSER=$covuser COVPW=$covpw DATE="`git rev-parse --short HEAD`" coverity
+    cd $here
     exit 0
 fi
 
