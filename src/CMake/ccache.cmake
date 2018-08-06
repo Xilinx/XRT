bf1fb649f98fb0d86bb3fd6bc0fb737b2e1ec4c2
Soren Soe <soren.soe@xilinx.com>
2018-08-06 16:54:36 -0700

    Replace path to compile wrapper
    Also add -verbose option to build script

--- src/CMake/ccache.cmake
+++ src/CMake/ccache.cmake
@@ -1,12 +1,20 @@
 if (RDI_CCACHE)
 
-  find_program(gccwrap /home/soeren/perforce/sbx-p4/HEAD/src/misc/compiler/gccwrap/lnx64/bin/gccwrap)
-  find_program(gccarwrap /home/soeren/perforce/sbx-p4/HEAD/src/misc/compiler/gccwrap/lnx64/bin/gccarwrap)
+  find_program(gccwrap /tools/batonroot/rodin/devkits/lnx64/ccwrap/gccwrap)
+  find_program(gccarwrap /tools/batonroot/rodin/devkits/lnx64/ccwrap/gccarwrap)
 
   if (gccwrap)
-    message ("-- Using compile cache from $ENV{RDI_CCACHEROOT}")
+    message ("-- Using compile cache wrapper ${gccwrap} with cache in $ENV{RDI_CCACHEROOT}")
     set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${gccwrap} --with-cache-rw")
-    #  set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK "${gccarwrap} --with-cache-rw")
+  endif ()
+
+  if (gccarwrap)
+    set(CMAKE_CXX_ARCHIVE_CREATE "<CMAKE_AR> -qc -o <TARGET> <LINK_FLAGS> <OBJECTS>")
+    set(CMAKE_C_ARCHIVE_CREATE "<CMAKE_AR> -qc -o <TARGET> <LINK_FLAGS> <OBJECTS>")
+    set(CMAKE_CXX_ARCHIVE_FINISH)
+    set(CMAKE_C_ARCHIVE_FINISH)
+    message ("-- Using link cache wrapper ${gccarwrap} with cache in $ENV{RDI_CCACHEROOT}")
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK "${gccarwrap} --with-cache-rw")
   endif ()
 
 endif()
