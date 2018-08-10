100d6816228bb04d18b43e3320f0482037a7bca5
NaAl <nassera@xilinx.com>
2018-08-10 17:22:28 -0600

    Revert "added license and change permissions"
    This reverts commit 2996bd42ca4fa5fe3eb6294251b293fda53a1868.

--- src/runtime_src/tools/scripts/pkgdsa.sh
+++ src/runtime_src/tools/scripts/pkgdsa.sh
@@ -6,6 +6,10 @@
 # The script is assumed to run on a host or docker that has all the
 # necessary rpm/deb tools installed.
 #
+# The script uses xbinst to extract the platform deployment files
+#   - dsabin, msc, bit
+# xbinst is invoked from the specified or default sdx location
+#
 # Examples:
 #
 #  Package DSA from platform dir in default sdx install
@@ -21,7 +25,7 @@
 #       -xrt 2.1.0 \
 #       -cl 12345678
 #
-#  Package DSA from specified DSA platform dir 
+#  Package DSA from specified DSA platform dir using xbinst from default sdx
 #  % pkgdsa.sh -dsa xilinx_vcu1525_dynamic_5_1 \
 #       -dsadir <workspace>2018.2/prep/rdi/sdx/platforms/xilinx_vcu1525_dynamic_5_1 \
 #       -xrt 2.1.0 \
@@ -34,7 +38,6 @@ opt_sdx="/proj/xbuilds/2018.2_daily_latest/installs/lin64/SDx/2018.2"
 opt_xrt=""
 opt_cl=0
 opt_dev=0
-license_dir=""
 
 dsa_version="5.1"
 
@@ -47,10 +50,9 @@ usage()
     echo "-sdx <path>                Full path to SDx install (default: 2018.2_daily_latest)"
     echo "-xrt <version>             Requires xrt >= <version>"
     echo "-cl <changelist>           Changelist for package revision"
-    echo "[-dsadir <path>]           Full path to directory with platforms (default: <sdx>/platforms/<dsa>)"
+    echo "[-dsadir <path>]           Full path to directory with platform (default: <sdx>/platform/<dsa>)"
     echo "[-pkgdir <path>]           Full path to direcory used by rpm,dep,xbins (default: /tmp/pkgdsa)"
     echo "[-dev]                     Build development package"
-    echo "[-license <path>]          Include license file(s) from the <path> in the package"
     echo "[-help]                    List this help"
 
     exit 1
@@ -75,11 +77,6 @@ while [ $# -gt 0 ]; do
             opt_dsa=$1
             shift
             ;;
-        -license)
-            shift
-	    license_dir=$1
-	    shift
-	    ;;
         -dsadir)
             shift
             opt_dsadir=$1
@@ -135,280 +132,39 @@ if [ "X$opt_xrt" == "X" ]; then
   exit 1;
 fi
 
-if [ "X${XILINX_XRT}" == "X" ]; then
-  echo "Environment variable XILINX_XRT is not set.  Please the XRT setup script."
-#  exit 1;
-fi
-
 # get dsa, version, and revision
-# Parse the platform into the basic parts
-#    Syntax: <vender>_<board>_<name>_<versionMajor>_<versionMinor>
-set -- `echo $opt_dsa | tr '_' ' '`
-dsa="${1}-${2}-${3}"
-version="${4}.${5}"
+dsa=$(echo ${opt_dsa:0:${#opt_dsa}-4} | tr '_' '-')
+version=$(echo ${opt_dsa:(-3)} | tr '_' '.')
 revision=$opt_cl
 
 echo "================================================================"
-echo "DSA       : $dsa"
-echo "DSADIR    : $opt_dsadir"
-echo "PKGDIR    : $opt_pkgdir"
-echo "XRT       : $opt_xrt"
-echo "VERSION   : $version"
-echo "REVISION  : $revision"
-echo "XILINX_XRT: $XILINX_XRT"
+echo "DSA     : $dsa"
+echo "DSADIR  : $opt_dsadir"
+echo "PKGDIR  : $opt_pkgdir"
+echo "XRT     : $opt_xrt"
+echo "VERSION : $version"
+echo "REVISION: $revision"
 echo "================================================================"
 
-# DSABIN variables
-dsaFile=""
-mcsPrimary=""
-mcsSecondary=""
-fullBitFile=""
-clearBitstreamFile=""
-dsaXmlFile="dsa.xml"
-featureRomTimestamp=""
-fwScheduler=""
-fwManagement=""
-vbnv=""
-pci_vendor_id="0x0000"
-pci_device_id="0x0000"
-pci_subsystem_id="0x0000"
-dsabinOutputFile=""
-
-createEntityAttributeArray ()
+doxbinst()
 {
-  unset ENTITY_ATTRIBUTES_ARRAY
-  declare -A -g ENTITY_ATTRIBUTES_ARRAY
-
-  for kvp in $ENTITY_ATTRIBUTES; do
-    set -- `echo $kvp | tr '=' ' '`
-    # Remove leading and trailing quotes
-    value=$2
-    value="${value%\"}"
-    value="${value#\"}"
-    ENTITY_ATTRIBUTES_ARRAY[$1]=$value
-  done
-}
-
-readSAX () {
-  # Set Input Field Spearator to be local to this function and change it to
-  # the '>' character
-  local IFS=\>
-
-  # Read the input from stdin and stop when the '<' character is seen.
-  read -d \< ENTITY_LINE
-
-  local ret=$?
-
-  # Remove any trailing "/>" characters
-  ENTITY_LINE="${ENTITY_LINE///>/}"
-
-  # Remove any carriage returns
-  ENTITY_LINE="${ENTITY_LINE//$'\n'/}"
-
-  # Remove any training whitespaces
-  ENTITY_LINE="${ENTITY_LINE%"${ENTITY_LINE##*[![:space:]]}"}" 
-
-  ENTITY_NAME="${ENTITY_LINE%% *}"
-  ENTITY_ATTRIBUTES="${ENTITY_LINE#* }"
-
-  return $ret
-}
-
-recordDsaFiles()
-{
-   # Full Static Bitstream
-   if [ "${ENTITY_ATTRIBUTES_ARRAY[Type]}" == "FULL_BIT" ]; then
-     fullBitFile="${ENTITY_ATTRIBUTES_ARRAY[Name]}"
-   fi
-
-   # MCS Primary
-   if [ "${ENTITY_ATTRIBUTES_ARRAY[Type]}" == "MCS" ]; then
-     mcsPrimary="firmware/${ENTITY_ATTRIBUTES_ARRAY[Name]}"
-   fi
-
-   # MCS Secondary
-   if [ "${ENTITY_ATTRIBUTES_ARRAY[Type]}" == "SECONDARY_MCS" ]; then
-     mcsSecondary="firmware/${ENTITY_ATTRIBUTES_ARRAY[Name]}"
-   fi
-
-   # Clear Bitstream
-   if [ "${ENTITY_ATTRIBUTES_ARRAY[Type]}" == "CLEAR_BIT" ]; then
-     clearBitstreamFile="${ENTITY_ATTRIBUTES_ARRAY[Name]}"
-   fi
-}
-
-readDsaMetaData()
-{
-  # -- Extract the dsa.xml metadata file --
-  unzip -q -d . "${dsaFile}" "${dsaXmlFile}"
-
-  while readSAX; do
-    # Record the data types
-    if [ "${ENTITY_NAME}" == "File" ]; then
-      createEntityAttributeArray
-      recordDsaFiles
-    fi    
-
-    # Record the FeatureRomTimestamp
-    if [ "${ENTITY_NAME}" == "DSA" ]; then
-      createEntityAttributeArray
-
-      featureRomTimestamp="${ENTITY_ATTRIBUTES_ARRAY[FeatureRomTimestamp]}"
-
-      vendor="${ENTITY_ATTRIBUTES_ARRAY[Vendor]}"
-      board="${ENTITY_ATTRIBUTES_ARRAY[BoardId]}"
-      name="${ENTITY_ATTRIBUTES_ARRAY[Name]}"
-      versionMajor="${ENTITY_ATTRIBUTES_ARRAY[VersionMajor]}"
-      versionMinor="${ENTITY_ATTRIBUTES_ARRAY[VersionMinor]}"
-      vbnv=$(printf "%s:%s:%s:%s.%s" "${vendor}" "${board}" "${name}" "${versionMajor}" "${versionMinor}")
-    fi    
-
-    # Record the PCIeID information 
-    if [ "${ENTITY_NAME}" == "PCIeId" ]; then
-      createEntityAttributeArray
-
-      pci_vendor_id="${ENTITY_ATTRIBUTES_ARRAY[Vendor]}"
-      pci_device_id="${ENTITY_ATTRIBUTES_ARRAY[Device]}"
-      pci_subsystem_id="${ENTITY_ATTRIBUTES_ARRAY[Subsystem]}"
-    fi    
-
-    # FeatureRom Data
-    if [ "${ENTITY_NAME}" == "FeatureRom" ]; then
-      createEntityAttributeArray
-
-      # Overright previous value
-      featureRomTimestamp="${ENTITY_ATTRIBUTES_ARRAY[TimeSinceEpoch]}"
-    fi    
-
-  done < "${dsaXmlFile}"
-}
-
-initDsaBinEnvAndVars()
-{
-    # Clean out the dsabin directory
-    /bin/rm -rf "${opt_pkgdir}/dsabin"
-    mkdir -p "${opt_pkgdir}/dsabin"
-    cd "${opt_pkgdir}/dsabin"
-
-    # -- Get the DSA for this platform --
-    dsaFile="${opt_dsadir}/hw/${opt_dsa}.dsa"
-    if [ ! -f "${dsaFile}" ]; then
-       echo "Error: DSA file does not exist: ${dsaFile}"
-       popd >/dev/null
-       exit 1
-    fi
-  
-    # Read the metadata from the dsa.xml file 
-    readDsaMetaData
-  
-    # -- Extract the MCS Files --
-    if [ "${mcsPrimary}" != "" ]; then
-       echo "Info: Extracting MCS Primary file: ${mcsPrimary}"
-       unzip -q -d . "${dsaFile}" "${mcsPrimary}"
-    fi
-
-    if [ "${mcsSecondary}" != "" ]; then
-       echo "Info: Extracting MCS Secondary file: ${mcsSecondary}"
-       unzip -q -d . "${dsaFile}" "${mcsSecondary}"
-    fi
-
-    # -- Extract the bitstreams --
-    if [ "${fullBitFile}" != "" ]; then
-       echo "Info: Extracting Full Bitstream file: ${fullBitFile}"
-       unzip -q -d "./firmware" "${dsaFile}" "${fullBitFile}"
-    fi
-
-    if [ "${clearBitstreamFile}" != "" ]; then
-       echo "Info: Extracting Clear Bitstream file: ${clearBitstreamFile}"
-       unzip -q -d "./firmware" "${dsaFile}" "${clearBitstreamFile}"
-    fi
-
-    # -- Determine firmware --
-    if [[ ${opt_dsa} =~ "xdma" ]]; then
-      fwScheduler="${XILINX_XRT}/share/fw/sched.bin"
-      fwManagement="${XILINX_XRT}/share/fw/xmc.bin"
-    else
-      fwScheduler="${XILINX_XRT}/share/fw/sched.bin"
-      fwManagement="${XILINX_XRT}/share/fw/mgmt.bin"
-    fi
-}
-
-dodsabin()
-{
+    mkdir -p $opt_pkgdir/xbinst
     pushd $opt_pkgdir > /dev/null
-    echo "Creating dsabin for: ${opt_dsa}"
-
-    initDsaBinEnvAndVars
-
-    # Build the xclbincat options
-    xclbinOpts=""
-
-    # -- MCS_PRIMARY image --
-    if [ "$mcsPrimary" != "" ]; then
-       xclbinOpts+=" -s MCS_PRIMARY ${mcsPrimary}"
+    cd $opt_pkgdir
+    if [ -d $opt_pkgdir/xbinst/$opt_dsa ]; then
+        /bin/rm -rf $opt_pkgdir/xbinst/$opt_dsa
     fi
-    
-    # -- MCS_SECONDARY image --
-    if [ "$mcsSecondary" != "" ]; then
-       xclbinOpts+=" -s MCS_SECONDARY ${mcsSecondary}"
-    fi
-    
-    # -- Firmware: Scheduler --
-    if [ "${fwScheduler}" != "" ]; then
-       if [ -f "${fwScheduler}" ]; then
-         xclbinOpts+=" -s SCHEDULER ${fwScheduler}"
-       else
-         echo "Warning: Scheduler firmware does not exist: ${fwScheduler}"
-       fi
-    fi
-    
-    # -- Firmware: Management --
-    if [ "${fwManagement}" != "" ]; then
-       if [ -f "${fwManagement}" ]; then
-         xclbinOpts+=" -s FIRMWARE ${fwManagement}"
-       else
-         echo "Warning: Management firmware does not exist: ${fwManagement}"
-      fi
-    fi
-
-    # -- Clear bitstream --
-    if [ "${clearBitstreamFile}" != "" ]; then
-       xclbinOpts+=" -s CLEAR_BITSTREAM ./firmware/${clearBitstreamFile}"
-    fi
-
-    # -- FeatureRom Timestamp --
-    if [ "${featureRomTimestamp}" != "" ]; then
-       xclbinOpts+=" --kvp featureRomTimestamp:${featureRomTimestamp}"
-    else
-       echo "Warning: Missing featureRomTimestamp"
-    fi
-
-    # -- VBNV --
-    if [ "${vbnv}" != "" ]; then
-       xclbinOpts+=" --kvp platformVBNV:${vbnv}"
-    else
-       echo "Warning: Missing Platform VBNV value"
-    fi
-
-    # -- Mode Hardware PR --
-    xclbinOpts+=" --kvp mode:hw_pr"
-
-    # -- Output filename --
-    localFeatureRomTimestamp="${featureRomTimestamp}"
-    if [ "${localFeatureRomTimestamp}" == "" ]; then
-      localFeatureRomTimestamp="0"
-    fi
-
-    # Build output file and lowercase the name
-    dsabinOutputFile=$(printf "%s-%s-%s-%016x.dsabin" "${pci_vendor_id#0x}" "${pci_device_id#0x}" "${pci_subsystem_id#0x}" "${localFeatureRomTimestamp}")
-    dsabinOutputFile="${dsabinOutputFile,,}"
-    xclbinOpts+=" -o ./firmware/${dsabinOutputFile}"    
-
-
-    echo "${XILINX_XRT}/bin/xclbincat ${xclbinOpts}"
-    ${XILINX_XRT}/bin/xclbincat ${xclbinOpts}
-
+    $opt_sdx/bin/xbinst -f $opt_dsadir/$opt_dsa.xpfm -d $opt_pkgdir/xbinst/$opt_dsa
+    test=$?
     popd >/dev/null
+    if [ "$test" != 0 ]; then
+	echo
+	echo
+	echo "There was an unexpected ERROR executing: "
+	echo "$opt_sdx/bin/xbinst -f $opt_dsadir/$opt_dsa.xpfm -d $opt_pkgdir/xbinst/$opt_dsa"
+	echo "################ xbinst failed! ###############"
+	exit $test
+    fi
 }
 
 dodebdev()
@@ -427,19 +183,9 @@ maintainer: soren.soe@xilinx.com
 
 EOF
 
-    mkdir -p $opt_pkgdir/$dir/opt/xilinx/platforms/$opt_dsa/hw
-    mkdir -p $opt_pkgdir/$dir/opt/xilinx/platforms/$opt_dsa/sw
-    if [ "${license_dir}" != "" ] ; then
-	if [ -d ${license_dir} ] ; then
-	  mkdir -p $opt_pkgdir/$dir/opt/xilinx/platforms/$opt_dsa/license
-	  cp -f ${license_dir}/*  $opt_pkgdir/$dir/opt/xilinx/platforms/$opt_dsa/license
-	fi
-    fi
-    
-    rsync -avz $opt_dsadir/$opt_dsa.xpfm $opt_pkgdir/$dir/opt/xilinx/platforms/$opt_dsa/
-    rsync -avz $opt_dsadir/hw/$opt_dsa.dsa $opt_pkgdir/$dir/opt/xilinx/platforms/$opt_dsa/hw/
-    rsync -avz $opt_dsadir/sw/$opt_dsa.spfm $opt_pkgdir/$dir/opt/xilinx/platforms/$opt_dsa/sw/
-    chmod -R +r $opt_pkgdir/$dir/opt/xilinx/platforms/$opt_dsa
+    mkdir -p $opt_pkgdir/$dir/opt/xilinx/platform/$opt_dsa/hw
+    rsync -avz $opt_dsadir/$opt_dsa.xpfm $opt_pkgdir/$dir/opt/xilinx/platform/$opt_dsa/
+    rsync -avz $opt_dsadir/hw/$opt_dsa.dsa $opt_pkgdir/$dir/opt/xilinx/platform/$opt_dsa/hw/
     dpkg-deb --build $opt_pkgdir/$dir
 
     echo "================================================================"
@@ -465,16 +211,9 @@ maintainer: soren.soe@xilinx.com
 EOF
 
     mkdir -p $opt_pkgdir/$dir/lib/firmware/xilinx
-    if [ "${license_dir}" != "" ] ; then
-	if [ -d ${license_dir} ] ; then
-	  mkdir -p $opt_pkgdir/$dir/opt/xilinx/platforms/$opt_dsa/license
-	  cp -f ${license_dir}/*  $opt_pkgdir/$dir/opt/xilinx/platforms/$opt_dsa/license
-	fi
-    fi
-    rsync -avz $opt_pkgdir/dsabin/firmware/ $opt_pkgdir/$dir/lib/firmware/xilinx
+    rsync -avz $opt_pkgdir/xbinst/$opt_dsa/xbinst/firmware/ $opt_pkgdir/$dir/lib/firmware/xilinx
     mkdir -p $opt_pkgdir/$dir/opt/xilinx/dsa/$opt_dsa/test
-    rsync -avz ${opt_dsadir}/test/ $opt_pkgdir/$dir/opt/xilinx/dsa/$opt_dsa/test
-    chmod -R +r $opt_pkgdir/$dir/opt/xilinx/dsa/$opt_dsa
+    rsync -avz $opt_pkgdir/xbinst/$opt_dsa/xbinst/test/ $opt_pkgdir/$dir/opt/xilinx/dsa/$opt_dsa/test
     dpkg-deb --build $opt_pkgdir/$dir
 
     echo "================================================================"
@@ -489,11 +228,9 @@ dorpmdev()
 
 cat <<EOF > $opt_pkgdir/$dir/SPECS/$opt_dsa-dev.spec
 
-%define _rpmfilename %%{ARCH}/%%{NAME}-%%{VERSION}-dev.%%{ARCH}.rpm
-
 buildroot:  %{_topdir}
 summary: Xilinx development DSA
-name: $dsa
+name: $dsa-dev
 version: $version
 release: $revision
 license: apache
@@ -507,11 +244,9 @@ Xilinx development DSA.
 %prep
 
 %install
-mkdir -p %{buildroot}/opt/xilinx/platforms/$opt_dsa/hw
-mkdir -p %{buildroot}/opt/xilinx/platforms/$opt_dsa/sw
-rsync -avz $opt_dsadir/$opt_dsa.xpfm %{buildroot}/opt/xilinx/platforms/$opt_dsa/
-rsync -avz $opt_dsadir/hw/$opt_dsa.dsa %{buildroot}/opt/xilinx/platforms/$opt_dsa/hw/
-rsync -avz $opt_dsadir/sw/$opt_dsa.spfm %{buildroot}/opt/xilinx/platforms/$opt_dsa/sw/
+mkdir -p %{buildroot}/opt/xilinx/platform/$opt_dsa/hw
+rsync -avz $opt_dsadir/$opt_dsa.xpfm %{buildroot}/opt/xilinx/platform/$opt_dsa/
+rsync -avz $opt_dsadir/hw/$opt_dsa.dsa %{buildroot}/opt/xilinx/platform/$opt_dsa/hw/
 
 %files
 %defattr(-,root,root,-)
@@ -555,9 +290,9 @@ Xilinx deployment DSA.  This DSA depends on xrt >= $opt_xrt.
 
 %install
 mkdir -p %{buildroot}/lib/firmware/xilinx
-cp $opt_pkgdir/dsabin/firmware/* %{buildroot}/lib/firmware/xilinx
+cp $opt_pkgdir/xbinst/$opt_dsa/xbinst/firmware/* %{buildroot}/lib/firmware/xilinx
 mkdir -p %{buildroot}/opt/xilinx/dsa/$opt_dsa/test
-cp ${opt_dsadir}/test/* %{buildroot}/opt/xilinx/dsa/$opt_dsa/test
+cp $opt_pkgdir/xbinst/$opt_dsa/xbinst/test/* %{buildroot}/opt/xilinx/dsa/$opt_dsa/test
 
 %files
 %defattr(-,root,root,-)
@@ -585,7 +320,7 @@ if [ $FLAVOR == "centos" ]; then
  if [ $opt_dev == 1 ]; then
      dorpmdev
  else
-     dodsabin
+     doxbinst
      dorpm
  fi
 fi
@@ -594,7 +329,7 @@ if [ $FLAVOR == "ubuntu" ]; then
  if [ $opt_dev == 1 ]; then
      dodebdev
  else
-     dodsabin
+     doxbinst
      dodeb
  fi
 fi
