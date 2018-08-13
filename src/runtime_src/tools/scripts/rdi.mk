d4a100518db8dbe7b706c453120db554c1bf2985
Stephen Rozum <stephen.rozum@xilinx.com>
2018-08-13 13:07:10 -0600

    Removed obsolete files and no longer needed executables.
    Work Done
    1) Removed obsolete 'rdi.mk' makefiles.
    2) Removed bitswap and xclkernelinfohash executables.

--- src/runtime_src/tools/scripts/rdi.mk
+++
@@ -1,8 +0,0 @@
-include $(RDI_MAKEROOT)/top.mk
-
-RELEASE_DATA_DIR := runtime/bin
-RELEASE_DATA_FILES := install.sh rtplot xbrecover xrtdeps.sh
-RELEASE_TYPE := customer
-include $(RDI_MAKEROOT)/release.mk
-
-include $(RDI_MAKEROOT)/bottom.mk
