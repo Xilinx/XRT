9391c28b5bce105f33c46e192ee574ba523c95fd
Soren Soe <soren.soe@xilinx.com>
2018-08-31 17:07:15 -0700

    Clean up overloaded use of cl_mem_ext_ptr_t (#264)

--- src/runtime_src/xocl/core/device.cpp
+++ src/runtime_src/xocl/core/device.cpp
@@ -236,23 +236,23 @@ alloc(memory* mem)
   return boh;
 }
 
-int 
+int
 device::
-get_stream(xrt::device::stream_flags flags, xrt::device::stream_attrs attrs, cl_mem_ext_ptr_t* ext, xrt::device::stream_handle* stream) 
+get_stream(xrt::device::stream_flags flags, xrt::device::stream_attrs attrs, const cl_mem_ext_ptr_t* ext, xrt::device::stream_handle* stream)
 {
-  const cl_kernel kernel = (const cl_kernel)(ext->param);
+  auto kernel = xocl::xocl(ext->kernel);
   uint64_t route = 0;
   uint64_t flow = 0;
 
- if(kernel != nullptr) {
-   const std::string& kernel_name = xocl(kernel)->get_name_from_constructor();
+ if (kernel != nullptr) {
+   auto& kernel_name = kernel->get_name_from_constructor();
    auto memidx = m_xclbin.get_memidx_from_arg(kernel_name,ext->flags);
-   const mem_topology* mems = m_xclbin.get_mem_topology();
+   auto mems = m_xclbin.get_mem_topology();
 
-   if(!mems)
+   if (!mems)
      throw xocl::error(CL_INVALID_OPERATION,"Mem topology section does not exist");
-  
-   if((memidx+1) < mems->m_count)
+
+   if ((memidx+1) < mems->m_count)
      throw xocl::error(CL_INVALID_OPERATION,"Mem topology section count is less than memidex");
 
 
@@ -260,23 +260,23 @@ get_stream(xrt::device::stream_flags flags, xrt::device::stream_attrs attrs, cl_
     flow = mems->m_mem_data[memidx].flow_id;
   }
 
-  if(flags & CL_STREAM_READ_ONLY) 
+  if (flags & CL_STREAM_READ_ONLY)
     return m_xdevice->createReadStream(flags, attrs, route, flow, stream);
-  else if(flags & CL_STREAM_WRITE_ONLY)
+  else if (flags & CL_STREAM_WRITE_ONLY)
     return m_xdevice->createWriteStream(flags, attrs, route, flow, stream);
   else
     throw xocl::error(CL_INVALID_OPERATION,"Unknown stream type specified");
   return -1;
 }
 
-int 
+int
 device::
-close_stream(xrt::device::stream_handle stream) 
+close_stream(xrt::device::stream_handle stream)
 {
   return m_xdevice->closeStream(stream);
 }
 
-ssize_t 
+ssize_t
 device::
 write_stream(xrt::device::stream_handle stream, const void* ptr, size_t offset, size_t size, xrt::device::stream_xfer_flags flags)
 {
@@ -285,7 +285,7 @@ write_stream(xrt::device::stream_handle stream, const void* ptr, size_t offset, 
 
 ssize_t
 device::
-read_stream(xrt::device::stream_handle stream, void* ptr, size_t offset, size_t size, xrt::device::stream_xfer_flags flags) 
+read_stream(xrt::device::stream_handle stream, void* ptr, size_t offset, size_t size, xrt::device::stream_xfer_flags flags)
 {
   return m_xdevice->readStream(stream, ptr, offset, size, flags);
 }
@@ -297,7 +297,7 @@ alloc_stream_buf(size_t size, xrt::device::stream_buf_handle* handle)
   return m_xdevice->allocStreamBuf(size,handle);
 }
 
-int 
+int
 device::
 free_stream_buf(xrt::device::stream_buf_handle handle)
 {
@@ -499,21 +499,21 @@ allocate_buffer_object(memory* mem)
     //Rest 24 bits directly indexes into mem topology section OR.
     //have legacy one-hot encoding.
     auto flag = mem->get_ext_flags();
-    auto param = mem->get_xlnx_ext_param();
     int32_t memidx = 0;
-    if(param) {
+    if (auto kernel = mem->get_ext_kernel()) {
       //param<==>kernel; flag<==>arg_index
       flag = flag & 0xffffff;
-      const cl_kernel kernel = (const cl_kernel)(param);
-      const std::string& kernel_name = xocl(kernel)->get_name_from_constructor();
+      auto& kernel_name = kernel->get_name_from_constructor();
       memidx = m_xclbin.get_memidx_from_arg(kernel_name,flag);
-    } else if(flag & XCL_MEM_TOPOLOGY) {
+    }
+    else if (flag & XCL_MEM_TOPOLOGY) {
       memidx = flag & 0xffffff;
-    }else {
+    }
+    else {
       flag = flag & 0xffffff;
       auto bank = myctz(flag);
       memidx = m_xclbin.banktag_to_memidx(std::string("bank")+std::to_string(bank));
-      if(memidx==-1){
+      if (memidx==-1){
         memidx = bank;
       }
     }
--- src/runtime_src/xocl/xclbin/xclbin.cpp
+++ src/runtime_src/xocl/xclbin/xclbin.cpp
@@ -987,29 +987,32 @@ public:
   }
 
   xocl::xclbin::memidx_type
-  get_memidx_from_arg(const std::string& kernel_name, int32_t arg) 
+  get_memidx_from_arg(const std::string& kernel_name, int32_t arg)
   {
     if (!is_valid())
       return -1;
 
+    // iterate connectivity and look for CU that with name that matches kernel_name
     for (int32_t i=0; i<m_con->m_count; ++i) {
       if (m_con->m_connection[i].arg_index!=arg)
         continue;
-      //ip_layout section has format : kernel_name:cu_name
       auto ipidx = m_con->m_connection[i].m_ip_layout_index;
-      const char *ip_name = reinterpret_cast<const char*>(m_ip->m_ip_data[ipidx].m_name);
-      const char* sub = strstr(ip_name,kernel_name.c_str());
+      auto ip_name = reinterpret_cast<const char*>(m_ip->m_ip_data[ipidx].m_name);
+
+      // ip_name has format : kernel_name:cu_name
+      // For a match, kernel_name should be found at first location in ip_name
+      auto sub = strstr(ip_name,kernel_name.c_str());
       if (sub!=ip_name)
         continue;
 
-      //This connection already has a device storage allocated, so skip to 
-      //the next connection in the connection range which matches the 
-      //criteria - multiple cu case. 
-      //TODO: Check if this is ever hit. 
-      if(std::find(m_used_connections.begin(), m_used_connections.end(), i)
-	     != m_used_connections.end()) {
+      // This connection already has a device storage allocated, so skip to
+      // the next connection in the connection range which matches the
+      // criteria - multiple cu case.
+      // TODO: Check if this is ever hit.
+      if (std::find(m_used_connections.begin(), m_used_connections.end(), i)
+	     != m_used_connections.end())
 	  continue;
-      }
+
       // found the connection that match kernel_name,arg
       size_t memidx = m_con->m_connection[i].mem_data_index;
       assert(m_mem->m_mem_data[memidx].m_used);
@@ -1026,8 +1029,8 @@ public:
     return m_clk;
   }
 
-  const mem_topology* 
-  get_mem_topology() const 
+  const mem_topology*
+  get_mem_topology() const
   {
     return m_mem;
   }
@@ -1252,7 +1255,7 @@ struct xclbin::impl
   { return m_sections.banktag_to_memidx(banktag); }
 
   memidx_type
-  get_memidx_from_arg(const std::string& kernel_name, int32_t arg) 
+  get_memidx_from_arg(const std::string& kernel_name, int32_t arg)
   { return m_sections.get_memidx_from_arg(kernel_name, arg); }
 
   unsigned int
@@ -1480,7 +1483,7 @@ banktag_to_memidx(const std::string& tag) const
 
 xclbin::memidx_type
 xclbin::
-get_memidx_from_arg(const std::string& kernel_name, int32_t arg) 
+get_memidx_from_arg(const std::string& kernel_name, int32_t arg)
 {
   return m_impl->get_memidx_from_arg(kernel_name, arg);
 }
