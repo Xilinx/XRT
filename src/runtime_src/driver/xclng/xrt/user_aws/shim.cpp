0f8b7fd6b20624f9d8437f5e579cdd7eaba27e20
rradjabi <rradjabi@gmail.com>
2018-09-14 14:39:03 -0700

    * Don't need to implement awsbwhal::AwsXcl:: xclExecBuf or xclWait

--- src/runtime_src/driver/xclng/xrt/user_aws/shim.cpp
+++ src/runtime_src/driver/xclng/xrt/user_aws/shim.cpp
@@ -924,48 +924,6 @@ namespace awsbwhal {
       drm_xocl_pread_unmgd unmgd = {0, 0, offset, count, reinterpret_cast<uint64_t>(buf)};
       return ioctl(mUserHandle, DRM_IOCTL_XOCL_PREAD_UNMGD, &unmgd);
     }
-    
-    /*
-     * xclExecBuf()
-     */
-    int AwsXcl::xclExecBuf(unsigned int cmdBO)
-    {
-	int ret;
-	if (mLogStream.is_open()) {
-	    mLogStream << __func__ << ", " << std::this_thread::get_id() << ", " << cmdBO << std::endl;
-	}
-	drm_xocl_execbuf exec = {0, cmdBO, 0,0,0,0,0,0,0,0};
-	ret = ioctl(mUserHandle, DRM_IOCTL_XOCL_EXECBUF, &exec);
-	return ret ? -errno : ret;
-    }
-    
-    /*
-     * xclExecBuf()
-     */
-    int AwsXcl::xclExecBuf(unsigned int cmdBO, size_t num_bo_in_wait_list, unsigned int *bo_wait_list)
-    {
-	if (mLogStream.is_open()) {
-	    mLogStream << __func__ << ", " << std::this_thread::get_id() << ", "
-		       << cmdBO << ", " << num_bo_in_wait_list << ", " << bo_wait_list << std::endl;
-	}
-	int ret;
-	unsigned int bwl[8] = {0};
-	std::memcpy(bwl,bo_wait_list,num_bo_in_wait_list*sizeof(unsigned int));
-	drm_xocl_execbuf exec = {0, cmdBO, bwl[0],bwl[1],bwl[2],bwl[3],bwl[4],bwl[5],bwl[6],bwl[7]};
-	ret = ioctl(mUserHandle, DRM_IOCTL_XOCL_EXECBUF, &exec);
-	return ret ? -errno : ret;
-    }
-    
-    /*
-     * xclExecWait()
-     */
-    int AwsXcl::xclExecWait(int timeoutMilliSec)
-    {
-	std::vector<pollfd> uifdVector;
-	pollfd info = {mUserHandle, POLLIN, 0};
-	uifdVector.push_back(info);
-	return poll(&uifdVector[0], uifdVector.size(), timeoutMilliSec);
-    }
 
     int AwsXcl::xclExportBO(unsigned int boHandle)
     {
@@ -1453,16 +1411,13 @@ ssize_t xclUnmgdPread(xclDeviceHandle handle, unsigned flags, void *buf,
 
 int xclExecBuf(xclDeviceHandle handle, unsigned int cmdBO)
 {
-    awsbwhal::AwsXcl *drv = awsbwhal::AwsXcl::handleCheck(handle);
-    return drv ? drv->xclExecBuf(cmdBO) : -ENODEV;
+  return -EPERM; // unsupported on AWS
 }
 
 int xclExecWait(xclDeviceHandle handle, int timeoutMilliSec)
 {
-  awsbwhal::AwsXcl *drv = awsbwhal::AwsXcl::handleCheck(handle);
-  return drv ? drv->xclExecWait(timeoutMilliSec) : -ENODEV;
+  return -EPERM; // unsported on AWS
 }
-
 
 int xclUpgradeFirmwareXSpi(xclDeviceHandle handle, const char *fileName, int index)
 {
