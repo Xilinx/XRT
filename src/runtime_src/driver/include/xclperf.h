be074333faf19d3afe965a805b6a51b11b4d31c2
hackwa <anurag6dubey@gmail.com>
2018-09-17 16:33:12 -0600

    fix comma

--- src/runtime_src/driver/include/xclperf.h
+++ src/runtime_src/driver/include/xclperf.h
@@ -250,7 +250,7 @@ enum xclPerfMonType {
 	XCL_PERF_MON_ACCEL  = 2,
 	XCL_PERF_MON_STALL  = 3,
   XCL_PERF_MON_STR = 4,
-	XCL_PERF_MON_TOTAL_PROFILE = 5,
+	XCL_PERF_MON_TOTAL_PROFILE = 5
 };
 
 /* Performance monitor start event */
