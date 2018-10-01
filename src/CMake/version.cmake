cb93145f6f8aee5d7e240474eddce65b3a1feb32
Stephen Rozum <stephen.rozum@xilinx.com>
2018-10-01 12:30:18 -0600

    Updated versioning support Work Done --------- + Added new versioning types:   - xrt_build_version_branch_extended: git --no-pager describe --tags --always)   - xrt_modified_files: git status --porcelain + Added inline XRTVersion::printVersion() helper method (C++ only) to version.h to print out the version header. + Updated version.cmake to use the $(GIT_EXECUTABLE} variable instead of hardcoding "git" + Updated XclBinUtilMain to use XRTVersion::printVersion
    Sample Output:
    -------------
           XRT Build Version: 2.1.0
      Build Version Extended: v2.1.0-2018.2_XDF-RC1-76-gea8fa61
        Build Version Branch: master
          Build Version Hash: ea8fa611be1a49f0af69ccb2556af6324b85e9b0
          Build Version Date: 2018-10-01 12:29:05
      Current Modified Files: 1) M  src/CMake/config/version.h.in
                              2) MM src/CMake/version.cmake
                              3) M  src/runtime_src/tools/xclbin/CMakeLists.txt
                              4) M  src/runtime_src/tools/xclbin/XclBinUtilMain.cxx

--- src/CMake/version.cmake
+++ src/CMake/version.cmake
@@ -1,17 +1,37 @@
+# Get the branch 
 execute_process(
-  COMMAND git rev-parse --abbrev-ref HEAD
+  COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
   OUTPUT_VARIABLE XRT_BRANCH
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
+# Get the extended branch
+execute_process(
+  COMMAND ${GIT_EXECUTABLE} --no-pager describe --tags --always
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  OUTPUT_VARIABLE XRT_BRANCH_EXTENDED
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+
 # Get the latest abbreviated commit hash of the working branch
 execute_process(
-  COMMAND git rev-parse HEAD
+  COMMAND ${GIT_EXECUTABLE} rev-parse --verify HEAD
   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
   OUTPUT_VARIABLE XRT_HASH
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
+
+# Get all of the modified files in the current git environment
+execute_process(
+  COMMAND ${GIT_EXECUTABLE} status --porcelain
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  OUTPUT_VARIABLE XRT_MODIFIED_FILES
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+string(REPLACE "\n" "," XRT_MODIFIED_FILES "${XRT_MODIFIED_FILES}")
+
 
 string(TIMESTAMP XRT_DATE "%Y-%m-%d %H:%M:%S")
 
