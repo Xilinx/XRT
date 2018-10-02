fe1c4a318681b75800669454187bc8168e1814d4
Sarabjeet Singh <sarab96@gmail.com>
2018-10-02 08:35:25 -0700

    Updated LD_LIB Path

--- tests/xma/xma_tests.sh
+++ tests/xma/xma_tests.sh
@@ -4,7 +4,7 @@ set -e
 
 
 echo "PWD is $PWD"
-export LD_LIBRARY_PATH=$PWD/../../build/Debug/opt/xilinx/xrt/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/xilinx/xrt/lib:$LD_LIBRARY_PATH
 echo "LD_LIBRARY_PATH is $LD_LIBRARY_PATH"
 make
 
