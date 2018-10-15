191e00e4e2cc34116c08a33816dfc0b9f0dabbf5
Sarab <sarab96@gmail.com>
2018-10-15 10:32:58 -0700

    Fix for XMA TA (#506)

--- tests/xma/xma_tests.sh
+++ tests/xma/xma_tests.sh
@@ -12,8 +12,10 @@ echo "XILINX_SDX is: $XILINX_SDX"
 #set xma include path
 BUILD=2018.3
 export XMA_INCLUDE=${XMA_INCLUDE:=/proj/xbuilds/${BUILD}_daily_latest/xbb/xrt/packages/xrt-2.1.0-centos/opt/xilinx/xrt/include}
+export XMA_LIBS=${XMA_LIBS:=/proj/xbuilds/${BUILD}_daily_latest/xbb/xrt/packages/xrt-2.1.0-centos/opt/xilinx/xrt/lib}
 
 echo "XMA_INCLUDE is $XMA_INCLUDE"
+echo "XMA_LIBS is $XMA_LIBS"
 
 
 make
