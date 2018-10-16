49523af8e9e696acff4a1e511752e65577503f59
Stephen Rozum <stephen.rozum@xilinx.com>
2018-10-15 18:53:48 -0600

    Added XRT versioning support to the xclbin archive and xclbinutil.

--- src/runtime_src/tools/xclbin/XclBin.cxx
+++ src/runtime_src/tools/xclbin/XclBin.cxx
@@ -31,10 +31,34 @@
 namespace XUtil = XclBinUtilities;
 
 #include "FormattedOutput.h"
-
+// Generated include files
+#include <version.h>
 static const std::string MIRROR_DATA_START = "XCLBIN_MIRROR_DATA_START";
 static const std::string MIRROR_DATA_END = "XCLBIN_MIRROR_DATA_END";
 
+
+static
+bool getVersionMajorMinorPath(const char * _pVersion, uint8_t & _major, uint8_t & _minor, uint16_t & _patch)
+{
+  std::string sVersion(_pVersion);
+  std::vector<std::string> tokens;
+  boost::split(tokens, sVersion, boost::is_any_of("."));
+  if ( tokens.size() == 1 ) {
+    _major = 0;
+    _minor = 0;
+    _patch = (uint16_t) std::stoi(tokens[0]);
+    return true;
+  } 
+
+  if ( tokens.size() == 3 ) {
+    _major = (uint8_t) std::stoi(tokens[0]);
+    _minor = (uint8_t) std::stoi(tokens[1]);
+    _patch = (uint16_t) std::stoi(tokens[2]);
+    return true;
+  } 
+
+  return false;
+}
 
 XclBin::XclBin()
     : m_xclBinHeader({ 0 })
@@ -61,7 +85,12 @@ XclBin::initializeHeader(axlf &_xclBinHeader)
   memset( _xclBinHeader.m_keyBlock, 0xFF, sizeof(_xclBinHeader.m_keyBlock) );
   _xclBinHeader.m_uniqueId = time( nullptr );
   _xclBinHeader.m_header.m_timeStamp = time( nullptr );
-  _xclBinHeader.m_header.m_version = 2017;
+
+  // Now populate the version information
+  getVersionMajorMinorPath(xrt_build_version, 
+                           _xclBinHeader.m_header.m_versionMajor, 
+                           _xclBinHeader.m_header.m_versionMinor, 
+                           _xclBinHeader.m_header.m_versionPatch);
 }
 
 void
@@ -461,7 +490,12 @@ XclBin::readXclBinHeader(const boost::property_tree::ptree& _ptHeader,
 
   _axlfHeader.m_header.m_timeStamp = XUtil::stringToUInt64(_ptHeader.get<std::string>("TimeStamp"));
   _axlfHeader.m_header.m_featureRomTimeStamp = XUtil::stringToUInt64(_ptHeader.get<std::string>("FeatureRomTimeStamp"));
-  _axlfHeader.m_header.m_version = _ptHeader.get<int>("Version");
+  std::string sVersion = _ptHeader.get<std::string>("Version");
+  getVersionMajorMinorPath(sVersion.c_str(), 
+                           _axlfHeader.m_header.m_versionMajor, 
+                           _axlfHeader.m_header.m_versionMinor, 
+                           _axlfHeader.m_header.m_versionPatch);
+
   _axlfHeader.m_header.m_mode = _ptHeader.get<uint32_t>("Mode");
 
   std::string sFeatureRomUUID = _ptHeader.get<std::string>("FeatureRomUUID");
