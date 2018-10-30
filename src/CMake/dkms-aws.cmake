aabec0cee4db943e4e39094f32d66d3947451a45
rradjabi <rradjabi@gmail.com>
2018-10-30 15:53:06 -0700

    awsmgmt driver files added to COMPONENT aws with install()

--- src/CMake/dkms-aws.cmake
+++ src/CMake/dkms-aws.cmake
@@ -1,16 +1,16 @@
 # Custom variables imported by this CMake stub which should be defined by parent CMake:
 # XRT_DKMS_DRIVER_SRC_BASE_DIR
 
-set (XRT_DKMS_INSTALL_DIR "/usr/src/xrt-aws-${XRT_VERSION_STRING}")
+set (XRT_DKMS_INSTALL_DIR_aws "/usr/src/xrt-aws-${XRT_VERSION_STRING}")
 
 message("-- XRT DRIVER SRC BASE DIR ${XRT_DKMS_DRIVER_SRC_BASE_DIR}")
 
-SET (DKMS_FILE_NAME "dkms.conf-aws")
+SET (DKMS_FILE_NAME "dkms.conf")
 SET (DKMS_POSTINST "postinst-aws")
 SET (DKMS_PRERM "prerm-aws")
 
 configure_file (
-  "${CMAKE_SOURCE_DIR}/CMake/config/${DKMS_FILE_NAME}.in"
+  "${CMAKE_SOURCE_DIR}/CMake/config/xrt-awsmgmt/${DKMS_FILE_NAME}.in"
   ${DKMS_FILE_NAME}
   )
 
@@ -24,7 +24,7 @@ configure_file (
   ${DKMS_PRERM}
   )
 
-SET (XRT_DKMS_SRCS
+SET (XRT_DKMS_SRCS_aws
   driver/aws/kernel/mgmt/mgmt-bit.c
   driver/aws/kernel/mgmt/mgmt-bit.h
   driver/aws/kernel/mgmt/mgmt-core.c
@@ -47,15 +47,15 @@ SET (XRT_DKMS_SRCS
   driver/include/xclerr.h
   )
 
-SET (XRT_DKMS_ABS_SRCS)
+SET (XRT_DKMS_ABS_SRCS_aws)
 
-foreach (DKMS_FILE ${XRT_DKMS_SRCS})
+foreach (DKMS_FILE ${XRT_DKMS_SRCS_aws})
   get_filename_component(DKMS_DIR ${DKMS_FILE} DIRECTORY)
-  install (FILES ${XRT_DKMS_DRIVER_SRC_BASE_DIR}/${DKMS_FILE} DESTINATION ${XRT_DKMS_INSTALL_DIR}/${DKMS_DIR})
-  list (APPEND XRT_DKMS_ABS_SRCS ${XRT_DKMS_DRIVER_SRC_BASE_DIR}/${DKMS_FILE})
+  install (FILES ${XRT_DKMS_DRIVER_SRC_BASE_DIR}/${DKMS_FILE} DESTINATION ${XRT_DKMS_INSTALL_DIR_aws}/${DKMS_DIR} COMPONENT aws)
+  list (APPEND XRT_DKMS_ABS_SRCS_aws ${XRT_DKMS_DRIVER_SRC_BASE_DIR}/${DKMS_FILE})
 endforeach()
 
-install (FILES ${CMAKE_CURRENT_BINARY_DIR}/${DKMS_FILE_NAME} DESTINATION ${XRT_DKMS_INSTALL_DIR})
+install (FILES ${CMAKE_CURRENT_BINARY_DIR}/${DKMS_FILE_NAME} DESTINATION ${XRT_DKMS_INSTALL_DIR_aws} COMPONENT aws)
 
 #find_program(CHECKPATCH checkpatch.pl PATHS /lib/modules/${LINUX_KERNEL_VERSION}/build/scripts/ NO_DEFAULT_PATH)
 
