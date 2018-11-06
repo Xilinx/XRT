1544e8205cd288963ef0848a240beaa31656bd74
rradjabi <rradjabi@gmail.com>
2018-11-06 12:05:07 -0800

    cleanup and comments

--- src/CMake/dkms-aws.cmake
+++ src/CMake/dkms-aws.cmake
@@ -1,30 +1,30 @@
 # Custom variables imported by this CMake stub which should be defined by parent CMake:
 # XRT_DKMS_DRIVER_SRC_BASE_DIR
 
-set (XRT_DKMS_INSTALL_DIR_aws "/usr/src/xrt-aws-${XRT_VERSION_STRING}")
+set(XRT_DKMS_INSTALL_DIR_aws "/usr/src/xrt-aws-${XRT_VERSION_STRING}")
 
 message("-- XRT DRIVER SRC BASE DIR ${XRT_DKMS_DRIVER_SRC_BASE_DIR}")
 
-SET (DKMS_FILE_NAME "dkms.conf")
-SET (DKMS_POSTINST "postinst-aws")
-SET (DKMS_PRERM "prerm-aws")
+set(DKMS_FILE_NAME "dkms.conf")
+set(DKMS_POSTINST "postinst-aws")
+set(DKMS_PRERM "prerm-aws")
 
-configure_file (
+configure_file(
   "${CMAKE_SOURCE_DIR}/CMake/config/dkms-awsmgmt/${DKMS_FILE_NAME}.in"
   "xrt-aws-prep/${DKMS_FILE_NAME}"
   )
 
-configure_file (
+configure_file(
   "${CMAKE_SOURCE_DIR}/CMake/config/${DKMS_POSTINST}.in"
   "xrt-aws-prep/${DKMS_POSTINST}"
   )
 
-configure_file (
+configure_file(
   "${CMAKE_SOURCE_DIR}/CMake/config/${DKMS_PRERM}.in"
   "xrt-aws-prep/${DKMS_PRERM}"
   )
 
-SET (XRT_DKMS_SRCS_aws
+set(XRT_DKMS_SRCS_aws
   driver/aws/kernel/mgmt/mgmt-bit.c
   driver/aws/kernel/mgmt/mgmt-bit.h
   driver/aws/kernel/mgmt/mgmt-core.c
@@ -47,12 +47,12 @@ SET (XRT_DKMS_SRCS_aws
   driver/include/xclerr.h
   )
 
-SET (XRT_DKMS_ABS_SRCS_aws)
+set(XRT_DKMS_ABS_SRCS_aws)
 
-foreach (DKMS_FILE ${XRT_DKMS_SRCS_aws})
+foreach(DKMS_FILE ${XRT_DKMS_SRCS_aws})
   get_filename_component(DKMS_DIR ${DKMS_FILE} DIRECTORY)
   install(FILES ${XRT_DKMS_DRIVER_SRC_BASE_DIR}/${DKMS_FILE} DESTINATION ${XRT_DKMS_INSTALL_DIR_aws}/${DKMS_DIR})
-  list (APPEND XRT_DKMS_ABS_SRCS_aws ${XRT_DKMS_DRIVER_SRC_BASE_DIR}/${DKMS_FILE})
+  list(APPEND XRT_DKMS_ABS_SRCS_aws ${XRT_DKMS_DRIVER_SRC_BASE_DIR}/${DKMS_FILE})
 endforeach()
 
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/xrt-aws-prep/${DKMS_FILE_NAME} DESTINATION ${XRT_DKMS_INSTALL_DIR_aws})
