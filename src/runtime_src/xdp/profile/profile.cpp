2f134b148e2b3c424e981ed347fd92df6c07494c
Tianhao Zhou <tianhaoz@umich.edu>
2018-12-07 15:41:52 -0800

    fix: profile ignores the offset and size user puts in clEnqueueReadBuffer

--- src/runtime_src/xdp/profile/profile.cpp
+++ src/runtime_src/xdp/profile/profile.cpp
@@ -170,7 +170,7 @@ cb_action_ndrange (xocl::event* event,cl_int status,const std::string& cu_name, 
 }
 
 void
-cb_action_read (xocl::event* event,cl_int status, cl_mem buffer, size_t size, uint64_t address, const std::string& bank)
+cb_action_read (xocl::event* event,cl_int status, cl_mem buffer, size_t size, uint64_t address, const std::string& bank, bool entire_buffer, size_t user_size, size_t user_offset)
 {
     if (!isProfilingOn())
       return;
@@ -193,11 +193,18 @@ cb_action_read (xocl::event* event,cl_int status, cl_mem buffer, size_t size, ui
     auto threadId = std::this_thread::get_id();
     double timestampMsec = (status == CL_COMPLETE) ? event->time_end() / 1e6 : 0.0;
 
+    size_t actual_size = 0;
+    if (entire_buffer) {
+      actual_size = size;
+    } else {
+      actual_size = user_size;
+    }
+
     XCL::RTSingleton::Instance()->getProfileManager()->logDataTransfer
       (reinterpret_cast<uint64_t>(buffer)
        ,XCL::RTProfile::READ_BUFFER
        ,commandState
-       ,size
+       ,actual_size
        ,contextId
        ,numDevices
        ,deviceName
