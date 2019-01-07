0e48e1d36fed77a051a1ed9acb42433db16bcce3
Jason Villarreal <jvillar@xilinx.com>
2019-01-07 09:41:52 -0800

    Adding support to xbutil to print out the values read from streaming AXI protocol checkers

--- src/runtime_src/driver/xclng/xrt/user_gem/shim.h
+++ src/runtime_src/driver/xclng/xrt/user_gem/shim.h
@@ -234,6 +234,7 @@ public:
     size_t xclDebugReadCheckers(xclDebugCheckersResults* checkerResult);
     size_t xclDebugReadStreamingCounters(xclStreamingDebugCountersResults* streamingResult);
     size_t xclDebugReadAccelMonitorCounters(xclAccelMonitorCounterResults* samResult);
+    size_t xclDebugReadStreamingCheckers(xclDebugStreamingCheckersResults* streamingCheckerResult);
 
     // Trace
     size_t xclPerfMonStartTrace(xclPerfMonType type, uint32_t startTrigger);
