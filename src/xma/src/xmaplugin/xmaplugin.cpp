640149864c7431c01a8054164f9a7a03de6ff2a7
John Nichols <jnichol@xilinx.com>
2019-02-04 15:54:00 -0800

    Add support for multi-process safe TDM of kernels

--- src/xma/src/xmaplugin/xmaplugin.cpp
+++ src/xma/src/xmaplugin/xmaplugin.cpp
@@ -14,9 +14,20 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+#include <string.h>
 #include <stdio.h>
 #include "xclhal2.h"
 #include "xmaplugin.h"
+
+#define XMA_ADDR_AP_CTRL 0x00   /* AXI-Lite control signals */
+#define XMA_ADDR_AP_GIER 0x04   /* Global Interrupt enable register */
+#define XMA_ADDR_AP_IIER 0x08   /* IP Interrupt enable register */
+#define XMA_ADDR_AP_IISR 0x0C   /* IP Interrupt status register */ 
+
+#define XMA_AP_CTRL_START 0x01  /* AXI-Lite control start bit */
+#define XMA_AP_CTRL_DONE  0x02  /* AXI-Lite control done bit */
+#define XMA_AP_CTRL_IDLE  0x04  /* AXI-Lite control idle bit */
+#define XMA_AP_CTRL_READY 0x08  /* AXI-Lite control ready bit */
 
 XmaBufferHandle
 xma_plg_buffer_alloc(XmaHwSession s_handle, size_t size)
@@ -117,6 +128,107 @@ xma_plg_buffer_read(XmaHwSession s_handle,
     return rc;
 }
 
+int32_t
+xma_plg_register_prep_write(XmaHwSession  s_handle,
+                       void         *src,
+                       size_t        size,
+                       size_t        offset)
+{
+    uint8_t *dst = (uint8_t*)s_handle.reg_map;
+    size_t   cur_min = offset; 
+    size_t   cur_max = offset + size; 
+
+    dst += offset;
+    memcpy(dst, src, size);
+
+    if (cur_max > s_handle.max_offset)
+        s_handle.max_offset = cur_max;
+
+    if (s_handle.min_offset == 0)
+        s_handle.min_offset = cur_min;
+        
+    if (cur_min < s_handle.min_offset)
+        s_handle.min_offset = cur_min;
+
+    return 0;
+}
+
+void xma_plg_kernel_lock(XmaHwSession s_handle)
+{
+    /* Only acquire the lock if we don't already own it */
+    if (s_handle.have_lock)
+        return;
+
+    pthread_mutex_lock(s_handle.lock);
+    s_handle.have_lock = true;
+}
+
+void xma_plg_kernel_unlock(XmaHwSession s_handle)
+{
+    if (s_handle.have_lock)
+    {
+        pthread_mutex_unlock(s_handle.lock);
+        s_handle.have_lock = false;
+    }
+}
+
+void xma_plg_kernel_wait_on_finish(XmaHwSession s_handle)
+{
+    uint32_t val = 0;
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+    while((val & XMA_AP_CTRL_IDLE) != XMA_AP_CTRL_IDLE)
+    {
+        xma_plg_register_read(s_handle, &val, sizeof(val), XMA_ADDR_AP_CTRL);
+    }
+#pragma GCC diagnostic pop
+}
+
+void xma_plg_kernel_start(XmaHwSession s_handle)
+{
+    
+    uint32_t val = XMA_AP_CTRL_START;
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+    xma_plg_register_write(s_handle, &val, sizeof(val), XMA_ADDR_AP_CTRL);
+#pragma GCC diagnostic pop
+}
+
+int32_t
+xma_plg_kernel_exec(XmaHwSession s_handle, bool wait_on_kernel_finish)
+{
+    void   *src = s_handle.reg_map;
+    size_t  offset = s_handle.min_offset;
+    size_t  size = s_handle.max_offset;
+    
+    /* The lock is only acquired if the session does not already own it */
+    xma_plg_kernel_lock(s_handle);
+
+    /* Must wait for the kernel to be idle before configuring it */
+    xma_plg_kernel_wait_on_finish(s_handle);
+
+    /* Write the kernel registers */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+    xma_plg_register_write(s_handle, src, size, offset);
+#pragma GCC diagnostic pop
+
+    /* Start the kernel */
+    xma_plg_kernel_start(s_handle);
+
+    /* Only wait for the kernel to complete if the caller explicitly requests */
+    if (wait_on_kernel_finish)
+    {
+        /* Block, waiting for the IDLE status */
+        xma_plg_kernel_wait_on_finish(s_handle);
+
+        /* Can only unlock if we've finished using the kernel */
+        xma_plg_kernel_unlock(s_handle);
+    }
+
+    return 0;
+}
+    
 int32_t
 xma_plg_register_write(XmaHwSession  s_handle,
                        void         *src,
