c92f55434ba54f072b48f6aa1214d344b15061f7
Tianhao Zhou <tianhaoz@umich.edu>
2019-02-06 16:03:41 -0800

    xdp(hal plugin): dev

--- src/runtime_src/driver/xclng/xrt/user_gem/shim.cpp
+++ src/runtime_src/driver/xclng/xrt/user_gem/shim.cpp
@@ -1745,7 +1745,7 @@ static int findMgmtDeviceID(int user_slot)
 unsigned xclProbe()
 {
     std::cout << "ready to call the callback macro" << std::endl;
-    XDP_LOG_API_CALL
+    hal_api_call_logger hal_plugin_object();
     return pcidev::get_dev_ready();
 }
 
