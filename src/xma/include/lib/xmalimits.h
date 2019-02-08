395b357d92e297abed471abd5754d9f7b61a2bc9
Sarabjeet Singh <sarab96@gmail.com>
2019-02-08 00:20:01 -0800

    Fix

--- src/xma/include/lib/xmalimits.h
+++ src/xma/include/lib/xmalimits.h
@@ -29,7 +29,7 @@
 #define MAX_FUNCTION_NAME       32
 #define MAX_PLUGIN_NAME         32
 #define MAX_VENDOR_NAME         32
-#define MAX_KERNEL_NAME         256
+#define MAX_KERNEL_NAME         64
 #define MAX_DSA_NAME            256
 #define XMA_MAX_PLANES           3
 #define MAX_PLUGINS             16
