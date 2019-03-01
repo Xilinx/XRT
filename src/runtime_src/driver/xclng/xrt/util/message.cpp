2ed04751a8e6aa8f5cc6d96a6936d551c6ecd202
Shivangi Agarwal <shivangiagarwal53@gmail.com>
2019-03-01 11:14:58 -0800

    xrt msgg formatting (#993) (#1014)
    * xrt msgg formatting
    
    * fix
    
    * check
    
    * fix2
    
    * requested change
    
    * made requested changes
    
    * catch err
    
    (cherry picked from commit dea7fe94511f146c0263947ddcb9c2e1b020fb0d)

--- src/runtime_src/driver/xclng/xrt/util/message.cpp
+++ src/runtime_src/driver/xclng/xrt/util/message.cpp
@@ -15,6 +15,7 @@
  */
 
 #include "message.h"
+#include "driver/xclng/xrt/util/t_time.h"
 
 #include "config_reader.h"
 #include <unistd.h>
@@ -22,8 +23,33 @@
 #include <map>
 #include <fstream>
 #include <iostream>
+#include <chrono>
+#include <ctime>
+#include <stdio.h>
+#include <string.h>
+#include <thread>
+#include <sstream>
+#include <limits.h>
+#include <version.h>
+#include <sys/types.h>
+#ifdef __GNUC__
+# include <linux/limits.h>
+# include <sys/stat.h>
+#endif
 
 namespace {
+
+static std::string
+get_exe_path()
+{
+#ifdef __GNUC__
+  char buf[PATH_MAX] = {0};
+  auto len = ::readlink("/proc/self/exe", buf, PATH_MAX);
+  return std::string(buf, (len>0) ? len : 0);
+#else
+  return "";
+#endif
+}
 
 using severity_level = xrt_core::message::severity_level;
 
@@ -35,7 +61,7 @@ public:
   virtual ~message_dispatch() {}
   static message_dispatch* make_dispatcher(const std::string& choice);
 public:
-  virtual void send(severity_level l, const char* msg) = 0;
+  virtual void send(severity_level l, const char* tag, const char* msg) = 0;
 };
 
 //--
@@ -44,27 +70,26 @@ class null_dispatch : public message_dispatch
 public:
   null_dispatch() {}
   virtual ~null_dispatch() {}
-  virtual void send(severity_level l, const char* msg) {};
+  virtual void send(severity_level l, const char* tag, const char* msg) {};
 };
 
 //--
 class console_dispatch : public message_dispatch
 {
 public:
-  console_dispatch() {}
+  console_dispatch();
   virtual ~console_dispatch() {}
-  virtual void send(severity_level l, const char* msg) override;
+  virtual void send(severity_level l, const char* tag, const char* msg) override;
 private:
   std::map<severity_level, const char*> severityMap = {
+    { severity_level::EMERGENCY, "EMERGENCY: "},
     { severity_level::ALERT,     "ALERT: "},
     { severity_level::CRITICAL,  "CRITICAL: "},
-    { severity_level::DEBUG,     "DEBUG: "},
-    { severity_level::EMERGENCY, "EMERGENCY: "},
     { severity_level::ERROR,     "ERROR: "},
-    { severity_level::INFO,      "INFO: "},
-    { severity_level::INTERNAL,  "INTERNAL: "},
+    { severity_level::WARNING,   "WARNING: "},
     { severity_level::NOTICE,    "NOTICE: "},
-    { severity_level::WARNING,   "WARNING: "}
+    { severity_level::INFO,      "INFO: "},
+    { severity_level::DEBUG,     "DEBUG: "}
   };
 };
 
@@ -74,18 +99,17 @@ class syslog_dispatch : public message_dispatch
 public:
   syslog_dispatch();
   virtual ~syslog_dispatch();
-  virtual void send(severity_level l, const char* msg) override;
+  virtual void send(severity_level l, const char* tag, const char* msg) override;
 private:
   std::map<severity_level, int> severityMap = {
+    { severity_level::EMERGENCY, LOG_EMERG},
     { severity_level::ALERT,     LOG_ALERT},
     { severity_level::CRITICAL,  LOG_CRIT},
-    { severity_level::DEBUG,     LOG_DEBUG},
-    { severity_level::EMERGENCY, LOG_EMERG},
     { severity_level::ERROR,     LOG_ERR},
-    { severity_level::INFO,      LOG_INFO},
-    //{ severity_level::INTERNAL,  INTERNAL_NOPRI},
+    { severity_level::WARNING,   LOG_WARNING},
     { severity_level::NOTICE,    LOG_NOTICE},
-    { severity_level::WARNING,   LOG_WARNING}
+    { severity_level::INFO,      LOG_INFO},
+    { severity_level::DEBUG,     LOG_DEBUG}
   };
 };
 
@@ -96,19 +120,18 @@ public:
   explicit
   file_dispatch(const std::string& file);
   virtual ~file_dispatch();
-  virtual void send(severity_level l, const char* msg) override;
+  virtual void send(severity_level l, const char* tag, const char* msg) override;
 private:
   std::ofstream handle;
   std::map<severity_level, const char*> severityMap = {
+    { severity_level::EMERGENCY, "EMERGENCY: "},
     { severity_level::ALERT,     "ALERT: "},
     { severity_level::CRITICAL,  "CRITICAL: "},
-    { severity_level::DEBUG,     "DEBUG: "},
-    { severity_level::EMERGENCY, "EMERGENCY: "},
     { severity_level::ERROR,     "ERROR: "},
-    { severity_level::INFO,      "INFO: "},
-    { severity_level::INTERNAL,  "INTERNAL: "},
+    { severity_level::WARNING,   "WARNING: "},
     { severity_level::NOTICE,    "NOTICE: "},
-    { severity_level::WARNING,   "WARNING: "}
+    { severity_level::INFO,      "INFO: "},
+    { severity_level::DEBUG,     "DEBUG: "}
   };
 };
 
@@ -144,13 +167,25 @@ syslog_dispatch::~syslog_dispatch() {
 }
 
 void
-syslog_dispatch::send(severity_level l, const char* msg) {
+syslog_dispatch::send(severity_level l, const char* tag, const char* msg) {
   syslog(severityMap[l], "%s", msg);
 }
 
 //file ops
 file_dispatch::file_dispatch(const std::string &file) {
   handle.open(file.c_str());
+  handle << "XRT build version: " << xrt_build_version << std::endl;
+  handle << "Build hash: " << xrt_build_version_hash << std::endl;
+  handle << "Build date: " << xrt_build_version_date << std::endl;
+  handle << "Git branch: " << xrt_build_version_branch<< std::endl;
+  handle << xrt_core::timestamp() << std::endl;
+  handle << "PID: " << getpid() << std::endl;
+  handle << "UID: " << getuid() << std::endl;
+  //hostname
+  char hostname[HOST_NAME_MAX];
+  gethostname(hostname, HOST_NAME_MAX);
+  handle << "HOST: " <<  hostname << std::endl;
+  handle << "EXE: " <<get_exe_path() << std::endl;
 }
 
 file_dispatch::~file_dispatch() {
@@ -158,14 +193,33 @@ file_dispatch::~file_dispatch() {
 }
 
 void
-file_dispatch::send(severity_level l, const char* msg) {
-  handle << severityMap[l] << msg << std::endl;
+file_dispatch::send(severity_level l, const char* tag, const char* msg) {
+
+  handle << xrt_core::timestamp() <<" [" << tag << "] Tid: "
+         << std::this_thread::get_id() << ", " << " " << severityMap[l]
+         << msg << std::endl;
 }
 
 //console ops
+console_dispatch::console_dispatch() {
+  std::cout << "XRT build version: " << xrt_build_version << std::endl;
+  std::cout << "Build hash: " << xrt_build_version_hash << std::endl;
+  std::cout << "Build date: " << xrt_build_version_date << std::endl;
+  std::cout << "Git branch: " << xrt_build_version_branch<< std::endl;
+  std::cout << "PID: " << getpid() << std::endl;
+  std::cout << "UID: " << getuid() << std::endl;
+  std::cout << xrt_core::timestamp() << std::endl;
+  //hostname
+  char hostname[HOST_NAME_MAX];
+  gethostname(hostname, HOST_NAME_MAX);
+  std::cout << "HOST: " <<  hostname << std::endl;
+  std::cout << "EXE: " <<get_exe_path() << std::endl;
+}
 void
-console_dispatch::send(severity_level l, const char* msg) {
-  std::cout << severityMap[l]  << msg << std::endl;
+console_dispatch::send(severity_level l, const char* tag, const char* msg) {
+  std::cout << xrt_core::timestamp() << " [" << tag << "] Tid:"
+            << std::this_thread::get_id() << ", "<< " " << severityMap[l]
+            << msg << std::endl;
 }
 
 } //end unnamed namespace
@@ -173,11 +227,16 @@ console_dispatch::send(severity_level l, const char* msg) {
 namespace xrt_core { namespace message {
 
 void
-send(severity_level l, const char* msg)
+send(severity_level l, const char* tag, const char* msg)
 {
   static const std::string logger =  xrt_core::config::get_logging();
+  int ver = xrt_core::config::get_verbosity();
+  int lev = static_cast<int>(l);
+
+  if(ver >= lev) {
   static message_dispatch* dispatcher = message_dispatch::make_dispatcher(logger);
-  dispatcher->send(l, msg);
+  dispatcher->send(l, tag, msg);
+  }
 }
 
 }} // message,xrt
