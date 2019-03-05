12d324d5d69459e66bb792f70d43a77c3d70f634
rradjabi <rradjabi@gmail.com>
2019-03-05 10:18:59 -0800

    Fixed the dynamic mem alloc for daemon.

--- tests/daemon-proxy/mproxyd.cpp
+++ tests/daemon-proxy/mproxyd.cpp
@@ -21,11 +21,8 @@
 
 #include "xclhal2.h"
 
-#define INIT_BUF_SZ 39523893//4923871//128
+#define INIT_BUF_SZ 64
 
-/*
- * struct drm_xocl_sw_mailbox *args
- **/
 struct drm_xocl_sw_mailbox {
     uint64_t flags;
     uint32_t *pData;
@@ -36,121 +33,124 @@ struct drm_xocl_sw_mailbox {
 
 struct s_handle {
     xclDeviceHandle uDevHandle;
-    xclDeviceHandle mDevHandle;
 };
-
 
 int resize_buffer( uint32_t *&buf, const size_t new_sz )
 {
-    std::cout << "Desired buffer size " << new_sz << std::endl;
-    std::cout << "Trying to free: " << buf << std::endl;
-    free(buf);
+    if( buf != NULL ) {
+        free(buf);
+        buf = NULL;
+    }
     buf = (uint32_t *)malloc( new_sz );
     if( buf == NULL ) {
-        std::cout << "calloc failed \n";
+        std::cout << "alloc failed \n";
         return -1;
     }
-
-    std::cout << "resize_buffer Success: new size=" << new_sz << std::endl;
 
     return 0;
 }
 
-void *xcl_sw_chan_xocl_to_xclmgmt(void *s_handle_ptr)
+void *xcl_sw_chan_xocl_to_xclmgmt(void *handle_ptr)
 {
     int xferCount = 0;
     int ret;
     uint64_t flags = 0;
     uint64_t id = 0;
-    struct s_handle *handle = (struct s_handle*)s_handle_ptr;
+    struct s_handle *s_handle_ptr = (struct s_handle *)handle_ptr;
+    xclDeviceHandle handle = s_handle_ptr->uDevHandle;
     size_t prev_sz = INIT_BUF_SZ;
-    uint32_t *gBuf1;
-    gBuf1 = (uint32_t *)malloc(prev_sz);
-    struct drm_xocl_sw_mailbox args = { flags, gBuf1, true, prev_sz, id };
+    uint32_t *buf1;
+    buf1 = (uint32_t *)malloc(prev_sz);
+    struct drm_xocl_sw_mailbox args = { flags, buf1, true, prev_sz, id };
 
     printf( "[XOCL->XCLMGMT Intercept ON (HAL)]\n" );
     for( ;; ) {
         args.isTx = true;
         args.sz = prev_sz;
-        ret = xclDaemonUserpf(handle->uDevHandle, &args);
+        ret = xclDaemonUserpf(handle, &args);
         if( ret != 0 ) {
-            std::cout << "sw channel xfer errno=" << errno << " (" << strerror(errno) << ")\n";
+            //std::cout << "sw channel xfer errno=" << errno << " (" << strerror(errno) << ")\n";
             if( errno == EMSGSIZE ) {
-                std::cout << "buffer was of insufficient size, resizing from: " << prev_sz << " to: " << args.sz;
-                if( resize_buffer( gBuf1, args.sz ) != 0 ) {
-                    std::cout << "resize_buffer() failed...exiting\n";
-                    break;
+                //std::cout << "buffer was of insufficient size, resizing from: " << prev_sz << " to: " << args.sz;
+                if( resize_buffer( buf1, args.sz ) != 0 ) {
+                    std::cout << "user: resize_buffer() failed...exiting\n";
+                    exit(1);
                 }
                 prev_sz = args.sz;
-                ret = xclDaemonUserpf(handle->uDevHandle, &args);
+                args.pData = buf1;
+                ret = xclDaemonUserpf(handle, &args);
             } else {
-                std::cout << "user-transfer failed for other reason\n";
-                break;
+                std::cout << "user: transfer failed for other reason\n";
+                exit(1);
             }
+
             if( ret != 0 ) {
-                std::cout << "second transfer failed, exiting.\n";
+                std::cout << "user: second transfer failed, exiting.\n";
                 exit(1);
             }
         }
         printf("[XOCL-PKT-TX]\n");
 
         args.isTx = false;
-        ret = xclDaemonMgmtpf(handle->uDevHandle, &args);
+        ret = xclDaemonMgmtpf(handle, &args);
         if( ret != 0 ) {
-            printf("mgmt-transfer Errno: %s\n", strerror(errno));
+            std::cout << "mgmt: transfer error: " << strerror(errno) << std::endl;
             break;
         }
-        printf("[MGMT-PKT-RX]: %i\n", xferCount);
+        std::cout << "[MGMT-PKT-RX]: " << xferCount << std::endl;
         xferCount++;
     }
-    printf("Exit thread XOCL->XCLMGMT\n");
+    std::cout << "Exit thread XOCL->XCLMGMT\n";
 }
 
-void *xcl_sw_chan_xclmgmt_to_xocl(void *s_handle_ptr)
+void *xcl_sw_chan_xclmgmt_to_xocl(void *handle_ptr)
 {
     int xferCount = 0;
     int ret;
     uint64_t flags = 0;
     uint64_t id = 0;
-    struct s_handle *handle = (struct s_handle*)s_handle_ptr;
+    struct s_handle *s_handle_ptr = (struct s_handle *)handle_ptr;
+    xclDeviceHandle handle = s_handle_ptr->uDevHandle;
     size_t prev_sz = INIT_BUF_SZ;
-    uint32_t *gBuf2;
-    gBuf2 = (uint32_t *)malloc(prev_sz);
-    struct drm_xocl_sw_mailbox args = { flags, gBuf2, true, prev_sz, id };
+    uint32_t *buf2;
+    buf2 = (uint32_t *)malloc(prev_sz);
+    struct drm_xocl_sw_mailbox args = { flags, buf2, true, prev_sz, id };
 
-    printf( "[XCLMGMT->XOCL Intercept ON (HAL)]\n" );
+    std::cout << "               [XCLMGMT->XOCL Intercept ON (HAL)]\n";
     for( ;; ) {
         args.isTx = true;
         args.sz = prev_sz;
-        ret = xclDaemonMgmtpf(handle->uDevHandle, &args);
+        ret = xclDaemonMgmtpf(handle, &args);
         if( ret != 0 ) {
-            std::cout << "sw channel xfer errno=" << errno << " (" << strerror(errno) << ")\n";
+            //std::cout << "sw channel xfer errno=" << errno << " (" << strerror(errno) << ")\n";
             if( errno == EMSGSIZE ) {
-                std::cout << "buffer was of insufficient size, resizing from: " << prev_sz << " to: " << args.sz;
-                if( resize_buffer( gBuf2, args.sz ) != 0 ) {
-                    std::cout << "resize_buffer() failed...exiting\n";
-                    break;
+                //std::cout << "buffer was of insufficient size, resizing from: " << prev_sz << " to: " << args.sz;
+                if( resize_buffer( buf2, args.sz ) != 0 ) {
+                    std::cout << "              mgmt: resize_buffer() failed...exiting\n";
+                    exit(1);
                 }
                 prev_sz = args.sz;
+                args.pData = buf2;
+                ret = xclDaemonMgmtpf(handle, &args);
             } else {
-                std::cout << "              mgmt-transfer failed for other reason\n";
-                break;
+                std::cout << "              mgmt: transfer failed for other reason\n";
+                exit(1);
             }
         }
 
         printf("                [MGMT-PKT-TX]\n");
 
         args.isTx = false;
-        ret = xclDaemonUserpf(handle->uDevHandle, &args);
+        ret = xclDaemonUserpf(handle, &args);
         if( ret != 0 ) {
-            printf("                    user-transfer Errno: %s\n", strerror(errno));
-            break;
+            std::cout << "                    user: transfer error: " << strerror(errno) << std::endl;
+            exit(1);
         }
-        printf("                [XOCL-PKT-RX] %i\n", xferCount);
+        std::cout << "                [XOCL-PKT-RX] " << xferCount << std::endl;
         xferCount++;
     }
 
-    printf("Exit thread XCLMGMT->XOCL\n");
+    std::cout << "Exit thread XCLMGMT->XOCL\n";
 }
 
 
@@ -159,9 +159,6 @@ int main(void)
     xclDeviceInfo2 deviceInfo;
     unsigned deviceIndex = 0;
     xclDeviceHandle uHandle;
-    xclDeviceHandle mHandle; // never init'd
-
-
 
     deviceIndex = 0;
     if( deviceIndex >= xclProbe() ) {
@@ -170,8 +167,7 @@ int main(void)
     }
 
     uHandle = xclOpen(deviceIndex, NULL, XCL_INFO);
-
-    struct s_handle devHandle = { uHandle, mHandle };
+    struct s_handle devHandle = { uHandle };
 
     if( xclGetDeviceInfo2(uHandle, &deviceInfo) ) {
         throw std::runtime_error("Unable to obtain device information");
