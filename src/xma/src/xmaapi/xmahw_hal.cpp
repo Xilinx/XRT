Xilinx/XRT

commit b6e94de660dd7366c94728a7ab324180c48abb41

covertiry fixes-1

src/xma/src/xmaapi/xmahw_hal.cpp
@@ -202,6 +202,7 @@ bool hal_configure(XmaHwCfg *hwcfg, XmaSystemCfg *systemcfg, bool hw_configured)
         {
             xma_logmsg("Could not get info for xclbin file %s\n",
                        xclfullname.c_str());
+            free(buffer);
             return false;
         }
 
@@ -232,6 +233,7 @@ bool hal_configure(XmaHwCfg *hwcfg, XmaSystemCfg *systemcfg, bool hw_configured)
                 continue;
             /* Download xclbin first */
             rc = load_xclbin_to_device(hal->dev_handle, buffer);
+            free(buffer);
             if (rc != 0)
             {
                 xma_logmsg("Could not download xclbin file %s to device %d\n",

src/runtime_src/driver/xclng/xrt/user_aws/xclbin.cpp
@@ -43,6 +43,8 @@
 const char *get_afi_from_axlf(const axlf *buffer)
 {
     const axlf_section_header *bit_header = xclbin::get_axlf_section(buffer, BITSTREAM);
+    if (!bit_header)
+        return nullptr;
     const char *afid = reinterpret_cast<const char *>(buffer);
     afid += bit_header->m_sectionOffset;
     if (bit_header->m_sectionSize > AFI_ID_STR_MAX)

src/runtime_src/driver/xclng/xrt/user_common/memaccess.h
@@ -538,11 +538,13 @@ namespace xcldev {
           if (xclUnmgdPwrite(mHandle, 0, buf, incr, phy) < 0) {
             //error
             std::cout << "Error (" << strerror (errno) << ") writing 0x" << std::hex << incr << " bytes to DDR at offset 0x" << std::hex << phy << std::dec << "\n";
+            free(buf);
             return -1;
           }
           count -= incr;
         }
 
+        free(buf);
         if (count != 0) {
           std::cout << "Error! Written " << std::dec << size-count << " bytes, requested " << size << std::endl;
           return -1;

src/xma/include/lib/xmalimits.h
@@ -26,11 +26,11 @@
 #define MAX_KERNEL_CHANS        64
 #define MAX_KERNEL_FREQS         2
 #define MAX_IMAGE_CONFIGS       16
-#define MAX_FUNCTION_NAME       32
-#define MAX_PLUGIN_NAME         32
-#define MAX_VENDOR_NAME         32
-#define MAX_KERNEL_NAME         64
-#define MAX_DSA_NAME            64
+#define MAX_FUNCTION_NAME       256
+#define MAX_PLUGIN_NAME         256
+#define MAX_VENDOR_NAME         256
+#define MAX_KERNEL_NAME         256
+#define MAX_DSA_NAME            256
 #define XMA_MAX_PLANES           3
 #define MAX_PLUGINS             16
 #define MAX_CONNECTION_ENTRIES  64

src/xma/src/xmaapi/xmalogger.cpp
@@ -149,8 +149,8 @@ xma_logmsg(XmaLogLevelType level, const char *name, const char *msg, ...)
     struct tm      *tm_info;
     struct timeval  tv;
     int32_t         millisec;
-    char            log_time[40];
-    char            log_name[40];
+    char            log_time[40] = {0};
+    char            log_name[40] = {0};
     const char     *log_level;
     int32_t         hdr_offset;
     bool            send2callback = false;
@@ -190,7 +190,7 @@ xma_logmsg(XmaLogLevelType level, const char *name, const char *msg, ...)
     if (name == NULL)
         strncpy(log_name, "XMA-default", sizeof(log_name));
     else
-        strncpy(log_name, name, sizeof(log_name));
+        strncpy(log_name, name, sizeof(log_name)-1);
 
     log_level = g_loglevel_tbl[level].lvl_str;
     

commit 19726cf0010a427de4a609cdff5357de8d8be951

2018.3 : Pull request to fix accessing after free() and fix permission on header (#1077)
* xma : include : xmascaler.h : Remove executable permission of header

Signed-off-by: Rohit Athavale <rathaval@xilinx.com>

* xma: xmahw_hal: Avoid accessing C-string buffer after free()

When more than one devices (cards) are being used by XMA,
the inner loop runs for as many devices present on the system.
The C-string 'buffer' gets freed on the first iteration of this
inner device loop and is accessed after the free() on subsequent
iterations.

Fixes: b6e94de (covertiry fixes-1)
Signed-off-by: Rohit Athavale <rathaval@xilinx.com>

src/xma/src/xmaapi/xmahw_hal.cpp
@@ -245,9 +245,9 @@ bool hal_configure(XmaHwCfg *hwcfg, XmaSystemCfg *systemcfg, bool hw_configured)
 
             /* Always attempt download xclbin */
             rc = load_xclbin_to_device(hal->dev_handle, buffer);
-            free(buffer);
             if (rc != 0)
             {
+                free(buffer);
                 xma_logmsg("Could not download xclbin file %s to device %d\n",
                            xclfullname.c_str(),
                            systemcfg->imagecfg[i].device_id_map[d]);
@@ -257,7 +257,10 @@ bool hal_configure(XmaHwCfg *hwcfg, XmaSystemCfg *systemcfg, bool hw_configured)
             /* Create all kernel contexts on the device */
             rc = create_contexts(hal->dev_handle, info);
             if (rc != XMA_SUCCESS)
+            {
+                free(buffer);
                 return false;
+	    }
 
             //Setup execbo for use with kernel commands
             for (int32_t k = 0, t = 0;
@@ -282,11 +285,13 @@ bool hal_configure(XmaHwCfg *hwcfg, XmaSystemCfg *systemcfg, bool hw_configured)
                     }
                     if (!found) 
                     {
+                        free(buffer);
                         xma_logmsg("ERROR: CU not found. Couldn't create cu_cmd execbo\n");
                         return false;
                     }
                     if (cu_bit_mask == 0) 
                     {
+                        free(buffer);
                         xma_logmsg("ERROR: XMA library doesn't support more than 32 CUs\n");
                         return false;
                     }
@@ -302,6 +307,7 @@ bool hal_configure(XmaHwCfg *hwcfg, XmaSystemCfg *systemcfg, bool hw_configured)
                                                execBO_flags);
                         if (!bo_handle || bo_handle == mNullBO) 
                         {
+                            free(buffer);
                             xma_logmsg("ERROR: Unable to create bo for cu start\n");
                             return false;
                         }
@@ -321,6 +327,7 @@ bool hal_configure(XmaHwCfg *hwcfg, XmaSystemCfg *systemcfg, bool hw_configured)
                 }
             }
         }
+        free(buffer);
     }
     return true;
 }