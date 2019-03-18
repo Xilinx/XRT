6a0876978992d4af921e4a7f3730f0789289949e
Jason Villarreal <jvillar@xilinx.com>
2019-03-18 10:53:54 -0700

    Fixing build issues due to replicated code after conflict resolution

--- src/runtime_src/driver/xclng/xrt/user_gem/shim.h
+++ src/runtime_src/driver/xclng/xrt/user_gem/shim.h
@@ -235,7 +235,6 @@ public:
     size_t xclDebugReadStreamingCounters(xclStreamingDebugCountersResults* streamingResult);
     size_t xclDebugReadStreamingCheckers(xclDebugStreamingCheckersResults* streamingCheckerResult);
     size_t xclDebugReadAccelMonitorCounters(xclAccelMonitorCounterResults* samResult);
-    size_t xclDebugReadStreamingCheckers(xclDebugStreamingCheckersResults* streamingCheckerResult);
 
 
 
