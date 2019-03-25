b686163daae334ca2c2f938ba7565b37d379d542
Sarab <sarab96@gmail.com>
2019-03-25 10:41:13 -0700

    coverity fixes-6 (#1126)

--- src/xma/src/xmaapi/xmaxclbin.cpp
+++ src/xma/src/xmaapi/xmaxclbin.cpp
@@ -37,9 +37,13 @@ char *xma_xclbin_file_open(const char *xclbin_name)
     file.seekg(0, std::ios::beg);
 
     char *buffer = (char*)malloc(size);
+    if (buffer == NULL) {
+        xma_logmsg("ERROR: Could not allocate buffer for file %s\n", xclbin_name);
+        return NULL;
+    }
     if (!file.read(buffer, size))
     {
-        xma_logmsg("Could not read file %s\n", xclbin_name);
+        xma_logmsg("ERROR: Could not read file %s\n", xclbin_name);
         free(buffer);
         buffer = NULL;
     }
