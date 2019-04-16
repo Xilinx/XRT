2c1d0562b2e02d48fc320469fbe2626fb1c3256b
Max Zhen <maxz@xilinx.com>
2019-04-16 13:06:30 -0700

    mailbox timeout too soon

--- src/runtime_src/driver/xclng/drm/xocl/subdev/mailbox.c
+++ src/runtime_src/driver/xclng/drm/xocl/subdev/mailbox.c
@@ -153,9 +153,9 @@ MODULE_PARM_DESC(mailbox_no_intr,
 #define	MBX_DBG(mbx, fmt, arg...)	\
 	xocl_dbg(&mbx->mbx_pdev->dev, fmt "\n", ##arg)
 
-#define	MAILBOX_TIMER	HZ	/* in jiffies */
-#define	MSG_RX_TTL	10UL	/* in MAILBOX_TIMER */
-#define	MSG_TX_TTL	1UL	/* in MAILBOX_TIMER */
+#define	MAILBOX_TIMER	(HZ / 5) /* in jiffies */
+#define	MSG_RX_TTL	50UL	/* in MAILBOX_TIMER */
+#define	MSG_TX_TTL	10UL	/* in MAILBOX_TIMER */
 #define	MSG_MAX_TTL	0xFFFFFFFF
 #define	TEST_MSG_LEN	128
 
