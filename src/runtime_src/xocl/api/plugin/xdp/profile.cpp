2366689acf0ce0eb61c9bf3ab9b20d21b7466c38
pgschuey <schuey@xilinx.com>
2019-04-17 12:36:34 -0600

    fixed issues with host CU showing up in emulation reports (#1226)
    * added DRC for OpenCL objects released; fixed issues with memory resource reporting
    
    * fixed memory resource tagging in trace
    
    * kept memory resource in port name for display purposes
    
    * added 64-bit counter support; added defined port/memory separator
    
    * fixes issues with memory reporting
    
    * corrected reporting usage of memory resources
    
    * HAL changes to support KDMA/XDMA/P2P monitoring in some shells
    
    * initial changes for KDMA and P2P monitor reporting
    
    * more changes to support XDMA/P2P/KDMA monitoring
    
    * correcting naming of monitor type
    
    * added comment
    
    * fixed xbutil formatting
    
    * added device checks for HBM/KDMA/P2P for use in guidance
    
    * minor fixes suggested by Sonal
    
    * allow stream cascades
    
    * appdebug support for k2k
    
    * move constants to xclperf
    
    * change format to hex
    
    * define std hex and dec only once
    
    * update portnames in appdebug
    
    * add pipes
    
    * simplify
    
    * fix cu trace and host data in profile summary
    
    * getting copy buffer times from runtime
    
    * changed name of stream table in profile summary
    
    * logging type and destination address/bank for copy buffers
    
    * removed reporting of clCreateProgramWithBinary in emulation profile summary (CR-1018643)
    
    * backing out clCreateProgramWithBinary change
    
    * added samsung to P2P device list; changed table titles
    
    * fixed issues with host CU showing up in emulation reports
    
    * corrections to same device detection for cdma vs. p2p
    
    * commented out debug

--- src/runtime_src/xocl/api/plugin/xdp/profile.cpp
+++ src/runtime_src/xocl/api/plugin/xdp/profile.cpp
@@ -227,13 +227,23 @@ bool is_same_device(cl_mem buffer1, cl_mem buffer2)
     auto xmem1 = xocl::xocl(buffer1);
     auto xmem2 = xocl::xocl(buffer2);
 
-    if (xmem1 && xmem2)
-      return (xmem1->get_resident_device() == xmem2->get_resident_device());
+    if (xmem1 && xmem2) {
+      auto device1 = xmem1->get_resident_device();
+      auto device2 = xmem2->get_resident_device();
+      //std::cout << "xmem1 resident device = " << device1 << std::endl;
+      //std::cout << "xmem2 resident device = " << device2 << std::endl;
+
+      // TODO: what do we do if one of them is not resident yet?
+      if ((device1 == 0) || (device2 == 0))
+        return true;
+
+      return (device1 == device2);
+    }
   }
   catch (const xocl::error& ex) {
   }
 
-  return false;
+  return true;
 }
 
 xocl::event::action_profile_type
