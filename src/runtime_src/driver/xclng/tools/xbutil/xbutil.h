2c712d6089e6519700bbfe2102360aca18846142
Sonal Santan <sonal.santan@gmail.com>
2019-04-18 16:59:28 -0700

    Dump OS and XRT information as part of query, dump and scan (#1253)
    * Dump OS and XRT information as part of query and dump
    
    * Formatting fixes
    
    * Simplified os-release handling
    
    * Up the JSON schema version and cleanup a few return codes
    
    * Remove unneeded includes
    
    * Changes based on Ryan's feedback

--- src/runtime_src/driver/xclng/tools/xbutil/xbutil.h
+++ src/runtime_src/driver/xclng/tools/xbutil/xbutil.h
@@ -600,14 +600,8 @@ public:
         lines.push_back(ss.str());
     }
 
-
     int readSensors( void ) const
     {
-        sensor_tree::put( "version",                 "1.1.0" ); // json schema version
-        sensor_tree::put( "runtime.build.version",   xrt_build_version );
-        sensor_tree::put( "runtime.build.hash",      xrt_build_version_hash );
-        sensor_tree::put( "runtime.build.date",      xrt_build_version_date );
-        sensor_tree::put( "runtime.build.branch",    xrt_build_version_branch );
         // info
         sensor_tree::put( "board.info.dsa_name", m_devinfo.mName );
         sensor_tree::put( "board.info.vendor", m_devinfo.mVendorId );
@@ -762,12 +756,6 @@ public:
      */
     int dump(std::ostream& ostr) const {
         readSensors();
-        ostr << std::left;
-        ostr << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
-        ostr << "XRT\nVersion:    " << sensor_tree::get<std::string>( "runtime.build.version", "N/A" )
-             <<    "\nGit Hash:   " << sensor_tree::get<std::string>( "runtime.build.hash", "N/A" )
-             <<    "\nGit Branch: " << sensor_tree::get<std::string>( "runtime.build.branch", "N/A" )
-             <<    "\nBuild Date: " << sensor_tree::get<std::string>( "runtime.build.date", "N/A" ) << std::endl;
         ostr << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
         ostr << std::setw(32) << "Shell" << std::setw(32) << "FPGA" << "IDCode" << std::endl;
         ostr << std::setw(32) << sensor_tree::get<std::string>( "board.info.dsa_name",  "N/A" )
