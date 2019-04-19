5fe71ba428a9f2402a80de0708f09f1c488c43cc
Jason Villarreal <jvillar@xilinx.com>
2019-04-19 09:10:14 -0700

    Changing the error message for xstatus to clarify which IP had a problem in reading.  Also, adds reading sspms to xstatus all

--- src/runtime_src/xdp/appdebug/appdebugint.py
+++ src/runtime_src/xdp/appdebug/appdebugint.py
@@ -17,8 +17,8 @@ class printSPMInfo (infCallUtil):
 			if (jsonformat == True):
 				print ("[{{\"info\": \"{}\"}}]".format (errmsg))
 			else :
-				print (errmsg)
-			return				
+				print ("Error reading SPMs : " + (errmsg))
+			return 
 
 		if (jsonformat):
 			stdstr = self.callmethod(spm_ptr,"getstring",[1, 1]);
@@ -44,7 +44,7 @@ class printSSPMInfo (infCallUtil):
 			if (jsonformat == True):
 				print ("[{{\"info\": \"{}\"}}]".format (errmsg))
 			else :
-				print (errmsg)
+				print ("Error reading SSPMs: " + (errmsg))
 			return				
 
 		if (jsonformat):
@@ -70,7 +70,7 @@ class printSAMInfo (infCallUtil):
 			if (jsonformat == True):
 				print ("[{{\"info\": \"{}\"}}]".format (errmsg))
 			else :
-				print (errmsg)
+				print ("Error reading SAMs : " + (errmsg))
 			return				
 
 		if (jsonformat):
@@ -96,7 +96,7 @@ class printLAPCInfo (infCallUtil):
 			if (jsonformat == True):
 				print ("[{{\"info\": \"{}\"}}]".format (errmsg))
 			else :
-				print (errmsg)
+				print ("Error reading LAPCs: " + (errmsg))
 			return				
 
 		if (jsonformat):
@@ -189,9 +189,10 @@ class xstatusAllInfo (gdb.Command,infCallUtil):
 		except ValueError as e:
 			print (e.message)
 			return
-		obj_spm.invoke(arg, 0)
-		obj_sam.invoke(arg, 0)
-		obj_lapc.invoke(arg, 0)
+		spm_error = obj_spm.invoke(arg, 0)
+                sspm_error = obj_sspm.invoke(arg, 0)
+		sam_error = obj_sam.invoke(arg, 0)
+		lapc_error = obj_lapc.invoke(arg, 0)
 xstatusAllInfo()
 
 class xstatusJSONPrefix(gdb.Command):
