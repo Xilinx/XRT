ee26fc3d0b0bcbcdb6d9c187e257882c045b6a5f
Sonal Santan <sonal.santan@xilinx.com>
2019-04-24 21:04:51 -0700

    Use DKMS common.postinst to build and install as it handles multiple kernel installs

--- src/runtime_src/xocl/api/clGetExtensionFunctionAddressForPlatform.cpp
+++ src/runtime_src/xocl/api/clGetExtensionFunctionAddressForPlatform.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2016-2017 Xilinx, Inc
+ * Copyright (C) 2016-2019 Xilinx, Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License"). You may
  * not use this file except in compliance with the License. A copy of the
@@ -14,7 +14,6 @@
  * under the License.
  */
 
-// Copyright 2017 Xilinx, Inc. All rights reserved.
 
 #include <CL/cl.h>
 #include <map>
