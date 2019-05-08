73ede1451f18c64edc76e5ce86a0adf6154545da
tianhaoz95 <tianhaoz@umich.edu>
2019-05-07 21:49:14 -0700

    init the platform monitor stub files

---
+++ src/runtime_src/xdp/profile/core/platform_monitor.cpp
@@ -0,0 +1,7 @@
+# include "xdp/profile/core/system_monitor.h"
+
+namespace xdp {
+
+
+
+}
---
+++ src/runtime_src/xdp/profile/core/platform_monitor.h
@@ -0,0 +1,10 @@
+#ifndef XDP_PROFILE_CORE_SYSTEM_MONITOR_H_
+#define XDP_PROFILE_CORE_SYSTEM_MONITOR_H_
+
+namespace xdp {
+
+
+
+}
+
+#endif
