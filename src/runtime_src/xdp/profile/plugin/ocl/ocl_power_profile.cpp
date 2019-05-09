804f853d22523748a7d391f2e82884ce1b1fb806
tianhaoz95 <tianhaoz@umich.edu>
2019-05-08 21:46:18 -0700

    added the skeleton for starting and closing

--- src/runtime_src/xdp/profile/plugin/ocl/ocl_power_profile.cpp
+++ src/runtime_src/xdp/profile/plugin/ocl/ocl_power_profile.cpp
@@ -5,6 +5,7 @@ namespace xdp {
 OclPowerProfile::OclPowerProfile(xrt::device* xrt_device) : status(PowerProfileStatus::IDLE) {
     std::string power_profile_config = xrt::config::get_power_profile(); // TODO: make this and the device a member
     if (power_profile_config != "off") {
+        power_profiling_output.open("ocl_power_profile.csv");
         start_polling(xrt_device);
     }
 }
@@ -12,12 +13,16 @@ OclPowerProfile::OclPowerProfile(xrt::device* xrt_device) : status(PowerProfileS
 OclPowerProfile::~OclPowerProfile() {
     std::string power_profile_config = xrt::config::get_power_profile();
     if (power_profile_config != "off") {
-
+        stop_polling();
+        polling_thread.join();
+        power_profiling_output.close();
     }
 }
 
 void OclPowerProfile::poll_power(xrt::device* xrt_device) {
-
+    while (should_continue()) {
+        // TODO: do the read of the data and pausing
+    }
 }
 
 bool OclPowerProfile::should_continue() {
