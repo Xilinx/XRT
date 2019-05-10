3560af269c314c14b92ac3ac510e6befeecef05e
Tianhao Zhou <tianhaoz@umich.edu>
2019-05-10 14:45:30 -0700

    debugging update

--- src/runtime_src/xdp/profile/plugin/ocl/ocl_power_profile.cpp
+++ src/runtime_src/xdp/profile/plugin/ocl/ocl_power_profile.cpp
@@ -79,7 +79,7 @@ void OclPowerProfile::poll_power() {
                             << vccint_vol << std::endl;
 
         // TODO: step 3 pause the thread for certain time
-        std::this_thread::sleep_for (std::chrono::milliseconds(100));
+        std::this_thread::sleep_for (std::chrono::milliseconds(200));
     }
 
     aux_curr_fs.close();
