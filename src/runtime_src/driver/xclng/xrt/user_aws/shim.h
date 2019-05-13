5aa9148959a6f371d8867ad06fe922c321698964
Ishita Ghosh <43157456+IshitaGhosh@users.noreply.github.com>
2019-05-13 13:39:19 -0700

    Fix for xstatus for Accel Monitor in App Debug for AWS PR 1418

--- src/runtime_src/driver/xclng/xrt/user_aws/shim.h
+++ src/runtime_src/driver/xclng/xrt/user_aws/shim.h
@@ -155,6 +155,9 @@ public:
                                uint8_t *properties, uint8_t *majorVersions, uint8_t *minorVersions, size_t size);
   size_t xclDebugReadCounters(xclDebugCountersResults* debugResult);
   size_t xclDebugReadCheckers(xclDebugCheckersResults* checkerResult);
+  size_t xclDebugReadStreamingCounters(xclStreamingDebugCountersResults* streamingResult);
+  size_t xclDebugReadStreamingCheckers(xclDebugStreamingCheckersResults* streamingCheckerResult);
+  size_t xclDebugReadAccelMonitorCounters(xclAccelMonitorCounterResults* samResult);
   void readDebugIpLayout();
 
   // Trace
