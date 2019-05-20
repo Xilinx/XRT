947cda842ceada01461a3d842e5cd6b0ba53f2ee
Min Ma <min.ma@xilinx.com>
2019-05-20 16:03:33 -0700

    update embedded platform build script

---
+++ src/runtime_src/tools/scripts/dsa_build.sh
@@ -0,0 +1,91 @@
+#!/bin/bash --
+#  (C) Copyright 2019, Xilinx, Inc.
+
+# This script is to build an embedded system DSA.
+#
+# This script need path to Vivado Tcl script. The script shoule be <dsa_name>_dsa.tcl
+# This script will create a dsa_build/ directory in current path and build dsa in there.
+# The generated DSA file is dsa_build/<dsa_name>.dsa
+
+error()
+{
+	echo "ERROR: $1" 1>&2
+	usage_and_exit 1
+}
+
+usage()
+{
+	echo "Usage: $PROGRAM [options] /path/to/dsa.tcl"
+	echo "	options:"
+	echo "		--help,-h           print this usage"
+	echo ""
+	echo "This script create dsa_build/ folder to build DSA"
+}
+
+usage_and_exit()
+{
+	usage
+	exit $1
+}
+
+SAVED_OPTIONS=$(set +o)
+
+# Don't print all commands
+set +x
+# Error on non-zero exit code, by default:
+set -e
+
+PROGRAM=`basename $0`
+PATH_TO_VIVADO=`which vivado`
+TCLSCRIPT=""
+
+while [ $# -gt 0 ]; do
+	case $1 in
+		--help | -h )
+			usage_and_exit 0
+			;;
+		--* | -* )
+			error "Unregognized option: $1"
+			;;
+		* )
+			TCLSCRIPT=$1
+			break
+			;;
+	esac
+	shift
+done
+
+# Sanity Check
+[ -z "$PATH_TO_VIVADO" ] && error "Vivado not found by 'which'. Please check Vivado installation."
+
+[ ! -e "$TCLSCRIPT" ] && error "Vivado Tcl script no exist. Is path correct? $TCLSCRIPT"
+
+# Normalized Tcl script path
+TCLSCRIPT=`readlink -f $TCLSCRIPT`
+DSA_TCL=`basename $TCLSCRIPT`
+SRC_DIR=`dirname $TCLSCRIPT`
+DSA_NAME=${DSA_TCL%_dsa.tcl}
+
+[ -z "$SRC_DIR" ] && error "Script path is empty"
+
+[ -z "$DSA_NAME" ] && error "Should be <dsa_name>_dsa.tcl"
+# Sanity check done
+
+ORIGINAL_DIR=`pwd`
+
+echo "Starting build DSA for $DSA_NAME"
+echo "Current DIR: $ORIGINAL_DIR"
+echo "Vivado: $PATH_TO_VIVADO"
+echo ""
+
+# Generate DSA and HDF
+cp -r $SRC_DIR ${ORIGINAL_DIR}/dsa_build
+cd ${ORIGINAL_DIR}/dsa_build
+echo " * Building Platform (DSA & HDF from: $PWD)"
+echo "   Starting: $PATH_TO_VIVADO"
+${PATH_TO_VIVADO} -mode batch -notrace -source $DSA_TCL
+
+eval "$SAVED_OPTIONS"; # Restore shell options
+echo "** COMPLETE [${BASH_SOURCE[0]}] **"
+echo ""
+
