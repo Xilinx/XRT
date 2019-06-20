486412ae8c7ca6c9980d8ff14f2cc31c517e4e9c
Himanshu Choudhary <hchoudha@xilinx.com>
2019-06-20 18:12:30 +0530

    zynq support without interrupt

--- src/runtime_src/tools/scripts/petalinux.sh
+++ src/runtime_src/tools/scripts/petalinux.sh
@@ -59,9 +59,17 @@ echo " * Setup PetaLinux: $PETALINUX_LOCATION"
 
 # dsa_build.sh will create ${PLATFORM_NAME}/ directory for petalinux project
 if [ -z $BSP ]; then
-	${XRT_REPO_DIR}/src/runtime_src/tools/scripts/peta_build.sh ${ORIGINAL_DIR}/dsa_build/${PLATFORM_NAME}.dsa
+	if [ -d "${XRT_REPO_DIR}/src/platform/${PLATFORM_NAME}/config.sh" ]; then
+		${XRT_REPO_DIR}/src/runtime_src/tools/scripts/peta_build.sh --config ${XRT_REPO_DIR}/src/platform/${PLATFORM_NAME}/config.sh ${ORIGINAL_DIR}/dsa_build/${PLATFORM_NAME}.dsa
+	else
+		${XRT_REPO_DIR}/src/runtime_src/tools/scripts/peta_build.sh ${ORIGINAL_DIR}/dsa_build/${PLATFORM_NAME}.dsa
+	fi
 else
-	${XRT_REPO_DIR}/src/runtime_src/tools/scripts/peta_build.sh --bsp $BSP ${ORIGINAL_DIR}/dsa_build/${PLATFORM_NAME}.dsa
+	if [ -d "${XRT_REPO_DIR}/src/platform/${PLATFORM_NAME}/config.sh" ]; then
+		${XRT_REPO_DIR}/src/runtime_src/tools/scripts/peta_build.sh --config ${XRT_REPO_DIR}/src/platform/${PLATFORM_NAME}/config.sh --bsp $BSP ${ORIGINAL_DIR}/dsa_build/${PLATFORM_NAME}.dsa
+	else
+		${XRT_REPO_DIR}/src/runtime_src/tools/scripts/peta_build.sh --bsp $BSP ${ORIGINAL_DIR}/dsa_build/${PLATFORM_NAME}.dsa
+	fi
 fi
 
 ${XRT_REPO_DIR}/src/runtime_src/tools/scripts/pfm_build.sh ${ORIGINAL_DIR}/dsa_build/${PLATFORM_NAME}_pfm.tcl
