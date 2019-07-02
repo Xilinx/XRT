cc2fefa905d5d8d692fd64f8d3f7b9243675a1e5
AShivangi <shivangiagarwal53@gmail.com>
2019-07-01 17:09:18 -0700

    add pyopencl as depedency

--- src/runtime_src/tools/scripts/xrtdeps.sh
+++ src/runtime_src/tools/scripts/xrtdeps.sh
@@ -121,7 +121,6 @@ UB_LIST=(\
      pciutils \
      pkg-config \
      protobuf-compiler \
-     python-pip \
      python3-sphinx \
      python3-sphinx-rtd-theme \
      sphinx-common \
@@ -175,8 +174,6 @@ install()
     if [ $FLAVOR == "ubuntu" ] || [ $FLAVOR == "debian" ]; then
         echo "Installing packages..."
         ${SUDO} apt install -y "${UB_LIST[@]}"
-        ${SUDO} -H pip install --upgrade setuptools 
-        ${SUDO} -H pip install pyopencl
     fi
 
     # Enable EPEL on CentOS/RHEL
@@ -207,8 +204,6 @@ install()
         echo "Installing RHEL/CentOS packages..."
         ${SUDO} yum install -y "${RH_LIST[@]}"
         ${SUDO} yum install -y devtoolset-6
-        ${SUDO} -H pip install pyopencl
-        ${SUDO} pip install --ignore-installed numpy==1.8
     fi
 }
 
