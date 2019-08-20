c107d4037780f08bc403a6890b376c335ebc97b8
Jan Stephan <j.stephan@hzdr.de>
2019-08-20 17:22:30 +0200

    Use legacy comparisons for older CMake

--- src/CMake/nativeLnx.cmake
+++ src/CMake/nativeLnx.cmake
@@ -65,7 +65,7 @@ INCLUDE (FindBoost)
 # link errors when XRT shared objects try to link with it.
 
 # Static linking with Boost is enabled on Ubuntu 18.04 and later versions.
-if ((${LINUX_FLAVOR} STREQUAL Ubuntu) AND (${LINUX_VERSION} STRGREATER_EQUAL 18.04))
+if ((${LINUX_FLAVOR} STREQUAL Ubuntu) AND (${LINUX_VERSION} VERSION_GREATER 17.10))
    set(Boost_USE_STATIC_LIBS  ON)
 endif()
 
