0d90448f2408e5da400f72f8dcb3c08011c60a0d
Sarabjeet Singh <sarab96@gmail.com>
2019-08-29 15:19:24 -0700

    minor change

--- src/xma/src/xmaapi/xmahw_hal.cpp
+++ src/xma/src/xmaapi/xmahw_hal.cpp
@@ -293,7 +293,8 @@ bool hal_configure(XmaHwCfg *hwcfg, XmaXclbinParameter *devXclbins, int32_t num_
                     tmp1.kernel_channels = true;
                     tmp1.max_channel_id = info.ip_layout[d].max_channel_id;
                 }
-                rc = xma_xclbin_map2ddr(info.ip_ddr_mapping[d], &tmp1.default_ddr_bank);
+                //Allow default ddr_bank of -1; When CU is not connected to any ddr
+                xma_xclbin_map2ddr(info.ip_ddr_mapping[d], &tmp1.default_ddr_bank);
 
                 //XMA now supports multiple DDR Banks per Kernel
                 tmp1.ip_ddr_mapping = info.ip_ddr_mapping[d];
