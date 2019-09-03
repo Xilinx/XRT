5b8fa2903c3259b1e05b35c59f8c5443034ed1bc
Siva Rajesh Jarugula <sivaraj@xilinx.com>
2019-09-03 14:06:43 +0530

    Added explicit metadata switches for design_intent
    Signed-off-by: Siva Rajesh Jarugula <sivaraj@xilinx.com>

--- src/platform/zc706_base/zc706_base_xsa.tcl
+++ src/platform/zc706_base/zc706_base_xsa.tcl
@@ -409,6 +409,9 @@ set_property platform.post_sys_link_tcl_hook        ./dynamic_postlink.tcl      
 
 set_property platform.default_output_type "sd_card" [current_project]
 set_property platform.design_intent.embedded "true" [current_project]
+set_property platform.design_intent.server_managed "false" [current_project]
+set_property platform.design_intent.external_host "false" [current_project]
+set_property platform.design_intent.datacenter "false" [current_project]
 
 write_hw_platform -force ./zc706_base.xsa
 
