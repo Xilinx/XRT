a48d8fd678843e59600fa7cdc1faa89d33d68851
subhransu-xilinx <49639126+subhransu-xilinx@users.noreply.github.com>
2019-09-13 21:33:38 +0530

    Fix interrupt postlink, resize pblock and consistent platform display name (#2033)

--- src/platform/zcu104_base/zcu104_base_pfm.tcl
+++ src/platform/zcu104_base/zcu104_base_pfm.tcl
@@ -1,6 +1,6 @@
 platform -name zcu104_base -desc "A basic platform targeting the ZCU102 evaluation board, which includes 4GB of DDR4 for the Processing System, 512MB of DDR4 for the Programmable Logic, 2x64MB Quad-SPI Flash and an SDIO card interface. More information at https://www.xilinx.com/products/boards-and-kits/zcu104.html" -hw ./zcu104_base.xsa -out ./output
 
-domain -name xrt -display-name "A53 OpenCL Linux" -proc psu_cortexa53 -os linux -image ./src/a53/xrt/image
+domain -name xrt -proc psu_cortexa53 -os linux -image ./src/a53/xrt/image
 domain config -boot ./src/boot
 domain config -bif ./src/a53/xrt/linux.bif
 domain -runtime opencl
