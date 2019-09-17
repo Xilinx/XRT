7e8a66d69c3c7a8eef9b888dc020fdab63c8f6d1
Siva Rajesh Jarugula <sivaraj@xilinx.com>
2019-09-17 20:02:52 +0530

    commenting the sysroot generation, it is already present in the ertbuild.sh script
    Signed-off-by: Siva Rajesh Jarugula <sivaraj@xilinx.com>

--- src/platform/zc702_base/zc702_base_pfm.tcl
+++ src/platform/zc702_base/zc702_base_pfm.tcl
@@ -8,7 +8,7 @@ domain config -bif ./src/a9/xrt/linux.bif
 domain -runtime opencl
 domain -qemu-args ./src/qemu/lnx/qemu_args.txt
 domain -qemu-data ./src/boot
-domain -sysroot ./src/arm-xilinx-linux
+#domain -sysroot ./src/arm-xilinx-linux
 
 platform -generate
 
