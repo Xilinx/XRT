126a5dba4b151251d2d56f1a1fad1ba4f283ca56
Stephen Rozum <stephenr@xilinx.com>
2019-09-26 11:41:08 -0600

    Reduced the inner loops around reporting the IINTF UUIDS. Work Done --------- + Broke down a large multiple indidention search to two smaller ones where one finds the PARTIION_METADATA section.  The other look examines the found section and reports the IINT UUID interfaces.

--- src/runtime_src/tools/xclbin/FormattedOutput.cxx
+++ src/runtime_src/tools/xclbin/FormattedOutput.cxx
@@ -350,27 +350,30 @@ reportXclbinInfo( std::ostream & _ostream,
   }
 
   {
-    // Find the PARTITION_METADATA section (if there is one)
+    // Get the PARTITION_METADATA property tree (if there is one)
+    boost::property_tree::ptree ptPartitionMetadata;
     for (auto pSection : _sections) {
       if (pSection->getSectionKind() == PARTITION_METADATA) {
         boost::property_tree::ptree ptRoot;
         pSection->getPayload(ptRoot);
         if (!ptRoot.empty()) {
-          boost::property_tree::ptree ptPartitionMetadata = ptRoot.get_child("partition_metadata");
-          if (!ptPartitionMetadata.empty()) {
-            boost::property_tree::ptree ptInterfaces = ptPartitionMetadata.get_child("interfaces");
-            if (!ptInterfaces.empty()) {
-              for (const auto& kv : ptInterfaces) {
-                boost::property_tree::ptree ptInterface = kv.second;
-                std::string sUUID = ptInterface.get<std::string>("interface_uuid", "");
-                if (!sUUID.empty()) {
-                  _ostream << XUtil::format("   %-23s %s", "UUID (IINTF):", sUUID.c_str()).c_str() << std::endl;
-                }
-              }
-            }
+          ptPartitionMetadata = ptRoot.get_child("partition_metadata");
+          break;
+        }
+      }
+    }
+
+    // If we have a property tree, report all of the interface_uuids
+    if (!ptPartitionMetadata.empty()) {
+      boost::property_tree::ptree ptInterfaces = ptPartitionMetadata.get_child("interfaces");
+      if (!ptInterfaces.empty()) {
+        for (const auto& kv : ptInterfaces) {
+          boost::property_tree::ptree ptInterface = kv.second;
+          std::string sUUID = ptInterface.get<std::string>("interface_uuid", "");
+          if (!sUUID.empty()) {
+            _ostream << XUtil::format("   %-23s %s", "UUID (IINTF):", sUUID.c_str()).c_str() << std::endl;
           }
         }
-        break;
       }
     }
   }
