816730281294b5f45c7fcabe270a50523a7de9c4
Stephen Rozum <40244911+rozumx@users.noreply.github.com>
2019-10-19 01:01:53 -0600

    Updated compile macros for XclBinUtilException() (#2309)

--- src/runtime_src/tools/xclbin/XclBinUtilities.h
+++ src/runtime_src/tools/xclbin/XclBinUtilities.h
@@ -57,19 +57,11 @@ class XclBinUtilException : public std::runtime_error {
     XclBinExceptionType m_eExceptionType;
 
 public:
-#ifndef _WIN32
     XclBinUtilException(XclBinExceptionType _eExceptionType,
                         const std::string & _msg,
                         const char * _file = __FILE__,
                         int _line = __LINE__,
                         const char * _function = __FUNCTION__)
-#else
-    XclBinUtilException(XclBinExceptionType _eExceptionType,
-                        const std::string & _msg,
-                        const char * _file = "fix me",
-                        int _line = 0,
-                        const char * _function = "fix me")
-#endif
     : std::runtime_error(_msg)
     , m_msg(_msg)
     , m_file(_file)
