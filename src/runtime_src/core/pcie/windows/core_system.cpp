25c0b88dc90b17354d44af6c72b7e010091f9d80
Rajkumar Rampelli <rajkumar@xilinx.com>
2019-10-31 14:31:58 +0530

    windows: Add support in getting OS information
    Add changes in core_system to read windows OS information
    Output looks like below:
    
    OS name:        Windows Server 2016 Datacenter Evaluation
    Release:        14393.rs1_release.161220-1747
    Version:        6.3
    Machine:        x86_64
    Now:            Wed Oct 30 11:02:18 2019
    
    Signed-off-by: Rahul Bramandlapalli <rbramand@xilinx.com>
    Signed-off-by: Rajkumar Rampelli <rajkumar@xilinx.com>

--- src/runtime_src/core/pcie/windows/core_system.cpp
+++ src/runtime_src/core/pcie/windows/core_system.cpp
@@ -17,6 +17,7 @@
 
 #include "common/core_system.h"
 #include "gen/version.h"
+#include <atlstr.h>
 
 
 void xrt_core::system::get_xrt_info(boost::property_tree::ptree &_pt)
@@ -32,11 +33,77 @@ void xrt_core::system::get_xrt_info(boost::property_tree::ptree &_pt)
 }
 
 
-void xrt_core::system::get_os_info(boost::property_tree::ptree &_pt)
+//function for converting CString type to std::string
+std::string convert(CString input)
 {
-  // TODO
-  _pt.put("windows", "");
+  CT2CA converted_input(input);
+  std::string string_input(converted_input);
+
+  return string_input;
 }
 
 
+CString GetStringFromReg(HKEY keyParent, CString keyName, CString keyValName)
+{
+  CRegKey key;
+  CString out;
 
+  if (key.Open(keyParent, keyName, KEY_READ) == ERROR_SUCCESS) {
+	ULONG len = 256;
+	key.QueryStringValue(keyValName, out.GetBuffer(256), &len);
+	out.ReleaseBuffer();
+	key.Close();
+  }
+
+  return out;
+}
+
+
+CString getmachinename()
+{
+  CString machine;
+  SYSTEM_INFO sysInfo;
+
+  // Get hardware info
+  ZeroMemory(&sysInfo, sizeof(SYSTEM_INFO));
+  GetSystemInfo(&sysInfo);
+  // Set processor architecture
+  switch (sysInfo.wProcessorArchitecture) {
+  case PROCESSOR_ARCHITECTURE_AMD64:
+	  machine = CString(_T("x86_64"));
+	  break;
+  case PROCESSOR_ARCHITECTURE_IA64:
+	  machine = CString(_T("ia64"));
+	  break;
+  case PROCESSOR_ARCHITECTURE_INTEL:
+	  machine = CString(_T("x86"));
+	  break;
+  case PROCESSOR_ARCHITECTURE_UNKNOWN:
+  default:
+	  machine = CString(_T("unknown"));
+	  break;
+  }
+
+  return machine;
+}
+
+
+void xrt_core::system::get_os_info(boost::property_tree::ptree &_pt)
+{
+  CString osversion = GetStringFromReg(HKEY_LOCAL_MACHINE,
+		L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", L"CurrentVersion");
+  CString osname = GetStringFromReg(HKEY_LOCAL_MACHINE,
+		L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", L"ProductName");
+  CString build = GetStringFromReg(HKEY_LOCAL_MACHINE,
+		L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", L"BuildLab");
+  CString machine = getmachinename();
+  char tnow[26];
+  time_t result = time(NULL);
+
+  ctime_s(tnow, sizeof tnow, &result);
+  _pt.put("sysname", convert(osname));
+  _pt.put("release", convert(build));
+  _pt.put("version", convert(osversion));
+  _pt.put("machine", convert(machine));
+  _pt.put("now", tnow);
+}
