2c28a7dd80699f2edf5563cccfe7d04e580b2824
David Zhang <davidzha@xilinx.com>
2019-10-30 20:46:46 -0700

    CR-1047439     multi_process example fails for zcu102_base_dfx hardware board run

--- src/runtime_src/core/edge/drm/zocl/sched_exec.c
+++ src/runtime_src/core/edge/drm/zocl/sched_exec.c
@@ -2930,8 +2930,10 @@ sched_init_exec(struct drm_device *drm)
 	SCHED_DEBUG("-> %s\n", __func__);
 
 	exec_core = devm_kzalloc(drm->dev, sizeof(*exec_core), GFP_KERNEL);
-	if (!exec_core)
+	if (!exec_core) {
+		DRM_ERROR("Alloc exec_core failed: no memory\n");
 		return -ENOMEM;
+	}
 
 	zdev->exec = exec_core;
 	spin_lock_init(&exec_core->ctx_list_lock);
