Xilinx/XRT

commit 9b40ccb89a78fbde04a0465534c46cbb5f2bbdb8

HAL level profiling with counters for PCIe and Zynq (#1936)
* Phase 1 commit

* Hal profling support using callbacks in HAL layer and methods in OCLProfiler. This is not the intended final product as this still uses xocl objects like platform, device. Data Transfer report looks good with this intermediate support

* Introduce Device representation in XDP library : This encapsulates different of device concepts in different flow like xrt::device in OpenCl flow, HAL handle in HAL flow. XDP now has a base interface xdp::Device which declares the APIs needed by XDP monitor IPs to access device. For OpenCL flow, this interface class is implemented as xdp::XrtDevice which contains and accesses xrt::device handle. For HAL only flow, the interface class is implemented as xdp::HalDevice which contains and accesses HAL handle. Corresponding updates in DeviceIntf, Profile monitor IPs are added. Also for DDR Trace, Device DDR buffer object management has been moved to XDP Profiler layer i.e. OCLProfiler (for now). Later, other Profiler (like newly added HALProfiler) should also implement DDR buffer management for trace

* Incorporate separation of xdp::HalDevice into a separate xdp_hal_plugin library (instead of xdp library which would have caused issues in Emulation)

* HAL profiling report : draft support for PCIe

* Add checks based on hal_profile configuration in xrt.ini

* Add xclCreateProfileResults to allocate and initialize ProfileResults buffer, update xclGetProfileResults, xclDestroyProfileResults. Remove profile_results.h and move the new data structures and APIs to xrt-next.h

* Zynq HAL profiling support

* Undo temporary changes in ocl_profiler.cpp

* Undo unrequired edits

* Initialize AIM profile results with device name, cuPortname

* Add 'deviceName' field in ProfileResults, remove 'deviceName' from AIM, ASM data in ProfileResults, Update initialization and destruction of ProfileResults

src/runtime_src/core/include/experimental/xrt-next.h
@@ -77,6 +77,76 @@ XCL_DRIVER_DLLESPEC int xclGetSysfsPath(xclDeviceHandle handle, const char* subd
  */
 XCL_DRIVER_DLLESPEC int xclGetDebugProfileDeviceInfo(xclDeviceHandle handle, xclDebugProfileDeviceInfo* info);
 
+
+struct KernelTransferData
+{
+   char* cuPortName;
+   char* argName;
+   char* memoryName;
+   
+   uint64_t totalReadBytes; 
+   uint64_t totalReadTranx; 
+   uint64_t totalReadLatency; 
+   uint64_t totalReadBusyCycles; 
+   uint64_t minReadLatency; 
+   uint64_t maxReadLatency; 
+
+   uint64_t totalWriteBytes; 
+   uint64_t totalWriteTranx; 
+   uint64_t totalWriteLatency; 
+   uint64_t totalWriteBusyCycles; 
+   uint64_t minWriteLatency; 
+   uint64_t maxWriteLatency; 
+};
+
+struct CuExecData
+{
+   char* cuName;
+   char* kernelName;
+
+   uint64_t cuExecCount;
+   uint64_t cuExecCycles;
+   uint64_t cuBusyCycles;
+   uint64_t cuMaxExecCycles;
+   uint64_t cuMinExecCycles;
+   uint64_t cuMaxParallelIter;
+   uint64_t cuStallExtCycles;
+   uint64_t cuStallIntCycles;
+   uint64_t cuStallStrCycles;
+
+}; 
+
+struct StreamTransferData
+{
+  char* masterPortName;
+  char* slavePortName;
+
+  uint64_t strmNumTranx;
+  uint64_t strmBusyCycles;
+  uint64_t strmDataBytes;
+  uint64_t strmStallCycles;
+  uint64_t strmStarveCycles;
+};
+
+struct ProfileResults
+{
+  char* deviceName;
+
+  uint64_t numAIM;
+  KernelTransferData* kernelTransferData;
+
+  uint64_t numAM;
+  CuExecData* cuExecData;
+
+  uint64_t numASM;
+  StreamTransferData* streamData;
+};
+
+
+XCL_DRIVER_DLLESPEC int xclCreateProfileResults(xclDeviceHandle, ProfileResults**);
+XCL_DRIVER_DLLESPEC int xclGetProfileResults(xclDeviceHandle, ProfileResults*);
+XCL_DRIVER_DLLESPEC int xclDestroyProfileResults(xclDeviceHandle, ProfileResults*);
+
 #ifdef __cplusplus
 }
 #endif

src/runtime_src/CMakeLists.txt
@@ -27,6 +27,12 @@ add_library(oclxdp SHARED ${OPENCL_SRC}
   )
 add_dependencies(oclxdp xdp xilinxopencl)
 
+add_library(xdp_hal_plugin SHARED 
+  $<TARGET_OBJECTS:xdp_hal_plugin_obj>
+  $<TARGET_OBJECTS:xdpobj>
+  )
+add_dependencies(xdp_hal_plugin xdp)
+
 option(XOCL_VERBOSE "Enable xocl verbosity" OFF)
 option(XRT_VERBOSE "Enable xrt verbosity" OFF)
 
@@ -78,6 +84,11 @@ target_link_libraries(oclxdp
   xilinxopencl
  )
 
+target_link_libraries(xdp_hal_plugin xdp)
+
+set_target_properties(xdp_hal_plugin PROPERTIES VERSION ${XRT_VERSION_STRING}
+  SOVERSION ${XRT_SOVERSION})
+
 set_target_properties(oclxdp PROPERTIES VERSION ${XRT_VERSION_STRING}
   SOVERSION ${XRT_SOVERSION})
 
@@ -95,10 +106,9 @@ target_link_libraries(xilinxopencl
   rt
   )
 
-install (TARGETS xilinxopencl oclxdp LIBRARY DESTINATION ${XRT_INSTALL_DIR}/lib)
+install (TARGETS xilinxopencl oclxdp xdp_hal_plugin LIBRARY DESTINATION ${XRT_INSTALL_DIR}/lib)
 
 add_subdirectory(core)
-
 
 else()
 # = WINDOWS ===================================================================

src/runtime_src/core/common/config_reader.h
@@ -157,6 +157,13 @@ get_trace_buffer_size()
 }
 
 inline bool
+get_hal_profile()
+{
+  static bool value = detail::get_bool_value("Debug.hal_profile", false);
+  return value;
+}
+
+inline bool
 get_api_checks()
 {
   static bool value = detail::get_bool_value("Runtime.api_checks",true);

src/runtime_src/core/edge/sw_em/generic_pcie_hal2/halapi.cxx
@@ -540,3 +540,19 @@ int xclCloseContext(xclDeviceHandle handle, uuid_t xclbinId, unsigned ipIndex)
   xclcpuemhal2::CpuemShim *drv = xclcpuemhal2::CpuemShim::handleCheck(handle);
   return drv ? drv->xclCloseContext(xclbinId, ipIndex) : -ENODEV;
 }
+
+int xclCreateProfileResults(xclDeviceHandle handle, ProfileResults** results)
+{
+  return 0;
+}
+
+int xclGetProfileResults(xclDeviceHandle handle, ProfileResults* results)
+{
+  return 0;
+}
+
+int xclDestroyProfileResults(xclDeviceHandle handle, ProfileResults* results)
+{
+  return 0;
+}
+

src/runtime_src/core/edge/user/CMakeLists.txt
@@ -3,6 +3,11 @@ include_directories(
   ${DRM_INCLUDE_DIRS}
   )
 
+file(GLOB XRT_EDGE_USER_PLUGIN_XDP_FILES
+  "plugin/xdp/*.h"
+  "plugin/xdp/*.cpp"
+  )
+
 file(GLOB XRT_USER_FILES
   "*.h"
   "*.cpp"
@@ -16,14 +21,21 @@ set(XRT_SRC
   ${XRT_USER_FILES}
   )
 
+add_library(core_edgeuser_plugin_xdp_no_dl_load_objects OBJECT ${XRT_EDGE_USER_PLUGIN_XDP_FILES})
+
+add_compile_options("-DXRT_LOAD_XDP_HAL_PLUGIN")
+add_library(core_edgeuser_plugin_xdp_objects OBJECT ${XRT_EDGE_USER_PLUGIN_XDP_FILES})
+
 add_library(core_edgeuser_objects OBJECT ${XRT_USER_FILES})
 
 add_library(xrt_core SHARED ${XRT_SRC}
+  $<TARGET_OBJECTS:core_edgeuser_plugin_xdp_objects>
   $<TARGET_OBJECTS:core_common_objects>
   #$<TARGET_OBJECTS:core_edgecommon_objects>
   )
 
 add_library(xrt_core_static STATIC ""
+  $<TARGET_OBJECTS:core_edgeuser_plugin_xdp_no_dl_load_objects>
   $<TARGET_OBJECTS:core_edgeuser_objects>
   #$<TARGET_OBJECTS:core_edgecommon_objects>
   $<TARGET_OBJECTS:core_common_objects>
@@ -36,6 +48,7 @@ target_link_libraries(xrt_core
   xrt_coreutil
   pthread
   rt
+  dl
   uuid
   boost_filesystem
   boost_system

src/runtime_src/core/edge/user/plugin/xdp/hal_profile.cpp
@@ -1,0 +1,307 @@
+#include "plugin/xdp/hal_profile.h"
+#include "core/common/config_reader.h"
+#include "core/common/message.h"
+
+namespace bfs = boost::filesystem;
+
+namespace xdphal {
+
+cb_func_type cb;
+
+bool loaded = false;
+std::atomic<unsigned> global_idcode(0);
+std::mutex lock;
+
+static bool cb_valid() {
+    return loaded && cb;
+}
+
+static boost::filesystem::path& dllExt() {
+  static boost::filesystem::path sDllExt(".so");
+  return sDllExt;
+}
+
+inline bool isDLL(const bfs::path& path) {
+  return (bfs::exists(path)
+          && bfs::is_regular_file(path)
+          && path.extension()==dllExt());
+}
+
+static int directoryOrError(const bfs::path& path) {
+  if (!bfs::is_directory(path)) {
+//    throw std::runtime_error("No such directory '" + path.string() + "'");
+    xrt_core::message::send(xrt_core::message::severity_level::XRT_ERROR, "XRT", std::string("No such directory '" + path.string() + "'"));
+    return -1;
+  }
+  return 0;
+}
+
+static const char* emptyOrValue(const char* cstr) {
+  return cstr ? cstr : "";
+}
+
+CallLogger::CallLogger(unsigned id)
+           : m_local_idcode(id)
+{
+  load_xdp_plugin_library(nullptr);
+}
+
+CallLogger::~CallLogger()
+{}
+
+AllocBOCallLogger::AllocBOCallLogger(xclDeviceHandle handle, size_t size, int unused, unsigned flags) 
+    : CallLogger(global_idcode)
+{
+    if (!cb_valid()) return;
+    global_idcode++;    // increment only if valid calllback
+    CBPayload payload = {m_local_idcode, handle};
+    cb(HalCallbackType::ALLOC_BO_START, &payload);
+}
+
+AllocBOCallLogger::~AllocBOCallLogger() {
+    if (!cb_valid()) return;
+    CBPayload payload = {m_local_idcode, 0};
+    cb(HalCallbackType::ALLOC_BO_END, &payload);
+}
+
+FreeBOCallLogger::FreeBOCallLogger(xclDeviceHandle handle, unsigned int boHandle) 
+    : CallLogger(global_idcode)
+{
+    if (!cb_valid()) return;
+    global_idcode++;    // increment only if valid calllback
+    CBPayload payload = {m_local_idcode, handle};
+    cb(HalCallbackType::FREE_BO_START, &payload);
+}
+
+FreeBOCallLogger::~FreeBOCallLogger() {
+    if (!cb_valid()) return;
+    CBPayload payload = {m_local_idcode, 0};
+    cb(HalCallbackType::FREE_BO_END, &payload);
+}
+
+WriteBOCallLogger::WriteBOCallLogger(xclDeviceHandle handle, unsigned int boHandle, const void *src, size_t size, size_t seek) 
+    : CallLogger(global_idcode)
+{
+    if (!cb_valid()) return;
+    global_idcode++;    // increment only if valid calllback
+    CBPayload payload = {m_local_idcode, handle};
+    cb(HalCallbackType::WRITE_BO_START, &payload);
+}
+
+WriteBOCallLogger::~WriteBOCallLogger() {
+    if (!cb_valid()) return;
+    CBPayload payload = {m_local_idcode, 0};
+    cb(HalCallbackType::WRITE_BO_END, &payload);
+}
+
+ReadBOCallLogger::ReadBOCallLogger(xclDeviceHandle handle, unsigned int boHandle, void *dst, size_t size, size_t skip) 
+    : CallLogger(global_idcode)
+{
+    if (!cb_valid()) return;
+    global_idcode++;    // increment only if valid calllback
+    CBPayload payload = {m_local_idcode, handle};
+    cb(HalCallbackType::READ_BO_START, &payload);
+}
+
+ReadBOCallLogger::~ReadBOCallLogger() {
+    if (!cb_valid()) return;
+    CBPayload payload = {m_local_idcode, 0};
+    cb(HalCallbackType::READ_BO_END, &payload);
+}  
+
+MapBOCallLogger::MapBOCallLogger(xclDeviceHandle handle, unsigned int boHandle, bool write) 
+    : CallLogger(global_idcode)
+{
+    if (!cb_valid()) return;
+    global_idcode++;    // increment only if valid calllback
+    CBPayload payload = {m_local_idcode, handle};
+    cb(HalCallbackType::MAP_BO_START, &payload);
+}
+
+MapBOCallLogger::~MapBOCallLogger() {
+    if (!cb_valid()) return;
+    CBPayload payload = {m_local_idcode, 0};
+    cb(HalCallbackType::MAP_BO_END, &payload);
+}
+
+SyncBOCallLogger::SyncBOCallLogger(xclDeviceHandle handle, unsigned int boHandle, xclBOSyncDirection dir, size_t size, size_t offset) 
+    : CallLogger(global_idcode)
+{
+    if (!cb_valid()) return;
+    global_idcode++;    // increment only if valid calllback
+    CBPayload payload = {m_local_idcode, handle};
+    cb(HalCallbackType::SYNC_BO_START, &payload);
+}
+
+SyncBOCallLogger::~SyncBOCallLogger() {
+    if (!cb_valid()) return;
+    CBPayload payload = {m_local_idcode, 0};
+    cb(HalCallbackType::SYNC_BO_END, &payload);
+}
+
+UnmgdPwriteCallLogger::UnmgdPwriteCallLogger(xclDeviceHandle handle, unsigned flags, const void *buf, size_t count, uint64_t offset) 
+    : CallLogger(global_idcode)
+{
+    if (!cb_valid()) return;
+    global_idcode++;    // increment only if valid calllback
+    UnmgdPreadPwriteCBPayload payload = {{m_local_idcode, handle}, flags, count, offset};
+    cb(HalCallbackType::UNMGD_WRITE_START, &payload);
+}
+
+UnmgdPwriteCallLogger::~UnmgdPwriteCallLogger() {
+    if (!cb_valid()) return;
+    CBPayload payload = {m_local_idcode, 0};
+    cb(HalCallbackType::UNMGD_WRITE_END, &payload);
+}
+
+UnmgdPreadCallLogger::UnmgdPreadCallLogger(xclDeviceHandle handle, unsigned flags, void *buf, size_t count, uint64_t offset) 
+    : CallLogger(global_idcode)
+{
+    if (!cb_valid()) return;
+    global_idcode++;    // increment only if valid calllback
+    UnmgdPreadPwriteCBPayload payload = {{m_local_idcode, handle}, flags, count, offset};
+    cb(HalCallbackType::UNMGD_READ_START, &payload);
+}
+
+UnmgdPreadCallLogger::~UnmgdPreadCallLogger() {
+    if (!cb_valid()) return;
+    CBPayload payload = {m_local_idcode, 0};
+    cb(HalCallbackType::UNMGD_READ_END, &payload);
+}
+
+ReadCallLogger::ReadCallLogger(xclDeviceHandle handle, xclAddressSpace space, uint64_t offset, void *hostBuf, size_t size) 
+    : CallLogger(global_idcode)
+{
+    if (!cb_valid()) return;
+    global_idcode++;    // increment only if valid calllback
+    ReadWriteCBPayload payload = {{m_local_idcode, handle}, space, offset, size};
+    cb(HalCallbackType::READ_START, &payload);
+}
+
+ReadCallLogger::~ReadCallLogger() {
+    if (!cb_valid()) return;
+    CBPayload payload = {m_local_idcode, 0};
+    cb(HalCallbackType::READ_END, &payload);
+}
+
+WriteCallLogger::WriteCallLogger(xclDeviceHandle handle, xclAddressSpace space, uint64_t offset, const void *hostBuf, size_t size) 
+    : CallLogger(global_idcode)
+{
+    if (!cb_valid()) return;
+    global_idcode++;    // increment only if valid calllback
+    ReadWriteCBPayload payload = { {m_local_idcode, handle}, space, offset, size};
+    cb(HalCallbackType::WRITE_START, (void*)(&payload));
+}
+
+WriteCallLogger::~WriteCallLogger() {
+    if (!cb_valid()) return;
+    CBPayload payload = {m_local_idcode, 0};
+    cb(HalCallbackType::WRITE_END, &payload);
+}
+
+
+StartDeviceProfilingCls::StartDeviceProfilingCls(xclDeviceHandle handle)
+{
+  load_xdp_plugin_library(nullptr);
+  if(!cb_valid()) return;
+  CBPayload payload = {0, handle};
+  cb(HalCallbackType::START_DEVICE_PROFILING, &payload);
+}
+
+StartDeviceProfilingCls::~StartDeviceProfilingCls()
+{}
+
+CreateProfileResultsCls::CreateProfileResultsCls(xclDeviceHandle handle, ProfileResults** results)
+{
+  load_xdp_plugin_library(nullptr);
+  if(!cb_valid()) return;
+  ProfileResultsCBPayload payload = {{0, handle}, static_cast<void*>(results)};   // pass ProfileResults** as void*
+  cb(HalCallbackType::CREATE_PROFILE_RESULTS, &payload);
+}
+
+CreateProfileResultsCls::~CreateProfileResultsCls()
+{}
+
+GetProfileResultsCls::GetProfileResultsCls(xclDeviceHandle handle, ProfileResults* results)
+{
+  load_xdp_plugin_library(nullptr);
+  if(!cb_valid()) return;
+  ProfileResultsCBPayload payload = {{0, handle}, static_cast<void*>(results)};
+  cb(HalCallbackType::GET_PROFILE_RESULTS, &payload);
+}
+
+GetProfileResultsCls::~GetProfileResultsCls()
+{}
+
+DestroyProfileResultsCls::DestroyProfileResultsCls(xclDeviceHandle handle, ProfileResults* results)
+{
+  load_xdp_plugin_library(nullptr);
+  if(!cb_valid()) return;
+  ProfileResultsCBPayload payload = {{0, handle}, static_cast<void*>(results)};
+  cb(HalCallbackType::DESTROY_PROFILE_RESULTS, &payload);
+}
+
+DestroyProfileResultsCls::~DestroyProfileResultsCls()
+{}
+
+void load_xdp_plugin_library(HalPluginConfig* )
+{
+#ifdef XRT_LOAD_XDP_HAL_PLUGIN
+
+    std::lock_guard<std::mutex> loader_guard(lock);
+    if (loaded) {
+        return;
+    }
+
+    if(!xrt_core::config::get_hal_profile()) {
+      // hal_profile is not set to correct configuration. Skip loading xdp_hal_plugin.
+      // There will be no profile support in this run.
+//      std::cout << "\"hal_profile\" is not set to true in xrt.ini Debug configuration. So, no HAL profiling is available." << std::endl;
+      return;
+    }
+
+    // hal_profile is set to "true". Try to load xdp_hal_plugin library
+    if(xrt_core::config::get_profile()) {
+      // "profile=true" is also set. This enables OpenCL based flow for profiling. 
+      // Currently, mix of OpenCL and HAL based profiling is not supported.
+      // So, give error and skip loading of xdp_hal_plugin library
+//      throw std::runtime_error("Both profile=true and hal_profile=true set in xrt.ini config. Currently, these flows are not supported to work together. Hence, profiling will not be available in this run. Please set only one of the configuration depending on type of application and re-run");
+      xrt_core::message::send(xrt_core::message::severity_level::XRT_ERROR, "XRT", std::string("Both profile=true and hal_profile=true set in xrt.ini config. Currently, these flows are not supported to work together. Hence, HAL level profiling will not be available in this run. To enable HAL level profiling, please set hal_profile=true only and re-run."));
+      return;
+    }
+
+//    std::cout << "Loading xdp plugins ..." << std::endl;
+    bfs::path xrt(emptyOrValue(getenv("XILINX_XRT")));
+    bfs::path libname("libxdp_hal_plugin.so");
+    if (xrt.empty()) {
+//        throw std::runtime_error("Library " + libname.string() + " not found! XILINX_XRT not set");
+      xrt_core::message::send(xrt_core::message::severity_level::XRT_ERROR, "XRT", std::string("Library " + libname.string() + " not found! XILINX_XRT not set"));
+      return;
+    }
+    bfs::path p(xrt / "lib");
+    if(directoryOrError(p)) {
+      return;
+    }
+    p /= libname;
+    if (!isDLL(p)) {
+//        throw std::runtime_error("Library " + p.string() + " not found!");
+      xrt_core::message::send(xrt_core::message::severity_level::XRT_ERROR, "XRT", std::string("Library " + p.string() + " not found!"));
+      return;
+    }
+    auto handle = dlopen(p.string().c_str(), RTLD_NOW | RTLD_GLOBAL);
+    if (!handle) {
+//        throw std::runtime_error("Failed to open XDP hal plugin library '" + p.string() + "'\n" + dlerror());
+      xrt_core::message::send(xrt_core::message::severity_level::XRT_ERROR, "XRT", std::string("Failed to open XDP hal plugin library '" + p.string() + "'\n" + dlerror()));
+      return;
+    }
+    const std::string cb_func_name = "hal_level_xdp_cb_func";
+    dlerror();
+    cb = cb_func_type(reinterpret_cast<cb_load_func_type>(dlsym(handle, cb_func_name.c_str())));
+    if(dlerror() != NULL) { // check if dlsym was successful
+      cb = nullptr;
+    }
+    loaded = true;
+#endif
+}
+
+}

src/runtime_src/core/edge/user/plugin/xdp/hal_profile.h
@@ -1,0 +1,167 @@
+#ifndef XDP_PROFILE_HAL_PLUGIN_H_
+#define XDP_PROFILE_HAL_PLUGIN_H_
+
+#include <functional>
+#include <iostream>
+#include <atomic>
+#include <mutex>
+#include <dlfcn.h>
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
+#include "core/include/xclperf.h"
+#include "core/include/xclhal2.h"
+
+namespace xdphal {
+
+/**
+ * This function type definition is used for
+ * dynamically loading the plugin function.
+ */
+typedef void(*cb_load_func_type)(unsigned, void*);
+
+/**
+ * This standard function is for storing the function
+ * loaded. Using cpp standard for robustness across 
+ * function calls and context sharing.
+ */
+using cb_func_type = std::function<void(unsigned, void*)>;
+
+class CallLogger
+{
+  public:
+    CallLogger(unsigned id = 0);
+    ~CallLogger();
+
+    unsigned m_local_idcode;  
+};
+
+/**
+ * Loggers are all alike except they each have different
+ * constructor arguments so that they can capture various
+ * information from different hal APIs.
+ * 
+ * @param local_idcode for identifying unique function calls
+ */
+class AllocBOCallLogger : public CallLogger
+{
+public:
+  AllocBOCallLogger(xclDeviceHandle handle, size_t size, int unused, unsigned flags);
+  ~AllocBOCallLogger();
+};
+
+class FreeBOCallLogger : public CallLogger
+{
+public:
+  FreeBOCallLogger(xclDeviceHandle handle, unsigned int boHandle);
+  ~FreeBOCallLogger();
+};
+
+class WriteBOCallLogger : public CallLogger
+{
+public:
+  WriteBOCallLogger(xclDeviceHandle handle, unsigned int boHandle, const void *src, size_t size, size_t seek);
+  ~WriteBOCallLogger();
+};
+
+class ReadBOCallLogger : public CallLogger
+{
+public:
+  ReadBOCallLogger(xclDeviceHandle handle, unsigned int boHandle, void *dst, size_t size, size_t skip);
+  ~ReadBOCallLogger();
+};
+
+class MapBOCallLogger : public CallLogger
+{
+public:
+  MapBOCallLogger(xclDeviceHandle handle, unsigned int boHandle, bool write);
+  ~MapBOCallLogger();
+};
+
+class SyncBOCallLogger : public CallLogger
+{
+public:
+  SyncBOCallLogger(xclDeviceHandle handle, unsigned int boHandle, xclBOSyncDirection dir, size_t size, size_t offset);
+  ~SyncBOCallLogger();
+};
+
+class UnmgdPwriteCallLogger : public CallLogger
+{
+public:
+  UnmgdPwriteCallLogger(xclDeviceHandle handle, unsigned flags, const void *buf, size_t count, uint64_t offset);
+  ~UnmgdPwriteCallLogger();
+};
+
+class UnmgdPreadCallLogger : public CallLogger
+{
+public:
+  UnmgdPreadCallLogger(xclDeviceHandle handle, unsigned flags, void *buf, size_t count, uint64_t offset);
+  ~UnmgdPreadCallLogger();
+};
+
+class ReadCallLogger : public CallLogger
+{
+public:
+  ReadCallLogger(xclDeviceHandle handle, xclAddressSpace space, uint64_t offset, void *hostBuf, size_t size);
+  ~ReadCallLogger();
+};
+
+class WriteCallLogger : public CallLogger
+{
+public:
+  WriteCallLogger(xclDeviceHandle handle, xclAddressSpace space, uint64_t offset, const void *hostBuf, size_t size);
+  ~WriteCallLogger();
+};
+/** End of the loggers */
+
+class StartDeviceProfilingCls
+{
+public:
+  StartDeviceProfilingCls(xclDeviceHandle handle);
+  ~StartDeviceProfilingCls();
+};
+
+class CreateProfileResultsCls
+{
+public:
+  CreateProfileResultsCls(xclDeviceHandle handle, ProfileResults**);
+  ~CreateProfileResultsCls();
+};
+
+class GetProfileResultsCls
+{
+public:
+  GetProfileResultsCls(xclDeviceHandle handle, ProfileResults*);
+  ~GetProfileResultsCls();
+};
+
+class DestroyProfileResultsCls
+{
+public:
+  DestroyProfileResultsCls(xclDeviceHandle handle, ProfileResults*);
+  ~DestroyProfileResultsCls();
+};
+
+void load_xdp_plugin_library(HalPluginConfig* config);
+
+} //  xdphal
+
+/**
+ * The declaration of the macros to be inserted into 
+ * the shim implementations
+ */
+#define ALLOC_BO_CB xdphal::AllocBOCallLogger alloc_bo_call_logger(handle, size, unused, flags);
+#define FREE_BO_CB xdphal::FreeBOCallLogger free_bo_call_logger(handle, boHandle);
+#define WRITE_BO_CB xdphal::WriteBOCallLogger write_bo_call_logger(handle, boHandle, src, size, seek);
+#define READ_BO_CB xdphal::ReadBOCallLogger read_bo_call_logger(handle, boHandle, dst, size, skip);
+#define MAP_BO_CB xdphal::MapBOCallLogger map_bo_call_logger(handle, boHandle, write);
+#define UNMGD_PWRITE_CB xdphal::UnmgdPwriteCallLogger unmgd_pwrite_call_logger(handle, flags, buf, count, offset);
+#define UNMGD_PREAD_CB xdphal::UnmgdPreadCallLogger unnmgd_pread_call_logger(handle, flags, buf, count, offset);
+#define WRITE_CB xdphal::WriteCallLogger write_call_logger(handle, space, offset, hostBuf, size);
+#define READ_CB xdphal::ReadCallLogger read_call_logger(handle, space, offset, hostBuf, size);
+
+#define START_DEVICE_PROFILING_CB(handle) xdphal::StartDeviceProfilingCls start_device_profiling_inst(handle);
+#define CREATE_PROFILE_RESULTS_CB(handle, results) xdphal::CreateProfileResultsCls create_profile_results_inst(handle, results);
+#define GET_PROFILE_RESULTS_CB(handle, results) xdphal::GetProfileResultsCls get_profile_results_inst(handle, results);
+#define DESTROY_PROFILE_RESULTS_CB(handle, results) xdphal::DestroyProfileResultsCls destroy_profile_results_inst(handle, results);
+
+#endif

src/runtime_src/core/edge/user/shim.cpp
@@ -40,6 +40,11 @@
 #include <assert.h>
 #include <cstdarg>
 
+#ifndef __HWEM__
+#include "plugin/xdp/hal_profile.h"
+#endif
+
+
 #define GB(x)   ((size_t) (x) << 30)
 
 static std::string parseCUStatus(unsigned val) {
@@ -991,6 +996,9 @@ int xclLoadXclBin(xclDeviceHandle handle, const xclBin *buffer)
         printf("Map Debug IPs Failed\n");
         return ret;
     }
+#ifndef __HWEM__
+    START_DEVICE_PROFILING_CB(handle);
+#endif
     return 0;
 }
 
@@ -1368,6 +1376,43 @@ ssize_t xclReadQueue(xclDeviceHandle handle, void *q_hdl, xclQueueRequest *wr_re
 {
   return -ENOSYS;
 }
+
+int xclCreateProfileResults(xclDeviceHandle handle, ProfileResults** results)
+{
+#ifndef __HWEM__
+  ZYNQ::ZYNQShim *drv = ZYNQ::ZYNQShim::handleCheck(handle);
+  if(!drv)
+    return -ENODEV;
+
+  CREATE_PROFILE_RESULTS_CB(handle, results);
+#endif
+  return 0;
+}
+
+int xclGetProfileResults(xclDeviceHandle handle, ProfileResults* results)
+{
+#ifndef __HWEM__
+  ZYNQ::ZYNQShim *drv = ZYNQ::ZYNQShim::handleCheck(handle);
+  if(!drv)
+    return -ENODEV;
+
+  GET_PROFILE_RESULTS_CB(handle, results);
+#endif
+  return 0;
+}
+
+int xclDestroyProfileResults(xclDeviceHandle handle, ProfileResults* results)
+{
+#ifndef __HWEM__
+  ZYNQ::ZYNQShim *drv = ZYNQ::ZYNQShim::handleCheck(handle);
+  if(!drv)
+    return -ENODEV;
+
+  DESTROY_PROFILE_RESULTS_CB(handle, results);
+#endif
+  return 0;
+}
+
 
 int xclRegWrite(xclDeviceHandle handle, uint32_t cu_index, uint32_t offset,
   uint32_t data)

commit 5675f3560acb93f6b950c60cb672d178907c8a52

xrt-next.h: Use structs for profile related members & routines (#2418)
Several routines in this header refer to ProfileResults, however it was declared as
a simple struct. Without the typedef specifier it will not be resolved when using
an application that eventually needs compile with xclhal2.h

ProfileResults is invoked in the following functions xclCreateProfileResults,
xclGetProfileResults, and xclDestroyProfileResults

Typedef needs to be added to the definitions of KernelTransferData, CuExecData, and
StreamTransferData as they are used without the struct * being used in the
definition of ProfileResults.

Fixes: 9b40ccb HAL level profiling with counters for PCIe and Zynq (#1936)

(cherry picked from XRT/master commit 1ee07ff97a342768657f036741344456ef4b8eda)

Reviewed-by: Soren Soe <soren.soe@xilinx.com>
Reviewed-by: Ishita Ghosh <ishita.ghosh@xilinx.com>
Signed-off-by: Rohit Athavale <rohit.athavale@xilinx.com>

src/runtime_src/core/include/experimental/xrt-next.h
@@ -133,13 +133,13 @@ struct ProfileResults
   char* deviceName;
 
   uint64_t numAIM;
-  KernelTransferData* kernelTransferData;
+  struct KernelTransferData *kernelTransferData;
 
   uint64_t numAM;
-  CuExecData* cuExecData;
+  struct CuExecData *cuExecData;
 
   uint64_t numASM;
-  StreamTransferData* streamData;
+  struct StreamTransferData  *streamData;
 };
 
 /**
@@ -153,7 +153,7 @@ struct ProfileResults
  *                     This argument remains unchanged if xclCreateProfileResults fails.
  *
  */
-XCL_DRIVER_DLLESPEC int xclCreateProfileResults(xclDeviceHandle, ProfileResults**);
+XCL_DRIVER_DLLESPEC int xclCreateProfileResults(xclDeviceHandle, struct ProfileResults**);
 
 /**
  * int xclGetProfileResults(xclDeviceHandle, ProfileResults*)
@@ -166,7 +166,7 @@ XCL_DRIVER_DLLESPEC int xclCreateProfileResults(xclDeviceHandle, ProfileResults*
  *                    This buffer should be created using previous call to "xclCreateProfileResults"
  *
  */
-XCL_DRIVER_DLLESPEC int xclGetProfileResults(xclDeviceHandle, ProfileResults*);
+XCL_DRIVER_DLLESPEC int xclGetProfileResults(xclDeviceHandle, struct ProfileResults*);
 
 /**
  * int xclDestroyProfileResults(xclDeviceHandle, ProfileResults*)
@@ -178,7 +178,7 @@ XCL_DRIVER_DLLESPEC int xclGetProfileResults(xclDeviceHandle, ProfileResults*);
  * @ProfileResults* : Pointer to buffer to be deleted
  *
  */
-XCL_DRIVER_DLLESPEC int xclDestroyProfileResults(xclDeviceHandle, ProfileResults*);
+XCL_DRIVER_DLLESPEC int xclDestroyProfileResults(xclDeviceHandle, struct ProfileResults*);
 
 /**
  * xclRegRead() - Read register in register space of a CU