e1f3075111167a16853c3c5f5ac7ad555810bc31
Stephen Rozum <40244911+rozumx@users.noreply.github.com>
2019-11-19 08:36:23 -0700

    xbutil2: Complete abstraction library query functionality (#2437)
    *     xbutil2: Complete abstraction library query functionality
    
        + Implemented multilay device_core <|-- device_pci <|-- device_linux or device_windows abstraction layers
        + Abstracted all query calls to enumeration values
        + Abstracted all sysdev or IOCTL calls to the lower level layers (e.g. device_linux and device_windows)
        + Added helper methods to collect query data and format it into a property table
        + Added stub body file non linux/window libraries that depends on core_common
        + On windows, the target executable is now xbutil
        + Refactored how sub-commands register themself to the xbutil main().
          - It is now cmake driven
        + Updated CMake to only build program, query, clock, reset, and version sub-commands
        + Sub-command help is now passed along with the other options

---
+++ src/runtime_src/core/common/device_core.h
@@ -0,0 +1,205 @@
+/**
+ * Copyright (C) 2019 Xilinx, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may
+ * not use this file except in compliance with the License. A copy of the
+ * License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef DEVICE_CORE_H
+#define DEVICE_CORE_H
+
+// Please keep eternal include file dependencies to a minimum
+#include <boost/property_tree/ptree.hpp>
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <list>
+#include <map>
+#include <boost/any.hpp>
+
+namespace xrt_core {
+
+void initialize_child_ctor();
+
+/**
+ * Helper function to initialize the child's class constructer.
+ */
+void initialize();
+
+class device_core {
+
+  public:
+    /**
+     * Returns the class handle use to query the abstract 
+     * libraries 
+     * 
+     * @return The handle instance
+     */
+    static const device_core & get_handle();
+
+  public:
+    virtual void get_devices(boost::property_tree::ptree &_pt) const = 0;
+    virtual void get_device_info(uint64_t _deviceID, boost::property_tree::ptree &_pt) const = 0;
+    virtual void read_device_dma_stats(uint64_t _deviceID, boost::property_tree::ptree &_pt) const = 0;
+    virtual uint64_t get_total_devices() const = 0;
+
+    void get_device_rom_info(uint64_t _deviceID, boost::property_tree::ptree & _pt) const;
+    void get_device_xmc_info(uint64_t _deviceID, boost::property_tree::ptree & _pt) const;
+    void get_device_platform_info(uint64_t _deviceID, boost::property_tree::ptree & _pt) const;
+    void read_device_thermal_pcb(uint64_t _deviceID, boost::property_tree::ptree &_pt) const;
+    void read_device_thermal_fpga(uint64_t _deviceID, boost::property_tree::ptree &_pt) const;
+    void read_device_fan_info(uint64_t _deviceID, boost::property_tree::ptree &_pt) const;
+    void read_device_thermal_cage(uint64_t _deviceID, boost::property_tree::ptree &_pt) const;
+    void read_device_electrical(uint64_t _deviceID, boost::property_tree::ptree &_pt) const;
+    void read_device_power(uint64_t _deviceID, boost::property_tree::ptree &_pt) const;
+    void read_device_firewall(uint64_t _deviceID, boost::property_tree::ptree &_pt) const;
+
+  public:
+    // Future TODO: Move a way from static enumeration to dynamic enumeration
+    typedef enum {
+      QR_PCIE_VENDOR,
+      QR_PCIE_DEVICE,
+      QR_PCIE_SUBSYSTEM_VENDOR,
+      QR_PCIE_SUBSYSTEM_ID,
+      QR_PCIE_LINK_SPEED,
+      QR_PCIE_EXPRESS_LANE_WIDTH,
+
+      QR_DMA_THREADS_RAW,               
+
+      QR_ROM_VBNV,
+      OR_ROM_DDR_BANK_SIZE,
+      QR_ROM_DDR_BANK_COUNT_MAX,
+      QR_ROM_FPGA_NAME,
+
+      QR_XMC_VERSION,
+      QR_XMC_SERIAL_NUM,
+      QR_XMC_MAX_POWER,
+      QR_XMC_BMC_VERSION,
+
+      QR_DNA_SERIAL_NUM,
+      QR_CLOCK_FREQS,
+      QR_IDCODE,
+
+      QR_STATUS_MIG_CALIBRATED,
+      QR_STATUS_P2P_ENABLED,
+
+      QR_TEMP_CARD_TOP_FRONT,
+      QR_TEMP_CARD_TOP_REAR,
+      QR_TEMP_CARD_BOTTOM_FRONT,
+
+      QR_TEMP_FPGA,
+
+      QR_FAN_TRIGGER_CRITICAL_TEMP,
+      QR_FAN_FAN_PRESENCE,
+      QR_FAN_SPEED_RPM,
+
+      QR_CAGE_TEMP_0,
+      QR_CAGE_TEMP_1,
+      QR_CAGE_TEMP_2,
+      QR_CAGE_TEMP_3,
+
+      QR_12V_PEX_MILLIVOLTS,
+      QR_12V_PEX_MILLIAMPS,
+
+      QR_12V_AUX_MILLIVOLTS,
+      QR_12V_AUX_MILLIAMPS,
+
+      QR_3V3_PEX_MILLIVOLTS,
+      QR_3V3_AUX_MILLIVOLTS,
+
+      QR_DDR_VPP_BOTTOM_MILLIVOLTS,
+      QR_DDR_VPP_TOP_MILLIVOLTS,
+      QR_5V5_SYSTEM_MILLIVOLTS,
+      QR_1V2_VCC_TOP_MILLIVOLTS,
+      QR_1V2_VCC_BOTTOM_MILLIVOLTS,
+      QR_1V8_MILLIVOLTS,
+      QR_0V85_MILLIVOLTS,
+      QR_0V9_VCC_MILLIVOLTS,
+      QR_12V_SW_MILLIVOLTS,
+      QR_MGT_VTT_MILLIVOLTS,
+      QR_INT_VCC_MILLIVOLTS,
+      QR_INT_VCC_MILLIAMPS,
+      QR_3V3_PEX_MILLIAMPS,
+      QR_0V85_MILLIAMPS,
+      QR_3V3_VCC_MILLIVOLTS,
+      QR_HBM_1V2_MILLIVOLTS,
+      QR_2V5_VPP_MILLIVOLTS,
+      QR_INT_BRAM_VCC_MILLIVOLTS,
+      QR_FIREWALL_DETECT_LEVEL,
+      QR_FIREWALL_STATUS,
+      QR_FIREWALL_TIME_SEC,
+      QR_POWER_MICROWATTS
+    } QueryRequest;
+
+    virtual void query_device(uint64_t _deviceID, QueryRequest _eQueryRequest, const std::type_info & _typeInfo, boost::any &_returnValue) const = 0;
+
+  // Helper methods
+  protected:
+    typedef std::string (*FORMAT_STRING_PTR)(const boost::any &);
+    static std::string format_primative(const boost::any & _data);
+    static std::string format_hex(const boost::any & _data);
+    static std::string format_hex_base2_shiftup30(const boost::any & _data);
+    static std::string format_base10_shiftdown3(const boost::any &_data);
+    static std::string format_base10_shiftdown6(const boost::any &_data);
+
+    void query_device_and_put(uint64_t _deviceID, 
+                              QueryRequest _eQueryRequest, 
+                              const std::type_info & _typeInfo, 
+                              boost::property_tree::ptree & _pt, 
+                              const std::string &_sPropertyName, 
+                              FORMAT_STRING_PTR stringFormat = format_primative) const;
+
+    void query_device_and_put(uint64_t _deviceID, QueryRequest _eQueryRequest, boost::property_tree::ptree & _pt) const;
+    
+  protected:
+    typedef struct {
+      std::string sPrettyName;
+      std::string sPtreeNodeName;
+      const std::type_info *pTypeInfo;
+      FORMAT_STRING_PTR string_formatter;
+    } QueryRequestEntry;
+
+    const QueryRequestEntry * get_query_entry(QueryRequest _eQueryRequest) const;
+
+  protected:
+    device_core();
+    virtual ~device_core();
+
+  private:
+    device_core(const device_core&);
+    device_core& operator=(const device_core&);
+
+  protected:
+    typedef std::function<xrt_core::device_core*()> device_core_factory;
+   
+    /**
+     * Register's the child's constructor to be used to create the singleton.
+     * 
+     * Note
+     * ----
+     * Last constructer registered win's prior to creating the singleton.  Once
+     * the singleton is created, exceptions will be thrown if this 
+     * method is called again. 
+     * 
+     * @param _core_device_factory
+     *               The child's class constructure.
+     */
+  public:
+    static unsigned int register_child_ctor( device_core_factory _device_core_factory);
+
+  private:
+    static device_core_factory m_singleton_ctor;
+    static std::map<QueryRequest, QueryRequestEntry> m_QueryTable;
+};
+}
+
+#endif 
