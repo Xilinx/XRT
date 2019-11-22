581839171c7d9e562c2b957366bc6b02443fbd25
AShivangi <shivangiagarwal53@gmail.com>
2019-11-22 11:18:34 -0800

    small changes

--- src/runtime_src/core/tools/xbmgmt2/SubCmdFlash.cpp
+++ src/runtime_src/core/tools/xbmgmt2/SubCmdFlash.cpp
@@ -56,23 +56,32 @@ int subCmdFlash(const std::vector<std::string> &_options)
 {
   XBU::verbose("SubCommand: flash");
   // -- Retrieve and parse the subcommand options -----------------------------
-  uint64_t card = 0;
   bool help = false;
   bool scan = false;
+  bool reset = false;
+  std::string bdf = "";
   bool shell = false;
   bool sc_firmware = false;
+  bool update = false;
+  bool force = false;
+  std::string file_path = "";
+  
 
   po::options_description flashDesc("flash options");
   flashDesc.add_options()
-    ("help,h", boost::program_options::bool_switch(&help), "Help to use this sub-command")
-    (",d", boost::program_options::value<uint64_t>(&card), "Card to be examined")
+    ("help", boost::program_options::bool_switch(&help), "Help to use this sub-command")
     ("scan", boost::program_options::bool_switch(&scan), "Information about the card")
+    ("factory_reset", boost::program_options::bool_switch(&reset), "Reset to golden image")
+    ("update", boost::program_options::bool_switch(&update), "Update the card with the installed shell")
+    ("force", boost::program_options::bool_switch(&force), "force") //option
+    ("card", boost::program_options::value<std::string>(&bdf), "bdf of the card") //option
   ;
 
   po::options_description expertsOnlyDesc("experts only");
   expertsOnlyDesc.add_options()
     ("shell", boost::program_options::bool_switch(&shell), "Flash platform from source")
     ("sc_firmware", boost::program_options::bool_switch(&sc_firmware), "Flash sc firmware from source")
+    ("path", boost::program_options::value<std::string>(&file_path), "path of shell or sc_firmware files") //option
   ;
 
   po::options_description allOptions("");
@@ -98,13 +107,34 @@ int subCmdFlash(const std::vector<std::string> &_options)
   }
 
   // -- Now process the subcommand --------------------------------------------
-  XBU::verbose(XBU::format("  Card: %ld", card));
+  XBU::verbose(XBU::format("  Card: %s", bdf.c_str()));
   XBU::verbose(XBU::format("  Scan: %ld", scan));
   XBU::verbose(XBU::format("  Shell: %ld", shell));
   XBU::verbose(XBU::format("  sc_firmware: %ld", sc_firmware));
+  XBU::verbose(XBU::format("  Reset: %ld", reset));
+  XBU::verbose(XBU::format("  Update: %ld", update));
+  XBU::verbose(XBU::format("  Force: %ld", force));
+  XBU::verbose(XBU::format("  File path: %s", file_path.c_str()));
+
+
 
   XBU::error("COMMAND BODY NOT IMPLEMENTED.");
   // TODO: Put working code here
+  if (scan) {
+      std::cout << "Call xbmgmt flash --scan\n"; //parse verbose|json??
+      return registerResult;
+  }
+
+  if (update) {
+    //   if (shell.empty() && !id.empty()) //--shell name [--id id]
+    //     return RC_ERROR_IN_COMMAND_LINE;
+      std::cout << "Call xbmgmt update\n"; //call autoFlash
+  }
+
+  if (reset) {
+      std::cout << "Call xbmgmt reset\n";
+
+  }
 
   return registerResult;
 }
--- src/runtime_src/core/tools/xbmgmt2/SubCmdVersion.cpp
+++ src/runtime_src/core/tools/xbmgmt2/SubCmdVersion.cpp
@@ -75,7 +75,7 @@ int subCmdVersion(const std::vector<std::string> &_options)
 
   po::options_description versionDesc("version options");
   versionDesc.add_options()
-    ("help,h", boost::program_options::bool_switch(&help), "Help to use this sub-command")
+    ("help,", boost::program_options::bool_switch(&help), "Help to use this sub-command")
   ;
 
   // Parse sub-command ...
