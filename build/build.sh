ddde577fcdda61a5e17641d88949d0d74b017d65
Soren Soe <soren.soe@xilinx.com>
2020-01-09 08:24:50 -0800

    Allow env vars to specify debug and release dir (#2654)
    (cherry picked from commit 4b4d231a0a1e02b9406c8d5f83f9429537c5b706)

--- build/build.sh
+++ build/build.sh
@@ -131,13 +131,16 @@ while [ $# -gt 0 ]; do
     esac
 done
 
+debug_dir=${DEBUG_DIR:-Debug}
+release_dir=${REL_DIR:-Release}
+
 here=$PWD
 cd $BUILDDIR
 
 if [[ $clean == 1 ]]; then
     echo $PWD
-    echo "/bin/rm -rf Release Debug"
-    /bin/rm -rf Release Debug
+    echo "/bin/rm -rf $debug_dir $release_dir"
+    /bin/rm -rf $debug_dir $release_dir
     exit 0
 fi
 
@@ -160,8 +163,8 @@ if [[ ! -z $ertfw ]]; then
 fi
 
 if [[ $dbg == 1 ]]; then
-  mkdir -p Debug
-  cd Debug
+  mkdir -p $debug_dir
+  cd $debug_dir
   if [[ $nocmake == 0 ]]; then
     echo "$CMAKE -DRDI_CCACHE=$ccache -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../../src"
     time $CMAKE -DRDI_CCACHE=$ccache -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../../src
@@ -173,8 +176,8 @@ if [[ $dbg == 1 ]]; then
 fi
 
 if [[ $opt == 1 ]]; then
-  mkdir -p Release
-  cd Release
+  mkdir -p $release_dir
+  cd $release_dir
   if [[ $nocmake == 0 ]]; then
     echo "$CMAKE -DRDI_CCACHE=$ccache -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../../src"
     time $CMAKE -DRDI_CCACHE=$ccache -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../../src
