8aca737bef48a1b05554e4e176c082d33e217302
Himanshu-xilinx <48508106+Himanshu-xilinx@users.noreply.github.com>
2020-01-12 00:23:19 +0530

    Fix libxdp.so load issue on edge (#2655)

---
+++ src/runtime_src/core/edge/user/device_linux.h
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2020 Xilinx, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may
+ * not use this file except in compliance with the License. A copy of the
+ * License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef EDGE_DEVICE_LINUX_H
+#define EDGE_DEVICE_LINUX_H
+
+#include "common/device_edge.h"
+
+namespace xrt_core {
+
+class device_linux : public device_edge
+{
+public:
+  struct SysDevEntry {
+    const std::string sSubDevice;
+    const std::string sEntry;
+  };
+  const SysDevEntry & get_sysdev_entry(QueryRequest qr) const;
+
+  device_linux(id_type device_id, bool user);
+
+  // query functions
+  virtual void read_dma_stats(boost::property_tree::ptree& pt) const;
+  virtual void query(QueryRequest qr, const std::type_info& tinfo, boost::any& value) const;
+
+  virtual void read(uint64_t addr, void* buf, uint64_t len) const;
+  virtual void write(uint64_t addr, const void* buf, uint64_t len) const;
+
+};
+}
+
+#endif /* EDGE_DEVICE_LINUX_H */
