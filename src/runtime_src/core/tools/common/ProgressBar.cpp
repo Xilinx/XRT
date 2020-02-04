f8179a918bd779a730afea8f4d70b0e529d55f23
Stephen Rozum <40244911+rozumx@users.noreply.github.com>
2020-02-03 21:12:34 -0700

    xbmgmt2 : Refactored progress bar (#2757)
    Work Done
    + Added DRCs with regards to bounding interations
    + Added support to limit non-batch updates to update no more then once every 0.5 seconds if nothing changes.
    + Addressed issue with batch update percentage ticks being incorrect
    + Move elapse time to end of line
    + Added custom failure/passing messages
    + Elapse time format changed from 00:00:00 to h m s (e.g. 1h 10m 2s)

--- src/runtime_src/core/tools/common/ProgressBar.cpp
+++ src/runtime_src/core/tools/common/ProgressBar.cpp
@@ -29,71 +29,176 @@
 // ------ N A M E S P A C E ---------------------------------------------------
 using namespace XBUtilities;
 
-// ------ F U N C T I O N S ---------------------------------------------------
+// ------ S T A T I C   V A R I A B L E S -------------------------------------
+static unsigned int ProgressBarWidth = 20;
 
-static std::string 
-format_time(std::chrono::duration<double> duration) {
+static boost::format fmtUpdate(EscapeCodes::cursor().hide()+
+                               EscapeCodes::fgcolor::reset()+ "[" +
+                               EscapeCodes::fgcolor(EscapeCodes::FGC_IN_PROGRESS).string()+ "%-" + std::to_string(ProgressBarWidth)+ "s" +
+                               EscapeCodes::fgcolor::reset()+ "]" +
+                               EscapeCodes::fgcolor(EscapeCodes::FGC_IN_PROGRESS).string()+ "%3d%%" +
+                               EscapeCodes::fgcolor::reset()+ ": %s... < %s >"
+                              );
+
+static boost::format fmtPassed(EscapeCodes::cursor().hide()+
+                               EscapeCodes::fgcolor(EscapeCodes::FGC_PASS).string()+ "[PASSED]" +
+                               EscapeCodes::fgcolor::reset()+ " : %s < %s >"
+                              );
+
+static boost::format fmtFailed(EscapeCodes::cursor().hide()+
+                               EscapeCodes::fgcolor(EscapeCodes::FGC_FAIL).string()+ "[FAILED]" +
+                               EscapeCodes::fgcolor::reset()+ " : %s < %s >"
+                              );
+
+static boost::format fmtBatchPF("\n[%s]: %s < %s >\n");
+
+
+// ------ S T A T I C   F U N C T I O N S -------------------------------------
+
+static std::string
+format_time(std::chrono::duration<double> duration) 
+{
   auto hours = std::chrono::duration_cast<std::chrono::hours>(duration);
   auto minutes = std::chrono::duration_cast<std::chrono::minutes>(duration - hours);
   auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration - minutes);
 
-  return boost::str(boost::format("%02d:%02d:%02d") % hours.count() % minutes.count() % seconds.count());
+  std::string formattedTime;
+  if (hours.count() != 0) 
+    formattedTime += std::to_string(hours.count()) + "h ";
+
+  if (hours.count() != 0 || minutes.count() != 0) 
+    formattedTime += std::to_string(minutes.count()) + "m ";
+
+  formattedTime += std::to_string(seconds.count()) + "s";
+
+  return formattedTime;
 }
 
-ProgressBar::ProgressBar(std::string _op_name, unsigned int _max_iter, bool _is_batch, std::ostream& _ostr) 
-    : op_name(_op_name), max_iter(_max_iter), is_batch(_is_batch), ostr(_ostr)
+
+
+ProgressBar::ProgressBar(const std::string &_opNname, unsigned int _maxNumIterations,
+                         bool _isBatch, std::ostream &_ostr)
+    : m_opName(_opNname)
+    , m_maxNumIterations(_maxNumIterations)
+    , m_isBatch(_isBatch)
+    , m_ostr(_ostr)
+    , m_runningIteration(0)
+    , m_finished(false)
+    , m_elapsedTime(std::chrono::seconds(0))
+    , m_lastUpdated(std::chrono::high_resolution_clock::now()) 
 {
-  percent_done = 0;
-  elapsed_time = std::chrono::seconds(0);
+  if (!m_isBatch) 
+    m_ostr << fmtUpdate % "" % /*Percent*/ 0 % m_opName % format_time(m_elapsedTime) << std::endl;
+  else 
+    m_ostr << m_opName << ": ";
 
-  if (!is_batch) {
-    ostr << EscapeCodes::cursor().hide() << EscapeCodes::fgcolor::reset() << "[ " << EscapeCodes::fgcolor(EscapeCodes::FGC_IN_PROGRESS).string() << std::string(20, ' ') 
-            << EscapeCodes::fgcolor::reset() << "]  " << EscapeCodes::fgcolor(EscapeCodes::FGC_IN_PROGRESS).string() << percent_done << "%" 
-            << EscapeCodes::fgcolor::reset() << " ("<< format_time(elapsed_time) << "): " << op_name 
-            << "\n" << EscapeCodes::fgcolor::reset();
-  } else {
-      ostr << op_name << ": ";
-  }
-  ostr.flush();
+  m_ostr.flush();
 }
 
-void 
-ProgressBar::finish() {
-  std::string status = percent_done == 100 ? EscapeCodes::fgcolor(EscapeCodes::FGC_PASS).string() + "[PASSED] " : EscapeCodes::fgcolor(EscapeCodes::FGC_FAIL).string() + "[FAILED] ";
-  if(is_batch) {
-    status = percent_done == 100 ? "[PASSED]\n" : "[FAILED]\n";
-    ostr << status;
-  } else {
-    ostr << EscapeCodes::cursor().prev_line() << EscapeCodes::cursor().clear_line() << status << EscapeCodes::fgcolor::reset() << "(" 
-          << format_time(elapsed_time) << "): " << op_name << "\n" << EscapeCodes::fgcolor::reset() << EscapeCodes::cursor().show();
-  }
-  ostr.flush();
+ProgressBar::~ProgressBar() 
+{
+  // If the class is destroyed and the developer has NOT executed the 
+  // finished operation, restore the cursor
+  if ((m_finished == false) && (m_isBatch == false)) 
+    m_ostr << EscapeCodes::cursor().show() << std::flush;
 }
 
-void 
-ProgressBar::update(int iteration) {
-  elapsed_time = timer.stop();
+void
+ProgressBar::finish(bool _successful, const std::string &_msg) 
+{
+  // Should only be called once (not more then once)
+  assert(m_finished == false);
 
-  percent_done = 100*iteration/max_iter;
+  // Update the runningTime
+  m_elapsedTime = m_timer.stop();
 
-  if (percent_done > 100) {
-    finish();
-    ostr <<  EscapeCodes::fgcolor::reset() << EscapeCodes::cursor().show();
-    throw std::runtime_error("Passed in iteration number is greater than the max iteration");
-  }
-
-  if(is_batch) {
-    if (percent_done % 5 == 0) {
-      ostr << ".." << percent_done << "%";
-    } else { ostr << ".."; }
-    ostr.flush();
+  // -- Batch --
+  if (m_isBatch) {
+    m_ostr << fmtBatchPF % (_successful ? "PASSED" : "FAILED") % _msg % format_time(m_elapsedTime);
+    m_ostr.flush();
     return;
   }
 
-  ostr << EscapeCodes::cursor().hide() << EscapeCodes::cursor().prev_line() << EscapeCodes::fgcolor::reset() << "[" << EscapeCodes::fgcolor(EscapeCodes::FGC_IN_PROGRESS).string() 
-        << std::string(percent_done/5, '=') << ">" << std::string(20 - (percent_done / 5), ' ') 
-        << EscapeCodes::fgcolor::reset() << "] " << EscapeCodes::fgcolor(EscapeCodes::FGC_IN_PROGRESS).string() << percent_done << "%" 
-        << EscapeCodes::fgcolor::reset() << " ("<< format_time(elapsed_time) << "): " << op_name 
-        << "\n" << EscapeCodes::fgcolor::reset();
-  ostr.flush();
+  // -- Non-Batch --
+  boost::format &fmt = _successful ? fmtPassed : fmtFailed;
+  m_ostr << EscapeCodes::cursor().prev_line()
+         << EscapeCodes::cursor().clear_line()
+         << fmt % _msg % format_time(m_elapsedTime)
+         << std::endl << EscapeCodes::cursor().show();
+
+  m_ostr.flush();
+  m_finished = true;
 }
+
+
+
+void
+ProgressBar::update(unsigned int _iteration) 
+{
+  // Some simple DRC checks
+  assert(_iteration <= m_maxNumIterations);
+  if (_iteration > m_maxNumIterations) 
+    _iteration = m_maxNumIterations;
+
+  // Going back in time (e.g., the iteration values are getting smaller)
+  assert(_iteration >= m_runningIteration);
+  if (_iteration > m_maxNumIterations) 
+    _iteration = m_maxNumIterations;
+
+  // -- Batch --
+  if (m_isBatch) {
+    // Has progress been made?
+    if (_iteration == m_runningIteration) 
+      return;
+
+    // Bring the current iterator up to the the latest
+    for (auto currentIteration = m_runningIteration + 1;
+         currentIteration <= _iteration;
+         ++currentIteration) {
+      m_ostr << ".";            // Progressd period '.'
+
+      // Now determine percentage progress values
+      static const std::vector<unsigned int> reportPercentages = { 25, 50, 75, 100 };
+      unsigned int prevPercent = currentIteration == 0 ? 0 : (100 * (currentIteration - 1)) / m_maxNumIterations;
+      unsigned int nextPercent = (100 * currentIteration) / m_maxNumIterations;
+
+      for (const auto &reportPercent : reportPercentages) {
+        if ((reportPercent > prevPercent) && (reportPercent <= nextPercent)) {
+          m_ostr << std::to_string(reportPercent) << "%";
+        }
+      }
+    }
+    m_runningIteration = _iteration;
+    m_ostr.flush();
+    return;
+  }
+
+  // -- Non-batch --
+  // Now check to see if we need to "refresh" the progress message.
+  // Note: Currently if nothing has changed in the past 0.5 seconds then do nothing.
+  static unsigned int MaxRefreshMSec = 500;
+  if ((_iteration == m_maxNumIterations) &&
+      (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - m_lastUpdated).count() < MaxRefreshMSec)) 
+    return;
+
+  // Process the data
+  m_runningIteration = _iteration;
+  unsigned int runningPercent = (100 * m_runningIteration) / m_maxNumIterations;
+
+  // Get the runningTime
+  m_elapsedTime = m_timer.stop();
+  m_lastUpdated = std::chrono::high_resolution_clock::now();
+
+  // Create the progress bar
+  std::string progressBar = std::string(runningPercent / (100 / ProgressBarWidth), '=');
+  if (runningPercent < 100) 
+    progressBar += '>';
+
+  // Write the new progress bar
+  m_ostr << EscapeCodes::cursor().prev_line()
+         << fmtUpdate % progressBar % runningPercent % m_opName % format_time(m_elapsedTime)
+         << std::endl;
+
+  m_ostr.flush();
+}
+
