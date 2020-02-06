2adc2f027da960d6d08807a07250fbfe813e73c6
Min Ma <min.ma@xilinx.com>
2020-02-06 15:11:22 -0800

    cleanup embedded platform source

--- src/platform/zcu102ng_svm/src/a53/xrt/image/init.sh
+++
@@ -1,6 +0,0 @@
-cp /mnt/platform_desc.txt /etc/xocl.txt
-
-# XRT is install into linux standard path for MPSoC
-# Headers in /usr/include
-# Libraries in /usr/lib
-export XILINX_XRT=/usr
--- src/platform/zcu102ng_svm/zcu102ng_svm_pfm.tcl
+++
@@ -1,9 +0,0 @@
-platform -name zcu102ng_svm -desc "A basic platform targeting the ZCU102 evaluation board, which includes 4GB of DDR4 for the Processing System, 512MB of DDR4 for the Programmable Logic, 2x64MB Quad-SPI Flash and an SDIO card interface. More information at https://www.xilinx.com/products/boards-and-kits/ek-u1-zcu102-g.html" -hw ./zcu102ng_svm.xsa -out ./output  -prebuilt
-platform -samples {./samples}
-
-domain -name xrt -display-name "A53 OpenCL Linux" -proc psu_cortexa53 -os linux -image ./src/a53/xrt/image
-domain config -boot ./src/boot
-domain config -bif ./src/a53/xrt/linux.bif
-domain -runtime opencl
-
-platform -generate
