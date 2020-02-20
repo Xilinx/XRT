54a54ae2f2aec78513e2642c5e10a28ef0466191
CW Lan <chienwei@xilinx.com>
2020-02-19 17:45:09 -0800

    XOCL_MIG_HBM

--- src/runtime_src/core/pcie/driver/linux/xocl/userpf/xocl_drv.c
+++ src/runtime_src/core/pcie/driver/linux/xocl/userpf/xocl_drv.c
@@ -1479,6 +1479,7 @@ static int (*xocl_drv_reg_funcs[])(void) __initdata = {
 	xocl_init_trace_fifo_full,
 	xocl_init_trace_funnel,
 	xocl_init_trace_s2mm,
+	xocl_init_mem_hbm,
 };
 
 static void (*xocl_drv_unreg_funcs[])(void) = {
@@ -1496,6 +1497,7 @@ static void (*xocl_drv_unreg_funcs[])(void) = {
 	xocl_fini_mig,
 	xocl_fini_dna,
 	xocl_fini_mailbox_versal,
+<<<<<<< cd21b745151c73473ab2e104662aef58ecd83e2a
 	xocl_fini_aim,
 	xocl_fini_am,
 	xocl_fini_asm,
@@ -1503,6 +1505,9 @@ static void (*xocl_drv_unreg_funcs[])(void) = {
 	xocl_fini_trace_fifo_full,
 	xocl_fini_trace_funnel,
 	xocl_fini_trace_s2mm,
+=======
+	xocl_fini_mem_hbm,
+>>>>>>> XOCL_MIG_HBM
 };
 
 static int __init xocl_init(void)
