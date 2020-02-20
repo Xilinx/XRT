621bb539e068fd95b562553df9b2129164f0c64e
Lizhi Hou <lizhih@xilinx.com>
2020-02-20 15:57:57 -0800

    stop drm first

--- src/runtime_src/core/pcie/driver/linux/xocl/userpf/xocl_drv.c
+++ src/runtime_src/core/pcie/driver/linux/xocl/userpf/xocl_drv.c
@@ -1244,17 +1244,16 @@ void xocl_userpf_remove(struct pci_dev *pdev)
 		return;
 	}
 
-	xocl_queue_destroy(xdev);
-
-	xocl_p2p_fini(xdev, false);
 	/*
-	 * need to shutdown drm and sysfs before destroy subdevices
-	 * drm and sysfs could access subdevices
+	 * need to shutdown drm first to stop app access
 	 */
 
 	if (xdev->core.drm)
 		xocl_drm_fini(xdev->core.drm);
 
+	xocl_queue_destroy(xdev);
+
+	xocl_p2p_fini(xdev, false);
 	xocl_fini_sysfs(xdev);
 
 	xocl_subdev_destroy_all(xdev);
