ce752f6d9603ff9e77eb57b756cfe209decaa1d7
larry9523 <larry9523@gmail.com>
2020-03-03 11:19:45 -0800

    xrt aie interface (#2945)
    * xrt aie interface
    
    * Skip build on ZYNQ/ZYNQMP platform

--- src/runtime_src/core/edge/user/shim.cpp
+++ src/runtime_src/core/edge/user/shim.cpp
@@ -92,6 +92,11 @@ ZYNQShim::ZYNQShim(unsigned index, const char *logfileName, xclVerbosityLevel ve
   }
   mCmdBOCache = std::make_unique<xrt_core::bo_cache>(this, xrt_core::config::get_cmdbo_cache());
   mDev = zynq_device::get_dev();
+
+#ifdef XRT_ENABLE_AIE
+  /* TODO is this necessary? We may want to initialize it when loading xclbin */
+  aieArray = NULL; 
+#endif
 }
 
 #ifndef __HWEM__
@@ -1240,6 +1245,17 @@ int ZYNQShim::xclLogMsg(xrtLogMsgLevel level, const char* tag,
     return (double)clockFreq;
   }
 
+#ifdef XRT_ENABLE_AIE
+  zynqaie::Aie * ZYNQShim::getAieArray()
+  {
+    return aieArray; 
+  }
+
+  void ZYNQShim::setAieArray(zynqaie::Aie *aie)
+  {
+    aieArray = aieArray;
+  }
+#endif
 
 }
 ;
