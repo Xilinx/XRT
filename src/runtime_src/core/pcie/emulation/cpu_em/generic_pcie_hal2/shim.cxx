48a45d3b320dcd130120d86c4903493d4162084e
vboggara-xilinx <50370340+vboggara-xilinx@users.noreply.github.com>
2020-03-09 19:09:47 +0530

    xclbin version check with tool version (#2977)

--- src/runtime_src/core/pcie/emulation/cpu_em/generic_pcie_hal2/shim.cxx
+++ src/runtime_src/core/pcie/emulation/cpu_em/generic_pcie_hal2/shim.cxx
@@ -622,6 +622,10 @@ namespace xclcpuemhal2 {
         delete []memTopology;
         memTopology = NULL;
       }
+      
+      //check xclbin version with vivado tool version
+      xclemulation::checkXclibinVersionWithTool(header);     
+      
       if (mIsKdsSwEmu)
       {
         mCore = new exec_core;
