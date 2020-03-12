610c1ea813ee3478db3f693ed67e6d7341c59153
Brian Xu <brianx@xilinx.com>
2020-03-12 10:14:17 -0700

    backport CR-1056045 hotplug does not work on SuSE linux (#3015)

--- src/runtime_src/core/pcie/driver/linux/xocl/subdev/qdma.c
+++ src/runtime_src/core/pcie/driver/linux/xocl/subdev/qdma.c
@@ -1748,6 +1748,7 @@ static int qdma_probe(struct platform_device *pdev)
 	}
 
 	qdma->pdev = pdev;
+	platform_set_drvdata(pdev, qdma);
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	if (!res) {
@@ -1808,8 +1809,6 @@ static int qdma_probe(struct platform_device *pdev)
 	qdma->user_msix_mask = QDMA_USER_INTR_MASK;
 
 	spin_lock_init(&qdma->user_msix_table_lock);
-
-	platform_set_drvdata(pdev, qdma);
 
 	return 0;
 
