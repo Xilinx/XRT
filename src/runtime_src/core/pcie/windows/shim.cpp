7cc1ed5c66077f883089dc078746517b91960685
Igor Sharovar <igors@xlnx.xilinx.com>
2020-03-27 18:02:17 -0700

    Add ImportBO and ExportBO in Windows

--- src/runtime_src/core/pcie/windows/shim.cpp
+++ src/runtime_src/core/pcie/windows/shim.cpp
@@ -1421,6 +1421,20 @@ xclExecWait(xclDeviceHandle handle, int timeoutMilliSec)
   return shim->exec_wait(timeoutMilliSec);
 }
 
+int xclExportBO(xclDeviceHandle handle, unsigned int boHandle)
+{
+  xrt_core::message::
+    send(xrt_core::message::severity_level::XRT_DEBUG, "XRT", "xclExportBO() NOT IMPLEMENTED");
+  return ERROR_INVALID_FUNCTION;
+}
+
+xclBufferHandle xclImportBO(xclDeviceHandle handle, int fd, unsigned flags)
+{
+  xrt_core::message::
+    send(xrt_core::message::severity_level::XRT_DEBUG, "XRT", "xclImportBO() NOT IMPLEMENTED");
+  return INVALID_HANDLE_VALUE;
+}
+
 int
 xclGetBOProperties(xclDeviceHandle handle, xclBufferHandle boHandle,
 		   struct xclBOProperties *properties)
