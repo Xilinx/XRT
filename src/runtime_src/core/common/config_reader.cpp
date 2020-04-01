Xilinx/XRT

commit 02f68b0a1f442ecfd5c6a69dec0d77aa35d653de

CR-1053029: Lazy initialization of config property tree (#3121)
* CR-1053029: Lazy initialization of config property tree

Due to static order of intialization some locale initialization was
not done properly when needed by boost::filesystem::path.

Now the config tree is constructed only the first time it is needed.

* Handle recursive use of tree during construction

src/runtime_src/core/common/config_reader.cpp
@@ -23,6 +23,7 @@
 #include <boost/filesystem/operations.hpp>
 #include <iostream>
 #include <cstdlib>
+#include <atomic>
 
 #ifdef __GNUC__
 # include <linux/limits.h>
@@ -107,19 +108,16 @@ get_ini_path()
   return full_path;
 }
 
+// This global  tree represents the config reader for this
+// process.  Once initialized  it is valid and can be used
+// even from the tree construtor itself.  Using the config
+// reader during static global destruction is not possible
+static std::atomic<struct tree*> g_tree {nullptr};
+
 struct tree
 {
   boost::property_tree::ptree m_tree;
   const boost::property_tree::ptree null_tree;
-
-  void
-  setenv()
-  {
-#ifndef _WIN32
-    if (xrt_core::config::get_multiprocess())
-      ::setenv("XCL_MULTIPROCESS_MODE","1",1);
-#endif
-  }
 
   void
   read(const std::string& path)
@@ -127,11 +125,20 @@ struct tree
     try {
       read_ini(path,m_tree);
 
+      // Need to log that ini file was read, but this in turn uses the tree
+      // object which is being constructed now. Use an atomic global to store
+      // this tree which is read for use.
+      g_tree = this;
+
       // inform which .ini was read
       xrt_core::message::send(xrt_core::message::severity_level::XRT_INFO, "XRT", std::string("Read ") + path);
     }
     catch (const std::exception& ex) {
-      xrt_core::message::send(xrt_core::message::severity_level::XRT_WARNING, "XRT", ex.what());
+      // Using the tree in this case is not safe, and since message
+      // infra accesses xrt_core::config it can't be used safely.  Log
+      // to stderr instead
+      // xrt_core::message::send(xrt_core::message::severity_level::XRT_WARNING, "XRT", ex.what());
+      std::cerr << "[XRT] Failed to read xrt.ini: " << ex.what() << std::endl;
     }
   }
 
@@ -140,11 +147,6 @@ struct tree
     auto ini_path = get_ini_path();
     if (!ini_path.empty())
       read(ini_path);
-
-    // set env vars to expose sdaccel.ini (or default) to hal layer
-    setenv();
-
-    return;
   }
 
   void
@@ -152,9 +154,16 @@ struct tree
   {
     read(fnm);
   }
-};
 
-static tree s_tree;
+  static tree*
+  instance()
+  {
+    if (g_tree)
+      return g_tree;
+    static tree s_tree;
+    return &s_tree;
+  }
+};
 
 }
 
@@ -174,7 +183,7 @@ get_bool_value(const char* key, bool default_value)
   if (auto env = get_env_value(key))
     return is_true(env);
 
-  return s_tree.m_tree.get<bool>(key,default_value);
+  return tree::instance()->m_tree.get<bool>(key,default_value);
 }
 
 std::string
@@ -182,7 +191,7 @@ get_string_value(const char* key, const std::string& default_value)
 {
   std::string val = default_value;
   try {
-    val = s_tree.m_tree.get<std::string>(key,default_value);
+    val = tree::instance()->m_tree.get<std::string>(key,default_value);
     // Although INI file entries are not supposed to have quotes around strings
     // but we want to be cautious
     if (!val.empty() && (val.front() == '"') && (val.back() == '"')) {
@@ -200,7 +209,7 @@ get_uint_value(const char* key, unsigned int default_value)
 {
   unsigned int val = default_value;
   try {
-    val = s_tree.m_tree.get<unsigned int>(key,default_value);
+    val = tree::instance()->m_tree.get<unsigned int>(key,default_value);
   } catch( std::exception const&) {
     // eat the exception, probably bad path
   }
@@ -211,17 +220,19 @@ get_uint_value(const char* key, unsigned int default_value)
 const boost::property_tree::ptree&
 get_ptree_value(const char* key)
 {
-  boost::property_tree::ptree::const_assoc_iterator i = s_tree.m_tree.find(key);
-  return (i != s_tree.m_tree.not_found()) ? i->second : s_tree.null_tree;
+  auto s_tree  = tree::instance();
+  boost::property_tree::ptree::const_assoc_iterator i = s_tree->m_tree.find(key);
+  return (i != s_tree->m_tree.not_found()) ? i->second : s_tree->null_tree;
 }
 
 std::ostream&
 debug(std::ostream& ostr, const std::string& ini)
 {
+  auto s_tree  = tree::instance();
   if (!ini.empty())
-    s_tree.reread(ini);
+    s_tree->reread(ini);
 
-  for(auto& section : s_tree.m_tree) {
+  for(auto& section : s_tree->m_tree) {
     ostr << "[" << section.first << "]\n";
     for (auto& key:section.second) {
       ostr << key.first << " = " << key.second.get_value<std::string>() << std::endl;

src/runtime_src/core/pcie/linux/shim.cpp
@@ -83,13 +83,6 @@
 
 namespace {
 
-inline bool
-is_multiprocess_mode()
-{
-  static bool val = std::getenv("XCL_MULTIPROCESS_MODE") != nullptr;
-  return val;
-}
-
 /*
  * numClocks()
  */
@@ -819,26 +812,29 @@ int shim::cmaEnable(bool enable, uint64_t size)
 /*
  * xclLockDevice()
  */
-bool shim::xclLockDevice()
+bool
+shim::
+xclLockDevice()
 {
-    if (!is_multiprocess_mode() &&
-        mDev->flock(mUserHandle, LOCK_EX | LOCK_NB) == -1)
-        return false;
+  if (!xrt_core::config::get_multiprocess() && mDev->flock(mUserHandle, LOCK_EX | LOCK_NB) == -1)
+    return false;
 
-    mLocked = true;
-    return true;
+  mLocked = true;
+  return true;
 }
 
 /*
  * xclUnlockDevice()
  */
-bool shim::xclUnlockDevice()
+bool
+shim::
+xclUnlockDevice()
 {
-    if (!is_multiprocess_mode())
-      mDev->flock(mUserHandle, LOCK_UN);
+  if (!xrt_core::config::get_multiprocess())
+    mDev->flock(mUserHandle, LOCK_UN);
 
-    mLocked = false;
-    return true;
+  mLocked = false;
+  return true;
 }
 
 /*

src/runtime_src/core/pcie/windows/shim.cpp
@@ -47,13 +47,6 @@
 
 namespace { // private implementation details
 
-inline bool
-is_multiprocess_mode()
-{
-  static bool val = xrt_core::config::get_multiprocess() || std::getenv("XCL_MULTIPROCESS_MODE") != nullptr;
-  return val;
-}
-
 struct shim
 {
   using buffer_handle_type = xclBufferHandle; // xrt.h
@@ -816,8 +809,8 @@ done:
   bool
   lock_device()
   {
-    if (!is_multiprocess_mode() && m_locked)
-        return false;
+    if (!xrt_core::config::get_multiprocess() && m_locked)
+      return false;
 
     return m_locked = true;
   }

tests/xrt/100_ert_ncu/ocl.cpp
@@ -261,7 +261,6 @@ int run(int argc, char** argv)
   size_t device_index = 0;
   size_t secs = 0;
   size_t jobs = 1;
-  size_t cus  = 1;
   
   std::string cur;
   for (auto& arg : args) {

tests/xrt/100_ert_ncu/xrt.cpp
@@ -26,6 +26,7 @@
 #include <thread>
 #include <atomic>
 #include <iostream>
+#include <vector>
 
 namespace {
 

tests/xrt/100_ert_ncu/xrtx.cpp
@@ -25,6 +25,7 @@
 #include <thread>
 #include <atomic>
 #include <iostream>
+#include <vector>
 
 static std::vector<char>
 load_xclbin(xclDeviceHandle device, const std::string& fnm)

tests/xrt/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+OSDIST=`lsb_release -i |awk -F: '{print tolower($2)}' | tr -d ' \t'`
 SRCDIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 BUILDDIR=$SRCDIR/build
 CMAKEDIR+="$BUILDDIR/cmake"
 WSL=0
+CMAKE=cmake
+
+if [[ $OSDIST == "centos" ]] || [[ $OSDIST == "amazon" ]]; then
+    CMAKE=cmake3
+    if [[ ! -x "$(command -v $CMAKE)" ]]; then
+        echo "$CMAKE is not installed, please run xrtdeps.sh"
+        exit 1
+    fi
+fi
 
 if [[ -z $XILINX_XRT ]]; then
     echo "Please set XILINX_XRT (source xrt setup script)"
@@ -20,7 +30,6 @@ if [[ "$(< /proc/sys/kernel/osrelease)" == *Microsoft ]]; then
     CMAKEGEN="-G \"Visual Studio 15 2017 Win64\""
 else
     CMAKEDIR+="/linux"
-    CMAKE=cmake
 fi
 
 usage()

commit 10223a461aaee69d5ce27448bfbfeb6e5d18a695

Prevent deadlock in config reader
Fallout from #3121.  Config reader is initialized on demand.  To prevent
deadlock, initialization cannot call the config reader recursively.

src/runtime_src/core/common/config_reader.cpp
@@ -108,12 +108,6 @@ get_ini_path()
   return full_path;
 }
 
-// This global  tree represents the config reader for this
-// process.  Once initialized  it is valid and can be used
-// even from the tree construtor itself.  Using the config
-// reader during static global destruction is not possible
-static std::atomic<struct tree*> g_tree {nullptr};
-
 struct tree
 {
   boost::property_tree::ptree m_tree;
@@ -125,13 +119,8 @@ struct tree
     try {
       read_ini(path,m_tree);
 
-      // Need to log that ini file was read, but this in turn uses the tree
-      // object which is being constructed now. Use an atomic global to store
-      // this tree which is read for use.
-      g_tree = this;
-
       // inform which .ini was read
-      xrt_core::message::send(xrt_core::message::severity_level::XRT_INFO, "XRT", std::string("Read ") + path);
+      //xrt_core::message::send(xrt_core::message::severity_level::XRT_INFO, "XRT", std::string("Read ") + path);
     }
     catch (const std::exception& ex) {
       // Using the tree in this case is not safe, and since message
@@ -158,8 +147,6 @@ struct tree
   static tree*
   instance()
   {
-    if (g_tree)
-      return g_tree;
     static tree s_tree;
     return &s_tree;
   }