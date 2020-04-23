c275c87e291af935194c106d02cb8da3ccf8215c
Lizhi Hou <lizhih@xilinx.com>
2020-04-22 19:23:52 -0700

    cleanup

--- src/runtime_src/core/pcie/driver/linux/xocl/subdev/feature_rom.c
+++ src/runtime_src/core/pcie/driver/linux/xocl/subdev/feature_rom.c
@@ -391,8 +391,12 @@ static int load_firmware_from_flash(struct platform_device *pdev,
 
 	xocl_info(&pdev->dev, "try loading fw from flash");
 
-	(void) xocl_flash_get_size(xdev, &flash_size);
-	if (flash_size == 0) {
+	ret = xocl_flash_get_size(xdev, &flash_size);
+	if (ret == -ENODEV) {
+		xocl_info(&pdev->dev,
+			"no flash subdev");
+		return ret;
+	} else if (flash_size == 0) {
 		xocl_err(&pdev->dev,
 			"failed to get flash size");
 		return -EINVAL;
