0fb758dbdf5540c47502abb74a5883482c28eac5
Chien-Wei Lan <40216861+chienwei-lan@users.noreply.github.com>
2020-05-01 07:07:24 +0900

    XRT-510 DDR gate GPIO naming change & save-restore bug fix (#3312)

--- src/runtime_src/core/pcie/driver/linux/xocl/xocl_fdt.h
+++ src/runtime_src/core/pcie/driver/linux/xocl/xocl_fdt.h
@@ -78,7 +78,7 @@
 #define NODE_GAPPING "ep_gapping_demand_00"
 #define NODE_UCS_CONTROL_STATUS "ep_ucs_control_status_00"
 #define NODE_P2P "ep_p2p_00"
-#define NODE_DDR4_RESET_GATE "ep_ddr4_reset_gate_00"
+#define NODE_DDR4_RESET_GATE "ep_ddr_mem_srsr_gate_00"
 #define NODE_ADDR_TRANSLATOR "ep_remap_data_c2h_00"
 
 #define RESNAME_GATEPLP       NODE_GATE_PLP
