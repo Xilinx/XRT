8a2ad91fdbddf6c0b3f2b56d168d91adf883b532
Karen Xie <karen.xie@xilinx.com>
2020-05-03 21:03:09 -0700

    CR-1028065 fixed missing return code of clPollStream() and clSetStreamOpt()

--- src/runtime_src/xocl/api/xlnx/clPollStream.cpp
+++ src/runtime_src/xocl/api/xlnx/clPollStream.cpp
@@ -34,7 +34,7 @@ validOrError(cl_stream                  stream,
 	cl_int*                         errcode_ret)
 
 {
-  if (min_num_completion < 0)
+  if (min_num_completion <= 0)
     throw error(CL_INVALID_VALUE,"minimum number of completion must be > 0");
 }
 
