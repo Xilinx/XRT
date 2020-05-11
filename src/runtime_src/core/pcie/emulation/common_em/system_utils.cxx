cb9f1550ba0a0159f57accb287393145459b533a
Aleti Venkata Prasad <venkatp@xilinx.com>
2020-05-11 14:50:26 +0530

    Added one more safe check

--- src/runtime_src/core/pcie/emulation/common_em/system_utils.cxx
+++ src/runtime_src/core/pcie/emulation/common_em/system_utils.cxx
@@ -13,62 +13,74 @@ namespace systemUtil {
   
   void makeSystemCall (std::string &operand1, systemOperation operation, std::string operand2 )
   {
-    switch (operation) {
-
-      case CREATE :
-        {
-          if (boost::filesystem::exists(operand1) == false)
+    std::string operationStr;
+    try {
+      switch (operation) {
+      
+        case CREATE :
           {
-            boost::filesystem::create_directories(operand1);
+            operationStr = "CREATE";
+            if (boost::filesystem::exists(operand1) == false)
+            {
+              boost::filesystem::create_directories(operand1);
+            }
+            break;
           }
-          break;
-        }
-      case REMOVE :
-        {
-          if (boost::filesystem::exists(operand1) )
+        case REMOVE :
           {
-            boost::filesystem::remove_all(operand1);
+            operationStr = "REMOVE";
+            if (boost::filesystem::exists(operand1) )
+            {
+              boost::filesystem::remove_all(operand1);
+            }
+            break;
           }
-          break;
-        }
-      case COPY   :
-        {
-          std::stringstream copyCommand;
-          copyCommand <<"cp "<<operand1<<" "<<operand2;
-          if (boost::filesystem::exists(operand1) )
+        case COPY   :
           {
-            int status = system(copyCommand.str().c_str());
-            printErrorMessage(copyCommand.str(),status);
+            operationStr = "COPY";
+            std::stringstream copyCommand;
+            copyCommand <<"cp "<<operand1<<" "<<operand2;
+            if (boost::filesystem::exists(operand1) )
+            {
+              int status = system(copyCommand.str().c_str());
+              printErrorMessage(copyCommand.str(),status);
+            }
+            break;
           }
-          break;
-        }
-      case APPEND   :
-        {
-          std::stringstream appendCommand;
-          appendCommand <<"cat "<<operand1<<">> "<<operand2;
-          if (boost::filesystem::exists(operand1) )
+        case APPEND   :
           {
-            int status = system(appendCommand.str().c_str());
-            printErrorMessage(appendCommand.str(),status);
+            operationStr = "APPEND";
+            std::stringstream appendCommand;
+            appendCommand <<"cat "<<operand1<<">> "<<operand2;
+            if (boost::filesystem::exists(operand1) )
+            {
+              int status = system(appendCommand.str().c_str());
+              printErrorMessage(appendCommand.str(),status);
+            }
+            break;
           }
-          break;
-        }
-      case UNZIP  :
-        {
-          std::stringstream unzipCommand;
-          unzipCommand <<"unzip -q " << operand1 << " -d " << operand2;
-          int status = system(unzipCommand.str().c_str());
-          printErrorMessage(unzipCommand.str(),status);
-          break;
-        }
-      case PERMISSIONS : 
-        {
-          std::stringstream permissionsCommand ;
-          permissionsCommand << "chmod -R " << operand2 << " " << operand1;
-          int status = system(permissionsCommand.str().c_str());
-          printErrorMessage(permissionsCommand.str(),status);
-          break;
-        }
+        case UNZIP  :
+          {
+            operationStr = "UNZIP";
+            std::stringstream unzipCommand;
+            unzipCommand <<"unzip -q " << operand1 << " -d " << operand2;
+            int status = system(unzipCommand.str().c_str());
+            printErrorMessage(unzipCommand.str(),status);
+            break;
+          }
+        case PERMISSIONS : 
+          {
+            operationStr = "PERMISSIONS";
+            std::stringstream permissionsCommand ;
+            permissionsCommand << "chmod -R " << operand2 << " " << operand1;
+            int status = system(permissionsCommand.str().c_str());
+            printErrorMessage(permissionsCommand.str(),status);
+            break;
+          }
+      }
+    } catch (...) {
+      std::cerr << "ERROR: [EMU 60-601] " << " Exception Caught - Failed with the command " << operationStr.c_str() << " operation" << std::endl;
+      exit(0);
     }
   }
 }
