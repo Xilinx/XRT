d540609ecf1582f98c459e5021d38430eb3f062d
Sonal Santan <sonal.santan@xilinx.com>
2020-05-17 22:09:46 -0700

    XRT governence changes (#3413)
    * Update CHANGELOG
    * Add Contributing file to guide PRs from community
    * Minor update to Changelog
    * Update release version in Sphinx conf file
    * Add AUTHORS file
    * Remove note about disabling multiprocess in multiprocess doc
    * Updated XSA shell diagram with better layout
    * Updates to security doc
    * Deleted our copy of checkpatch.p; now automatically downloaded from kernel.org

--- src/CMake/changelog.cmake
+++ src/CMake/changelog.cmake
@@ -1,6 +1,11 @@
 # Custom variables imported by this CMake stub which should be defined by parent CMake:
-# LINUX_FLAVOR
 # XRT_INSTALL_DIR
 
 install(FILES "${CMAKE_SOURCE_DIR}/../CHANGELOG.rst"
+  DESTINATION "${XRT_INSTALL_DIR}/share/doc")
+
+install(FILES "${CMAKE_SOURCE_DIR}/../CONTRIBUTING.rst"
+  DESTINATION "${XRT_INSTALL_DIR}/share/doc")
+
+install(FILES "${CMAKE_SOURCE_DIR}/../AUTHORS.rst"
   DESTINATION "${XRT_INSTALL_DIR}/share/doc")
