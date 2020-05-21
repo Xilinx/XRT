71469ea57d639e42d4c7f804bada12f25673a904
Sonal Santan <sonal.santan@xilinx.com>
2020-05-21 14:50:42 -0700

    Update NOTICE with correct Copyright notices

--- src/include/1_2/CL/cl_ext_xilinx.h
+++ src/include/1_2/CL/cl_ext_xilinx.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008-2015 The Khronos Group Inc.
+ * Copyright (c) 2008-2019 The Khronos Group Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and/or associated documentation files (the
@@ -27,7 +27,7 @@
  ******************************************************************************/
 
 /*
- * Copyright (C) 2018, Xilinx Inc - All rights reserved
+ * Copyright (C) 2018-2019, Xilinx Inc - All rights reserved
  */
 
 #ifndef __CL_EXT_XILINX_H
@@ -516,7 +516,7 @@ xclGetComputeUnitInfo(cl_kernel             kernel,
  *
  * @CL_DEVICE_HANDLE
  * @type: void*
- * Return: The underlying device handle for use with low 
+ * Return: The underlying device handle for use with low
  * level XRT APIs (xrt.h)
  */
 #define CL_DEVICE_PCIE_BDF              0x1120  // BUS/DEVICE/FUNCTION
