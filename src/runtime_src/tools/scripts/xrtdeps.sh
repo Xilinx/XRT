78f0d0ea0911f160ac0823074921d1fe86c07627
rbramand-xilinx <54270708+rbramand-xilinx@users.noreply.github.com>
2020-05-28 09:36:10 +0530

    XRT-472 Porting XRT code to RHEL 8.1 (#3450)
    * Porting XRT code to RHEL 8.1
    > Updated xrtdeps.sh to support rhel 8.1
    
    * Update xrtdeps.sh to support rhel 8.1
    
    changes made as per comments
    
    Co-authored-by: root <root@localhost.localdomain>

--- src/runtime_src/tools/scripts/xrtdeps.sh
+++ src/runtime_src/tools/scripts/xrtdeps.sh
@@ -89,7 +89,6 @@ rh_package_list()
      unzip \
      zlib-static \
      libcurl-devel \
-     openssl-devel \
     )
 
     # Centos8
@@ -99,8 +98,16 @@ rh_package_list()
          systemd-devel \
          python3 \
          python3-pip \
-         systemd-devel \
         )
+
+	if [ $FLAVOR == "rhel" ]; then
+  
+            RH_LIST+=(\
+             kernel-devel-$(uname -r) \
+             kernel-headers-$(uname -r) \
+            )
+  
+        fi
 
     else
 
@@ -245,8 +252,29 @@ prep_centos7()
 
 prep_rhel7()
 {
+    echo "Enabling EPEL repository..."
+    rpm -q --quiet epel-release
+    if [ $? != 0 ]; then
+    	 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+	 yum check-update
+    fi
+    
     echo "Enabling RHEL SCL repository..."
     yum-config-manager --enable rhel-server-rhscl-7-rpms
+    
+}
+
+prep_rhel8()
+{
+    echo "Enabling EPEL repository..."
+    rpm -q --quiet epel-release
+    if [ $? != 0 ]; then
+    	 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+	 yum check-update
+    fi
+    
+    echo "Enabling CodeReady-Builder repository..."
+    subscription-manager repos --enable "codeready-builder-for-rhel-8-x86_64-rpms"
 }
 
 prep_centos8()
@@ -273,21 +301,14 @@ prep_centos()
 
 prep_rhel()
 {
-    echo "Enabling EPEL repository..."
-    rpm -q --quiet epel-release
-    if [ $? != 0 ]; then
-	yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-	yum check-update
-    fi
-    echo "Installing cmake3 from EPEL repository..."
-    yum install -y cmake3
-
-    if [ $MAJOR == 8 ]; then
-        echo "RHEL8 not implemented yet"
-        exit 1;
+   if [ $MAJOR == 8 ]; then
+        prep_rhel8
     else
         prep_rhel7
     fi
+    
+    echo "Installing cmake3 from EPEL repository..."
+    yum install -y cmake3
 }
 
 install()
