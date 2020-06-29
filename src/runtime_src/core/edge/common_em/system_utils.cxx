9b79dd385b91d7b3e1bdbbfbfbd89b596a15df90
vboggara <vboggara@xilinx.com>
2020-06-29 19:16:55 +0530

    added device registration code in edge sw emu code base

--- src/runtime_src/core/edge/common_em/system_utils.cxx
+++ src/runtime_src/core/edge/common_em/system_utils.cxx
@@ -2,19 +2,28 @@
 
 namespace systemUtil {
 
-  inline void printErrorMessage(std::string command, int status)
+  inline void printErrorMessage(const std::string &command, int status, const std::string& LineNo )
   {
-    if(!status)// no error
+    if (!status)
       return;
-    std::cout<<"ERROR: [SDx 60-600] "<< command <<" failed with the error code "<< status<< ". PLEASE CHECK YOUR PERMISSIONS "<<std::endl;
+
+    if (!LineNo.empty()) {  
+      std::cerr << "ERROR: [EMU 60-600] " << command.c_str() << " Exception Caught - Failed with the error code " << status << " at the Line Number " << LineNo << ". PLEASE CHECK YOUR PERMISSIONS " << std::endl;
+    } else {
+      std::cerr << "ERROR: [EMU 60-600] " << command.c_str() << " Exception Caught - Failed with the error code " << status << ". PLEASE CHECK YOUR PERMISSIONS " << std::endl;
+    }
+    exit(1);
   }
   
-  void makeSystemCall (std::string &operand1, systemOperation operation, std::string operand2 )
+  void makeSystemCall (std::string &operand1, systemOperation operation, std::string operand2, std::string LineNo)
   {
+    std::string operationStr;
+    try {
     switch (operation) {
 
       case CREATE :
         {
+            operationStr = "CREATE";
           if (boost::filesystem::exists(operand1) == false)
           {
             boost::filesystem::create_directories(operand1);
@@ -23,6 +32,7 @@ namespace systemUtil {
         }
       case REMOVE :
         {
+            operationStr = "REMOVE";
           if (boost::filesystem::exists(operand1) )
           {
             boost::filesystem::remove_all(operand1);
@@ -31,42 +41,55 @@ namespace systemUtil {
         }
       case COPY   :
         {
+            operationStr = "COPY";
           std::stringstream copyCommand;
           copyCommand <<"cp "<<operand1<<" "<<operand2;
           if (boost::filesystem::exists(operand1) )
           {
             int status = system(copyCommand.str().c_str());
-            printErrorMessage(copyCommand.str(),status);
+              printErrorMessage(copyCommand.str(),status, LineNo);
           }
           break;
         }
       case APPEND   :
         {
+            operationStr = "APPEND";
           std::stringstream appendCommand;
           appendCommand <<"cat "<<operand1<<">> "<<operand2;
           if (boost::filesystem::exists(operand1) )
           {
             int status = system(appendCommand.str().c_str());
-            printErrorMessage(appendCommand.str(),status);
+              printErrorMessage(appendCommand.str(),status, LineNo);
           }
           break;
         }
       case UNZIP  :
         {
+            operationStr = "UNZIP";
           std::stringstream unzipCommand;
           unzipCommand <<"unzip -q " << operand1 << " -d " << operand2;
           int status = system(unzipCommand.str().c_str());
-          printErrorMessage(unzipCommand.str(),status);
+            printErrorMessage(unzipCommand.str(),status, LineNo);
           break;
         }
       case PERMISSIONS : 
         {
+            operationStr = "PERMISSIONS";
           std::stringstream permissionsCommand ;
           permissionsCommand << "chmod -R " << operand2 << " " << operand1;
           int status = system(permissionsCommand.str().c_str());
-          printErrorMessage(permissionsCommand.str(),status);
+            printErrorMessage(permissionsCommand.str(),status, LineNo);
           break;
         }
+      }
+    } catch (...) {
+      if (!LineNo.empty()) {
+        std::cerr << "ERROR: [EMU 60-601] " << " Exception Caught - Failed with the command " << operationStr.c_str() << " operation at the Line Number: " << LineNo.c_str() << std::endl;
+      }
+      else {
+        std::cerr << "ERROR: [EMU 60-601] " << " Exception Caught - Failed with the command " << operationStr.c_str() << std::endl;
+      }
+      exit(1);
     }
   }
 }
---
+++ src/runtime_src/core/edge/sw_em/generic_pcie_hal2/system_swemu.cxx
@@ -0,0 +1,82 @@
+/**
+ * Copyright (C) 2020 Xilinx, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may
+ * not use this file except in compliance with the License. A copy of the
+ * License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "system_swemu.h"
+#include "device_swemu.h"
+
+#include <memory>
+
+namespace {
+
+// Singleton registers with base class xrt_core::system
+// during static global initialization.  If statically
+// linking with libxrt_core, then explicit initialiation
+// is required
+static xrt_core::swemu::system*
+singleton_instance()
+{
+  static xrt_core::swemu::system singleton;
+  return &singleton;
+}
+
+// Dynamic linking automatically constructs the singleton
+struct X
+{
+  X() { singleton_instance(); }
+} x;
+
+}
+
+namespace xrt_core { namespace swemu {
+
+std::pair<device::id_type, device::id_type>
+system::
+get_total_devices(bool is_user) const
+{
+  return {0,0};
+}
+
+std::shared_ptr<xrt_core::device>
+system::
+get_userpf_device(device::id_type id) const
+{
+  return xrt_core::get_userpf_device(xclOpen(id, nullptr, XCL_QUIET));
+}
+
+std::shared_ptr<xrt_core::device>
+system::
+get_userpf_device(device::handle_type handle, device::id_type id) const
+{
+  // deliberately not using std::make_shared (used with weak_ptr)
+  return std::shared_ptr<xrt_core::swemu::device>(new xrt_core::swemu::device(handle, id, true));
+}
+
+std::shared_ptr<xrt_core::device>
+system::
+get_mgmtpf_device(device::id_type id) const
+{
+  // deliberately not using std::make_shared (used with weak_ptr)
+  return std::shared_ptr<xrt_core::swemu::device>(new xrt_core::swemu::device(nullptr, id, false));
+}
+
+std::shared_ptr<xrt_core::device>
+get_userpf_device(device::handle_type device_handle, device::id_type id)
+{
+  singleton_instance(); // force loading if necessary
+  return xrt_core::get_userpf_device(device_handle, id);
+}
+
+}} // swemu, xrt_core
