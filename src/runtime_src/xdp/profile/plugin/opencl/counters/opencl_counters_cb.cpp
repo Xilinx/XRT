c29470d466951b53554b9d97f0ca4c8710cf4d53
Jason Villarreal <jvillar@xilinx.com>
2020-08-06 10:36:20 -0700

    Removing debugging message

--- src/runtime_src/xdp/profile/plugin/opencl/counters/opencl_counters_cb.cpp
+++ src/runtime_src/xdp/profile/plugin/opencl/counters/opencl_counters_cb.cpp
@@ -1,6 +1,6 @@
 
 #include <map>
-#include <iostream>
+//#include <iostream>
 
 #include "xdp/profile/database/database.h"
 #include "xdp/profile/plugin/opencl/counters/opencl_counters_cb.h"
@@ -45,20 +45,11 @@ namespace xdp {
     if (isStart)
     {
       storedTimestamps[kernelName] = timestamp ;
-      std::cout << "Logging start of " << kernelName << " at time " 
-		<< timestamp
-		<< std::endl ;
     }
     else
     {
       auto executionTime = timestamp - storedTimestamps[kernelName] ;
       storedTimestamps.erase(kernelName) ;
-
-      std::cout << "Logging end of " << kernelName << " at time " 
-		<< timestamp
-		<< std::endl ;
-
-      std::cout << "Execution time is " << executionTime << std::endl ;
 
       (db->getStats()).logKernelExecution(kernelName, executionTime) ;
     }
