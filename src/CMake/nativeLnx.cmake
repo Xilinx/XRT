Xilinx/XRT

commit d2fd5d1c06f9aa1482a0096e5978aef33dc94378

NSPR-7327 NSPR-7327 XRT package split into dev and deploy (#3809)
Removed package dependencies on development packages.

Use RPATH with CMake to allow libraries to find XRT dependencies
relative to location.  This in turn enables CPack to discover package
dependencies at package time, greatly simplifying explicit package
dependencies.

By default only one combined XRT package is created for both run-time
and development. To enable separate packages adjust src/CMakeLists.txt
to specify XRT_DEV_COMPONENT.

src/CMake/nativeLnx.cmake
@@ -69,9 +69,9 @@ else()
   # On older systems libboost_system.a is not compiled with -fPIC which leads to
   # link errors when XRT shared objects try to link with it.
   # Static linking with Boost is enabled on Ubuntu 18.04 but not 20.04
-  if ((${LINUX_FLAVOR} STREQUAL Ubuntu) AND (${LINUX_VERSION} STREQUAL 18.04))
-    set(Boost_USE_STATIC_LIBS  ON)
-  endif()
+#  if ((${LINUX_FLAVOR} STREQUAL Ubuntu) AND (${LINUX_VERSION} STREQUAL 18.04))
+#    set(Boost_USE_STATIC_LIBS  ON)
+#  endif()
   find_package(Boost 
     REQUIRED COMPONENTS system filesystem)
 endif()
@@ -91,15 +91,20 @@ set (XRT_INSTALL_UNWRAPPED_DIR "${XRT_INSTALL_BIN_DIR}/unwrapped")
 set (XRT_INSTALL_INCLUDE_DIR   "${XRT_INSTALL_DIR}/include")
 set (XRT_INSTALL_LIB_DIR       "${XRT_INSTALL_DIR}/lib${LIB_SUFFIX}")
 
+# Define RPATH for embedding in libraries and executables.  This allows
+# package creation to automatically determine dependencies.
+# RPATH relative to location of binary:
+#  bin/../lib, lib/xrt/module/../.., bin/unwrapped/../../lib
+SET(CMAKE_INSTALL_RPATH "$ORIGIN/../lib${LIB_SUFFFIX}:$ORIGIN/../..:$ORIGIN/../../lib${LIB_SUFFIX}")
+
 # --- Release: OpenCL extension headers ---
 set(XRT_CL_EXT_SRC
   include/1_2/CL/cl_ext_xilinx.h
   include/1_2/CL/cl_ext.h)
-install (FILES ${XRT_CL_EXT_SRC} DESTINATION ${XRT_INSTALL_INCLUDE_DIR}/CL)
-message("-- XRT CL extension header files")
-foreach (header ${XRT_CL_EXT_SRC})
-  message("-- ${header}")
-endforeach()
+install (FILES ${XRT_CL_EXT_SRC}
+  DESTINATION ${XRT_INSTALL_INCLUDE_DIR}/CL
+  COMPONENT ${XRT_DEV_COMPONENT}
+)
 
 # --- Release: eula ---
 file(GLOB XRT_EULA

src/runtime_src/core/common/CMakeLists.txt
@@ -47,16 +47,18 @@ target_link_libraries(xrt_coreutil
   )
 
 if (NOT WIN32)
-target_link_libraries(xrt_coreutil uuid pthread dl)
+  target_link_libraries(xrt_coreutil uuid pthread dl)
+endif()
+
 install(TARGETS xrt_coreutil
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR})
-else()
-# No external linking with coreutil so release only runtime
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
+)
+
 install(TARGETS xrt_coreutil
-  ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR}
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR}
-  RUNTIME DESTINATION ${XRT_INSTALL_DIR}/bin)
-endif (NOT WIN32)
+  ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+)
 
 # The scheduler object files are for auto config of scheduler. These
 # files reference xrt_core symbols, hence are excluded from

src/runtime_src/core/pcie/emulation/cpu_em/CMakeLists.txt
@@ -41,4 +41,12 @@ target_link_libraries(xrt_swemu
   uuid
   )
 
-install (TARGETS xrt_swemu LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR})
+install (TARGETS xrt_swemu
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
+)
+
+install (TARGETS xrt_swemu
+  ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+)

src/runtime_src/core/pcie/emulation/hw_em/CMakeLists.txt
@@ -41,4 +41,12 @@ target_link_libraries(xrt_hwemu
   uuid
   )
 
-install (TARGETS xrt_hwemu LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR})
+install (TARGETS xrt_hwemu
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
+)
+
+install (TARGETS xrt_hwemu
+  ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+)

src/runtime_src/core/pcie/linux/CMakeLists.txt
@@ -52,5 +52,12 @@ target_link_libraries(xrt_core
   )
 
 install(TARGETS xrt_core
-  LIBRARY
-  DESTINATION ${XRT_INSTALL_LIB_DIR})
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
+)
+
+install(TARGETS xrt_core
+  ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+)
+

src/runtime_src/core/pcie/windows/CMakeLists.txt
@@ -46,6 +46,11 @@ target_link_libraries(xrt_core
 # ARCHIVE target. All Windows-based systems including Cygwin are DLL
 # platforms.
 install(TARGETS xrt_core
-  ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR}
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR}
-  RUNTIME DESTINATION ${XRT_INSTALL_DIR}/bin)
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
+)
+
+install(TARGETS xrt_core
+  ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+)

src/runtime_src/ert/scheduler/CMakeLists.txt
@@ -144,4 +144,12 @@ add_library(sched_em SHARED ${CURR_SOURCE}
 set_target_properties(sched_em PROPERTIES VERSION ${XRT_VERSION_STRING}
   SOVERSION ${XRT_SOVERSION})
 
-install (TARGETS sched_em LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR})
+install(TARGETS sched_em
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
+)
+
+install(TARGETS sched_em
+  ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+)

src/runtime_src/xdp/CMakeLists.txt
@@ -250,11 +250,24 @@ set_target_properties(xdp_device_offload_plugin PROPERTIES VERSION ${XRT_VERSION
 set_target_properties(xdp_hal_device_offload_plugin PROPERTIES VERSION ${XRT_VERSION_STRING} SOVERSION ${XRT_SOVERSION})
 set_target_properties(xdp_power_plugin PROPERTIES VERSION ${XRT_VERSION_STRING} SOVERSION ${XRT_SOVERSION})
 
-install (TARGETS xdp oclxdp xdp_core LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR})
+install (TARGETS xdp oclxdp xdp_core
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
+)
 
-install (TARGETS xdp_hal_plugin xdp_hal_api_interface_plugin xdp_debug_plugin xdp_appdebug_plugin xdp_lop_plugin xdp_user_plugin xdp_device_offload_plugin xdp_hal_device_offload_plugin xdp_power_plugin LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR}/xrt/module)
+install (TARGETS xdp oclxdp xdp_core
+  ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+)
 
-install (FILES "${XRT_XDP_PROFILE_XMA_PLUGIN_DIR}/xma_profile.h" DESTINATION ${XRT_INSTALL_INCLUDE_DIR})
+install (TARGETS xdp_hal_plugin xdp_hal_api_interface_plugin xdp_debug_plugin xdp_appdebug_plugin xdp_lop_plugin xdp_user_plugin xdp_device_offload_plugin xdp_hal_device_offload_plugin xdp_power_plugin
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR}/xrt/module
+)
+
+install (FILES "${XRT_XDP_PROFILE_XMA_PLUGIN_DIR}/xma_profile.h"
+  DESTINATION ${XRT_INSTALL_INCLUDE_DIR}
+  COMPONENT ${XRT_DEV_COMPONENT}
+)
 
 # Only install these files for PCIe device for now, which is .
 if (${XRT_NATIVE_BUILD} STREQUAL "yes")
@@ -321,10 +334,15 @@ target_link_libraries(oclxdp xdp xilinxopencl)
 target_link_libraries(xdp_hal_plugin xdp xrt_core)
 target_link_libraries(xdp_hal_api_interface_plugin xdp)
 
-install(TARGETS xdp oclxdp xdp_hal_plugin xdp_hal_api_interface_plugin
-  ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR}
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR}
-  RUNTIME DESTINATION ${XRT_INSTALL_DIR}/bin)
+install(TARGETS oclxdp xdp xdp_hal_plugin xdp_hal_api_interface_plugin
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
+)
+
+install(TARGETS oclxdp xdp xdp_hal_plugin xdp_hal_api_interface_plugin
+  ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+)
 
 # Only install these files for PCIe device for now, which is .
 if (${XRT_NATIVE_BUILD} STREQUAL "yes")

commit b27eb2f689357f21f8e170590a6db8f11abca7f4

Avoid NAMELINK on Windows
Fallout from #3809.  Turns out NAMELINK_SKIP and NAMELINK_ONLY are not
ignored on Windows as thought.

src/CMake/nativeLnx.cmake
@@ -90,6 +90,9 @@ set (XRT_INSTALL_BIN_DIR       "${XRT_INSTALL_DIR}/bin")
 set (XRT_INSTALL_UNWRAPPED_DIR "${XRT_INSTALL_BIN_DIR}/unwrapped")
 set (XRT_INSTALL_INCLUDE_DIR   "${XRT_INSTALL_DIR}/include")
 set (XRT_INSTALL_LIB_DIR       "${XRT_INSTALL_DIR}/lib${LIB_SUFFIX}")
+set (XRT_NAMELINK_ONLY NAMELINK_ONLY)
+set (XRT_NAMELINK_SKIP NAMELINK_SKIP)
+
 
 # Define RPATH for embedding in libraries and executables.  This allows
 # package creation to automatically determine dependencies.

src/runtime_src/core/common/CMakeLists.txt
@@ -51,13 +51,13 @@ if (NOT WIN32)
 endif()
 
 install(TARGETS xrt_coreutil
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
 )
 
 install(TARGETS xrt_coreutil
   ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )
 
 # The scheduler object files are for auto config of scheduler. These

src/runtime_src/core/pcie/emulation/cpu_em/CMakeLists.txt
@@ -42,11 +42,11 @@ target_link_libraries(xrt_swemu
   )
 
 install (TARGETS xrt_swemu
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
 )
 
 install (TARGETS xrt_swemu
   ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )

src/runtime_src/core/pcie/emulation/hw_em/CMakeLists.txt
@@ -42,11 +42,11 @@ target_link_libraries(xrt_hwemu
   )
 
 install (TARGETS xrt_hwemu
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
 )
 
 install (TARGETS xrt_hwemu
   ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )

src/runtime_src/core/pcie/linux/CMakeLists.txt
@@ -52,12 +52,12 @@ target_link_libraries(xrt_core
   )
 
 install(TARGETS xrt_core
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
 )
 
 install(TARGETS xrt_core
   ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )
 

src/runtime_src/core/pcie/windows/CMakeLists.txt
@@ -46,11 +46,11 @@ target_link_libraries(xrt_core
 # ARCHIVE target. All Windows-based systems including Cygwin are DLL
 # platforms.
 install(TARGETS xrt_core
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
 )
 
 install(TARGETS xrt_core
   ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )

src/runtime_src/ert/scheduler/CMakeLists.txt
@@ -145,11 +145,11 @@ set_target_properties(sched_em PROPERTIES VERSION ${XRT_VERSION_STRING}
   SOVERSION ${XRT_SOVERSION})
 
 install(TARGETS sched_em
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
 )
 
 install(TARGETS sched_em
   ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )

src/runtime_src/xdp/CMakeLists.txt
@@ -261,13 +261,13 @@ set_target_properties(xdp_hal_device_offload_plugin PROPERTIES VERSION ${XRT_VER
 set_target_properties(xdp_power_plugin PROPERTIES VERSION ${XRT_VERSION_STRING} SOVERSION ${XRT_SOVERSION})
 
 install (TARGETS xdp oclxdp xdp_core
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
 )
 
 install (TARGETS xdp oclxdp xdp_core
   ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )
 
 install (TARGETS xdp_hal_plugin xdp_hal_api_interface_plugin xdp_debug_plugin xdp_appdebug_plugin xdp_lop_plugin xdp_user_plugin xdp_device_offload_plugin xdp_hal_device_offload_plugin xdp_power_plugin xdp_vart_plugin
@@ -348,13 +348,13 @@ target_link_libraries(xdp_hal_api_interface_plugin xdp)
 target_link_libraries(xdp_vart_plugin xdp xrt_core)
 
 install(TARGETS oclxdp xdp xdp_hal_plugin xdp_hal_api_interface_plugin xdp_vart_plugin
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
 )
 
 install(TARGETS oclxdp xdp xdp_hal_plugin xdp_hal_api_interface_plugin xdp_vart_plugin
   ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )
 
 # Only install these files for PCIe device for now, which is .

src/runtime_src/xocl/CMakeLists.txt
@@ -99,11 +99,11 @@ endif ()
 # )
 
 install(TARGETS xilinxopencl
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
 )
 
 install(TARGETS xilinxopencl
   ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )

src/runtime_src/xrt/CMakeLists.txt
@@ -61,11 +61,11 @@ set_target_properties(xrt++ PROPERTIES VERSION ${XRT_VERSION_STRING}
   SOVERSION ${XRT_SOVERSION})
 
 install(TARGETS xrt++
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} NAMELINK_SKIP
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XRT_INSTALL_BIN_DIR}
 )
 
 install(TARGETS xrt++
   ARCHIVE DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XRT_INSTALL_LIB_DIR} COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )

src/xma/src/xmaapi/CMakeLists.txt
@@ -25,11 +25,11 @@ target_link_libraries(xma2api
   )
 
 install(TARGETS xma2api
-  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib NAMELINK_SKIP
+  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XMA_INSTALL_DIR}/bin
 )
 
 install(TARGETS xma2api
   ARCHIVE DESTINATION ${XMA_INSTALL_DIR}/lib COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )

src/xma/src/xmaplugin/CMakeLists.txt
@@ -14,11 +14,11 @@ set_target_properties(xma2plugin PROPERTIES VERSION ${XMA_VERSION_STRING}
   SOVERSION ${XMA_SOVERSION})
 
 install(TARGETS xma2plugin
-  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib NAMELINK_SKIP
+  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XMA_INSTALL_DIR}/bin
 )
 
 install(TARGETS xma2plugin
   ARCHIVE DESTINATION ${XMA_INSTALL_DIR}/lib COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )

src/xma/xma_legacy/src/xmaapi/CMakeLists.txt
@@ -27,11 +27,11 @@ target_link_libraries(xmaapi
 
 
 install(TARGETS xmaapi
-  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib NAMELINK_SKIP
+  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XMA_INSTALL_DIR}/bin
 )
 
 install(TARGETS xmaapi
   ARCHIVE DESTINATION ${XMA_INSTALL_DIR}/lib COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )

src/xma/xma_legacy/src/xmaplugin/CMakeLists.txt
@@ -14,11 +14,11 @@ set_target_properties(xmaplugin PROPERTIES VERSION ${XMA_VERSION_STRING}
   SOVERSION ${XMA_SOVERSION})
 
 install(TARGETS xmaplugin
-  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib NAMELINK_SKIP
+  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib ${XRT_NAMELINK_SKIP}
   RUNTIME DESTINATION ${XMA_INSTALL_DIR}/bin
 )
 
 install(TARGETS xmaplugin
   ARCHIVE DESTINATION ${XMA_INSTALL_DIR}/lib COMPONENT ${XRT_DEV_COMPONENT}
-  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib COMPONENT ${XRT_DEV_COMPONENT} NAMELINK_ONLY
+  LIBRARY DESTINATION ${XMA_INSTALL_DIR}/lib COMPONENT ${XRT_DEV_COMPONENT} ${XRT_NAMELINK_ONLY}
 )