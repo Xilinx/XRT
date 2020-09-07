Xilinx/XRT

commit a532a971da31afa589147d4538cfa02ebd62e124

Forking processes for xbutil2 (#3863)

src/CMakeLists.txt
@@ -11,6 +11,11 @@ include_directories(
   ${CMAKE_CURRENT_SOURCE_DIR}/runtime_src/core/include
   )
 
+find_package(Boost EXACT REQUIRED)
+if(Boost_VERSION_STRING VERSION_LESS 1.64.0)
+  add_definitions (-DBOOST_PRE_1_64=1)
+endif()
+
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 

src/runtime_src/core/tools/xbutil2/CMakeLists.txt
@@ -22,6 +22,7 @@ file(GLOB XBUTIL_V2_BASE_FILES
   "../common/XBHelpMenus.cpp"
   "../common/Report*.cpp"
   "../common/ProgressBar.cpp"
+  "../common/Process.cpp"
 )
 
 # Collect local directory files

src/runtime_src/core/tools/common/Process.cpp
@@ -1,0 +1,228 @@
+/**
+ * Copyright (C) 2020 Xilinx, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may
+ * not use this file except in compliance with the License. A copy of the
+ * License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+/**
+ * Boost processes were released with Boost 1.64. If the OS doesn't support 
+ * that version of boost, then we default to linux pipes
+ * 
+ *           Support map
+ *   Ubuntu 16.04 | Linux pipes
+ *   Ubuntu 18.04 | boost::process
+ *      Windows   | boost::process
+ * 
+ * This file hides the implementation of process running from SubCmdValidate
+ */
+
+// ------ I N C L U D E   F I L E S -------------------------------------------
+#define WIN32_LEAN_AND_MEAN
+#ifdef _WIN32
+#pragma warning (disable : 4244)
+#endif
+
+// Local - Include Files
+#include "ProgressBar.h"
+#include "Process.h"
+#include "EscapeCodes.h"
+#include "XBUtilities.h"
+
+// 3rd Party Library - Include Files
+#include <boost/format.hpp>
+#ifndef BOOST_PRE_1_64
+#include <boost/process.hpp>
+#endif
+
+// System - Include Files
+#include <iostream>
+#include <thread>
+// ------ S T A T I C   V A R I A B L E S -------------------------------------
+// bandwidth testcase takes up-to a min to run
+static const int max_test_duration = 60;
+
+// ------ F U N C T I O N S ---------------------------------------------------
+#ifdef BOOST_PRE_1_64
+
+/**
+ * helper functions for running testcase in linux pipe
+ */
+inline const char* 
+getenv_or_empty(const char* path)
+{
+  return getenv(path) ? getenv(path) : "";
+}
+
+static void 
+setShellPathEnv(const std::string& var_name, const std::string& trailing_path)
+{
+  std::string xrt_path(getenv_or_empty("XILINX_XRT"));
+  std::string new_path(getenv_or_empty(var_name.c_str()));
+  xrt_path += trailing_path + ":";
+  new_path = xrt_path + new_path;
+  setenv(var_name.c_str(), new_path.c_str(), 1);
+}
+
+static void 
+testCaseProgressReporter(std::shared_ptr<XBUtilities::ProgressBar> run_test, bool& is_done)
+{
+  int counter = 0;
+  while(counter < max_test_duration && !is_done) {
+    run_test.get()->update(counter);
+    counter++;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+}
+
+unsigned int
+XBUtilities::runPythonScript( const std::string & script, 
+                 const std::vector<std::string> & args,
+                 std::ostringstream & os_stdout,
+                 std::ostringstream & os_stderr)
+{
+  // Fix environment variables before running test case
+  setenv("XILINX_XRT", "/opt/xilinx/xrt", 0);
+  setShellPathEnv("PYTHONPATH", "/python");
+  setShellPathEnv("LD_LIBRARY_PATH", "/lib");
+  setShellPathEnv("PATH", "/bin");
+  unsetenv("XCL_EMULATION_MODE");
+
+  std::ostringstream args_str;
+  std::copy(args.begin(), args.end(), std::ostream_iterator<std::string>(args_str, ""));
+  std::string cmd = "/usr/bin/python3 " + script + args_str.str();
+
+  int stderr_fds[2];
+  if (pipe(stderr_fds)== -1) {
+    os_stderr << "Unable to create pipe";
+    return errno;
+  }
+
+  // Save stderr
+  int stderr_save = dup(STDERR_FILENO);
+  if (stderr_save == -1) {
+    os_stderr << "Unable to duplicate stderr";
+    return errno;
+  }
+
+  // Kick off progress reporter
+  bool is_done = false;
+  // Fix: create busy bar
+  auto run_test = std::make_shared<XBUtilities::ProgressBar>("Running Test", max_test_duration, XBUtilities::is_esc_enabled(), std::cout); 
+  std::thread t(testCaseProgressReporter, run_test, std::ref(is_done));
+
+  // Close existing stderr and set it to be the write end of the pipe.
+  // After fork below, our child process's stderr will point to the same fd.
+  dup2(stderr_fds[1], STDERR_FILENO);
+  close(stderr_fds[1]);
+  std::shared_ptr<FILE> stderr_child(fdopen(stderr_fds[0], "r"), fclose);
+  std::shared_ptr<FILE> stdout_child(popen(cmd.c_str(), "r"), pclose);
+  // Restore our normal stderr
+  dup2(stderr_save, STDERR_FILENO);
+  close(stderr_save);
+
+  if (stdout_child == nullptr) {
+    os_stderr << boost::str(boost::format("Failed to run %s") % cmd);
+    return errno;
+  }
+
+  // Read child's stdout and stderr without parsing the content
+  char buf[1024];
+  while (!feof(stdout_child.get())) {
+    if (fgets(buf, sizeof (buf), stdout_child.get()) != nullptr) {
+      os_stdout << buf;
+    }
+  }
+  while (stderr_child && !feof(stderr_child.get())) {
+    if (fgets(buf, sizeof (buf), stderr_child.get()) != nullptr) {
+      os_stderr << buf;
+    }
+  }
+
+  is_done = true;
+  bool exitCode = (os_stdout.str().find("PASS") != std::string::npos) ? true : false;
+  run_test.get()->finish(exitCode, "");
+  // Workaround: Clear the default progress bar output so as to print the Error: before printing [FAILED]
+  // Remove this once busybar is implemented
+  std::cout << EscapeCodes::cursor().prev_line() << EscapeCodes::cursor().clear_line();
+  t.join();
+
+  return exitCode ? 0 : 1;
+}
+#else
+
+unsigned int
+XBUtilities::runPythonScript( const std::string & script, 
+                 const std::vector<std::string> & args,
+                 std::ostringstream & os_stdout,
+                 std::ostringstream & os_stderr)
+{
+  // Find the python executable
+  boost::filesystem::path pythonAbsPath = boost::process::search_path("py");  
+  if (pythonAbsPath.string().empty()) 
+    pythonAbsPath = boost::process::search_path("python");   
+
+  if (pythonAbsPath.string().empty()) 
+    throw std::runtime_error("Error: Python executable not found in search path.");
+
+  // Make sure the script exists
+  if ( !boost::filesystem::exists( script ) ) {
+    std::string errMsg = (boost::format("Error: Given python script does not exist: '%s'") % script).str();
+    throw std::runtime_error(errMsg);
+  }
+
+  // Build the python arguments
+  std::vector<std::string> cmdArgs;
+  cmdArgs.push_back(script);
+
+  // Add the user arguments
+  cmdArgs.insert(cmdArgs.end(), args.begin(), args.end());
+
+  // Build the environment variables
+  // Copy the existing environment
+  boost::process::environment env = boost::this_process::environment();
+  env.erase("XCL_EMULATION_MODE");
+
+  // Please fix: Should be a busy bar and NOT a progress bar
+  ProgressBar run_test("Running Test", max_test_duration, XBUtilities::is_esc_enabled(), std::cout); 
+
+  // Execute the python script and capture the outputs
+  boost::process::ipstream ip_stdout;
+  boost::process::ipstream ip_stderr;
+  boost::process::child runningProcess( pythonAbsPath, 
+                                        cmdArgs, 
+                                        boost::process::std_out > ip_stdout,
+                                        boost::process::std_err > ip_stderr,
+                                        env);
+
+  // Wait for the process to finish and update the busy bar
+  unsigned int counter = 0;
+  while (runningProcess.running()) {
+    run_test.update(counter++);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+
+  // Not really needed, but should be added for completeness 
+  runningProcess.wait();
+
+  // Update the return buffers
+  os_stdout << ip_stdout.rdbuf();
+  os_stderr << ip_stderr.rdbuf();
+
+  // Obtain the exit code from the running process
+  int exitCode = runningProcess.exit_code();
+  run_test.finish(exitCode == 0 /*Success or failure*/, "Test duration:");
+
+  return exitCode;
+}
+#endif

src/runtime_src/core/tools/common/Process.h
@@ -1,0 +1,30 @@
+/**
+ * Copyright (C) 2020 Xilinx, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may
+ * not use this file except in compliance with the License. A copy of the
+ * License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef __Process_h_
+#define __Process_h_
+
+#include <vector>
+
+namespace XBUtilities {
+  unsigned int
+    runPythonScript( const std::string & script, 
+                 const std::vector<std::string> & args,
+                 std::ostringstream & os_stdout,
+                 std::ostringstream & os_stderr);
+};
+
+#endif

src/runtime_src/core/tools/xbutil2/SubCmdValidate.cpp
@@ -22,6 +22,7 @@
 #include "tools/common/XBHelpMenus.h"
 #include "core/tools/common/ProgressBar.h"
 #include "core/tools/common/EscapeCodes.h"
+#include "core/tools/common/Process.h"
 #include "core/common/query_requests.h"
 #include "core/pcie/common/dmatest.h"
 namespace XBU = XBUtilities;
@@ -36,17 +37,13 @@ namespace po = boost::program_options;
 
 // System - Include Files
 #include <iostream>
+#include <sstream>
 #include <thread>
 #include <regex>
 #ifdef __GNUC__
 #include <sys/mman.h> //munmap
 #endif
-#ifdef _WIN32
-#pragma warning (disable : 4996)
-/* Disable warning for use of getenv */
-#pragma warning (disable : 4996 4100 4505)
-/* disable unrefenced params and local functions - Remove these warnings asap*/
-#endif
+
 
 // =============================================================================
 
@@ -105,159 +102,10 @@ void logger(boost::property_tree::ptree& _ptTest, const std::string& tag, const 
 }
 
 /*
- * progarm an xclbin
- */
-void
-programXclbin(const std::shared_ptr<xrt_core::device>& _dev, const std::string& xclbin, boost::property_tree::ptree& _ptTest)
-{
-  std::ifstream stream(xclbin, std::ios::binary);
-  if (!stream) {
-    logger(_ptTest, "Error", boost::str(boost::format("Could not open %s for reading") % xclbin));
-    _ptTest.put("status", "failed");
-    return;
-  }
-
-  stream.seekg(0,stream.end);
-  size_t size = stream.tellg();
-  stream.seekg(0,stream.beg);
-  std::vector<char> raw(size);
-  stream.read(raw.data(),size);
-
-  std::string ver(raw.data(),raw.data()+7);
-  if (ver != "xclbin2") {
-    logger(_ptTest, "Error", boost::str(boost::format("Bad binary version '%s' for xclbin") % ver));
-    _ptTest.put("status", "failed");
-    return;
-  }
-
-  auto hdl = _dev->get_device_handle();
-  if (xclLoadXclBin(hdl,reinterpret_cast<const axlf*>(raw.data()))) {
-    logger(_ptTest, "Error", "Could not load xclbin");
-    _ptTest.put("status", "failed");
-    return;
-  }
-}
-
-inline const char* 
-getenv_or_empty(const char* path)
-{
-  return getenv(path) ? getenv(path) : "";
-}
-
-static void 
-setShellPathEnv(const std::string& var_name, const std::string& trailing_path)
-{
-  std::string xrt_path(getenv_or_empty("XILINX_XRT"));
-  std::string new_path(getenv_or_empty(var_name.c_str()));
-  xrt_path += trailing_path + ":";
-  new_path = xrt_path + new_path;
-#ifdef __GNUC__
-  setenv(var_name.c_str(), new_path.c_str(), 1);
-#endif
-}
-
-static void 
-testCaseProgressReporter(std::shared_ptr<XBU::ProgressBar> run_test, bool& is_done)
-{
-  int counter = 0;
-  while(counter < 60 && !is_done) {
-    run_test.get()->update(counter);
-    counter++;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-  }
-}
-
-/*
- * run standalone testcases in a fork
- */
-void
-runShellCmd(const std::string& cmd, boost::property_tree::ptree& _ptTest)
-{
-#ifdef __GNUC__
-  // Fix environment variables before running test case
-  setenv("XILINX_XRT", "/opt/xilinx/xrt", 0);
-  setShellPathEnv("PYTHONPATH", "/python");
-  setShellPathEnv("LD_LIBRARY_PATH", "/lib");
-  setShellPathEnv("PATH", "/bin");
-  unsetenv("XCL_EMULATION_MODE");
-
-  int stderr_fds[2];
-  if (pipe(stderr_fds)== -1) {
-    logger(_ptTest, "Error", "Unable to create pipe");
-    _ptTest.put("status", "failed");
-    return;
-  }
-
-  // Save stderr
-  int stderr_save = dup(STDERR_FILENO);
-  if (stderr_save == -1) {
-    logger(_ptTest, "Error", "Unable to duplicate stderr");
-    _ptTest.put("status", "failed");
-    return;
-  }
-
-  // Kick off progress reporter
-  bool is_done = false;
-  //bandwidth testcase takes up-to a min to run
-  auto run_test = std::make_shared<XBU::ProgressBar>("Running Test", 60, XBU::is_esc_enabled(), std::cout); 
-  std::thread t(testCaseProgressReporter, run_test, std::ref(is_done));
-
-  // Close existing stderr and set it to be the write end of the pipe.
-  // After fork below, our child process's stderr will point to the same fd.
-  dup2(stderr_fds[1], STDERR_FILENO);
-  close(stderr_fds[1]);
-  std::shared_ptr<FILE> stderr_child(fdopen(stderr_fds[0], "r"), fclose);
-  std::shared_ptr<FILE> stdout_child(popen(cmd.c_str(), "r"), pclose);
-  // Restore our normal stderr
-  dup2(stderr_save, STDERR_FILENO);
-  close(stderr_save);
-
-  if (stdout_child == nullptr) {
-    logger(_ptTest, "Error", boost::str(boost::format("Failed to run %s") % cmd));
-    _ptTest.put("status", "failed");
-    return;
-  }
-
-  std::string output = "\n\n";
-  // Read child's stdout and stderr without parsing the content
-  char buf[1024];
-  while (!feof(stdout_child.get())) {
-    if (fgets(buf, sizeof (buf), stdout_child.get()) != nullptr) {
-      output += buf;
-    }
-  }
-  while (stderr_child && !feof(stderr_child.get())) {
-    if (fgets(buf, sizeof (buf), stderr_child.get()) != nullptr) {
-      output += buf;
-    }
-  }
-  is_done = true;
-  if (output.find("PASS") == std::string::npos) {
-    run_test.get()->finish(false, "");
-    logger(_ptTest, "Error", output);
-    _ptTest.put("status", "failed");
-  } 
-  else {
-    run_test.get()->finish(true, "");
-    _ptTest.put("status", "passed");
-  }
-  std::cout << EscapeCodes::cursor().prev_line() << EscapeCodes::cursor().clear_line();
-  t.join();
-
-  // Get out max thruput for bandwidth testcase
-  size_t st = output.find("Maximum");
-  if (st != std::string::npos) {
-    size_t end = output.find("\n", st);
-    logger(_ptTest, "Details", output.substr(st, end - st));
-  }
-#endif
-}
-
-/*
  * search for xclbin for an SSV2 platform
  */
 std::string
-searchSSV2Xclbin(const std::shared_ptr<xrt_core::device>& _dev, const std::string& logic_uuid, 
+searchSSV2Xclbin(const std::string& logic_uuid, 
                   const std::string& xclbin, boost::property_tree::ptree& _ptTest)
 {
   std::string formatted_fw_path("/opt/xilinx/firmware/");
@@ -286,7 +134,6 @@ searchSSV2Xclbin(const std::shared_ptr<xrt_core::device>& _dev, const std::strin
 
       std::regex_match(name, cm, e);
       if (cm.size() > 0) {
-#ifdef __GNUC__
         auto dtbbuf = XBUtilities::get_axlf_section(name, PARTITION_METADATA);
         if (dtbbuf.empty()) {
           ++iter;
@@ -299,7 +146,6 @@ searchSSV2Xclbin(const std::shared_ptr<xrt_core::device>& _dev, const std::strin
         else if (uuids[0].compare(logic_uuid) == 0) {
           return cm.str(1) + "test/" + xclbin;
         }
-#endif
       }
       else if (iter.level() > 4) {
         iter.pop();
@@ -343,63 +189,84 @@ searchLegacyXclbin(const std::string& dev_name, const std::string& xclbin, boost
 
 /* 
  * helper funtion for kernel and bandwidth test cases
+ * Steps:
+ * 1. Find xclbin after determining if the shell is 1RP or 2RP
+ * 2. Find testcase
+ * 3. Spawn a testcase process
+ * 4. Check results
  */
 void 
 runTestCase(const std::shared_ptr<xrt_core::device>& _dev, const std::string& py, const std::string& xclbin, 
             boost::property_tree::ptree& _ptTest)
 {
-    std::string name;
-    try{
-      name = xrt_core::device_query<xrt_core::query::rom_vbnv>(_dev);
-    } catch(...) {
-      logger(_ptTest, "Error", "Unable to find device VBNV");
+  std::string name;
+  try {
+    name = xrt_core::device_query<xrt_core::query::rom_vbnv>(_dev);
+  } catch(...) {
+    logger(_ptTest, "Error", "Unable to find device VBNV");
 
-      _ptTest.put("status", "failed");
-      return;
+    _ptTest.put("status", "failed");
+    return;
+  }
+
+  //check if a 2RP platform
+  std::vector<std::string> logic_uuid;
+  try{
+    logic_uuid = xrt_core::device_query<xrt_core::query::logic_uuids>(_dev);
+  } catch(...) { }
+
+  std::string xclbinPath;
+  if(!logic_uuid.empty()) {
+    xclbinPath = searchSSV2Xclbin(logic_uuid.front(), xclbin, _ptTest);
+  } else {
+    xclbinPath = searchLegacyXclbin(name, xclbin, _ptTest);
     }
 
-    //check if a 2RP platform
-    std::vector<std::string> logic_uuid;
-    try{
-      logic_uuid = xrt_core::device_query<xrt_core::query::logic_uuids>(_dev);
-    } catch(...) { }
-
-    std::string xclbinPath;
-    if(!logic_uuid.empty()) {
-      xclbinPath = searchSSV2Xclbin(_dev, logic_uuid.front(), xclbin, _ptTest);
-    } else {
-      xclbinPath = searchLegacyXclbin(name, xclbin, _ptTest);
+  //check if xclbin is present
+  if(xclbinPath.empty()) {
+    if(xclbin.compare("bandwidth.xclbin") == 0) {
+      //if bandwidth xclbin isn't present, skip the test
+      logger(_ptTest, "Details", "Bandwidth xclbin not available. Skipping validation.");
+      _ptTest.put("status", "skipped");
     }
+    return;
+  }
+  // log xclbin path for debugging purposes
+  logger(_ptTest, "Xclbin", xclbinPath);
 
-    //check if xclbin is present
-    if(xclbinPath.empty()) {
-      if(xclbin.compare("bandwidth.xclbin") == 0) {
-        //if bandwidth xclbin isn't present, skip the test
-        logger(_ptTest, "Details", "Bandwidth xclbin not available. Skipping validation.");
-        _ptTest.put("status", "skipped");
-      }
-      return;
-    }
-
-    //check if testcase is present
-    std::string xrtTestCasePath = "/opt/xilinx/xrt/test/" + py;
-    boost::filesystem::path xrt_path(xrtTestCasePath);
-    if (!boost::filesystem::exists(xrt_path)) {
-      logger(_ptTest, "Error", boost::str(boost::format("Failed to find %s") % xrtTestCasePath));
-      logger(_ptTest, "Error", "Please check if the platform package is installed correctly");
-      _ptTest.put("status", "failed");
-      return;
-    }
-
-    // Program xclbin first.
-    programXclbin(_dev, xclbinPath, _ptTest);
-    if (_ptTest.get<std::string>("status", "N/A").compare("failed") == 0) 
-      return;
+  //check if testcase is present
+  std::string xrtTestCasePath = "/opt/xilinx/xrt/test/" + py;
+  boost::filesystem::path xrt_path(xrtTestCasePath);
+  if (!boost::filesystem::exists(xrt_path)) {
+    logger(_ptTest, "Error", boost::str(boost::format("Failed to find %s") % xrtTestCasePath));
+    logger(_ptTest, "Error", "Please check if the platform package is installed correctly");
+    _ptTest.put("status", "failed");
+    return;
+  }
+  // log testcase path for debugging purposes
+  logger(_ptTest, "Testcase", xrtTestCasePath);
     
-    //run testcase in a fork
-    std::string cmd = "/usr/bin/python " + xrtTestCasePath + " -k " + xclbinPath + " -d " + 
-                        std::to_string(_dev.get()->get_device_id());
-    runShellCmd(cmd, _ptTest);
+  std::vector<std::string> args = { " -k ", xclbinPath, " -d ", std::to_string(_dev.get()->get_device_id()) };
+  std::ostringstream os_stdout;
+  std::ostringstream os_stderr;
+  int exit_code = XBU::runPythonScript(xrtTestCasePath, args, os_stdout, os_stderr);
+  if (exit_code != 0) {
+    logger(_ptTest, "Error", os_stdout.str());
+    logger(_ptTest, "Error", os_stderr.str());
+    _ptTest.put("status", "failed");
+  } 
+  else {
+    _ptTest.put("status", "passed");
+  }
+
+  // Get out max thruput for bandwidth testcase
+  if(xclbin.compare("bandwidth.xclbin") == 0) {
+    size_t st = os_stdout.str().find("Maximum");
+    if (st != std::string::npos) {
+      size_t end = os_stdout.str().find("\n", st);
+      logger(_ptTest, "Details", os_stdout.str().substr(st, end - st));
+    }
+  }
 }
 
 /*
@@ -772,7 +639,7 @@ bandwidthKernelTest(const std::shared_ptr<xrt_core::device>& _dev, boost::proper
     return;
   }
   std::string testcase = (name.find("vck5000") != std::string::npos) ? "versal_23_bandwidth.py" : "23_bandwidth.py";
-  runTestCase(_dev, testcase, "bandwidth.xclbin", _ptTest);
+  runTestCase(_dev, testcase, std::string("bandwidth.xclbin"), _ptTest);
 }
 
 /*

commit 8c9c19fcae5af9a37f0ceccece1680bf1b5bb1f2

Fix building with local static Boost build
Linking was broken after #3863.  Make sure first find_package of Boost
locates and configures based on XRT_BOOST_INSTALL if this variable is
set.

src/CMake/nativeLnx.cmake
@@ -66,16 +66,14 @@ if (DEFINED ENV{XRT_BOOST_INSTALL})
     HINTS $ENV{XRT_BOOST_INSTALL}
     REQUIRED COMPONENTS system filesystem)
 else()
-  # On older systems libboost_system.a is not compiled with -fPIC which leads to
-  # link errors when XRT shared objects try to link with it.
-  # Static linking with Boost is enabled on Ubuntu 18.04 but not 20.04
-#  if ((${LINUX_FLAVOR} STREQUAL Ubuntu) AND (${LINUX_VERSION} STREQUAL 18.04))
-#    set(Boost_USE_STATIC_LIBS  ON)
-#  endif()
   find_package(Boost 
     REQUIRED COMPONENTS system filesystem)
 endif()
 set(Boost_USE_MULTITHREADED ON)             # Multi-threaded libraries
+
+if(Boost_VERSION_STRING VERSION_LESS 1.64.0)
+  add_definitions (-DBOOST_PRE_1_64=1)
+endif()
 
 include_directories(${Boost_INCLUDE_DIRS})
 add_compile_options("-DBOOST_LOCALE_HIDE_AUTO_PTR")

src/CMake/nativeWin.cmake
@@ -25,6 +25,10 @@ INCLUDE (FindGTest)
 include_directories(${Boost_INCLUDE_DIRS})
 add_compile_options("-DBOOST_LOCALE_HIDE_AUTO_PTR")
 
+if(Boost_VERSION_STRING VERSION_LESS 1.64.0)
+  add_definitions (-DBOOST_PRE_1_64=1)
+endif()
+
 # --- XRT Variables ---
 set (XRT_INSTALL_DIR "xrt")
 set (XRT_INSTALL_BIN_DIR       "${XRT_INSTALL_DIR}/bin")

src/CMakeLists.txt
@@ -12,11 +12,6 @@ include_directories(
   ${CMAKE_CURRENT_SOURCE_DIR}/../tests/validate
   )
 
-find_package(Boost EXACT REQUIRED)
-if(Boost_VERSION_STRING VERSION_LESS 1.64.0)
-  add_definitions (-DBOOST_PRE_1_64=1)
-endif()
-
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 