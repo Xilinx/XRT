9332fe895fec27a055818e126f758d7da63c9f1c
Ch Vamshi Krishna <40261882+chvamshi-xilinx@users.noreply.github.com>
2020-09-14 18:24:25 +0530

    Fixing BSP path in Edge platforms build (#4043)

--- build/build_edge.sh
+++ build/build_edge.sh
@@ -147,7 +147,7 @@ elif [[ $AARCH = $aarch32_dir ]]; then
     PETA_BSP="$PETALINUX/../../bsp/release/xilinx-zc706-v2020.2-final.bsp"
     YOCTO_MACHINE="zynq-generic"
 elif [[ $AARCH = $versal_dir ]]; then
-    PETA_BSP="$PETALINUX/../../bsp/release/xilinx-vck190-emmc-es1-v2020.2-final.bsp"
+    PETA_BSP="$PETALINUX/../../bsp/internal/versal/versal-rootfs-common-v2020.2-final.bsp"
     YOCTO_MACHINE="versal-generic"
 else
     error "$AARCH not exist"
