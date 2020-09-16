39dc97a9ae14ca2532b424e286257071f7ee4702
David Zhang <davidzha@xilinx.com>
2020-09-15 17:09:50 -0700

    CR-1074448 ASTeR Basic sanity - xbutil dump JSON format broken due to error message printed in output u250 and u50

--- src/runtime_src/core/pcie/tools/xbutil/xbutil.h
+++ src/runtime_src/core/pcie/tools/xbutil/xbutil.h
@@ -278,7 +278,7 @@ public:
         std::string errmsg;
         pcidev::get_dev(m_idx)->sysfs_get("rom", "VBNV", errmsg, m_devicename);
         if(!errmsg.empty())
-            throw std::runtime_error("Failed to determine device name. ");
+            throw std::runtime_error("Failed to determine device name. " + errmsg);
     }
 
     device(device&& rhs) = delete;
@@ -493,7 +493,7 @@ public:
 
         // If unknown status bits, can't support.
         if (status & ~(ce_mask | ue_mask)) {
-            std::cout << "Bad ECC status detected!" << std::endl;
+            std::cerr << "Bad ECC status detected!" << std::endl;
             return -EINVAL;
         }
 
