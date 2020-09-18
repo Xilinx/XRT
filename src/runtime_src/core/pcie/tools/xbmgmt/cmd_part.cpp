81f168c216c5e0f905208b3a0a34131ea5af144e
David Zhang <50243230+xdavidz@users.noreply.github.com>
2020-09-18 13:16:31 -0700

    CR-1074971 ASTeR 1.05 - y/n prompt when plp program executed while fa… (#4068)

--- src/runtime_src/core/pcie/tools/xbmgmt/cmd_part.cpp
+++ src/runtime_src/core/pcie/tools/xbmgmt/cmd_part.cpp
@@ -363,6 +363,17 @@ int program(int argc, char *argv[])
     std::string blp_uuid, logic_uuid;
     auto dev = pcidev::get_dev(index, false);
     std::string errmsg;
+    bool is_mfg = false;
+
+    dev->sysfs_get("", "mfg", errmsg, is_mfg, false);
+    if (!errmsg.empty()) {
+        std::cerr << "Unexpected error: " << errmsg << std::endl;
+        return -EINVAL;
+    }
+    if (is_mfg) {
+        std::cout << "Do not support downloading BLP/PLP onto Golden" << std::endl;
+        return -EINVAL;
+    }
 
     dev->sysfs_get("rom", "uuid", errmsg, logic_uuid);
     if (!errmsg.empty() || logic_uuid.empty())
