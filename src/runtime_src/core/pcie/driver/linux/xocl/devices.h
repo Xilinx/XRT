c9079e65266e10472400c18e2e77a0a0983630d5
Brian Xu <brianx@xilinx.com>
2020-09-24 12:46:30 -0700

    CR-1077604 xbutil hanging with xclbin download

--- src/runtime_src/core/pcie/driver/linux/xocl/devices.h
+++ src/runtime_src/core/pcie/driver/linux/xocl/devices.h
@@ -2622,7 +2622,6 @@ struct xocl_subdev_map {
 		.subdev_info = RES_MGMT_VSEC,                           \
 		.subdev_num  = ARRAY_SIZE(RES_MGMT_VSEC),               \
 		.flash_type  = FLASH_TYPE_SPI,                          \
-		.sched_bin   = "xilinx/sched_v20.bin",                  \
 		.board_name  = "u26z"                                   \
 	}
 
