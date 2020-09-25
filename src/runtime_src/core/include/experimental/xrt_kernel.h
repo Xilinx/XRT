Xilinx/XRT

commit 0a1b4862935f8341a4ff3b6647b9c594cb480d3e

CR-1075954 not using boolean for exclusive CU access API (#4098)
Add class enum for CU access mode.

src/runtime_src/core/include/experimental/xrt_kernel.h
@@ -307,14 +307,22 @@ private:
 class kernel_impl;
 class kernel
 {
-public:
+ public:
+  /**
+   * cu_access_mode - compute unit access mode
+   *
+   * @shared:    CUs can be shared between processes
+   * @exclusive: CUs are owned exclusively by this process
+   */
+  enum class cu_access_mode : bool { exclusive = false, shared = true };
+
   /**
    * kernel() - Constructor from a device and xclbin
    *
    * @device: Device on which the kernel should execute
    * @xclbin_id: UUID of the xclbin with the kernel
    * @name:  Name of kernel to construct
-   * @exclusive: Open the kernel instances with exclusive access (default shared)
+   * @mode: Open the kernel instances with specified access (default shared)
    *
    * The kernel name must uniquely identify compatible kernel
    * instances (compute units).  Optionally specify which kernel
@@ -322,17 +330,25 @@ public:
    * "kernelname:{instancename1,instancename2,...}" syntax.  The
    * compute units are default opened with shared access, meaning that
    * other kernels and other process will have shared access to same
-   * compute units.  If exclusive access is needed then set @exclusive
-   * argument to true.
+   * compute units.
    */
   XCL_DRIVER_DLLESPEC
-  kernel(const xrt::device& device, const xrt::uuid& xclbin_id, const std::string& name, bool exclusive=false);
+  kernel(const xrt::device& device, const xrt::uuid& xclbin_id, const std::string& name,
+         cu_access_mode mode = cu_access_mode::shared);
+
+  /// @cond
+  /// Deprecated construtor for exclusive access
+  kernel(const xrt::device& device, const xrt::uuid& xclbin_id, const std::string& name, bool ex=false)
+    : kernel(device, xclbin_id, name, ex ? cu_access_mode::exclusive : cu_access_mode::shared)
+  {}
+  /// @endcond
 
   /**
    * Obsoleted construction from xclDeviceHandle
    */
   XCL_DRIVER_DLLESPEC
-  kernel(xclDeviceHandle dhdl, const xrt::uuid& xclbin_id, const std::string& name, bool exclusive=false);
+  kernel(xclDeviceHandle dhdl, const xrt::uuid& xclbin_id, const std::string& name,
+         cu_access_mode mode = cu_access_mode::shared);
 
   /**
    * operator() - Invoke the kernel function

src/runtime_src/core/common/api/xrt_kernel.cpp
@@ -206,7 +206,7 @@ struct device_type
 class ip_context
 {
 public:
-  enum class access_mode : bool { exclusive = false, shared = true };
+  using access_mode = xrt::kernel::cu_access_mode;
   constexpr static unsigned int virtual_cu_idx = std::numeric_limits<unsigned int>::max();
 
   static std::shared_ptr<ip_context>
@@ -1695,17 +1695,15 @@ set_event(const std::shared_ptr<event_impl>& event) const
 }
 
 kernel::
-kernel(const xrt::device& xdev, const xrt::uuid& xclbin_id, const std::string& name, bool exclusive)
+kernel(const xrt::device& xdev, const xrt::uuid& xclbin_id, const std::string& name, cu_access_mode mode)
   : handle(std::make_shared<kernel_impl>
-     (get_device(xdev), xclbin_id, name,
-      exclusive ? ip_context::access_mode::exclusive : ip_context::access_mode::shared))
+      (get_device(xdev), xclbin_id, name, mode))
 {}
 
 kernel::
-kernel(xclDeviceHandle dhdl, const xrt::uuid& xclbin_id, const std::string& name, bool exclusive)
+kernel(xclDeviceHandle dhdl, const xrt::uuid& xclbin_id, const std::string& name, cu_access_mode mode)
   : handle(std::make_shared<kernel_impl>
-      (get_device(xrt_core::get_userpf_device(dhdl)), xclbin_id, name,
-      exclusive ? ip_context::access_mode::exclusive : ip_context::access_mode::shared))
+      (get_device(xrt_core::get_userpf_device(dhdl)), xclbin_id, name, mode))
 {}
 
 uint32_t

commit a67aa19ffdaff84ace616263410da317f4278dd1

Fix overload bugs
Regression from #4098 and #4076.

The xrt::bo overload of set_arg was not called as expected.  Had to
provide overloads for (const xrt::bo&), (xrt::bo&), and (xrt::bo&&) to
match forwarding type by std::forward.

src/runtime_src/core/include/experimental/xrt_kernel.h
@@ -107,8 +107,27 @@ class run
    * expires. Completion does not guarantee success, the run status
    * should be checked by using @state.
    */
+  XCL_DRIVER_DLLESPEC
   ert_cmd_state
   wait(const std::chrono::milliseconds& timeout = std::chrono::milliseconds{0}) const;
+
+  /**
+   * wait() - Wait for specified milliseconds for run to complete
+   *
+   * @timeout:  Timeout in milliseconds
+   * Return:    Command state upon return of wait
+   *
+   * The default timeout of 0ms indicates blocking until run completes.
+   *
+   * The current thread will block until the run completes or timeout
+   * expires. Completion does not guarantee success, the run status
+   * should be checked by using @state.
+   */
+  ert_cmd_state
+  wait(unsigned int timeout_ms) const
+  {
+    return wait(timeout_ms * std::chrono::milliseconds{1});
+  }
 
   /**
    * state() - Check the current state of a run object
@@ -185,7 +204,7 @@ class run
    * set_arg() - Set a specific kernel global argument for a run
    *
    * @index:      Index of kernel argument to update
-   * @boh:        The global buffer argument value to set.
+   * @boh:        The global buffer argument value to set (lvalue).
    * 
    * Use this API to explicit set or change a kernel argument prior
    * to starting kernel execution.  After setting arguments, the
@@ -194,11 +213,28 @@ class run
    * See also @operator() to set all arguments and start kernel.
    */
   void
+  set_arg(int index, xrt::bo& boh)
+  {
+    set_arg_at_index(index, boh);
+  }
+
+  /**
+   * set_arg - xrt::bo variant for const lvalue
+   */
+  void
   set_arg(int index, const xrt::bo& boh)
   {
     set_arg_at_index(index, boh);
   }
-  
+
+  /**
+   * set_arg - xrt::bo variant for rvalue
+   */
+  void
+  set_arg(int index, xrt::bo&& boh)
+  {
+    set_arg_at_index(index, boh);
+  }
 
   /**
    * udpdate_arg() - Asynchronous update of scalar kernel global argument
@@ -338,7 +374,7 @@ class kernel
 
   /// @cond
   /// Deprecated construtor for exclusive access
-  kernel(const xrt::device& device, const xrt::uuid& xclbin_id, const std::string& name, bool ex=false)
+  kernel(const xrt::device& device, const xrt::uuid& xclbin_id, const std::string& name, bool ex)
     : kernel(device, xclbin_id, name, ex ? cu_access_mode::exclusive : cu_access_mode::shared)
   {}
   /// @endcond