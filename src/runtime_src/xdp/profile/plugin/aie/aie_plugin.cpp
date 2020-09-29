520287abd37ffb461341804bb924f3bb31eff808
Anurag Dubey <anurag6dubey@gmail.com>
2020-09-29 14:16:36 -0600

    remove ++index and fix spacing

--- src/runtime_src/xdp/profile/plugin/aie/aie_plugin.cpp
+++ src/runtime_src/xdp/profile/plugin/aie/aie_plugin.cpp
@@ -67,43 +67,37 @@ namespace xdp {
 
     auto& should_continue = it->second;
     while (should_continue) {
+      // Wait until xclbin has been loaded and device has been updated in database
+      if (!(db->getStaticInfo().isDeviceReady(index)))
+        continue;
+      auto aieArray = drv->getAieArray();
+      if (!aieArray)
+        continue;
 
-        // Wait until xclbin has been loaded and device has been updated in database
-        if (!(db->getStaticInfo().isDeviceReady(index))) {
-          ++index;
+      // Iterate over all AIE Counters
+      auto numCounters = db->getStaticInfo().getNumAIECounter(index);
+      for (uint64_t c=0; c < numCounters; c++) {
+        auto aie = db->getStaticInfo().getAIECounter(index, c);
+        if (!aie)
           continue;
-        }
 
-        auto aieArray = drv->getAieArray();
-        if (!aieArray)
-          continue;
+        std::vector<uint64_t> values;
+        values.push_back(aie->column);
+        values.push_back(aie->row);
+        values.push_back(aie->startEvent);
+        values.push_back(aie->endEvent);
+        values.push_back(aie->resetEvent);
 
-        // Iterate over all AIE Counters
-        auto numCounters = db->getStaticInfo().getNumAIECounter(index);
-        for (uint64_t c=0; c < numCounters; c++) {
-          auto aie = db->getStaticInfo().getAIECounter(index, c);
-          if (!aie)
-            continue;
+        // Read counter value from device
+        XAie_LocType tileLocation = XAie_TileLoc(aie->column, aie->row+1);
+        uint32_t counterValue;
+        XAie_PerfCounterGet(aieArray->getDevInst(), tileLocation, XAIE_CORE_MOD, aie->counterNumber, &counterValue);
+        values.push_back(counterValue);
 
-          std::vector<uint64_t> values;
-          values.push_back(aie->column);
-          values.push_back(aie->row);
-          values.push_back(aie->startEvent);
-          values.push_back(aie->endEvent);
-          values.push_back(aie->resetEvent);
-
-          // Read counter value from device
-          XAie_LocType tileLocation = XAie_TileLoc(aie->column, aie->row+1);
-          uint32_t counterValue;
-          XAie_PerfCounterGet(aieArray->getDevInst(), tileLocation, XAIE_CORE_MOD, aie->counterNumber, &counterValue);
-          values.push_back(counterValue);
-
-          // Get timestamp in milliseconds
-          double timestamp = xrt_core::time_ns() / 1.0e6;
-
-	        db->getDynamicInfo().addAIESample(index, timestamp, values);
-        }
-
+        // Get timestamp in milliseconds
+        double timestamp = xrt_core::time_ns() / 1.0e6;
+        db->getDynamicInfo().addAIESample(index, timestamp, values);
+      }
       std::this_thread::sleep_for(std::chrono::milliseconds(mPollingInterval));     
     }
   }
@@ -135,7 +129,7 @@ namespace xdp {
     // Create and register writer and file
     std::string outputFile = "aie_profile_" + deviceName + ".csv";
     writers.push_back(new AIEProfilingWriter(outputFile.c_str(),
-		    deviceName.c_str(), mIndex));
+        deviceName.c_str(), mIndex));
     db->getStaticInfo().addOpenedFile(outputFile.c_str(), "AIE_PROFILE");
 
     // Start the AIE profiling thread
