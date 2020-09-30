e76c9a17835b590225f95106e119ae38925d020c
Shivangi Agarwal <shivangiagarwal53@gmail.com>
2020-09-30 08:42:32 -0700

    xbutil2/xbmgmt2 post bash cleanup (#4128)
    * address mini-bash issues for xbutil2/xbmgmt2
    
    * address mini-bash issues
    
    * CR-1077412
    
    * requested changes

--- src/runtime_src/core/tools/common/XBUtilities.h
+++ src/runtime_src/core/tools/common/XBUtilities.h
@@ -88,12 +88,6 @@ namespace XBUtilities {
                         bool _inUserDomain,
                         xrt_core::device_collection &_deviceCollection);
 
-  xrt_core::device_collection
-  collect_devices(const std::vector<std::string>& _devices, bool _inUserDomain);
-              
-  xrt_core::device_collection
-  collect_devices(const std::string& _devices, bool _inUserDomain);
-
   boost::property_tree::ptree
   get_available_devices(bool inUserDomain);
   std::string format_base10_shiftdown3(uint64_t value);
--- src/runtime_src/core/tools/xbmgmt2/OO_Config.cpp
+++ src/runtime_src/core/tools/xbmgmt2/OO_Config.cpp
@@ -239,7 +239,7 @@ memory_retention(xrt_core::device* device, mem_type, bool enable)
 
 OO_Config::OO_Config( const std::string &_longName, bool _isHidden)
     : OptionOptions(_longName, _isHidden, "Utility to modify the memory configuration(s)")
-    , m_device({})
+    , m_devices({})
     , m_help(false)
     , m_daemon(false)
     , m_host("")
@@ -254,7 +254,7 @@ OO_Config::OO_Config( const std::string &_longName, bool _isHidden)
 
 {
   m_optionsDescription.add_options()
-    ("device,d", boost::program_options::value<decltype(m_device)>(&m_device)->multitoken(), "The Bus:Device.Function (e.g., 0000:d8:00.0) device of interest")
+    ("device,d", boost::program_options::value<decltype(m_devices)>(&m_devices)->multitoken(), "The Bus:Device.Function (e.g., 0000:d8:00.0) device of interest")
     ("retention", boost::program_options::value<decltype(m_retention)>(&m_retention),"Enables / Disables memory retention.  Valid values are: [ENABLE | DISABLE]")
     ("help,h", boost::program_options::bool_switch(&m_help), "Help to use this sub-command")
   ;
@@ -317,15 +317,20 @@ OO_Config::execute(const SubCmdOptions& _options) const
     }
   }
 
-  if (m_device.empty() && !m_daemon)  {
+  if (m_devices.empty() && !m_daemon)  {
     std::cerr << "ERROR: If the daemon is to be used (e.g., set to true) then a device must also be declared." << std::endl;
     printHelp();
     return;
   }
 
   // -- process option: device -----------------------------------------------
-  auto devices = XBU::collect_devices(m_device, false);
+  std::set<std::string> deviceNames;
+  xrt_core::device_collection deviceCollection;
+  for (const auto & deviceName : m_devices) 
+    deviceNames.insert(boost::algorithm::to_lower_copy(deviceName));
   
+  XBU::collect_devices(deviceNames, false /*inUserDomain*/, deviceCollection);
+
   //Option:show
   if (m_show) {
     XBU::verbose("Sub command: --show");
@@ -334,7 +339,7 @@ OO_Config::execute(const SubCmdOptions& _options) const
       show_daemon_conf();
 
     //show device config
-    for (const auto& dev : devices)
+    for (const auto& dev : deviceCollection)
       show_device_conf(dev.get());
 
     return;
@@ -351,26 +356,26 @@ OO_Config::execute(const SubCmdOptions& _options) const
   }
 
   //-- process option: device -----------------------------------------------
-  if (!m_device.empty()) {
+  if (!m_devices.empty()) {
     XBU::verbose("Sub command: --device");
     //update security
     if (!m_security.empty())
-      for (const auto& dev : devices)
+      for (const auto& dev : deviceCollection)
         update_device_conf(dev.get(), m_security, config_type::security);
 
     //clock scaling
     if (!m_clk_scale.empty())
-      for (const auto& dev : devices)
+      for (const auto& dev : deviceCollection)
         update_device_conf(dev.get(), m_clk_scale, config_type::clk_scaling);
     
     //update threshold power override
     if (!m_power_override.empty())
-      for (const auto& dev : devices)
+      for (const auto& dev : deviceCollection)
         update_device_conf(dev.get(), m_power_override, config_type::threshold_power_override);
 
     //cs_reset
     if (!m_cs_reset.empty())
-      for (const auto& dev : devices)
+      for (const auto& dev : deviceCollection)
         update_device_conf(dev.get(), m_cs_reset, config_type::reset);
 
     //  enable/disable_retention
@@ -384,7 +389,7 @@ OO_Config::execute(const SubCmdOptions& _options) const
         throw xrt_core::system_error(EINVAL, "Please specify memory type: ddr or hbm");
 
       bool enableRetention = (m_retention == "ENABLE");
-      for (const auto& dev : devices)
+      for (const auto& dev : deviceCollection)
         memory_retention(dev.get(), mem, enableRetention);
     }
   }
