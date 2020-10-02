eb81c6d15035727333794d81f7ee20df1bc26e54
Lizhi Hou <lizhih@xilinx.com>
2020-10-02 12:06:26 -0700

    codereview comments

--- src/runtime_src/core/pcie/driver/linux/xocl/mgmtpf/mgmt-utils.c
+++ src/runtime_src/core/pcie/driver/linux/xocl/mgmtpf/mgmt-utils.c
@@ -280,13 +280,14 @@ long xclmgmt_hot_reset(struct xclmgmt_dev *lro, bool force)
 #endif
 		/*
 		 * For U50 built with 2RP flow. PLP Gate is closed after
-		 * pci hot reset. Any access to PLP IPs will hangs the host.
-		 * E.g. read/write BAR2 (DMA BAR). 
-		 * To be able to open PLP gate after hot reset, the first 64
-		 * bytes of config space has to be restored. 
-		 * In the meanwhile, XRT expects firewall trip instead of
-		 * hard hang if there is an unexpected access of non-exist
+		 * pci hot reset.
+		 * XRT expects firewall trip instead of * hard hang if there
+		 * is an unexpected access of non-exist
 		 * IPs. (E.g. Invalid access from a active VM)
+		 *
+		 * for the new platforms which support pcie firewall which
+		 * should be able to block some of the unexpected access
+		 * (BAR 0 access will not be blocked by pcie firewall.
 		 */
 		(void) xocl_subdev_online_by_id(lro, XOCL_SUBDEV_AXIGATE);
 
