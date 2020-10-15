295e81bf6b3ec2b750924e5b95f31b9075171b85
IshitaGhosh <ighosh.account@gmail.com>
2020-10-14 21:49:26 -0600

    Remove redundant code under #if 0 macro

--- src/runtime_src/xdp/profile/plugin/device_offload/opencl/opencl_device_offload_plugin.cpp
+++ src/runtime_src/xdp/profile/plugin/device_offload/opencl/opencl_device_offload_plugin.cpp
@@ -79,18 +79,6 @@ namespace xdp {
     }
 
     clearOffloaders();
-#if 0
-    for (auto o : offloaders)
-    {
-      auto offloader = std::get<0>(o.second) ;
-      auto logger    = std::get<1>(o.second) ;
-      auto intf      = std::get<2>(o.second) ;
-
-      delete offloader ;
-      delete logger ;
-      delete intf ;
-    }
-#endif
 
   }
 
@@ -146,21 +134,6 @@ namespace xdp {
     deviceId = db->addDevice(path) ;
 
     clearOffloader(deviceId);
-#if 0
-    if (offloaders.find(deviceId) != offloaders.end())
-    {
-      // Clean up the old offloader.  It has already been flushed.
-      auto info = offloaders[deviceId] ;
-
-      auto offloader = std::get<0>(info) ;
-      auto logger    = std::get<1>(info) ;
-      auto intf      = std::get<2>(info) ;
-
-      delete offloader ;
-      delete logger ;
-      delete intf ;
-    }
-#endif
 
     // Update the static database with all the information that will
     //  be needed later.
