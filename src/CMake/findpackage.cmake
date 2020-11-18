a3988db12a3006c3fb70d34f636c20ddae2fd3bf
bryanloz <bryanloz@xilinx.com>
2020-11-17 18:33:49 -0800

    Add support for cmake's find_package on linux

---
+++ src/CMake/findpackage.cmake
@@ -0,0 +1,49 @@
+message("-- Preparing XRT find_package")
+
+# Provides write_basic_package_version_file
+include(CMakePackageConfigHelpers)
+
+string(TOLOWER ${PROJECT_NAME} LOWER_NAME)
+
+# Generate xrt-config.cmake
+# For use by xrt consumers (using cmake) to import xrt libraries
+configure_package_config_file (
+  ${CMAKE_SOURCE_DIR}/CMake/config/xrt.fp.in
+  ${CMAKE_CURRENT_BINARY_DIR}/${LOWER_NAME}-config.cmake
+  INSTALL_DESTINATION ${XRT_INSTALL_DIR}/share/cmake/${PROJECT_NAME}
+  )
+
+# Generate xrt-config-version.cmake
+# Consumers my require a particular version
+# This enables version checking
+write_basic_package_version_file (
+  ${CMAKE_CURRENT_BINARY_DIR}/${LOWER_NAME}-config-version.cmake
+  VERSION ${XRT_VERSION_STRING}
+  COMPATIBILITY AnyNewerVersion
+  )
+
+# Install xrt-config.cmake and xrt-config-version.cmake
+install (
+  FILES ${CMAKE_CURRENT_BINARY_DIR}/${LOWER_NAME}-config.cmake ${CMAKE_CURRENT_BINARY_DIR}/${LOWER_NAME}-config-version.cmake
+  DESTINATION ${XRT_INSTALL_DIR}/share/cmake/${PROJECT_NAME}
+  COMPONENT ${XRT_DEV_COMPONENT}
+  )
+
+# Generate and install xrt-targets.cmake
+# This will generate a file that details all targets we have marked for export
+# as part of the xrt-targets export group
+# It will provide information such as the library file names and locations post install
+install(
+  EXPORT xrt-targets
+  NAMESPACE ${PROJECT_NAME}::
+  DESTINATION ${XRT_INSTALL_DIR}/share/cmake/${PROJECT_NAME}
+  )
+
+# Generate xrt-build-tree-targets.cmake
+# This file is not installed
+# It is only used when you are testing with XRT sourced from your local build
+export(
+  EXPORT xrt-targets
+  NAMESPACE ${PROJECT_NAME}::
+  FILE "${CMAKE_CURRENT_BINARY_DIR}/${XRT_INSTALL_DIR}/share/cmake/${PROJECT_NAME}/xrt-build-tree-targets.cmake"
+  )
