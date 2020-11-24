ceba5e1f103e3fd5e4c49c9bc23b8285124b0f72
ch vamshi krishna <chvamshi@xilinx.com>
2020-11-24 14:03:38 +0530

    removing petalinux hardcoded path

--- build/build_edge.sh
+++ build/build_edge.sh
@@ -138,7 +138,6 @@ fi
 if [ -f $SETTINGS_FILE ]; then
     source $SETTINGS_FILE
 fi
-PETALINUX="/proj/petalinux/2020.2/petalinux-v2020.2_1108_1/tool/petalinux-v2020.2-final"
 source $PETALINUX/settings.sh 
 
 if [[ $AARCH = $aarch64_dir ]]; then
