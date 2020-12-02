2d4aa7d6a72039bd3d5c95cbf6f7d1f91f486ed5
Shivangi Agarwal <shivangiagarwal53@gmail.com>
2020-12-02 14:20:07 -0800

    CR-1082806 (#4521)

--- tests/python/23_bandwidth/versal_23_bandwidth.py
+++ tests/python/23_bandwidth/versal_23_bandwidth.py
@@ -18,7 +18,6 @@
 """
 
 import ctypes.util
-import numpy as np
 import sys
 import time
 import math
@@ -44,13 +43,6 @@ current_micro_time = lambda: int(round(time.time() * 1000000))
 globalbuffersize = int(1024*1024*0.07)    #0.07 MB
 
 def getInputOutputBuffer(devhdl, krnlhdl, argno, isInput):
-    if isInput:
-        lst = [i%256 for i in range(globalbuffersize)]
-    else:
-        lst = [0 for i in range(globalbuffersize)]
-    hostbuf = np.array(lst).astype(np.uint8)
-    assert hostbuf.size == globalbuffersize
-
     grpid = xrtKernelArgGroupId(krnlhdl, argno)
     if grpid < 0:
         raise RuntimeError("failed to find BO group ID: %d" % grpid)
@@ -61,7 +53,11 @@ def getInputOutputBuffer(devhdl, krnlhdl, argno, isInput):
     bobuf = xrtBOMap(bo)
     if bobuf == 0:
         raise RuntimeError("failed to map buffer")
-    libc.memcpy(bobuf, hostbuf.ctypes.data, globalbuffersize)
+    lst = ctypes.cast(bobuf, ctypes.POINTER(ctypes.c_char))
+
+    for i in range(globalbuffersize):
+        lst[i] = i%256 if isInput else 0
+
     xrtBOSync(bo, xclBOSyncDirection.XCL_BO_SYNC_BO_TO_DEVICE, globalbuffersize, 0)
     return bo, bobuf
 
