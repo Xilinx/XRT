796661f9f48d8adfccd2ca76254ef89a076ca639
uday610 <udayd@xilinx.com mailto:udayd@xilinx.com>
2020-12-04 12:38:47 -0800

    updated 2020.2 documentation

---
+++ 2020.2/html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['bo',['bo',['../classxrt_1_1bo.html',1,'xrt']]]
+];
---
+++ 2020.2/html/search/namespaces_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['ini',['ini',['../namespacexrt_1_1ini.html',1,'xrt']]],
+  ['xrt',['xrt',['../namespacexrt.html',1,'']]],
+  ['xrt_5fcore',['xrt_core',['../namespacexrt__core.html',1,'']]]
+];
