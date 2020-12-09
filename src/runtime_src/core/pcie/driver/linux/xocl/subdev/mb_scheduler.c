20d8cd92e31eb5ee08e99956283a1f9240bfb230
Sarabjeet Singh <sarab96@gmail.com>
2020-12-09 11:40:21 -0800

    updated

--- src/runtime_src/core/pcie/driver/linux/xocl/subdev/mb_scheduler.c
+++ src/runtime_src/core/pcie/driver/linux/xocl/subdev/mb_scheduler.c
@@ -2541,11 +2541,12 @@ exec_mark_cmd_complete(struct exec_core *exec, struct xocl_cmd *xcmd)
 			struct ert_start_kernel_cmd tmp_pkt;
 			xocl_memcpy_fromio((void*)&tmp_pkt.header, xert->cq_base + slot_addr, 2 * sizeof(u32));
 			cmd_state = tmp_pkt.state;
-			//Possible to upgrade XRT on host without changing zocl on PS
-			if (cmd_state < ERT_CMD_STATE_COMPLETED) {//It is old zocl
+			/* Possible to upgrade XRT on host without changing zocl on PS */
+			if (cmd_state < ERT_CMD_STATE_COMPLETED) {
+				/* It is old shell */
 				cmd_state = ERT_CMD_STATE_COMPLETED;
-				pkt->return_code = -ENODATA;//return code is missing
-			} else {//It is new zocl
+				pkt->return_code = -ENODATA;/* return code is missing */
+			} else {/* It is new shell like: xilinx-u30-gen3x4-base_1 */
 				pkt->return_code = tmp_pkt.return_code;
 			}
 		}
