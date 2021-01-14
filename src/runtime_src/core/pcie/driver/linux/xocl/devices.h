160c0b038cc7fb760801950fa0b7e7f99d01a9c6
David Zhang <davidzha@xilinx.com>
2021-01-14 13:10:02 -0800

    copyright 2021

--- src/runtime_src/core/pcie/driver/linux/xocl/devices.h
+++ src/runtime_src/core/pcie/driver/linux/xocl/devices.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2018-2020, Xilinx Inc
+ *  Copyright (C) 2018-2021, Xilinx Inc
  *
  *  This file is dual licensed.  It may be redistributed and/or modified
  *  under the terms of the Apache 2.0 License OR version 2 of the GNU
