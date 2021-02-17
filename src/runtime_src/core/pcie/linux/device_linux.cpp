857efc2c7c1437b1310dece6f54ca4bb08cee1bc
AShivangi <shivangiagarwal53@gmail.com>
2021-02-16 19:34:00 -0800

    mailbox metrics

--- src/runtime_src/core/pcie/linux/device_linux.cpp
+++ src/runtime_src/core/pcie/linux/device_linux.cpp
@@ -415,6 +415,7 @@ initialize_query_table()
   emplace_sysfs_getput<query::rp_program_status>             ("", "rp_program");
   emplace_sysfs_get<query::shared_host_mem>                  ("address_translator", "host_mem_size");
   emplace_sysfs_get<query::cpu_affinity>                     ("", "local_cpulist");
+  emplace_sysfs_get<query::mailbox_metrics>                  ("mailbox", "recv_metrics");
 
   emplace_func0_request<query::pcie_bdf,                     bdf>();
   emplace_func0_request<query::kds_cu_info,                  kds_cu_info>();
--- src/runtime_src/core/tools/xbmgmt2/SubCmdExamine.cpp
+++ src/runtime_src/core/tools/xbmgmt2/SubCmdExamine.cpp
@@ -38,9 +38,8 @@ namespace po = boost::program_options;
 #include "tools/common/Report.h"
 #include "tools/common/ReportHost.h"
 #include "tools/common/ReportFirewall.h"
-#include "tools/common/ReportElectrical.h"
 #include "tools/common/ReportMechanical.h"
-#include "tools/common/ReportThermal.h"
+#include "tools/common/ReportMailbox.h"
 #include "ReportPlatform.h"
 
 // Note: Please insert the reports in the order to be displayed (current alphabetical)
@@ -50,10 +49,9 @@ static const ReportCollection fullReportCollection = {
     std::make_shared<ReportPlatform>(),
   // Native only reports
   #ifdef ENABLE_NATIVE_SUBCMDS_AND_REPORTS
-    std::make_shared<ReportElectrical>(),
     std::make_shared<ReportMechanical>(),
     std::make_shared<ReportFirewall>(),
-    std::make_shared<ReportThermal>(),
+    std::make_shared<ReportMailbox>()
   #endif
 };
 
