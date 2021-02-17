b22e6dfbe33f2aa00ac81cddd14958c812b6f482
larry9523 <larry9523@gmail.com>
2021-02-17 09:51:51 -0800

    NSPR-9558 Reconcile MEM TOPOLOGY for U30/VCK5000 (#4739)
    * NSPR-9558 Reconcile MEM TOPOLOGY for U30/VCK5000 between device (zocl) and host (xocl)

--- src/runtime_src/core/pcie/driver/linux/xocl/xocl_drm.h
+++ src/runtime_src/core/pcie/driver/linux/xocl/xocl_drm.h
@@ -25,6 +25,8 @@
 typedef void (*xocl_execbuf_callback)(unsigned long data, int error);
 
 #define IS_HOST_MEM(m_tag)	(!strncmp(m_tag, "HOST[0]", 7))
+#define IS_PLRAM(m_tag)		(!strncmp(m_tag, "PLRAM[", 6))
+
 /**
  * struct drm_xocl_exec_metadata - Meta data for exec bo
  *
