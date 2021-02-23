f7f369260cdf80bf5156a0f32dfb31169b72b761
Min Ma <min.ma@xilinx.com>
2021-02-23 12:56:41 -0800

    memcpy is not stable (#4777)

--- src/runtime_src/ert/scheduler/scheduler_v30.cpp
+++ src/runtime_src/ert/scheduler/scheduler_v30.cpp
@@ -638,7 +638,10 @@ configure_cu(addr_type cu_addr, addr_type regmap_addr, size_type regmap_size)
   uint32_t *addr_ptr = (uint32_t *)(uintptr_t)cu_addr;
   uint32_t *regmap_ptr = (uint32_t *)(uintptr_t)regmap_addr;
 
-  memcpy(addr_ptr+4, regmap_ptr+4, (regmap_size-4)<<2);
+  //memcpy(addr_ptr+4, regmap_ptr+4, (regmap_size-4)<<2);
+  for (int i = 4; i < regmap_size; i++)
+    *(addr_ptr + i) = *(regmap_ptr + i);
+
 #endif
   // start kernel at base + 0x0
   write_reg(cu_addr, 0x1);
