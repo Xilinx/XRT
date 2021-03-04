527ced4a6e394bd09dcd0e4f586b530abe37242f
Chien-Wei Lan <40216861+chienwei-lan@users.noreply.github.com>
2021-03-04 11:03:11 -0800

    Mb sched off chk (#4841)
    * Don't probe ert if XOCL_DSAFLAG_MB_SCHE_OFF is set

--- src/runtime_src/core/pcie/driver/linux/xocl/subdev/ert_user.c
+++ src/runtime_src/core/pcie/driver/linux/xocl/subdev/ert_user.c
@@ -1303,6 +1303,11 @@ static int ert_user_probe(struct platform_device *pdev)
 	int err = 0;
 	xdev_handle_t xdev = xocl_get_xdev(pdev);
 	struct xocl_ert_sched_privdata *priv = NULL;
+	bool ert = xocl_mb_sched_on(xdev);
+
+	/* If XOCL_DSAFLAG_MB_SCHE_OFF is set, we should not probe ert */
+	if (!ert)
+		return -ENODEV;
 
 	ert_user = xocl_drvinst_alloc(&pdev->dev, sizeof(struct xocl_ert_user));
 	if (!ert_user)
