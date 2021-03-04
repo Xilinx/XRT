04f0f4aba49e1c5ee4b0f97bf2838a8975328e2e
Sonal Santan <sonal.santan@xilinx.com>
2021-03-04 04:49:29 -0800

    Update documentation on clang-tidy usage

--- src/CMake/lint.cmake
+++ src/CMake/lint.cmake
@@ -1,6 +1,8 @@
 
 # Clang Tidy ia integrated into CMake since 3.7.2 and is automatically run if CMAKE_CXX_CLANG_TIDY
-# is set. In future we should refine the checks run using clang-tidy's config files.
+# is set. We currently rely on a global .clang-tidy placed inside "src" directory that is automatically
+# found by clang-tidy. In future we should refine the checks run for specific directories using
+# custom .clang-tidy config files in those directories.
 
 IF((${CMAKE_VERSION} VERSION_GREATER "3.7.2") AND (XRT_CLANG_TIDY STREQUAL "ON"))
   find_program(CLANG_TIDY clang-tidy)
