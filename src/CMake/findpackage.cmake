f51eecb03ef97887925da12caf7c90dc4bb9feb9
Soren Soe <soren.soe@xilinx.com>
2021-03-05 08:47:27 -0800

    Adjust exporting for windows

--- src/CMake/findpackage.cmake
+++ src/CMake/findpackage.cmake
@@ -33,15 +33,16 @@ install (
 # This will generate a file that details all targets we have marked for export
 # as part of the xrt-targets export group
 # It will provide information such as the library file names and locations post install
+if (NOT WIN32)
 install(
   EXPORT xrt-targets
   NAMESPACE ${PROJECT_NAME}::
   DESTINATION ${XRT_INSTALL_DIR}/share/cmake/${PROJECT_NAME}
   )
+endif()
 
 install(
   EXPORT xrt-dev-targets
   NAMESPACE ${PROJECT_NAME}::
   DESTINATION ${XRT_INSTALL_DIR}/share/cmake/${PROJECT_NAME}
   )
-
