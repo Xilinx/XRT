d743b422f0f67c43e6577375d2e91b14f267779f
Himanshu Choudhary <hchoudha@xilinx.com>
2021-03-10 16:34:20 +0530

    remove goto

--- src/runtime_src/core/edge/user/shim.cpp
+++ src/runtime_src/core/edge/user/shim.cpp
@@ -1546,19 +1546,19 @@ xclProbe()
 
   int result = ioctl(fd, DRM_IOCTL_VERSION, &version);
   if (result) {
-    result = 0;
-    goto out;
+    close(fd);
+    delete []version.name;
+    delete []version.desc;
+    delete []version.date;
+    return 0;
   }
 
   result = std::strncmp(version.name, "zocl", 4);
-
-  result = ((result == 0) ? 1 : 0);
-out:
   close(fd);
   delete []version.name;
   delete []version.desc;
   delete []version.date;
-  return result;
+  return (result == 0) ? 1 : 0;
 }
 #endif
 
