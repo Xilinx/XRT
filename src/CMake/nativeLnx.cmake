e1aa014e185dba249fbffe070f57983744ba7fa9
Soren Soe <soren.soe@xilinx.com>
2021-03-18 15:17:00 -0700

    Update static boost build for later versions of boost
    % boost.sh -prefix <dir>/boost-1.75.0 boost-1.75.0
    % env XRT_BOOST_INSTALL=<dir>/boost-1.75.0/xrt build/build.sh -opt
    
    Above ensures that XRT shared libaries are linked with static
    boost libraries and all boost symbols are resolved.
    
    This PR fixes a few build errors when using latest version of boost.
    It also adds support for static linking of boost on aarch64 (arm64).
    Finally, the PR ensures that local version of boost can be used when
    static tool build of XRT is enabled.

--- src/CMake/nativeLnx.cmake
+++ src/CMake/nativeLnx.cmake
@@ -82,6 +82,18 @@ if (DEFINED ENV{XRT_BOOST_INSTALL})
   find_package(Boost
     HINTS $ENV{XRT_BOOST_INSTALL}
     REQUIRED COMPONENTS system filesystem program_options)
+
+  # A bug in FindBoost maybe?  Doesn't set Boost_LIBRARY_DIRS when
+  # Boost install has only static libraries. For static tool linking
+  # this variable is needed in order for linker to locate the static
+  # libraries.  Another bug in FindBoost fails to find static
+  # libraries when shared ones are present too.
+  if (Boost_FOUND AND "${Boost_LIBRARY_DIRS}" STREQUAL "")
+    set (Boost_LIBRARY_DIRS $ENV{XRT_BOOST_INSTALL}/lib)
+  endif()
+
+  # Some later versions of boost spews warnings form property_tree
+  add_compile_options("-DBOOST_BIND_GLOBAL_PLACEHOLDERS")
 else()
   find_package(Boost 
     REQUIRED COMPONENTS system filesystem program_options)
