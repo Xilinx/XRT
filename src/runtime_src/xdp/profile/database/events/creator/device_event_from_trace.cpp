16bf82f411bb7172d9dea5f2e2de92e804b908c5
Ishita Ghosh <43157456+IshitaGhosh@users.noreply.github.com>
2021-03-24 07:56:09 -0700

    Add approximation for missing end events on ASM connection based on CU end event (#4959)

--- src/runtime_src/xdp/profile/database/events/creator/device_event_from_trace.cpp
+++ src/runtime_src/xdp/profile/database/events/creator/device_event_from_trace.cpp
@@ -19,6 +19,13 @@
 #include "xdp/profile/database/events/creator/device_event_from_trace.h"
 #include "xdp/profile/plugin/vp_base/utility.h"
 
+#include "core/common/message.h"
+
+#ifdef _WIN32
+#pragma warning (disable : 4244)
+/* Disable warnings for conversion from uint32_t to uint16_t */
+#endif
+
 namespace xdp {
 
   DeviceEventCreatorFromTrace::DeviceEventCreatorFromTrace(uint64_t devId)
@@ -370,7 +377,8 @@ namespace xdp {
       if(0 == cuLastTimestamp) {
         continue; // nothing to do? what about unmatched start?
       }
-      // Warning : "Incomplete CU profile trace detected. Timeline trace will have approximate CU End."
+      const char* msg = "Incomplete CU profile trace detected. Timeline trace will have approximate CU End.";
+      xrt_core::message::send(xrt_core::message::severity_level::warning, "XRT", msg) ;
 
       // end event
       cuStarts[amIndex].pop_front();
@@ -379,6 +387,76 @@ namespace xdp {
       KernelEvent* event = new KernelEvent(cuStartEvent->getEventId(), hostTimestamp, KERNEL, deviceId, amIndex, cuId);
       event->setDeviceTimestamp(cuLastTimestamp);
       db->getDynamicInfo().addEvent(event); 
+    }
+
+    // Find unfinished ASM events
+    bool unfinishedASMevents = false;
+    for(uint64_t asmIndex = 0; asmIndex < (db->getStaticInfo()).getNumASM(deviceId, xclbin); ++asmIndex) {
+      uint64_t asmTraceID = asmIndex + MIN_TRACE_ID_ASM;
+      Monitor* mon  = db->getStaticInfo().getASMonitor(deviceId, xclbin, asmIndex);
+      if(!mon) {
+        continue;
+      }
+      int32_t  cuId = mon->cuIndex;
+      int32_t  amId = -1;
+      uint64_t cuLastTimestamp = 0, asmAppxLastTransTimeStamp = 0;
+      if(-1 != cuId) {
+        ComputeUnitInstance* cu = db->getStaticInfo().getCU(deviceId, cuId);
+        if(cu) {
+          amId = cu->getAccelMon();
+        }
+        if(-1 != amId) {
+          cuLastTimestamp  = amLastTrans[amId];
+        }
+      }
+
+      VTFEventType streamEventType = (mon->isRead) ? KERNEL_STREAM_READ : KERNEL_STREAM_WRITE;
+      addApproximateStreamEndEvent(asmIndex, asmTraceID, streamEventType, cuId, amId, cuLastTimestamp, asmAppxLastTransTimeStamp, unfinishedASMevents);
+
+      streamEventType = (mon->isRead) ? KERNEL_STREAM_READ_STALL : KERNEL_STREAM_WRITE_STALL;
+      addApproximateStreamEndEvent(asmIndex, asmTraceID, streamEventType, cuId, amId, cuLastTimestamp, asmAppxLastTransTimeStamp, unfinishedASMevents);
+
+      streamEventType = (mon->isRead) ? KERNEL_STREAM_READ_STARVE : KERNEL_STREAM_WRITE_STARVE;
+      addApproximateStreamEndEvent(asmIndex, asmTraceID, streamEventType, cuId, amId, cuLastTimestamp, asmAppxLastTransTimeStamp, unfinishedASMevents);
+
+      asmLastTrans[asmIndex] = asmAppxLastTransTimeStamp;
+    }
+
+    if(unfinishedASMevents) {
+      const char* msg = "Found unfinished events on Stream connections. Adding approximate ends for Stream Activity/Stall/Starve on timeline trace.";
+      xrt_core::message::send(xrt_core::message::severity_level::warning, "XRT", msg) ;
+    }
+  }
+
+  void DeviceEventCreatorFromTrace::addApproximateStreamEndEvent(uint64_t asmIndex, uint64_t asmTraceID, VTFEventType streamEventType, 
+                                                                 int32_t cuId, int32_t  amId, uint64_t cuLastTimestamp,
+                                                                 uint64_t &asmAppxLastTransTimeStamp, bool &unfinishedASMevents)
+  {
+    uint64_t asmStartTimestamp = 0, asmAppxEndTimestamp = 0;
+    double   asmAppxEndHostTimestamp = 0;
+    double   halfCycleTimeInMs = (0.5/traceClockRateMHz)/1000.0;
+
+    VTFEvent* matchingStart = db->getDynamicInfo().matchingDeviceEventStart(asmTraceID, streamEventType);
+    while(matchingStart) {
+      unfinishedASMevents = true;
+      asmStartTimestamp = (dynamic_cast<VTFDeviceEvent*>(matchingStart))->getDeviceTimestamp();
+      if(-1 == amId) {
+        // For floating ASM i.e. ASM not attached to any CU or for ASMs attached to free running CUs which don't have AM attached
+        asmAppxEndTimestamp = asmStartTimestamp;
+        asmAppxEndHostTimestamp = convertDeviceToHostTimestamp(asmStartTimestamp) + halfCycleTimeInMs;
+      } else {
+        asmAppxEndTimestamp = (asmStartTimestamp < cuLastTimestamp) ? cuLastTimestamp : asmStartTimestamp;
+        asmAppxEndHostTimestamp = (asmStartTimestamp < cuLastTimestamp) ? convertDeviceToHostTimestamp(cuLastTimestamp) : (convertDeviceToHostTimestamp(asmStartTimestamp) + halfCycleTimeInMs);
+      }
+      asmAppxLastTransTimeStamp = (asmAppxLastTransTimeStamp < asmAppxEndTimestamp) ? asmAppxEndTimestamp : asmAppxLastTransTimeStamp;
+
+      // Add approximate end event
+      DeviceStreamAccess* strmEvent = new DeviceStreamAccess(matchingStart->getEventId(), asmAppxEndHostTimestamp,
+                                                           streamEventType, deviceId, asmIndex, cuId);
+      strmEvent->setDeviceTimestamp(asmAppxEndTimestamp);
+      db->getDynamicInfo().addEvent(strmEvent);
+
+      matchingStart = db->getDynamicInfo().matchingDeviceEventStart(asmTraceID, streamEventType);
     }
   }
 
--- src/runtime_src/xdp/profile/database/events/creator/device_event_from_trace.h
+++ src/runtime_src/xdp/profile/database/events/creator/device_event_from_trace.h
@@ -61,6 +61,10 @@ class DeviceEventCreatorFromTrace
 
   void addKernelDataTransferEvent(VTFEventType ty, xclTraceResults& trace, uint32_t slot, int32_t cuId, double hostTimestamp) ;
 
+  void addApproximateStreamEndEvent(uint64_t asmIndex, uint64_t asmTraceID, VTFEventType streamEventType,
+                                    int32_t cuId, int32_t  amId, uint64_t cuLastTimestamp,
+                                    uint64_t &asmAppxLastTransTimeStamp, bool &unfinishedASMevents);
+
   public :
   XDP_EXPORT DeviceEventCreatorFromTrace(uint64_t devId);
   ~DeviceEventCreatorFromTrace() {}
