fa36bcc09cbad0730f2be261a6fe217dc74a5d0f
Soren Soe <soren.soe@xilinx.com>
2021-03-26 18:46:18 -0700

    Update public test include paths for moved headers

--- tests/validate/xrt_iops_test/src/xrt_api_iops.cpp
+++ tests/validate/xrt_iops_test/src/xrt_api_iops.cpp
@@ -8,9 +8,9 @@
 #include "cmdlineparser.h"
 
 #include "xilutil.hpp"
-#include "experimental/xrt_device.h"
-#include "experimental/xrt_bo.h"
-#include "experimental/xrt_kernel.h"
+#include "xrt/xrt_device.h"
+#include "xrt/xrt_bo.h"
+#include "xrt/xrt_kernel.h"
 
 using ms_t = std::chrono::microseconds;
 using Clock = std::chrono::high_resolution_clock;
--- tests/xrt/00_hello/main.cpp
+++ tests/xrt/00_hello/main.cpp
@@ -20,9 +20,9 @@
 #include <vector>
 #include <cstdlib>
 
-#include "experimental/xrt_device.h"
-#include "experimental/xrt_kernel.h"
-#include "experimental/xrt_bo.h"
+#include "xrt/xrt_device.h"
+#include "xrt/xrt_kernel.h"
+#include "xrt/xrt_bo.h"
 #include "experimental/xrt_ini.h"
 /**
  * Trivial loopback example which runs OpenCL loopback kernel. Does not use OpenCL
