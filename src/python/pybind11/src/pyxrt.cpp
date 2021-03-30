5425c2e78eca1d0a78f8b3baffa23c87504be8df
Sonal Santan <sonal.santan@xilinx.com>
2021-03-29 23:13:01 -0700

    Update 04_swizzle to use the new pyxrt

--- src/python/pybind11/src/pyxrt.cpp
+++ src/python/pybind11/src/pyxrt.cpp
@@ -1,17 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 OR Apache-2.0 */
+
 /**
- * Copyright (C) 2016-2021 Xilinx, Inc
+ * Pybind11 module for XRT C++ APIs
  *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may
- * not use this file except in compliance with the License. A copy of the
- * License is located at
+ * Copyright (C) 2019-2021 Xilinx, Inc
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
+ * Authors: graham.schelle@xilinx.com
+ *          sonal.santan@xilinx.com
  */
 
 
@@ -169,6 +164,7 @@ pybo.def(py::init<xrt::device, size_t, xrt::bo::flags, xrt::memory_group>())
     .def("sync", ([](xrt::bo &b, xclBOSyncDirection dir, size_t size, size_t offset)  {
                       b.sync(dir, size, offset);
                   }))
+    .def("map", static_cast<void *(xrt::bo::*)()>(&xrt::bo::map))
     .def("size", &xrt::bo::size)
     .def("address", &xrt::bo::address)
     ;
