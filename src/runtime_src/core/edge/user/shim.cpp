b5d5c9ea61870369071d13b8efa04e52f06e6d0a
ch vamshi krishna <chvamshi@xilinx.com>
2021-04-08 15:13:50 +0530

    use same logic to find the ip index between edge vs dc

--- src/runtime_src/core/edge/user/shim.cpp
+++ src/runtime_src/core/edge/user/shim.cpp
@@ -938,7 +938,10 @@ xclIPName2Index(const char *name)
     return -EINVAL;
   }
   if (buf.empty())
+  {
+    xclLog(XRT_ERROR, "ip_layout sysfs node is empty");
     return -ENOENT;
+  }
 
   const ip_layout *map = (ip_layout *)buf.data();
   if(map->m_count < 0) {
@@ -957,32 +960,23 @@ xclIPName2Index(const char *name)
   }
 
   if (i == map->m_count)
+  {
+    xclLog(XRT_ERROR, "no ip with %s found in ip_layout", name);
     return -ENOENT;
+  }
+
   if (addr == bad_addr)
     return -EINVAL;
 
-  std::vector<std::string> custat;
-  mDev->sysfs_get("kds_custat", errmsg, custat);
-  if (!errmsg.empty()) {
-    xclLog(XRT_ERROR, "can't read kds_custat sysfs node: %s",
-           errmsg.c_str());
-    return -EINVAL;
+  auto cus = xrt_core::xclbin::get_cus(map);
+  auto itr = std::find(cus.begin(), cus.end(), addr);
+  if (itr == cus.end())
+  {
+    xclLog(XRT_ERROR, "no cu with 0x%lx found", addr);
+    return -ENOENT;
   }
+  return std::distance(cus.begin(),itr);
 
-  uint32_t idx = 0;
-  for (auto& line : custat) {
-    // convert and compare parsed hex address CU[@0x[0-9]+]
-    size_t pos = line.find("0x");
-    if (pos == std::string::npos)
-      continue;
-    if (static_cast<unsigned long>(addr) ==
-        std::stoul(line.substr(pos).c_str(), 0, 16)) {
-      return idx;
-    }
-    ++idx;
-  }
-
-  return -ENOENT;
 }
 
 int
