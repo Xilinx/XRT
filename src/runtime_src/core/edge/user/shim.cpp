fff0682d7171afabb9d3f0bb740e95545185f18f
Ch Vamshi Krishna <40261882+chvamshi-xilinx@users.noreply.github.com>
2021-04-15 17:11:43 +0530

    build fix (#5107) (#5109)
    (cherry picked from commit f66170a392cda840db8407be79943ebe5ffbb554)
    
    Co-authored-by: Himanshu-xilinx <48508106+Himanshu-xilinx@users.noreply.github.com>

--- src/runtime_src/core/edge/user/shim.cpp
+++ src/runtime_src/core/edge/user/shim.cpp
@@ -2322,6 +2322,12 @@ xclUpdateSchedulerStat(xclDeviceHandle handle)
 }
 
 int
+xclInternalResetDevice(xclDeviceHandle handle, xclResetKind kind)
+{
+  return -ENOSYS;
+}
+
+int
 xclErrorInject(xclDeviceHandle handle, uint16_t num, uint16_t driver, uint16_t severity, uint16_t module, uint16_t eclass)
 {
   ZYNQ::shim *drv = ZYNQ::shim::handleCheck(handle);
