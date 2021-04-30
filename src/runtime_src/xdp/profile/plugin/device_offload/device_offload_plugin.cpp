e2cfeb01c6a24c6aed9638238fe1bf6554ae2276
Anurag Dubey <hackwa@users.noreply.github.com>
2021-04-30 09:43:18 -0600

    print circular buffer warning only at the end and use user markers (#5179)

--- src/runtime_src/xdp/profile/plugin/device_offload/device_offload_plugin.cpp
+++ src/runtime_src/xdp/profile/plugin/device_offload/device_offload_plugin.cpp
@@ -28,6 +28,7 @@
 
 #include "core/common/config_reader.h"
 #include "core/common/message.h"
+#include "experimental/xrt_profile.h"
 
 // Anonymous namespace for helper functions
 namespace {
@@ -356,11 +357,24 @@ namespace xdp {
     if (!(getFlowMode() == HW))
       return;
 
-    if (offloader->has_fifo() && offloader->trace_buffer_full())
+    if (offloader->has_fifo() && offloader->trace_buffer_full()) {
       xrt_core::message::send(xrt_core::message::severity_level::warning, "XRT", FIFO_WARN_MSG);
+      xrt::profile::user_event events;
+      events.mark("Trace FIFO Full");
+    }
 
-    if (offloader->has_ts2mm() && offloader->trace_buffer_full())
-      xrt_core::message::send(xrt_core::message::severity_level::warning, "XRT", TS2MM_WARN_MSG_BUF_FULL);
+    if (offloader->has_ts2mm()) {
+      if (offloader->trace_buffer_full()) {
+        xrt_core::message::send(xrt_core::message::severity_level::warning, "XRT", TS2MM_WARN_MSG_BUF_FULL);
+        xrt::profile::user_event events;
+        events.mark("Trace Buffer Full");
+      }
+      if (offloader->circular_buffer_overwrite_detected()) {
+        xrt_core::message::send(xrt_core::message::severity_level::warning, "XRT", TS2MM_WARN_MSG_CIRC_BUF_OVERWRITE);
+        xrt::profile::user_event events;
+        events.mark("Trace Buffer Overwrite Detected");
+      }
+    }
   }
 
   void DeviceOffloadPlugin::broadcast(VPDatabase::MessageType msg, void* /*blob*/)
