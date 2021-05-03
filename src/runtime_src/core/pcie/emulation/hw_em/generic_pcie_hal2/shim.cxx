179b30364a90ceb9fd2afc2c99ca478c6d558439
Aleti Venkata Prasad <venkatp@xilinx.com>
2021-05-03 22:09:21 +0530

    CR-1096474 Remove the GDB debug_mode option as the support is discontinued in Vitis HLS

--- src/runtime_src/core/pcie/emulation/hw_em/generic_pcie_hal2/shim.cxx
+++ src/runtime_src/core/pcie/emulation/hw_em/generic_pcie_hal2/shim.cxx
@@ -846,10 +846,10 @@ namespace xclhwemhal2 {
         setenv("VITIS_LAUNCH_WAVEFORM_BATCH", "1", true);
       }
 
-      if (lWaveform == xclemulation::DEBUG_MODE::GDB) {
+      /*if (lWaveform == xclemulation::DEBUG_MODE::GDB) {
         sim_path = binaryDirectory + "/behav_gdb/" + simulatorType;
         setSimPath(sim_path);
-      }
+      }*/
 
       if (userSpecifiedSimPath.empty() == false)
       {
@@ -861,11 +861,12 @@ namespace xclhwemhal2 {
       {
         if (sim_path.empty())
         {
-          sim_path = binaryDirectory + "/behav_gdb/" + simulatorType;
+          sim_path = binaryDirectory + "/behav_waveform/" + simulatorType;
           setSimPath(sim_path);
         }
 
-        if (boost::filesystem::exists(sim_path) == false)
+        // As GDB feature is unsupported for 2021.1, we removed this cross check. We will re-enable it once we have 2 possibilities
+        /*if (boost::filesystem::exists(sim_path) == false)
         {
           if (lWaveform == xclemulation::DEBUG_MODE::GDB) {
             sim_path = binaryDirectory + "/behav_waveform/" + simulatorType;
@@ -905,7 +906,7 @@ namespace xclhwemhal2 {
 
             logMessage(dMsg, 0);
           }
-        }
+        }*/
       }
 
       if (mLogStream.is_open())
