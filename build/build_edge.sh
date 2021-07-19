33c9617cb5f1fba7a8edd75381bc6f11fb096b90
Ch Vamshi Krishna <40261882+chvamshi-xilinx@users.noreply.github.com>
2021-07-19 21:30:20 +0530

    Added -full option in build_edge script to run full petalinux build which generates Images aswell (#5498)
    * added -full option in this script to run full petalinux build which generates images aswell
    
    * moving to 2021.2 petalinux also

--- build/build_edge.sh
+++ build/build_edge.sh
@@ -15,6 +15,7 @@ usage()
     echo "          -cache                          path to sstate-cache"
     echo "          -setup                          setup file to use"
     echo "          -clean, clean                   Remove build directories"
+    echo "          -full, full                     Full Petalinux build which builds images along with XRT RPMs"
     echo ""
 }
 
@@ -88,6 +89,7 @@ PROJ_NAME=""
 PLATFROM=""
 XRT_REPO_DIR=`readlink -f ${THIS_SCRIPT_DIR}/..`
 clean=0
+full=0
 SSTATE_CACHE=""
 SETTINGS_FILE="petalinux.build"
 while [ $# -gt 0 ]; do
@@ -105,6 +107,9 @@ while [ $# -gt 0 ]; do
 			;;
 		-clean | clean )
 			clean=1
+			;;
+		-full | full )
+			full=1
 			;;
 		-cache )
                         shift
@@ -191,12 +196,11 @@ if [ ! -d $PETALINUX_NAME ]; then
     echo " * Create PetaLinux Project: $PETALINUX_NAME"
     echo "[CMD]: petalinux-create -t project -n $PETALINUX_NAME $PETA_CREATE_OPT"
     $PETA_BIN/petalinux-create -t project -n $PETALINUX_NAME $PETA_CREATE_OPT
+    cd ${PETALINUX_NAME}/project-spec/meta-user/
+    install_recipes .
 else
-    error " * PetaLinux Project existed: $PETALINUX_NAME."
+  echo "INFO: Project Already exists on Disk. Running incremental build"
 fi
-
-cd ${PETALINUX_NAME}/project-spec/meta-user/
-install_recipes .
 
 cd $ORIGINAL_DIR/$PETALINUX_NAME
 
@@ -213,8 +217,21 @@ fi
 
 # Build package
 echo " * Performing PetaLinux Build (from: ${PWD})"
-echo "[CMD]: petalinux-build -c xrt"
-$PETA_BIN/petalinux-build -c xrt
+#Run a full build if -full option is provided
+if [[ $full == 1 ]]; then
+  # remove following unused packages from rootfs sothat its size would fit in QSPI
+  sed -i 's/^CONFIG_packagegroup-petalinux-opencv.*//g' project-spec/configs/rootfs_config
+  sed -i 's/^CONFIG_packagegroup-petalinux-jupyter.*//g' project-spec/configs/rootfs_config
+  sed -i 's/^CONFIG_kernel-devsrc.*//g' project-spec/configs/rootfs_config
+  sed -i 's/^CONFIG_xrt-dev.*//g' project-spec/configs/rootfs_config
+  echo "[CMD]: petalinux-build"
+  $PETA_BIN/petalinux-build
+else
+#Run just xrt build if -full option is not provided
+  echo "[CMD]: petalinux-build -c xrt"
+  $PETA_BIN/petalinux-build -c xrt
+fi
+
 if [ $? != 0 ]; then
    error "XRT build failed"
 fi
