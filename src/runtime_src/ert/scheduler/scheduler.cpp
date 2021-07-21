67f01589511ede72edba891db57d7cc431fcb4e9
Min Ma <min.ma@xilinx.com>
2021-07-21 14:02:53 -0700

    CR-1101219 Implementation between Edge/DC shouldn't be different for EXEC_WRITE opcode (#5507)
    * remove reserved 6 words for EXEC_WRITE command
    
    * on DC, convert EXEC_WRITE cmd to START_KEY_VAL to not break existed apps

--- src/runtime_src/ert/scheduler/scheduler.cpp
+++ src/runtime_src/ert/scheduler/scheduler.cpp
@@ -649,8 +649,7 @@ inline void
 configure_cu_ooo(addr_type cu_addr, addr_type regmap_addr, size_type regmap_size)
 {
   // write register map addr, value pairs starting 
-  // past reserved 4 ctrl + 2 ctx 
-  for (size_type idx = 6; idx < regmap_size; idx += 2) {
+  for (size_type idx = 0; idx < regmap_size; idx += 2) {
     addr_type offset = read_reg(regmap_addr + (idx << 2));
     value_type value = read_reg(regmap_addr + ((idx + 1) << 2));
     write_reg(cu_addr + offset, value);
