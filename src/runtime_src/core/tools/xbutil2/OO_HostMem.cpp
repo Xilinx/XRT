907bf563456fc09ae81e8c3fc1146dd0a0627e84
AShivangi <shivangiagarwal53@gmail.com>
2021-08-06 14:54:16 -0700

    add files

---
+++ src/runtime_src/core/tools/xbutil2/OO_HostMem.cpp
@@ -0,0 +1,169 @@
+/**
+ * Copyright (C) 20211 Licensed under the Apache License, Version
+ * 2.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is located
+ * at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+// ------ I N C L U D E   F I L E S -------------------------------------------
+// Local - Include Files
+#include "OO_HostMem.h"
+#include "tools/common/XBUtilities.h"
+
+// 3rd Party Library - Include Files
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string.hpp>
+namespace po = boost::program_options;
+
+namespace {
+bool
+string2action(std::string str)
+{
+  std::transform(str.begin(), str.end(), str.begin(),
+                 [](auto c) { return static_cast<char>(std::tolower(c)); });
+
+  if (str == "enable")
+    return true;
+  else if (str == "disable")
+    return false;
+  else
+    throw xrt_core::generic_error(EINVAL, "Invalid host-mem action '" + str + "'");
+}
+
+void
+host_mem(xrt_core::device* device, bool action, uint64_t size)
+{
+  XBUtilities::sudo_or_throw("Root privileges required to enable host-mem");
+  device->set_cma(action, size); //can throw
+}
+
+} //end namespace 
+
+// ----- C L A S S   M E T H O D S -------------------------------------------
+
+OO_HostMem::OO_HostMem( const std::string &_longName, bool _isHidden )
+    : OptionOptions(_longName, _isHidden, "Controls host-mem functionality")
+    , m_devices({})
+    , m_action("")
+    , m_size("")
+    , m_help(false)
+{
+  m_optionsDescription.add_options()
+    ("device,d", boost::program_options::value<decltype(m_devices)>(&m_devices)->multitoken(), "The Bus:Device.Function (e.g., 0000:d8:00.0) device of interest")
+    ("action", boost::program_options::value<decltype(m_action)>(&m_action)->required(), "Action to perform: ENABLE or DISABLE")
+    ("size,s", boost::program_options::value<decltype(m_size)>(&m_size), "Size of host memory to be enabled (e.g. 256M, 1G)")
+    ("help,h", boost::program_options::bool_switch(&m_help), "Help to use this sub-command")
+  ;
+
+  m_positionalOptions.
+    add("action", 1 /* max_count */)
+  ;
+}
+
+void
+OO_HostMem::execute(const SubCmdOptions& _options) const
+{
+  XBUtilities::verbose("SubCommand option: Host Mem");
+
+  XBUtilities::verbose("Option(s):");
+  for (auto & aString : _options)
+    XBUtilities::verbose(std::string(" ") + aString);
+
+  // Honor help option first
+  if (std::find(_options.begin(), _options.end(), "--help") != _options.end()) {
+    printHelp();
+    return;
+  }
+
+  // Parse sub-command ...
+  po::variables_map vm;
+
+  try {
+    po::store(po::command_line_parser(_options).options(m_optionsDescription).positional(m_positionalOptions).run(), vm);
+    po::notify(vm); // Can throw
+  }
+  catch (po::error& e) {
+    std::cerr << "ERROR: " << e.what() << "\n\n";
+
+    printHelp();
+    return;
+  }
+
+  // Exit if neither action or device specified
+  if(m_help || (m_action.empty() || m_devices.empty())) {
+    printHelp();
+    return;
+  }
+
+  bool enable = false;
+  try {
+    uint64_t size = 0;
+    if(!m_size.empty()) {
+      uint64_t bytes = 0;
+      auto units = m_size.substr(m_size.length()-1);
+      boost::to_upper(units);
+      if(units.compare("M") == 0)
+        bytes = 1024;
+      else if(units.compare("G") ==0)
+        bytes = 1024*1024;
+      else
+        throw xrt_core::error(std::errc::invalid_argument, "Please specify a valid size unit (M|G), eg: `256M`");
+
+      try {
+        size = std::stoll(m_size.substr(0, m_size.length()-1));
+      } 
+      catch (const std::exception& ex) {
+        //out of range, invalid argument ex
+        throw xrt_core::error(std::errc::invalid_argument, "Value supplied to --size option is invalid");
+      }
+      size = size*bytes;
+    }
+
+    enable = string2action(m_action);
+    // Exit if ENABLE action is specified but size is not
+    if(enable && size == 0)
+      throw xrt_core::error(std::errc::invalid_argument, "Please specify the memory size for enablement");
+
+    // Collect all of the devices of interest
+    std::set<std::string> deviceNames;
+    xrt_core::device_collection deviceCollection;
+    for (const auto & deviceName : m_devices) 
+      deviceNames.insert(boost::algorithm::to_lower_copy(deviceName));
+    
+    XBUtilities::collect_devices(deviceNames, true /*inUserDomain*/, deviceCollection);
+
+    // enforce 1 device specification
+    if(deviceCollection.size() > 1) {
+      std::stringstream errmsg;
+      errmsg << "Multiple devices are not supported. Please specify a single device using --device option\n\n";
+      errmsg << "List of available devices:\n";
+      boost::property_tree::ptree available_devices = XBUtilities::get_available_devices(true);
+      for(auto& kd : available_devices) {
+        boost::property_tree::ptree& _dev = kd.second;
+        errmsg << boost::format("  [%s] : %s\n") % _dev.get<std::string>("bdf") % _dev.get<std::string>("vbnv");
+      }
+      throw xrt_core::error(std::errc::operation_canceled, errmsg.str());
+    }
+
+    //Set host-mem
+    host_mem(deviceCollection[0].get(), enable, size);
+  } 
+  catch(const xrt_core::error& e) {
+    std::cerr << boost::format("\nERROR: %s\n") % e.what();
+    return;
+  }
+  catch (const std::runtime_error& e) {
+    std::cerr << boost::format("\nERROR: %s\n") % e.what();
+    return;
+  }
+
+  std::cout << boost::format("\nHost-mem %s successfully\n") % (enable ? "enabled" : "disabled");
+}
---
+++ src/runtime_src/core/tools/xbutil2/SubCmdConfigure.cpp
@@ -0,0 +1,134 @@
+/**
+ * Copyright (C) 2021 Xilinx, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may
+ * not use this file except in compliance with the License. A copy of the
+ * License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+// ------ I N C L U D E   F I L E S -------------------------------------------
+// Local - Include Files
+#include "SubCmdConfigure.h"
+#include "OO_HostMem.h"
+
+#include "common/system.h"
+#include "common/device.h"
+
+// 3rd Party Library - Include Files
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+// System - Include Files
+#include <iostream> 
+
+#include "common/system.h"
+#include "common/device.h"
+#include <boost/format.hpp>
+#include <map>
+
+// ----- C L A S S   M E T H O D S -------------------------------------------
+
+SubCmdConfigure::SubCmdConfigure(bool _isHidden, bool _isDepricated, bool _isPreliminary)
+    : SubCmd("configure", 
+             "Device and host configuration")
+{
+  const std::string longDescription = "Device and host configuration.";
+  setLongDescription(longDescription);
+  setExampleSyntax("");
+  setIsHidden(_isHidden);
+  setIsDeprecated(_isDepricated);
+  setIsPreliminary(_isPreliminary);
+}
+
+
+void
+SubCmdConfigure::execute(const SubCmdOptions& _options) const
+{
+  // -- Common top level options ---
+  bool help = false;
+
+  po::options_description commonOptions("Common Options"); 
+  commonOptions.add_options()
+    ("help,h", boost::program_options::bool_switch(&help), "Help to use this sub-command")
+  ;
+
+  po::options_description hiddenOptions("Hidden Options"); 
+
+  // -- Define the supporting option options ----
+  SubOptionOptions subOptionOptions;
+  subOptionOptions.emplace_back(std::make_shared<OO_HostMem>("host-mem"));
+
+  for (auto & subOO : subOptionOptions) {
+    if (subOO->isHidden()) 
+      hiddenOptions.add_options()(subOO->longName().c_str(), subOO->description().c_str());
+    else
+      commonOptions.add_options()(subOO->longName().c_str(), subOO->description().c_str());
+    subOO->setExecutable(getExecutableName());
+    subOO->setCommand(getName());
+  }
+
+  po::options_description allOptions("All Options");
+  allOptions.add(commonOptions);
+  allOptions.add(hiddenOptions);
+
+  // =========== Process the options ========================================
+
+  // 1) Process the common top level options 
+  po::parsed_options parsedCommonTop = 
+    po::command_line_parser(_options).
+    options(allOptions).          
+    allow_unregistered().           // Allow for unregistered options
+    run();                          // Parse the options
+
+  po::variables_map vm;
+
+  try {
+    po::store(parsedCommonTop, vm);  // Can throw
+    po::notify(vm);                  // Can throw (but really isn't used)
+
+    // Multual DRC
+    for (unsigned int index1 = 0; index1 < subOptionOptions.size(); ++index1) {
+      for (unsigned int index2 = index1 + 1; index2 < subOptionOptions.size(); ++index2) {
+        conflictingOptions(vm, subOptionOptions[index1]->longName(), subOptionOptions[index2]->longName());
+      }
+    }
+  } catch (const std::exception & e) {
+    std::cerr << "ERROR: " << e.what() << std::endl;
+    printHelp(commonOptions, hiddenOptions, subOptionOptions);
+    return;
+  }
+
+  // Find the subOption;
+  std::shared_ptr<OptionOptions> optionOption;
+  for (auto& subOO : subOptionOptions) {
+    if (vm.count(subOO->longName().c_str()) != 0) {
+      optionOption = subOO;
+      break;
+    }
+  }
+
+  // No suboption print help
+  if (!optionOption) {
+    printHelp(commonOptions, hiddenOptions, subOptionOptions);
+    return;
+  }
+
+  // 2) Process the top level options
+  std::vector<std::string> topOptions = po::collect_unrecognized(parsedCommonTop.options, po::include_positional);
+  if (help) {
+    topOptions.push_back("--help");
+  }
+
+  optionOption->setGlobalOptions(getGlobalOptions());
+  
+  // Execute the option
+  optionOption->execute(topOptions);
+}
