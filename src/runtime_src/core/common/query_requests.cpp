0d5374f4a2886b3a88d6f7ab605bb8d316733d6d
Sarabjeet Singh <sarab96@gmail.com>
2021-08-11 07:40:14 -0700

    updated

--- src/runtime_src/core/common/query_requests.cpp
+++ src/runtime_src/core/common/query_requests.cpp
@@ -127,24 +127,21 @@ std::pair<uint64_t, uint64_t>
 xrt_core::query::xocl_errors::
 to_value(const std::vector<char>& buf, xrtErrorClass ecl)
 {
-  uint64_t errcode = 0;
-  uint64_t ts = 0;
   if (buf.empty())
     return {0, 0};
+
   auto errors_buf = reinterpret_cast<const xcl_errors *>(buf.data());
   if (errors_buf->num_err <= 0)
     return {0, 0};
+
   if (errors_buf->num_err > XCL_ERROR_CAPACITY)
     throw xrt_core::system_error(EINVAL, "Invalid data in sysfs");
 
-  for (int i = errors_buf->num_err; i >= 0; i--) {
-    if (XRT_ERROR_CLASS(errors_buf->errors[i].err_code) != ecl)
-      continue;
-    errcode = errors_buf->errors[i].err_code;
-    ts = errors_buf->errors[i].ts;
-    break;
+  for (int i = errors_buf->num_err-1; i >= 0; i--) {
+    if (XRT_ERROR_CLASS(errors_buf->errors[i].err_code) == ecl)
+      return {errors_buf->errors[i].err_code, errors_buf->errors[i].ts};
   }
-  return {errcode, ts};
+  return {0, 0};
 }
 
 std::vector<xclErrorLast>
