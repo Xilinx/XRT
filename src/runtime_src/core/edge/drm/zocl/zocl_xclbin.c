974aa39510067acaa6f23cb8bff4afd17d7b9633
ch vamshi krishna <chvamshi@xilinx.com>
2021-08-13 23:32:32 +0530

    fixed review comments

--- src/runtime_src/core/edge/drm/zocl/zocl_xclbin.c
+++ src/runtime_src/core/edge/drm/zocl/zocl_xclbin.c
@@ -752,12 +752,10 @@ zocl_xclbin_read_axlf(struct drm_zocl_dev *zdev, struct drm_zocl_axlf *axlf_obj,
 
 	/* Check unique ID */
 	if (zocl_xclbin_same_uuid(zdev, &axlf_head.m_header.uuid)) {
-		if ( !(axlf_obj->za_flags & DRM_ZOCL_FORCE_PROGRAM) )
-		{
+		if (!(axlf_obj->za_flags & DRM_ZOCL_FORCE_PROGRAM)) {
 			if (is_aie_only(axlf)) {
 				write_unlock(&zdev->attr_rwlock);
-				ret = zocl_load_aie_only_pdi(zdev, axlf, xclbin,
-				client);
+				ret = zocl_load_aie_only_pdi(zdev, axlf, xclbin,client);
 				write_lock(&zdev->attr_rwlock);
 				if (ret)
 					DRM_WARN("read xclbin: fail to load AIE");
@@ -769,9 +767,7 @@ zocl_xclbin_read_axlf(struct drm_zocl_dev *zdev, struct drm_zocl_axlf *axlf_obj,
 				DRM_INFO("%s The XCLBIN already loaded", __func__);
 			}
 			goto out0;
-		}
-		else
-		{
+		} else {
 			// We come here if user sets force_xclbin_program
 			// option "true" in xrt.ini under [Runtime] section
 			DRM_WARN("%s The XCLBIN already loaded. Force xclbin download", __func__);
@@ -838,7 +834,7 @@ zocl_xclbin_read_axlf(struct drm_zocl_dev *zdev, struct drm_zocl_axlf *axlf_obj,
 			goto out0;
 		}
 
-		if ( !(axlf_obj->za_flags & DRM_ZOCL_PLATFORM_PR) ) {
+		if (!(axlf_obj->za_flags & DRM_ZOCL_PLATFORM_PR)) {
 			DRM_INFO("disable partial bitstream download, "
 			    "axlf flags is %d", axlf_obj->za_flags);
 		} else {
@@ -873,7 +869,7 @@ zocl_xclbin_read_axlf(struct drm_zocl_dev *zdev, struct drm_zocl_axlf *axlf_obj,
 			goto out0;
 
 		zocl_cache_xclbin(zdev, axlf, xclbin);
-	} else if ( (axlf_obj->za_flags & DRM_ZOCL_PLATFORM_FLAT) &&
+	} else if ((axlf_obj->za_flags & DRM_ZOCL_PLATFORM_FLAT) &&
 		   axlf_head.m_header.m_mode == XCLBIN_FLAT &&
 		   axlf_head.m_header.m_mode != XCLBIN_HW_EMU &&
 		   axlf_head.m_header.m_mode != XCLBIN_HW_EMU_PR) {
