9e1a4a4a0187a121941a3f3053c3c7d03d10741d
Rajkumar Rampelli <rajkumar@xilinx.com>
2021-09-06 17:30:24 +0530

    fix review comments
    Signed-off-by: Rajkumar Rampelli <rajkumar@xilinx.com>

--- src/runtime_src/core/tools/common/ReportCmcStatus.cpp
+++ src/runtime_src/core/tools/common/ReportCmcStatus.cpp
@@ -36,25 +36,39 @@ ReportCmcStatus::getPropertyTree20202( const xrt_core::device * _pDevice,
   boost::property_tree::ptree pt;
   pt.put("Description", "CMC");
   try {
-    pt.put("heartbeat_err_time", xrt_core::device_query<xrt_core::query::heartbeat_err_time>(_pDevice));
-    pt.put("heartbeat_count", xrt_core::device_query<xrt_core::query::heartbeat_count>(_pDevice));
-    pt.put("heartbeat_err_code", xrt_core::device_query<xrt_core::query::heartbeat_err_code>(_pDevice));
-    pt.put("heartbeat_stall", xrt_core::device_query<xrt_core::query::heartbeat_stall>(_pDevice));
-    pt.put("status", xrt_core::utils::parse_cmc_status(static_cast<unsigned int>(xrt_core::device_query<xrt_core::query::heartbeat_err_code>(_pDevice))));
+    boost::property_tree::ptree pth;
+    pth.put("Description", "CMC heartbeat");
+    pth.put("heartbeat_err_time", xrt_core::device_query<xrt_core::query::heartbeat_err_time>(_pDevice));
+    pth.put("heartbeat_count", xrt_core::device_query<xrt_core::query::heartbeat_count>(_pDevice));
+    pth.put("heartbeat_err_code", xrt_core::device_query<xrt_core::query::heartbeat_err_code>(_pDevice));
+    pth.put("heartbeat_stall", xrt_core::device_query<xrt_core::query::heartbeat_stall>(_pDevice));
+    pth.put("status", xrt_core::utils::parse_cmc_status(static_cast<unsigned int>(xrt_core::device_query<xrt_core::query::heartbeat_err_code>(_pDevice))));
+    pt.add_child("cmc_heartbeat", pth);
   }
   catch(const xrt_core::query::no_such_key&) {}
   catch(const xrt_core::query::sysfs_error&) {}
   try {
-    pt.put("scaling_enabled", xrt_core::device_query<xrt_core::query::xmc_scaling_enabled>(_pDevice));
-    pt.put("scaling_support", xrt_core::device_query<xrt_core::query::xmc_scaling_support>(_pDevice));
-    pt.put("scaling_critical_power_threshold", xrt_core::device_query<xrt_core::query::xmc_scaling_critical_pow_threshold>(_pDevice));
-    pt.put("scaling_critical_temp_threshold", xrt_core::device_query<xrt_core::query::xmc_scaling_critical_temp_threshold>(_pDevice));
-    pt.put("scaling_threshold_power_limit", xrt_core::device_query<xrt_core::query::xmc_scaling_threshold_power_limit>(_pDevice));
-    pt.put("scaling_threshold_temp_limit", xrt_core::device_query<xrt_core::query::xmc_scaling_threshold_temp_limit>(_pDevice));
-    pt.put("scaling_power_override_en", xrt_core::device_query<xrt_core::query::xmc_scaling_power_override_enable>(_pDevice));
-    pt.put("scaling_power_override", xrt_core::device_query<xrt_core::query::xmc_scaling_power_override>(_pDevice));
-    pt.put("scaling_temp_override_en", xrt_core::device_query<xrt_core::query::xmc_scaling_temp_override_enable>(_pDevice));
-    pt.put("scaling_temp_override", xrt_core::device_query<xrt_core::query::xmc_scaling_temp_override>(_pDevice));
+    boost::property_tree::ptree pts;
+    pts.put("Description", "Runtime Clock Scaling");
+    pts.put("enabled", xrt_core::device_query<xrt_core::query::xmc_scaling_enabled>(_pDevice));
+    pts.put("supported", xrt_core::device_query<xrt_core::query::xmc_scaling_support>(_pDevice));
+    boost::property_tree::ptree pts1;
+    pts1.put("power_watts", xrt_core::device_query<xrt_core::query::xmc_scaling_critical_pow_threshold>(_pDevice));
+    pts1.put("temp_celsius", xrt_core::device_query<xrt_core::query::xmc_scaling_critical_temp_threshold>(_pDevice));
+    pts.add_child("shutdown_threshold_limits", pts1);
+    boost::property_tree::ptree pts2;
+    pts2.put("power_watts", xrt_core::device_query<xrt_core::query::xmc_scaling_threshold_power_limit>(_pDevice));
+    pts2.put("temp_celsius", xrt_core::device_query<xrt_core::query::xmc_scaling_threshold_temp_limit>(_pDevice));
+    pts.add_child("override_threshold_limits", pts2);
+    boost::property_tree::ptree pts3;
+    pts3.put("enabled", xrt_core::device_query<xrt_core::query::xmc_scaling_power_override_enable>(_pDevice));
+    pts3.put("power_watts", xrt_core::device_query<xrt_core::query::xmc_scaling_power_override>(_pDevice));
+    pts.add_child("power_threshold_override", pts3);
+    boost::property_tree::ptree pts4;
+    pts4.put("enabled", xrt_core::device_query<xrt_core::query::xmc_scaling_temp_override_enable>(_pDevice));
+    pts4.put("temp_celsius", xrt_core::device_query<xrt_core::query::xmc_scaling_temp_override>(_pDevice));
+    pts.add_child("temp_threshold_override", pts4);
+    pt.add_child("scaling", pts);
   }
   catch(const xrt_core::query::no_such_key&) {}
   catch(const xrt_core::query::sysfs_error&) {}
@@ -68,8 +82,6 @@ ReportCmcStatus::writeReport( const xrt_core::device* /*_pDevice*/,
                             const std::vector<std::string>& /*_elementsFilter*/,
                             std::ostream & _output) const
 {
-  boost::property_tree::ptree empty_ptree;
-
   _output << "CMC\n";
   auto& cmc = _pt.get_child("cmc");
   if(cmc.empty()) {
@@ -77,26 +89,32 @@ ReportCmcStatus::writeReport( const xrt_core::device* /*_pDevice*/,
     return;
   }
   try {
-    uint32_t err_code = cmc.get<uint32_t>("heartbeat_err_code");
-    _output << boost::format("  %-22s : 0x%x %s\n") % "Status" % err_code % cmc.get<std::string>("status");
+    boost::property_tree::ptree cmc_hb = cmc.get_child("cmc_heartbeat");
+    uint32_t err_code = cmc_hb.get<uint32_t>("heartbeat_err_code");
+    _output << boost::format("  %s : 0x%x %s\n") % "Status" % err_code % cmc_hb.get<std::string>("status");
     if (err_code)
-      _output << boost::format("  %-22s : %s sec\n\n") % "err time" % cmc.get<std::string>("heartbeat_err_time");
+      _output << boost::format("  %s : %s sec\n\n") % "err time" % cmc_hb.get<std::string>("heartbeat_err_time");
   } catch(...) {}
   try {
+    boost::property_tree::ptree cmc_scale = cmc.get_child("scaling");
     _output << boost::format("  %-22s :\n") % "Runtime clock scaling feature";
-    _output << boost::format("  \t%-22s : %s\n") % "Supported" % cmc.get<std::string>("scaling_support");
-    _output << boost::format("  \t%-22s : %s\n") % "Enabled" % cmc.get<std::string>("scaling_enabled");
-    _output << boost::format("  \t%-22s:\n") % "Critical threshold (clock shutdown) limits";
-    _output << boost::format("  \t\t%-22s : %s\n") % "Power" % cmc.get<std::string>("scaling_critical_power_threshold");
-    _output << boost::format("  \t\t%-22s : %s\n") % "Temperature" % cmc.get<std::string>("scaling_critical_temp_threshold");
-    _output << boost::format("  \t%-22s:\n") % "Throttling threshold limits";
-    _output << boost::format("  \t\t%-22s : %s\n") % "Power" % cmc.get<std::string>("scaling_threshold_power_limit");
-    _output << boost::format("  \t\t%-22s : %s\n") % "Temperature" % cmc.get<std::string>("scaling_threshold_temp_limit");
-    _output << boost::format("  \t%-22s:\n") % "Power threshold override";
-    _output << boost::format("  \t\t%-22s : %s\n") % "Override" % cmc.get<std::string>("scaling_power_override_en");
-    _output << boost::format("  \t\t%-22s : %s\n") % "Override limit" % cmc.get<std::string>("scaling_power_override");
-    _output << boost::format("  \t%-22s:\n") % "Temperature threshold override";
-    _output << boost::format("  \t\t%-22s : %s\n") % "Override" % cmc.get<std::string>("scaling_temp_override_en");
-    _output << boost::format("  \t\t%-22s : %s\n") % "Override limit" % cmc.get<std::string>("scaling_temp_override");
+    _output << boost::format("    %s : %s\n") % "Supported" % cmc_scale.get<std::string>("supported");
+    _output << boost::format("    %s : %s\n") % "Enabled" % cmc_scale.get<std::string>("enabled");
+    cmc_scale = cmc.get_child("scaling").get_child("shutdown_threshold_limits");
+    _output << boost::format("    %-22s:\n") % "Critical threshold (clock shutdown) limits";
+    _output << boost::format("      %s : %s W\n") % "Power" % cmc_scale.get<std::string>("power_watts");
+    _output << boost::format("      %s : %s C\n") % "Temperature" % cmc_scale.get<std::string>("temp_celsius");
+    cmc_scale = cmc.get_child("scaling").get_child("override_threshold_limits");
+    _output << boost::format("    %-22s:\n") % "Throttling threshold limits";
+    _output << boost::format("      %s : %s W\n") % "Power" % cmc_scale.get<std::string>("power_watts");
+    _output << boost::format("      %s : %s C\n") % "Temperature" % cmc_scale.get<std::string>("temp_celsius");
+    cmc_scale = cmc.get_child("scaling").get_child("power_threshold_override");
+    _output << boost::format("    %-22s:\n") % "Power threshold override";
+    _output << boost::format("      %s : %s\n") % "Override" % cmc_scale.get<std::string>("enabled");
+    _output << boost::format("      %s : %s W\n") % "Override limit" % cmc_scale.get<std::string>("power_watts");
+    cmc_scale = cmc.get_child("scaling").get_child("temp_threshold_override");
+    _output << boost::format("    %-22s:\n") % "Temperature threshold override";
+    _output << boost::format("      %s : %s\n") % "Override" % cmc_scale.get<std::string>("enabled");
+    _output << boost::format("      %s : %s C\n") % "Override limit" % cmc_scale.get<std::string>("temp_celsius");
   } catch(...) {}
 }
