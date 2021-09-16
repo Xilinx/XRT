95948a6b83f2aa5ca1931dc6834bf1ff73cf33ed
Soren Soe <2106410+stsoe@users.noreply.github.com>
2021-09-15 22:48:18 -0700

    Add host mem warning in general (#5798)
    Show host mem error when ever allocating host only BO fails.
    Previously only shown for nodma platforms.

--- src/runtime_src/core/common/api/xrt_bo.cpp
+++ src/runtime_src/core/common/api/xrt_bo.cpp
@@ -730,18 +730,10 @@ alloc_nodma(xclDeviceHandle dhdl, size_t sz, xrtBufferFlags, xrtMemoryGroup grp)
     throw xrt_core::error(EINVAL, "Invalid buffer size '" + std::to_string(sz) +
                           "', must be multiple of 64 bytes for NoDMA platforms");
 
-  try {
-    auto hbuf_handle = alloc_bo(dhdl, sz, XCL_BO_FLAGS_HOST_ONLY, grp);
-    auto dbuf_handle = alloc_bo(dhdl, sz, XCL_BO_FLAGS_DEV_ONLY, grp);
-    auto boh = std::make_shared<xrt::buffer_nodma>(dhdl, hbuf_handle, dbuf_handle, sz);
-    return boh;
-  }
-  catch (const std::exception& ex) {
-    auto fmt = boost::format("Failed to allocate host memory buffer (%s), make sure host bank is enabled "
-                             "(see xbutil configure --host-mem)") % ex.what();
-    send_exception_message(fmt.str());
-    throw;
-  }
+  auto hbuf_handle = alloc_bo(dhdl, sz, XCL_BO_FLAGS_HOST_ONLY, grp);
+  auto dbuf_handle = alloc_bo(dhdl, sz, XCL_BO_FLAGS_DEV_ONLY, grp);
+  auto boh = std::make_shared<xrt::buffer_nodma>(dhdl, hbuf_handle, dbuf_handle, sz);
+  return boh;
 }
 
 static std::shared_ptr<xrt::bo_impl>
