93c392c7ed04a5181679512e46b62e874356b57d
AShivangi <shivangiagarwal53@gmail.com>
2021-09-20 18:09:14 -0700

    add linux pipe support back

--- src/runtime_src/core/tools/common/Process.cpp
+++ src/runtime_src/core/tools/common/Process.cpp
@@ -14,6 +14,19 @@
  * under the License.
  */
 
+
+/**
+ * Boost processes were released with Boost 1.64. If the OS doesn't support 
+ * that version of boost, then we default to linux pipes
+ * 
+ *           Support map
+ *   Ubuntu 16.04 | Linux pipes
+ *   Ubuntu 18.04 | boost::process
+ *      Windows   | boost::process
+ * 
+ * This file hides the implementation of process running from SubCmdValidate
+ */
+
 // ------ I N C L U D E   F I L E S -------------------------------------------
 #define WIN32_LEAN_AND_MEAN
 #ifdef _WIN32
@@ -45,6 +58,124 @@
 // ------ S T A T I C   V A R I A B L E S -------------------------------------
 
 // ------ F U N C T I O N S ---------------------------------------------------
+#ifdef NO_BOOST_PROCESS
+
+/**
+ * helper functions for running testcase in linux pipe
+ */
+inline const char* 
+getenv_or_empty(const char* path)
+{
+  return getenv(path) ? getenv(path) : "";
+}
+
+static void 
+setShellPathEnv(const std::string& var_name, const std::string& trailing_path)
+{
+  std::string xrt_path(getenv_or_empty("XILINX_XRT"));
+  std::string new_path(getenv_or_empty(var_name.c_str()));
+  xrt_path += trailing_path + ":";
+  new_path = xrt_path + new_path;
+  setenv(var_name.c_str(), new_path.c_str(), 1);
+}
+
+static void 
+testCaseProgressReporter(std::shared_ptr<XBUtilities::ProgressBar> run_test, bool& is_done)
+{
+  unsigned int counter = 0;
+  while((counter < run_test.get()->getMaxIterations()) && !is_done) {
+    run_test.get()->update(counter++);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+}
+
+unsigned int
+XBUtilities::runScript( const std::string & env, 
+                        const std::string & script, 
+                        const std::vector<std::string> & args,
+                        const std::string & running_description,
+                        const std::string & final_description,
+                        int max_running_duration,
+                        std::ostringstream & os_stdout,
+                        std::ostringstream & os_stderr,
+                        bool /*erasePassFailMessage*/)
+{
+  // Fix environment variables before running test case
+  setenv("XILINX_XRT", "/opt/xilinx/xrt", 0);
+  setShellPathEnv("PYTHONPATH", "/python");
+  setShellPathEnv("LD_LIBRARY_PATH", "/lib");
+  setShellPathEnv("PATH", "/bin");
+  unsetenv("XCL_EMULATION_MODE");
+
+  std::ostringstream args_str;
+  std::copy(args.begin(), args.end(), std::ostream_iterator<std::string>(args_str, " "));
+  std::string cmd;
+  if(env.compare("python") == 0) {
+    cmd = "/usr/bin/python3 ";
+  }
+  cmd += script + " " + args_str.str();
+
+  int stderr_fds[2];
+  if (pipe(stderr_fds)== -1) {
+    os_stderr << "Unable to create pipe";
+    return errno;
+  }
+
+  // Save stderr
+  int stderr_save = dup(STDERR_FILENO);
+  if (stderr_save == -1) {
+    os_stderr << "Unable to duplicate stderr";
+    return errno;
+  }
+
+  // Kick off progress reporter
+  bool is_done = false;
+  // Fix: create busy bar
+  auto run_test = std::make_shared<XBUtilities::ProgressBar>(running_description, max_running_duration, XBUtilities::is_escape_codes_disabled(), std::cout); 
+  std::thread t(testCaseProgressReporter, run_test, std::ref(is_done));
+
+  // Close existing stderr and set it to be the write end of the pipe.
+  // After fork below, our child process's stderr will point to the same fd.
+  dup2(stderr_fds[1], STDERR_FILENO);
+  close(stderr_fds[1]);
+  std::shared_ptr<FILE> stderr_child(fdopen(stderr_fds[0], "r"), fclose);
+  std::shared_ptr<FILE> stdout_child(popen(cmd.c_str(), "r"), pclose);
+  // Restore our normal stderr
+  dup2(stderr_save, STDERR_FILENO);
+  close(stderr_save);
+
+  if (stdout_child == nullptr) {
+    os_stderr << boost::str(boost::format("Failed to run %s") % cmd);
+    return errno;
+  }
+
+  // Read child's stdout and stderr without parsing the content
+  char buf[1024];
+  while (!feof(stdout_child.get())) {
+    if (fgets(buf, sizeof (buf), stdout_child.get()) != nullptr) {
+      os_stdout << buf;
+    }
+  }
+  while (stderr_child && !feof(stderr_child.get())) {
+    if (fgets(buf, sizeof (buf), stderr_child.get()) != nullptr) {
+      os_stderr << buf;
+    }
+  }
+
+  is_done = true;
+  bool exitCode = (os_stdout.str().find("PASS") != std::string::npos) ? true : false;
+  run_test.get()->finish(exitCode, final_description);
+  // Workaround: Clear the default progress bar output so as to print the Error: before printing [FAILED]
+  // Remove this once busybar is implemented
+  std::cout << EscapeCodes::cursor().prev_line() << EscapeCodes::cursor().clear_line();
+  t.join();
+
+  return exitCode ? 0 : 1;
+}
+#else
+
+
+
 boost::filesystem::path
 findEnvPath(const std::string & env)
 {
@@ -141,4 +272,4 @@ XBUtilities::runScript( const std::string & env,
 
   return exitCode;
 }
-
+#endif
