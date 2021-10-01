8f6f4dbf600a9e14a9860e5526962a4f66a7a22d
Soren Soe <2106410+stsoe@users.noreply.github.com>
2021-10-01 07:42:58 -0700

    Add xrt::bo constructor from xclBufferHandle (#5873)
    Allow applications to bridge into XRT APIs from old XCL APIs.
    
    Specifically requested to support an IVAS framwork where buffers are
    allocated using old style APIs but kernel APIs are used for execution.
    
    By allowing construction of xrt::bo from xclBufferHandle, the xrt::bo
    can be used as an xrt::kernel argument.
    
    The C++ API addition has been tested with XRT/tests/xrt/22_verify/xcl.cpp.
    There has been no testing of the correspond C API.

--- src/runtime_src/core/include/xrt/xrt_bo.h
+++ src/runtime_src/core/include/xrt/xrt_bo.h
@@ -46,6 +46,12 @@ typedef uint64_t xrtBufferFlags;
  * typedef xrtMemoryGroup - Memory bank group for buffer
  */
 typedef uint32_t xrtMemoryGroup;
+
+/**
+ * Typed xclBufferHandle used to prevent ambiguity
+ * Use when constructing xrt::bo from xclBufferHandle
+ */
+struct xcl_buffer_handle { xclBufferHandle bhdl; };
   
 #ifdef __cplusplus
 
@@ -203,6 +209,31 @@ public:
    */
   XCL_DRIVER_DLLESPEC
   bo(const bo& parent, size_t size, size_t offset);
+
+  /// @cond
+  /**
+   * bo() - Constructor from xclBufferHandle
+   *
+   * @param dhdl
+   *  Device on which the buffer handle was created
+   * @param xhdl
+   *  Typified shim buffer handle created with xclAllocBO variants
+   *
+   * This function allows construction of xrt::bo object from an
+   * xclBufferHandle supposedly allocated using deprecated xcl APIs.
+   * The buffer handle is allocated with xclAllocBO and must be
+   * freed with xclFreeBO. 
+   *
+   * Note that argument xclBufferHandle must be wrapped as
+   * an xcl_buffer_handle in order to disambiguate the untyped
+   * xclBufferHandle.
+   *
+   * Mixing xcl style APIs and xrt APIs is discouraged and
+   * as such this documentation is not included in doxygen.
+   */
+  XCL_DRIVER_DLLESPEC
+  bo(xclDeviceHandle dhdl, xcl_buffer_handle xhdl);
+  /// @endcond
 
   /**
    * bo() - Copy ctor
@@ -519,6 +550,31 @@ xrtBOExport(xrtBufferHandle bhdl);
 XCL_DRIVER_DLLESPEC
 xrtBufferHandle
 xrtBOSubAlloc(xrtBufferHandle parent, size_t size, size_t offset);
+
+/* 
+ * xrtBOAllocFromXcl() - Undocumented allocation from an xclBufferHandle
+ *
+ * @dhdl:  XRT device handle on which the buffer is residing.
+ * @xhdl:  The xcl buffer handle to convert to an xrtBufferHandle
+ * Return: xrtBufferHandle which must be freed using `xrtBOFree`
+ *
+ * Note that argument xclBufferHandle must be wrapped as
+ * an xcl_buffer_handle in order to disambiguate the untyped
+ * xclBufferHandle.
+ *
+ * Please note that the device is an xrtDeviceHandle.  It is the
+ * responsibility of the user to convert an xclDeviceHandle to 
+ * an xrtDeviceHandle before calling this API.
+ *
+ * The xrtBufferHandle returned by this API
+ *
+ * The argument xcl buffer handle must be explicitly freed using
+ * xclFreeBO, in other words xclAllocBO requires xclFreeBO and
+ * xrtBOAlloc requires xrtBOFree.
+ */
+XCL_DRIVER_DLLESPEC
+xrtBufferHandle
+xrtBOAllocFromXcl(xrtDeviceHandle dhdl, xclBufferHandle xhdl);
 
 /**
  * xrtBOFree() - Free a previously allocated BO
