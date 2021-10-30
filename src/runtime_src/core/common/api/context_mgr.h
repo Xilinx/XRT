d8a604d3114c5539c2d58581c78e8cd053a940e3
Soren Soe <2106410+stsoe@users.noreply.github.com>
2021-10-30 13:59:19 -0700

    C-API thread safety (#5963)
    This PR extends #5960 to make all C-APIs thread safe.
    The earlier PR addressed only xrtXclbin APIs.
    
    In addition to thread safe handling of XRT handles created by C APIs,
    this PR also addresses a general thread problem (C and C++) for
    acquiring compute unit context.  If multiple threads share the same
    device object and acquire / release context on the same CUs, then
    careful synchronization of low level xclOpen/CloseContext is required.
    
    The synchronization ensures that when a thread is in the process
    of releaseing a context, another thread wont call xclOpenContext
    before the former has closed its context.
    
    Multiple testing has been done with arbitrary number of threads
    creating xclbin, device, kernel, and bo objects in tight loops.  All
    threading problems have been present since inception, so we need to
    add specific stress tests for cover the multi-threaded use case.

---
+++ src/runtime_src/core/common/api/context_mgr.h
@@ -0,0 +1,62 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2021 Xilinx, Inc. All rights reserved.
+ */
+
+#include "core/include/xrt/xrt_uuid.h"
+#include <memory>
+
+// This file defines APIs for compute unit (ip) context management
+// It is used by xrt::kernel and xrt::ip implementation.
+
+namespace xrt_core {
+
+class device;
+
+// Context management is somewhat complex in multi-threaded host
+// applications where same device object is shared between threads.
+//
+// A call to xclOpenContext must be protected against a race where
+// one thread is in the process of call xclCloseContext.
+namespace context_mgr {
+
+class device_context_mgr;
+
+// Create a context manager for a specific device The manager is
+// shared and cached so that it is constructed only if necessary.  In
+// other words, multi threads using same device can share the same
+// context manager.
+std::shared_ptr<device_context_mgr>
+create(const xrt_core::device* device);
+
+// Open a device context a specified compute unit (ip)
+//
+// @device: device to open context on
+// @uuid:   xclbin uuid with the CU
+// @cuidx:  index of CU
+// @shared: open in shared (true) or exclusive mode (false)
+//
+// The function blocks until the context can be acquired.  If
+// timeout, then the function throws.
+//
+// Note that the context manager is not intended to support two or
+// more threads opening a context on the same compute unit. This
+// situation must be guarded by the client (xrt::kernel) of the
+// manager.
+//
+// The function is simply a synchronization between two threads
+// simultanous use of open_context and close_context.
+void
+open_context(xrt_core::device* device, const xrt::uuid& uuid, unsigned int cuidx, bool shared);
+
+// Close a previously opened device context
+//
+// @device: device to close context on
+// @uuid:   xclbin uuid with the CU
+// @cuidx:  index of CU
+//
+// The function throws if no context is open on specified CU.
+void
+close_context(xrt_core::device* device, const xrt::uuid& uuid, unsigned int cuidx);
+ 
+}} // context_mgr, xrt_core
--- src/runtime_src/core/common/api/handle.h
+++ src/runtime_src/core/common/api/handle.h
@@ -20,36 +20,129 @@ namespace xrt_core {
 template <typename HandleType, typename ImplType>
 class handle_map
 {
-  std::mutex mutex;
-  std::map<HandleType, ImplType> handles;
+};
+
+template <typename HandleType, typename ImplType>
+class handle_map<HandleType, std::shared_ptr<ImplType>>
+{
+  mutable std::mutex mutex;
+  std::map<HandleType, std::shared_ptr<ImplType>> handles;
+
+  std::shared_ptr<ImplType>
+  get_no_lock(HandleType handle) const
+  {
+    auto itr = handles.find(handle);
+    return (itr == handles.end())
+      ? nullptr
+      : itr->second;
+  }
 
 public:
-  // get() - Get implementation for handle
-  ImplType
-  get_impl(HandleType handle)
+  const std::shared_ptr<ImplType>&
+  get_or_error(HandleType handle) const
   {
     std::lock_guard<std::mutex> lk(mutex);
     auto itr = handles.find(handle);
     if (itr == handles.end())
       throw xrt_core::error(-EINVAL, "No such handle");
-    return itr->second;
+
+    return (*itr).second;
   }
 
-  // add() - Record handle impl pair
+  std::shared_ptr<ImplType>
+  get(HandleType handle) const
+  {
+    std::lock_guard<std::mutex> lk(mutex);
+    auto itr = handles.find(handle);
+    return (itr == handles.end())
+      ? nullptr
+      : (*itr).second;
+  }
+
   void
-  add(HandleType handle, ImplType&& impl)
+  add(HandleType handle, std::shared_ptr<ImplType>&& impl)
   {
     std::lock_guard<std::mutex> lk(mutex);
     handles.emplace(handle, std::move(impl));
   }
 
-  // remove() - Remove handle from map
+  void
+  remove_or_error(HandleType handle)
+  {
+    std::lock_guard<std::mutex> lk(mutex);
+    if (handles.erase(handle) == 0)
+      throw xrt_core::error(-EINVAL, "No such handle");
+  }
+
   void
   remove(HandleType handle)
   {
     std::lock_guard<std::mutex> lk(mutex);
+    handles.erase(handle);
+  }
+
+  size_t
+  count(HandleType handle) const
+  {
+    std::lock_guard<std::mutex> lk(mutex);
+    return handles.count(handle);
+  }
+};
+
+template <typename HandleType, typename ImplType>
+class handle_map<HandleType, std::unique_ptr<ImplType>>
+{
+  mutable std::mutex mutex;
+  std::map<HandleType, std::unique_ptr<ImplType>> handles;
+
+public:
+  ImplType*
+  get_or_error(HandleType handle) const
+  {
+    std::lock_guard<std::mutex> lk(mutex);
+    auto itr = handles.find(handle);
+    if (itr == handles.end())
+      throw xrt_core::error(-EINVAL, "No such handle");
+    return (*itr).second.get();
+  }
+
+  ImplType*
+  get(HandleType handle) const
+  {
+    std::lock_guard<std::mutex> lk(mutex);
+    auto itr = handles.find(handle);
+    return (itr == handles.end())
+      ? nullptr
+      : (*itr).second.get();
+  }
+
+  void
+  add(HandleType handle, std::unique_ptr<ImplType>&& impl)
+  {
+    std::lock_guard<std::mutex> lk(mutex);
+    handles.emplace(handle, std::move(impl));
+  }
+
+  void
+  remove_or_error(HandleType handle)
+  {
+    std::lock_guard<std::mutex> lk(mutex);
     if (handles.erase(handle) == 0)
-    throw xrt_core::error(-EINVAL, "No such handle");
+      throw xrt_core::error(-EINVAL, "No such handle");
+  }
+
+  void
+  remove(HandleType handle)
+  {
+    std::lock_guard<std::mutex> lk(mutex);
+    handles.erase(handle);
+  }
+
+  size_t
+  count(HandleType handle) const
+  {
+    std::lock_guard<std::mutex> lk(mutex);
+    return handles.count(handle);
   }
 };
 
