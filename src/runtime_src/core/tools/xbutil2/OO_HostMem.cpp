848b7e4b701cea1948b44177d1f5b68a47b67782
Daniel Benusovich <94083344+dbenusov-xilinx@users.noreply.github.com>
2021-12-03 18:16:46 -0800

    CR-1112292 CR-1112292 Add zero size check for readabilityC
    Co-authored-by: Stephen Rozum <40244911+rozumx@users.noreply.github.com>

--- src/runtime_src/core/tools/xbutil2/OO_HostMem.cpp
+++ src/runtime_src/core/tools/xbutil2/OO_HostMem.cpp
@@ -118,7 +118,7 @@ OO_HostMem::execute(const SubCmdOptions& _options) const
 
     // Exit if ENABLE action is specified and 
     // size is not a power of 2
-    if(enable && !XBUtilities::isPowerOf2(size))
+    if(enable && ((0 == size) || !XBUtilities::isPowerOf2(size)))
       throw xrt_core::error(std::errc::invalid_argument, "Please specify a non-zero memory size between 4M and 1G as a power of 2.");
 
     // Collect all of the devices of interest
