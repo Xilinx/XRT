b356efaf3d01aab64a11c6a9aa6888afa6376c94
eshwarpr <eshwarpr@xilinx.com>
2022-01-04 18:18:27 +0530

    Add xbmgmt examine -d for u250
     > Added 'xbmgmt examine -d' in install_platform.sh

--- .github/scripts/pipeline/install_platform.sh
+++ .github/scripts/pipeline/install_platform.sh
@@ -27,4 +27,5 @@ else
     sudo apt install ./xilinx*validate*.deb
     sudo apt install ./xilinx*shell*.deb
     sudo /opt/xilinx/xrt/bin/xbmgmt program --shell /opt/xilinx/firmware/u250/gen3x16/xdma-shell/partition.xsabin -d
+    sudo /opt/xilinx/xrt/bin/xbmgmt examine -d
 fi
