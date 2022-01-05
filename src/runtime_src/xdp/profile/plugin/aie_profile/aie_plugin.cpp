353743f5ec9fe6ba62dea6d152d553703820f975
Paul Schumacher <schuey@xilinx.com>
2022-01-05 13:12:29 -0700

    added check for valid BD

--- src/runtime_src/xdp/profile/plugin/aie_profile/aie_plugin.cpp
+++ src/runtime_src/xdp/profile/plugin/aie_profile/aie_plugin.cpp
@@ -564,25 +564,32 @@ namespace xdp {
     constexpr int NUM_BDS = 8;
     constexpr uint32_t BYTES_PER_WORD = 4;
     constexpr uint32_t ACTUAL_OFFSET = 1;
-    uint64_t offsets[NUM_BDS] = {XAIEGBL_MEM_DMABD0CTRL,          XAIEGBL_MEM_DMABD1CTRL,
-                                 XAIEGBL_MEM_DMABD2CTRL,          XAIEGBL_MEM_DMABD3CTRL,
-                                 XAIEGBL_MEM_DMABD4CTRL,          XAIEGBL_MEM_DMABD5CTRL,
-                                 XAIEGBL_MEM_DMABD6CTRL,          XAIEGBL_MEM_DMABD7CTRL};
-    uint32_t lsbs[NUM_BDS]    = {XAIEGBL_MEM_DMABD0CTRL_LEN_LSB,  XAIEGBL_MEM_DMABD1CTRL_LEN_LSB,
-                                 XAIEGBL_MEM_DMABD2CTRL_LEN_LSB,  XAIEGBL_MEM_DMABD3CTRL_LEN_LSB,
-                                 XAIEGBL_MEM_DMABD4CTRL_LEN_LSB,  XAIEGBL_MEM_DMABD5CTRL_LEN_LSB,
-                                 XAIEGBL_MEM_DMABD6CTRL_LEN_LSB,  XAIEGBL_MEM_DMABD7CTRL_LEN_LSB};
-    uint32_t masks[NUM_BDS]   = {XAIEGBL_MEM_DMABD0CTRL_LEN_MASK, XAIEGBL_MEM_DMABD1CTRL_LEN_MASK,
-                                 XAIEGBL_MEM_DMABD2CTRL_LEN_MASK, XAIEGBL_MEM_DMABD3CTRL_LEN_MASK,
-                                 XAIEGBL_MEM_DMABD4CTRL_LEN_MASK, XAIEGBL_MEM_DMABD5CTRL_LEN_MASK,
-                                 XAIEGBL_MEM_DMABD6CTRL_LEN_MASK, XAIEGBL_MEM_DMABD7CTRL_LEN_MASK};
+    uint64_t offsets[NUM_BDS] = {XAIEGBL_MEM_DMABD0CTRL,            XAIEGBL_MEM_DMABD1CTRL,
+                                 XAIEGBL_MEM_DMABD2CTRL,            XAIEGBL_MEM_DMABD3CTRL,
+                                 XAIEGBL_MEM_DMABD4CTRL,            XAIEGBL_MEM_DMABD5CTRL,
+                                 XAIEGBL_MEM_DMABD6CTRL,            XAIEGBL_MEM_DMABD7CTRL};
+    uint32_t lsbs[NUM_BDS]    = {XAIEGBL_MEM_DMABD0CTRL_LEN_LSB,    XAIEGBL_MEM_DMABD1CTRL_LEN_LSB,
+                                 XAIEGBL_MEM_DMABD2CTRL_LEN_LSB,    XAIEGBL_MEM_DMABD3CTRL_LEN_LSB,
+                                 XAIEGBL_MEM_DMABD4CTRL_LEN_LSB,    XAIEGBL_MEM_DMABD5CTRL_LEN_LSB,
+                                 XAIEGBL_MEM_DMABD6CTRL_LEN_LSB,    XAIEGBL_MEM_DMABD7CTRL_LEN_LSB};
+    uint32_t masks[NUM_BDS]   = {XAIEGBL_MEM_DMABD0CTRL_LEN_MASK,   XAIEGBL_MEM_DMABD1CTRL_LEN_MASK,
+                                 XAIEGBL_MEM_DMABD2CTRL_LEN_MASK,   XAIEGBL_MEM_DMABD3CTRL_LEN_MASK,
+                                 XAIEGBL_MEM_DMABD4CTRL_LEN_MASK,   XAIEGBL_MEM_DMABD5CTRL_LEN_MASK,
+                                 XAIEGBL_MEM_DMABD6CTRL_LEN_MASK,   XAIEGBL_MEM_DMABD7CTRL_LEN_MASK};
+    uint32_t valids[NUM_BDS]  = {XAIEGBL_MEM_DMABD0CTRL_VALBD_MASK, XAIEGBL_MEM_DMABD1CTRL_VALBD_MASK,
+                                 XAIEGBL_MEM_DMABD2CTRL_VALBD_MASK, XAIEGBL_MEM_DMABD3CTRL_VALBD_MASK,
+                                 XAIEGBL_MEM_DMABD4CTRL_VALBD_MASK, XAIEGBL_MEM_DMABD5CTRL_VALBD_MASK,
+                                 XAIEGBL_MEM_DMABD6CTRL_VALBD_MASK, XAIEGBL_MEM_DMABD7CTRL_VALBD_MASK};
 
     auto tileOffset = _XAie_GetTileAddr(aieDevInst, row + 1, column);
     for (int bd = 0; bd < NUM_BDS; ++bd) {
       uint32_t regValue = 0;
       XAie_Read32(aieDevInst, tileOffset + offsets[bd], &regValue);
-      uint32_t bdBytes = BYTES_PER_WORD * (((regValue >> lsbs[bd]) & masks[bd]) + ACTUAL_OFFSET);
-      payloadValue = std::max(bdBytes, payloadValue);
+      
+      if (regValue & valids[bd]) {
+        uint32_t bdBytes = BYTES_PER_WORD * (((regValue >> lsbs[bd]) & masks[bd]) + ACTUAL_OFFSET);
+        payloadValue = std::max(bdBytes, payloadValue);
+      }
     }
 
     return payloadValue;
