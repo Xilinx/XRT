e99010628c9c2e53fe62d14844ab8144bd277908
pgschuey <paul.schumacher@xilinx.com>
2022-01-07 17:12:57 -0700

    Added ability to control counter usage for AIE trace (#6015)

--- src/runtime_src/xdp/profile/database/static_info/aie_constructs.h
+++ src/runtime_src/xdp/profile/database/static_info/aie_constructs.h
@@ -40,8 +40,8 @@ namespace xdp {
 
     AIECounter(uint32_t i, uint16_t col, uint16_t r, uint8_t num, 
                uint16_t start, uint16_t end, uint8_t reset,
-               uint32_t load, double freq, std::string mod, 
-               std::string aieName)
+               uint32_t load, double freq, const std::string& mod, 
+               const std::string& aieName)
       : id(i)
       , column(col)
       , row(r)
