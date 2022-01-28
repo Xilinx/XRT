cf41735136c49a561641699af8fe1acae096a255
rbramand <rbramand@xilinx.com>
2022-01-28 11:47:32 +0530

    CR-1119089 Add init script to inform apu booted successfully
    > Added changes in build_edge.sh to create a init script that will run after apu boot
    > This init script will write to sysfs entry exposed by zocl to indicate apu booted successfully
    > Fixed build_edge.sh -full compilation error
    > Minor fix: Added -n option to sbuild to print log to stdout

--- build/build_edge.sh
+++ build/build_edge.sh
@@ -119,6 +119,76 @@ do_install:append(){
 }
 EOF
 
+    # Create startup script to write to sysfs entry to indicate apu booted
+    SERVICE_FILE=project-spec/meta-user/recipes-apps/apu-boot/files/apu-boot.service
+    BB_FILE=project-spec/meta-user/recipes-apps/apu-boot/apuboot.bb
+    INIT_SCRIPT=project-spec/meta-user/recipes-apps/apu-boot/files/apu-boot
+
+    petalinux-create -t apps --template install -n apu-boot --enable
+
+    # Create service file
+cat << EOF > $SERVICE_FILE
+[Unit]
+Description=apu-boot
+ 
+[Service]
+ExecStart=/usr/bin/apu-boot
+StandardOutput=journal+console
+ 
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    # Create script to check apu boot
+    /bin/rm -rf $INIT_SCRIPT
+cat << EOF > $INIT_SCRIPT
+#!/bin/sh
+if [ -e /sys/bus/platform/devices/rpu-channel/ready ]; then
+	echo 1 > /sys/bus/platform/devices/rpu-channel/ready
+fi
+EOF
+
+    # Recreate bb file
+    /bin/rm -rf $BB_FILE
+cat << EOF > $BB_FILE
+SUMMARY = "apu-boot application"
+SECTION = "PETALINUX/apps"
+LICENSE = "MIT"
+ 
+LIC_FILES_CHKSUM = "file://\${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+ 
+SRC_URI = "file://apu-boot \
+        file://apu-boot.service \
+"
+ 
+S = "\${WORKDIR}"
+ 
+FILESEXTRAPATHS:prepend := "\${THISDIR}/files:"
+ 
+inherit update-rc.d systemd
+ 
+INITSCRIPT_NAME = "apu-boot"
+INITSCRIPT_PARAMS = "start 99 S ."
+ 
+SYSTEMD_PACKAGES = "\${PN}"
+SYSTEMD_SERVICE:\${PN} = "apu-boot.service"
+SYSTEMD_AUTO_ENABLE:\${PN}="enable"
+ 
+do_install() {
+        if \${@bb.utils.contains('DISTRO_FEATURES', 'sysvinit', 'true', 'false', d)}; then
+                install -d \${D}\${sysconfdir}/init.d/
+                install -m 0755 \${WORKDIR}/apu-boot \${D}\${sysconfdir}/init.d/
+        fi
+ 
+        install -d \${D}\${bindir}
+        install -m 0755 \${WORKDIR}/apu-boot \${D}\${bindir}/
+        install -d \${D}\${systemd_system_unitdir}
+        install -m 0644 \${WORKDIR}/apu-boot.service \${D}\${systemd_system_unitdir}
+}
+ 
+FILES:\${PN} += "\${@bb.utils.contains('DISTRO_FEATURES','sysvinit','\${sysconfdir}/*', '', d)}"
+EOF
+ 
 }
 
 # --- End internal functions
