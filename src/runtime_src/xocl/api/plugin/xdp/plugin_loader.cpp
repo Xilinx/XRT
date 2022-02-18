031e38218234f806935ce2eca99e5e150726f9d4
Jason Villarreal <jvillar@xilinx.com>
2022-02-18 13:00:56 -0800

    Adding warning message for combination of xrt.ini switches that may not work as expected

--- src/runtime_src/xocl/api/plugin/xdp/plugin_loader.cpp
+++ src/runtime_src/xocl/api/plugin/xdp/plugin_loader.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2016-2022 Xilinx, Inc
+ * Copyright (C) 2016-2022 Xilinx, Inc and AMD, Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License"). You may
  * not use this file except in compliance with the License. A copy of the
@@ -61,6 +61,12 @@ namespace plugins {
       std::string msg = "The xrt.ini flag \"opencl_summary\" is deprecated and will be removed in a future release.  A summary file is generated when when any profiling is enabled, so please use the appropriate settings from \"opencl_trace=true\", \"device_counter=true\", and \"device_trace=true.\"" ;
       xrt_core::message::send(xrt_core::message::severity_level::warning, "XRT",
                               msg) ;
+    }
+
+    if (xrt_core::config::get_opencl_summary() && xrt_core::config::get_host_trace()) {
+      std::string msg = "The generic host_trace option may not work as expected due to the inclusion of the deprecated opencl_summary option.  For OpenCL level trace, please specify opencl_trace=true when using opencl_summary=true in the xrt.ini file.";
+      xrt_core::message::send(xrt_core::message::severity_level::warning, "XRT",
+                              msg);
     }
 
     if (xrt_core::config::get_data_transfer_trace() != "off") {
