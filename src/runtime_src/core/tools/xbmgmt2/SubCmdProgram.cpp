609cb79601594e70a73d040358c266d05b657c18
Daniel Benusovich <dbenusov@xilinx.com>
2022-02-21 11:55:24 -0800

    CR-1101252 Add positional argument detection to throw errors for unexpected arguments

--- src/runtime_src/core/tools/xbmgmt2/SubCmdProgram.cpp
+++ src/runtime_src/core/tools/xbmgmt2/SubCmdProgram.cpp
@@ -703,12 +703,13 @@ SubCmdProgram::execute(const SubCmdOptions& _options) const
   allOptions.add(commonOptions);
   allOptions.add(hiddenOptions);
 
+  po::positional_options_description positionals;
 
   // Parse sub-command ...
   po::variables_map vm;
 
   try {
-    po::store(po::command_line_parser(_options).options(allOptions).run(), vm);
+    po::store(po::command_line_parser(_options).options(allOptions).positional(positionals).run(), vm);
     po::notify(vm); // Can throw
   } catch (po::error& e) {
     std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
