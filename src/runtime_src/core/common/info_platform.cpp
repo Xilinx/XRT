7fa7c1356505006fee5ad57b2679a619fc360369
Rajkumar Rampelli <rajkumar@xilinx.com>
2022-03-07 23:20:01 +0530

    Fix review comments
    Signed-off-by: Rajkumar Rampelli <rajkumar@xilinx.com>

--- src/runtime_src/core/common/info_platform.cpp
+++ src/runtime_src/core/common/info_platform.cpp
@@ -157,12 +157,12 @@ add_controller_info(const xrt_core::device* device, ptree_type& pt)
                           % ((versionValue >> (0 * 8)) & 0xFF)); // Version
     cmc.add("version", version);
     std::string sn = xrt_core::device_query<xq::xmc_serial_num>(device);
-    if(sn.empty() || (sn == "N/A"))
+    if(sn.empty() || boost::iequals(sn, "N/A"))
       sn = xrt_core::device_query<xq::hwmon_sdm_serial_num>(device);
     cmc.add("serial_number", sn);
 
     std::string oid = xq::oem_id::parse(xrt_core::device_query<xq::oem_id>(device));
-    if(oid.empty() || (oid == "N/A"))
+    if(oid.empty() || boost::iequals(oid, "N/A"))
       oid = xrt_core::device_query<xq::hwmon_sdm_oem_id>(device);
     cmc.add("oem_id", oid);
 
