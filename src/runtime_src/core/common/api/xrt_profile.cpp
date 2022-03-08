4905c9b3a45643c5b70361572eed956751b1d5e1
Anurag Dubey <anurag6dubey@gmail.com>
2022-03-08 15:48:33 -0700

    cleanup xrt profile

--- src/runtime_src/core/common/api/xrt_profile.cpp
+++ src/runtime_src/core/common/api/xrt_profile.cpp
@@ -18,72 +18,75 @@
 // This file implements XRT kernel APIs as declared in 
 // core/include/experimental/xrt_profile.h
 
-#include <functional>
-#include <mutex>
-
 #define XCL_DRIVER_DLL_EXPORT 
 #define XRT_CORE_COMMON_SOURCE
 #include "core/include/experimental/xrt_profile.h"
 
-#include "core/common/module_loader.h"
-#include "core/common/utils.h"
 #include "core/common/dlfcn.h"
 #include "core/common/message.h"
+#include "core/common/module_loader.h"
+#include "core/common/utils.h"
+
+#include <functional>
+#include <mutex>
 
 namespace xrt { namespace profile {
 
-  user_range::user_range(const char* label, const char* tooltip) : active(true)
-  {
-    id = static_cast<uint32_t>(xrt_core::utils::issue_id()) ;
+user_range::user_range(const char* label, const char* tooltip) : active(true)
+{
+  id = static_cast<uint32_t>(xrt_core::utils::issue_id());
 
-    xrtURStart(id, label, tooltip) ;
-  }
+  xrtURStart(id, label, tooltip);
+}
 
-  user_range::user_range() : id(0), active(false)
-  {
-  }
+user_range::user_range() : id(0), active(false)
+{
+}
 
-  user_range::~user_range()
-  {
-    if (active) xrtUREnd(id) ;
-  }
+user_range::~user_range()
+{
+  if (active)
+    xrtUREnd(id);
+}
 
-  void user_range::start(const char* label, const char* tooltip)
-  {
-    // Handle case where start is called while started
-    if (active) xrtUREnd(id) ;
+void user_range::start(const char* label, const char* tooltip)
+{
+  // Handle case where start is called while started
+  if (active)
+    xrtUREnd(id);
 
-    id = static_cast<uint32_t>(xrt_core::utils::issue_id()) ;
-    xrtURStart(id, label, tooltip) ;
-    active = true ;
-  }
+  id = static_cast<uint32_t>(xrt_core::utils::issue_id());
+  xrtURStart(id, label, tooltip);
+  active = true;
+}
 
-  void user_range::end()
-  {
-    // Handle case when end when not tracking time
-    if (!active) return ;
+void user_range::end()
+{
+  // Handle case when end when not tracking time
+  if (!active)
+    return;
 
-    xrtUREnd(id) ;
-    active = false ;
-  }
+  xrtUREnd(id);
+  active = false;
+}
 
-  user_event::user_event()
-  {
-  }
+user_event::user_event()
+{
+}
 
-  user_event::~user_event()
-  {
-  }
+user_event::~user_event()
+{
+}
 
-  void user_event::mark(const char* label)
-  {
-    xrtUEMark(label) ;
-  }
+void user_event::mark(const char* label)
+{
+  xrtUEMark(label);
+}
 
-  void user_event::mark_time_ns(const std::chrono::nanoseconds& time_ns, const char* label)
-  {
-    xrtUEMarkTimeNs(static_cast<unsigned long long int>(time_ns.count()), label);
-  }
+void user_event::mark_time_ns(const std::chrono::nanoseconds& time_ns, const char* label)
+{
+  xrtUEMarkTimeNs(static_cast<unsigned long long int>(time_ns.count()), label);
+}
 
 }} // end namespaces profile and xrt
 
@@ -91,90 +94,95 @@ namespace xrt { namespace profile {
 // Anonymous namespace for dynamic loading and connection
 namespace {
   
-  std::function<void (unsigned int, const char*, const char*)> user_range_start_cb ;
-  std::function<void (unsigned int)> user_range_end_cb ;
-  std::function<void (const char*)> user_event_cb ;
-  std::function<void (unsigned long long int, const char*)> user_event_time_ns_cb ;
+std::function<void (unsigned int, const char*, const char*)> user_range_start_cb;
+std::function<void (unsigned int)> user_range_end_cb;
+std::function<void (const char*)> user_event_cb;
+std::function<void (unsigned long long int, const char*)> user_event_time_ns_cb;
 
-  static void register_user_functions(void* handle)
-  {
-    using dtype = void(*)(unsigned int, const char*, const char*);
-    user_range_start_cb = (dtype)(xrt_core::dlsym(handle, "user_event_start_cb"));
-    if (xrt_core::dlerror() != NULL) user_range_start_cb = nullptr ;
+static void register_user_functions(void* handle)
+{
+  using dtype = void(*)(unsigned int, const char*, const char*);
+  user_range_start_cb = (dtype)(xrt_core::dlsym(handle, "user_event_start_cb"));
+  if (xrt_core::dlerror() != NULL)
+    user_range_start_cb = nullptr;
 
-    using ftype = void(*)(unsigned int);
-    user_range_end_cb = (ftype)(xrt_core::dlsym(handle, "user_event_end_cb")) ;
-    if (xrt_core::dlerror() != NULL) user_range_end_cb = nullptr ;
+  using ftype = void(*)(unsigned int);
+  user_range_end_cb = (ftype)(xrt_core::dlsym(handle, "user_event_end_cb"));
+  if (xrt_core::dlerror() != NULL)
+    user_range_end_cb = nullptr;
 
-    using btype = void(*)(const char*);
-    user_event_cb = (btype)(xrt_core::dlsym(handle, "user_event_happened_cb")) ;
-    if (xrt_core::dlerror() != NULL) user_event_cb = nullptr ;
+  using btype = void(*)(const char*);
+  user_event_cb = (btype)(xrt_core::dlsym(handle, "user_event_happened_cb"));
+  if (xrt_core::dlerror() != NULL)
+    user_event_cb = nullptr;
 
-    using ctype = void(*)(unsigned long long int, const char*);
-    user_event_time_ns_cb = (ctype)(xrt_core::dlsym(handle, "user_event_time_ns_cb")) ;
-    if (xrt_core::dlerror() != NULL) user_event_time_ns_cb = nullptr ;
-  }
+  using ctype = void(*)(unsigned long long int, const char*);
+  user_event_time_ns_cb = (ctype)(xrt_core::dlsym(handle, "user_event_time_ns_cb"));
+  if (xrt_core::dlerror() != NULL)
+    user_event_time_ns_cb = nullptr;
+}
 
-  static void load_user_profiling_plugin()
-  {
-    static xrt_core::module_loader user_event_loader("xdp_user_plugin",
-                                                     register_user_functions,
-                                                     nullptr) ;
-  }
+static void load_user_profiling_plugin()
+{
+  static xrt_core::module_loader user_event_loader("xdp_user_plugin",
+                                                   register_user_functions,
+                                                   nullptr);
+}
 
 } // end anonymous
 
-extern "C"
+extern "C" {
+
+void xrtURStart(unsigned int id, const char* label, const char* tooltip)
 {
-  void xrtURStart(unsigned int id, const char* label, const char* tooltip) 
-  {
-    try {
-      load_user_profiling_plugin() ;
-      if (user_range_start_cb != nullptr) 
-        user_range_start_cb(id, label, tooltip) ;
-    }
-    catch(const std::exception& ex)
-    {
-      xrt_core::message::send(xrt_core::message::severity_level::error, "XRT", ex.what()) ;
-    }
+  try {
+    load_user_profiling_plugin();
+    if (user_range_start_cb != nullptr)
+      user_range_start_cb(id, label, tooltip);
   }
-
-  void xrtUREnd(unsigned int id)
+  catch (const std::exception& ex)
   {
-    try {
-      load_user_profiling_plugin() ;
-      if (user_range_end_cb != nullptr) 
-        user_range_end_cb(id);
-    }
-    catch(const std::exception& ex)
-    {
-      xrt_core::message::send(xrt_core::message::severity_level::error, "XRT", ex.what()) ;
-    }
-  }
-
-  void xrtUEMark(const char* label)
-  {
-    try {
-      load_user_profiling_plugin() ;
-      if (user_event_cb != nullptr) 
-        user_event_cb(label) ;
-    }
-    catch(const std::exception& ex)
-    {
-      xrt_core::message::send(xrt_core::message::severity_level::error, "XRT", ex.what()) ;
-    }
-  }
-
-  void xrtUEMarkTimeNs(unsigned long long int time_ns, const char* label)
-  {
-    try {
-      load_user_profiling_plugin();
-      if (user_event_time_ns_cb != nullptr)
-        user_event_time_ns_cb(time_ns, label);
-    }
-    catch(const std::exception& ex)
-    {
-      xrt_core::message::send(xrt_core::message::severity_level::error, "XRT", ex.what()) ;
-    }
+    xrt_core::message::send(xrt_core::message::severity_level::error, "XRT", ex.what());
   }
 }
+
+void xrtUREnd(unsigned int id)
+{
+  try {
+    load_user_profiling_plugin();
+    if (user_range_end_cb != nullptr)
+      user_range_end_cb(id);
+  }
+  catch (const std::exception& ex)
+  {
+    xrt_core::message::send(xrt_core::message::severity_level::error, "XRT", ex.what());
+  }
+}
+
+void xrtUEMark(const char* label)
+{
+  try {
+    load_user_profiling_plugin();
+    if (user_event_cb != nullptr)
+      user_event_cb(label);
+  }
+  catch (const std::exception& ex)
+  {
+    xrt_core::message::send(xrt_core::message::severity_level::error, "XRT", ex.what());
+  }
+}
+
+void xrtUEMarkTimeNs(unsigned long long int time_ns, const char* label)
+{
+  try {
+    load_user_profiling_plugin();
+    if (user_event_time_ns_cb != nullptr)
+      user_event_time_ns_cb(time_ns, label);
+  }
+  catch (const std::exception& ex)
+  {
+    xrt_core::message::send(xrt_core::message::severity_level::error, "XRT", ex.what());
+  }
+}
+
+} // extern C
--- src/runtime_src/core/include/experimental/xrt_profile.h
+++ src/runtime_src/core/include/experimental/xrt_profile.h
@@ -26,168 +26,168 @@
 
 namespace xrt { namespace profile {
 
-  /*!
-   * @class user_range
+/*!
+ * @class user_range
+ *
+ * @brief
+ * xrt::profile::user_range xrt::profile::user_range is used to track start and stop times
+ * between two user defined points in host program and add them to profiling output for
+ * visualization using post-processing tools.
+ *
+ * @details
+ * By turning on various trace options in the xrt.ini file,
+ * XRT applications will track events and generate files, which are turned into a
+ * timeline visualization and summary via post-processing tools.  A user range is used
+ * to explicitly add events measured from start to stop from the host code to that
+ * timeline visualization and summary.
+ * Usage -
+ * 1. If a user_range is instantiated using the default constructor, no time is marked
+ *    until the user calls start with two strings (label and tooltip)
+ * 2. The user must call start() and stop() to mark ranges of time they are interested in.
+ *    If stop() is not called, then any range being tracked lasts until the user_range object is destructed
+ * 3. As a shortcut, the user can instantiate a user_range object passing two strings (label and tooltip).  This starts monitoring the range immediately
+ *    The user_range object can be reused any number of times, by calling start()/stop() pairs in the host code
+ * 4. Multiple sequential calls to start() ignore all but the first call
+ * 5. Multiple sequential calls to stop() ignore all but the first call
+ */
+
+class user_range
+{
+private:
+  uint32_t id;
+  bool active;
+public:
+  /**
+   * user_range() - Default Constructor for user range
    *
-   * @brief
-   * xrt::profile::user_range xrt::profile::user_range is used to track start and stop times
-   * between two user defined points in host program and add them to profiling output for
-   * visualization using post-processing tools.
-   *
-   * @details
-   * By turning on various trace options in the xrt.ini file,
-   * XRT applications will track events and generate files, which are turned into a
-   * timeline visualization and summary via post-processing tools.  A user range is used
-   * to explicitly add events measured from start to stop from the host code to that
-   * timeline visualization and summary.
-   * Usage -
-   * 1. If a user_range is instantiated using the default constructor, no time is marked
-   *    until the user calls start with two strings (label and tooltip)
-   * 2. The user must call start() and stop() to mark ranges of time they are interested in.
-   *    If stop() is not called, then any range being tracked lasts until the user_range object is destructed
-   * 3. As a shortcut, the user can instantiate a user_range object passing two strings (label and tooltip).  This starts monitoring the range immediately
-   *    The user_range object can be reused any number of times, by calling start()/stop() pairs in the host code
-   * 4. Multiple sequential calls to start() ignore all but the first call
-   * 5. Multiple sequential calls to stop() ignore all but the first call
+   * If this constructor is called, the object is created but the 
+   * beginning of the range time is not marked.
    */
+  XCL_DRIVER_DLLESPEC
+  user_range();
 
-  class user_range
-  {
-  private:
-    uint32_t id ;
-    bool active ;
-  public:
-    /**
-     * user_range() - Default Constructor for user range
-     *
-     * If this constructor is called, the object is created but the 
-     * beginning of the range time is not marked.
-     */
-    XCL_DRIVER_DLLESPEC
-    user_range() ;
-
-    /**
-     * user_range() - Constructor for user range with text
-     *
-     * @param label
-     * The string that appears embedded on the waveform for this range
-     * @param tooltip
-     * The string that appears on the waveform when hovering over the range
-     *
-     * Construct an object and start keeping track of time immediately
-     * upon construction.
-     */
-    XCL_DRIVER_DLLESPEC
-    user_range(const char* label, const char* tooltip = nullptr) ;
-
-    /**
-     * user_range() - Copy constructor
-     * 
-     * Explicitly removed as ranges are actively keeping track of real time
-     *  and cannot be copied.
-     */
-    user_range(const user_range& rhs) = delete ;
-
-    /**
-     * user_range() - Move constructor
-     *
-     * Explicitly removed as ranges are actively keeping track of real time
-     *  and cannot be moved.
-     */
-    user_range(user_range&& rhs) = delete ;
-
-    /**
-     * operator=()
-     * 
-     * Explicitly removed as ranges should not be assigned
-     */
-    user_range& operator=(const user_range& rhs) = delete ;
-    user_range& operator=(user_range&& rhs) = delete ;
-
-    /**
-     * ~user_range() - Destructor
-     *
-     * If the range is still active, mark this point as the end of the range
-     */
-    XCL_DRIVER_DLLESPEC
-    ~user_range() ;
-
-    /**
-     * start() - Mark the start position of a user range
-     *
-     * @param label
-     * The string that appears embedded on the waveform for this range
-     * @param tooltip
-     * The string that appears on the waveform when hovering over the range
-     *
-     * If the range is still actively recording time, end the current
-     * range and start a new one.
-     */
-    XCL_DRIVER_DLLESPEC
-    void start(const char* label, const char* tooltip = nullptr) ;
-
-    /**
-     * end() - Mark the end position of a user range
-     *
-     * If the range has already been ended, do nothing.  Otherwise
-     * mark the end position of the range and stop tracking time.
-     */    
-    XCL_DRIVER_DLLESPEC
-    void end() ;
-  } ;
-
-  /*!
-   * @class user_event
+  /**
+   * user_range() - Constructor for user range with text
    *
-   * @brief
-   * xrt::profile::user_event mark a specific point in execution with a label for later visualization
+   * @param label
+   * The string that appears embedded on the waveform for this range
+   * @param tooltip
+   * The string that appears on the waveform when hovering over the range
    *
-   * @details
-   * A user event can be generated from inside host code and optionally tagged with a label.
-   * These events are post-processed and represented on summary and trace waveforms as markers.
+   * Construct an object and start keeping track of time immediately
+   * upon construction.
    */
-  class user_event
-  {
-  public:
-    /**
-     * user_event() - Constructor
-     *
-     * Create an object, but do not mark any particular time
-     */
-    XCL_DRIVER_DLLESPEC
-    user_event() ;
+  XCL_DRIVER_DLLESPEC
+  user_range(const char* label, const char* tooltip = nullptr);
 
-    /**
-     * ~user_event() - Destructor
-     *
-     * Destroy the object, but do not mark any particular time
-     */
-    XCL_DRIVER_DLLESPEC
-    ~user_event() ;
+  /**
+   * user_range() - Copy constructor
+   *
+   * Explicitly removed as ranges are actively keeping track of real time
+   *  and cannot be copied.
+   */
+  user_range(const user_range& rhs) = delete;
+
+  /**
+   * user_range() - Move constructor
+   *
+   * Explicitly removed as ranges are actively keeping track of real time
+   *  and cannot be moved.
+   */
+  user_range(user_range&& rhs) = delete;
+
+  /**
+   * operator=()
+   *
+   * Explicitly removed as ranges should not be assigned
+   */
+  user_range& operator=(const user_range& rhs) = delete;
+  user_range& operator=(user_range&& rhs) = delete;
+
+  /**
+   * ~user_range() - Destructor
+   *
+   * If the range is still active, mark this point as the end of the range
+   */
+  XCL_DRIVER_DLLESPEC
+  ~user_range();
+
+  /**
+   * start() - Mark the start position of a user range
+   *
+   * @param label
+   * The string that appears embedded on the waveform for this range
+   * @param tooltip
+   * The string that appears on the waveform when hovering over the range
+   *
+   * If the range is still actively recording time, end the current
+   * range and start a new one.
+   */
+  XCL_DRIVER_DLLESPEC
+  void start(const char* label, const char* tooltip = nullptr);
+
+  /**
+   * end() - Mark the end position of a user range
+   *
+   * If the range has already been ended, do nothing.  Otherwise
+   * mark the end position of the range and stop tracking time.
+   */
+  XCL_DRIVER_DLLESPEC
+  void end();
+};
+
+/*!
+ * @class user_event
+ *
+ * @brief
+ * xrt::profile::user_event mark a specific point in execution with a label for later visualization
+ *
+ * @details
+ * A user event can be generated from inside host code and optionally tagged with a label.
+ * These events are post-processed and represented on summary and trace waveforms as markers.
+ */
+class user_event
+{
+public:
+  /**
+   * user_event() - Constructor
+   *
+   * Create an object, but do not mark any particular time
+   */
+  XCL_DRIVER_DLLESPEC
+  user_event();
+
+  /**
+   * ~user_event() - Destructor
+   *
+   * Destroy the object, but do not mark any particular time
+   */
+  XCL_DRIVER_DLLESPEC
+  ~user_event();
     
-    /**
-     * mark() - Mark the current moment in time with a marker on the waveform
-     *
-     * @param label
-     * An optional label that will be displayed on top of marker in waveform
-     *
-     */
-    XCL_DRIVER_DLLESPEC
-    void mark(const char* label = nullptr) ;
+  /**
+   * mark() - Mark the current moment in time with a marker on the waveform
+   *
+   * @param label
+   * An optional label that will be displayed on top of marker in waveform
+   *
+   */
+  XCL_DRIVER_DLLESPEC
+  void mark(const char* label = nullptr);
 
-    /**
-     * mark_time_ns() - Mark a custom moment in time with a marker on the waveform
-     *
-     * @param time_ns
-     * Time duration in nanoseconds since appliction start.
-     * This must be compatible with xrt_core::time_ns() API.
-     * @param label
-     * An optional label that will be displayed on top of marker in waveform
-     *
-     */
-    XCL_DRIVER_DLLESPEC
-    void mark_time_ns(const std::chrono::nanoseconds& time_ns, const char* label = nullptr) ;
-  } ;
+  /**
+   * mark_time_ns() - Mark a custom moment in time with a marker on the waveform
+   *
+   * @param time_ns
+   * Time duration in nanoseconds since appliction start.
+   * This must be compatible with xrt_core::time_ns() API.
+   * @param label
+   * An optional label that will be displayed on top of marker in waveform
+   *
+   */
+  XCL_DRIVER_DLLESPEC
+  void mark_time_ns(const std::chrono::nanoseconds& time_ns, const char* label = nullptr);
+};
 
 } // end namespace profile
 } // end namespace xrt
@@ -205,7 +205,7 @@ extern "C" {
  *
  */
 XCL_DRIVER_DLLESPEC
-void xrtURStart(unsigned int id, const char* label, const char* tooltip = nullptr) ;
+void xrtURStart(unsigned int id, const char* label, const char* tooltip = nullptr);
 
 /**
  * xrtUREnd() - Mark the end time in the user code of a range
@@ -215,7 +215,7 @@ void xrtURStart(unsigned int id, const char* label, const char* tooltip = nullpt
  *
  */
 XCL_DRIVER_DLLESPEC
-void xrtUREnd(unsigned int id) ;
+void xrtUREnd(unsigned int id);
 
 /**
  * xrtUEMark() - Mark the current time as when something happened
@@ -225,7 +225,7 @@ void xrtUREnd(unsigned int id) ;
  *
  */
 XCL_DRIVER_DLLESPEC
-void xrtUEMark(const char* label) ;
+void xrtUEMark(const char* label);
 
 /**
  * xrtUEMarkTimeNs() - Mark a custom time as when something happened
@@ -237,7 +237,7 @@ void xrtUEMark(const char* label) ;
  *
  */
 XCL_DRIVER_DLLESPEC
-void xrtUEMarkTimeNs(unsigned long long int time_ns, const char* label) ;
+void xrtUEMarkTimeNs(unsigned long long int time_ns, const char* label);
 
 #ifdef __cplusplus
 }
