5421a33933ba27f4487d62065d9afa0db0d95d69
bryanloz-xilinx <90728438+bryanloz-xilinx@users.noreply.github.com>
2022-03-09 16:49:54 -0800

    Update src/runtime_src/tools/scripts/build_xrt_with_docker.sh
    Co-authored-by: Soren Soe <2106410+stsoe@users.noreply.github.com>

--- src/runtime_src/tools/scripts/build_xrt_with_docker.sh
+++ src/runtime_src/tools/scripts/build_xrt_with_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # SPDX-License-Identifier: Apache-2.0
-# Copyright (C) 2019-2022 Xilinx, Inc. All rights reserved.
+# Copyright (C) 2022 Xilinx, Inc. All rights reserved.
 #
 
 # Must be Ubuntu 18.04 or Ubuntu 20.04
