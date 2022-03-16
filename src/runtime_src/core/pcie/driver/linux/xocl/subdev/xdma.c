361b7611f67ea12f7bada43692578f847277a33c
Lizhi Hou <36547078+houlz0507@users.noreply.github.com>
2022-03-16 08:24:54 -0700

    Workaround: limit versal to use 2 DMA channels (#6425)

--- src/runtime_src/core/pcie/driver/linux/xocl/subdev/xdma.c
+++ src/runtime_src/core/pcie/driver/linux/xocl/subdev/xdma.c
@@ -473,6 +473,11 @@ static int xdma_probe(struct platform_device *pdev)
 		goto failed;
 	}
 
+	if (XOCL_DSA_IS_VERSAL_ES3(xdev)) {
+		xocl_info(&pdev->dev, "VERSAL ES3, set to 2 channels");
+		xdma->channel = 2;
+	}
+
 	xdma->dma_handle = xdma_device_open(XOCL_MODULE_NAME, XDEV(xdev)->pdev,
 			&xdma->max_user_intr,
 			&xdma->channel, &xdma->channel, false);
@@ -481,11 +486,6 @@ static int xdma_probe(struct platform_device *pdev)
 		ret = -EIO;
 		goto failed;
 	}
-	if (XOCL_DSA_IS_VERSAL_ES3(xdev)) {
-		xocl_info(&pdev->dev, "VERSAL ES3, set to 2 channels");
-		xdma->channel = 2;
-	}
-
 	xdma->user_msix_table = devm_kzalloc(&pdev->dev,
 			xdma->max_user_intr *
 			sizeof(struct xdma_irq), GFP_KERNEL);
