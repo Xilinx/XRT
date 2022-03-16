c9bd25c86090e6fa0f324236d6ef2881c2bfe57d
Nishant Mysore <nishraptor@gmail.com>
2022-03-16 10:21:21 -0600

    fixed space

--- src/runtime_src/xdp/profile/plugin/aie_trace/aie_trace_plugin.cpp
+++ src/runtime_src/xdp/profile/plugin/aie_trace/aie_trace_plugin.cpp
@@ -448,7 +448,7 @@ namespace xdp {
         std::string msg("Unable to parse aie_trace_start_time. Setting start time to 0.");
         xrt_core::message::send(xrt_core::message::severity_level::warning, "XRT", msg);
       }
-    }else{  
+    } else {  
       std::string msg("Unable to parse aie_trace_start_time. Setting start time to 0.");
       xrt_core::message::send(xrt_core::message::severity_level::warning, "XRT", msg);
     }
