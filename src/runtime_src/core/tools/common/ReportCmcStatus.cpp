6a64fb45febe29645e3c8e4653b6ad99165b0cb5
Raj Kumar Rampelli <49669992+rajkumar-xilinx@users.noreply.github.com>
2022-03-18 14:59:12 +0530

    Update src/runtime_src/core/tools/common/ReportCmcStatus.cpp
    Co-authored-by: Shivangi Agarwal <shivangiagarwal53@gmail.com>

--- src/runtime_src/core/tools/common/ReportCmcStatus.cpp
+++ src/runtime_src/core/tools/common/ReportCmcStatus.cpp
@@ -98,11 +98,15 @@ ReportCmcStatus::writeReport( const xrt_core::device* /*_pDevice*/,
   try {
     boost::property_tree::ptree cmc_scale = cmc.get_child("scaling");
     _output << boost::format("  %-22s :\n") % "Runtime clock scaling feature";
-    auto cmc_support = cmc_scale.get<bool>("supported");
-    if (!cmc_support) {
+    if (!cmc_scale.get<bool>("supported")) {
       _output << "    Information unavailable or not applicable" << std::endl;
       return;
     }
+    if (!cmc_scale.get<bool>("enabled")) {
+      _output << "    Not enabled" << std::endl;
+      return;
+    }
+
     _output << boost::format("    %s : %s\n") % "Supported" % cmc_scale.get<std::string>("supported");
     _output << boost::format("    %s : %s\n") % "Enabled" % cmc_scale.get<std::string>("enabled");
     cmc_scale = cmc.get_child("scaling").get_child("shutdown_threshold_limits");
