1ccae37bf42a3f968a6ac4aaefb90248ed485b8f
AShivangi <shivangiagarwal53@gmail.com>
2022-03-23 11:45:06 -0700

    merge master

--- src/runtime_src/core/tools/common/ReportVmrStatus.cpp
+++ src/runtime_src/core/tools/common/ReportVmrStatus.cpp
@@ -17,7 +17,7 @@
 // ------ I N C L U D E   F I L E S -------------------------------------------
 // Local - Include Files
 #include "ReportVmrStatus.h"
-#include "XBUtilities.h"
+#include "XBUtilitiesCore.h"
 #include "core/common/info_vmr.h"
 
 #include <algorithm>
