64d60c48a86f76861e26a720772183247bc85e53
Rahul Bramandlapalli <54270708+rbramand-xilinx@users.noreply.github.com>
2022-04-05 00:02:47 +0530

    Update src/runtime_src/core/include/xrt.h
    Co-authored-by: Soren Soe <2106410+stsoe@users.noreply.github.com>

--- src/runtime_src/core/include/xrt.h
+++ src/runtime_src/core/include/xrt.h
@@ -335,9 +335,11 @@ xclGetErrorStatus(xclDeviceHandle handle, struct xclErrorStatus *info);
  * @buffer:        Pointer to device image (xclbin) in memory
  * Return:         0 on success or appropriate error number
  *
- * | Download FPGA image (AXLF) to the device. The PR bitstream is encapsulated inside xclbin as a section. 
- * | xclbin may also contains other sections which are suitably handled by the driver.
- * | **NOTE**: This API also downloads OVERLAY (dtbo) section. (Applicable only for Edge Platforms)
+ * Download FPGA image (AXLF) to the device. The PR bitstream is
+ * encapsulated inside xclbin as a section.  The xclbin may also
+ * contain other sections, which are suitably handled by the driver.
+ *
+ * This API also downloads OVERLAY (dtbo) section (Edge only).
  */
 XCL_DRIVER_DLLESPEC
 int
