5cb8870ab3f8c5e26844b13bd7338a3831d49927
Ishita Ghosh <43157456+IshitaGhosh@users.noreply.github.com>
2022-04-07 08:49:29 -0700

    Fix CR 1122451 : XDP should access xclbin sections only after it is registered with xrt_core::device (#6578)
    * No need to use rollover results for monitors as monitors are now 64 bit.
    
    * Remove redundant method
    
    * Add warning if continuous offload of AIE trace is enabled with GMIO flow
    
    * Fix for 1125738 : Device time 0 for HW Emu
    
    * Total kernel runtime is a device info and should be omitted from SW Emu guidance rule.
    
    * Update Copyright Year
    
    * Add guards for ts2mm_info buffers
    
    * Undo checks on ts2mm_info buffers emptiness
    
    * Read trace methods, buffer fullness check methods should be called only when device trace is enabled
    
    * Use a member in DeviceOffloadPlugin to store whether device trace is enabled. This avoids costly string comparison in config methods
    
    * Add AMD in Copyright message
    
    * Add AMD in Copyright message
    
    * When no fifo or ts2mm is present, trace inbfra cannot be initialized. But if device_counters is enabled, keep offloader object to facilitate reading of counters.
    
    * Call update device for HW Emu XDP plugin after the current xclbin gets registered in xrt_core::device
    
    * xdp::hw_emu::update_device needs to be called only when xclbin is loaded successfully.

--- src/runtime_src/core/pcie/emulation/hw_em/generic_pcie_hal2/halapi.cxx
+++ src/runtime_src/core/pcie/emulation/hw_em/generic_pcie_hal2/halapi.cxx
@@ -365,12 +365,13 @@ int xclLoadXclBin(xclDeviceHandle handle, const xclBin *buffer)
 #else
   xdp::hw_emu::flush_device(handle);
   auto ret = drv->xclLoadXclBin(buffer);
-  xdp::hw_emu::update_device(handle);
 #endif
   if (!ret) {
     auto device = xrt_core::get_userpf_device(drv);
     device->register_axlf(buffer);
 #ifndef DISABLE_DOWNLOAD_XCLBIN
+    // Call update_device only when xclbin is loaded and registered successfully
+    xdp::hw_emu::update_device(handle);
     ret = xrt_core::scheduler::init(handle, buffer);
 #endif
   }
