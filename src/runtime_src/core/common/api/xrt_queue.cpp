d507f3a8f0eeb43e9515aafdd8385d65b6b708b0
Soren Soe <2106410+stsoe@users.noreply.github.com>
2022-04-08 07:33:52 -0700

    Improve modularity of some core CMake files (#6583)
    * Improve modularity of some core CMake files
    
    Small step towards better structured makefiles using more modern CMake
    design.  Use library targets and properties with targets; avoid
    variables.
    
    * Condition xrt_graph.cpp based on XRT_AIE_BUILD

--- src/runtime_src/core/common/api/xrt_queue.cpp
+++ src/runtime_src/core/common/api/xrt_queue.cpp
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (C) 2022 Xilinx, Inc. All rights reserved.
+// Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
 
 // This file implements XRT xclbin APIs as declared in
 // core/include/experimental/xrt_queue.h
-#define XCL_DRIVER_DLL_EXPORT  // exporting xrt_queue.h
+#define XRT_API_SOURCE         // exporting xrt_queue.h
 #define XRT_CORE_COMMON_SOURCE // in same dll as core_common
 #include "core/include/experimental/xrt_queue.h"
 
