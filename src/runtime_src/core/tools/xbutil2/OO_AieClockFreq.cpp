7db9a79e3aee8038c7b84decbf55e68c300ae817
Daniel Benusovich <dbenusov@xilinx.com>
2022-04-22 09:11:58 -0700

    CR-1101252 Remove whitespace

--- src/runtime_src/core/tools/xbutil2/OO_AieClockFreq.cpp
+++ src/runtime_src/core/tools/xbutil2/OO_AieClockFreq.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2022 Xilinx, Inc 
+ * Copyright (C) 2022 Xilinx, Inc
  * Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"). You may
