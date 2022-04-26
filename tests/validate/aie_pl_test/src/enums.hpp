bbb9eb80537051a2ebc201ea0ec1842a87b2eeeb
Jeff Lin <jeffylin@xilinx.com>
2022-04-26 13:52:04 -0700

    Coding style update

--- tests/validate/aie_pl_test/src/enums.hpp
+++ tests/validate/aie_pl_test/src/enums.hpp
@@ -19,28 +19,29 @@
 namespace xf {
 namespace plctrl {
 namespace enums {
-enum class aie_cmd {
-  ADF_GRAPH_RUN = 0,
-  ADF_GRAPH_WAIT = 1,
-  ADF_GRAPH_RTP_UPDATE = 2,
-  ADF_GRAPH_RTP_READ = 3,
-  WAIT_FOR_DMA_IDLE = 4,
-  SET_DMA_BD_LENGTH = 5,
-  ENQUEUE_DMA_BD = 6,
-  LOAD_AIE_PM = 7,
-  SLEEP = 8,
-  HALT = 9,
-  SET_AIE_ITERATION = 10,
-  ENABLE_AIE_CORES = 11,
-  DISABLE_AIE_CORES = 12,
-  SYNC = 13,
-  LOOP_BEGIN = 14,
-  LOOP_END = 15,
-  SET_DMA_BD = 16,
-  UPDATE_AIE_RTP = 17
+enum class aie_cmd
+{
+    ADF_GRAPH_RUN = 0,
+    ADF_GRAPH_WAIT = 1,
+    ADF_GRAPH_RTP_UPDATE = 2,
+    ADF_GRAPH_RTP_READ = 3,
+    WAIT_FOR_DMA_IDLE = 4,
+    SET_DMA_BD_LENGTH = 5,
+    ENQUEUE_DMA_BD = 6,
+    LOAD_AIE_PM = 7,
+    SLEEP = 8,
+    HALT = 9,
+    SET_AIE_ITERATION = 10,
+    ENABLE_AIE_CORES = 11,
+    DISABLE_AIE_CORES = 12,
+    SYNC = 13,
+    LOOP_BEGIN = 14,
+    LOOP_END = 15,
+    SET_DMA_BD = 16,
+    UPDATE_AIE_RTP = 17
 };
 } // end of namespace enums
-  using namespace enums;
+using namespace enums;
 } // end of namespace plctrl
 } // end of pl
 #endif
--- tests/validate/aie_pl_test/src/pl_controller.hpp
+++ tests/validate/aie_pl_test/src/pl_controller.hpp
@@ -37,112 +37,120 @@ namespace xf {
 namespace plctrl {
 
 // re-use this code from "./core/edge/common/aie_parser.h"
-struct tile_type {
-  uint16_t row;
-  uint16_t col;
-  uint16_t itr_mem_row;
-  uint16_t itr_mem_col;
-  uint64_t itr_mem_addr;
+struct tile_type
+{
+    uint16_t row;
+    uint16_t col;
+    uint16_t itr_mem_row;
+    uint16_t itr_mem_col;
+    uint64_t itr_mem_addr;
 
-  bool is_trigger;
+    bool is_trigger;
 };
-struct rtp_type {
-  std::string name;
+struct rtp_type
+{
+    std::string name;
 
-  uint16_t selector_row;
-  uint16_t selector_col;
-  uint16_t selector_lock_id;
-  uint64_t selector_addr;
+    uint16_t selector_row;
+    uint16_t selector_col;
+    uint16_t selector_lock_id;
+    uint64_t selector_addr;
 
-  uint16_t ping_row;
-  uint16_t ping_col;
-  uint16_t ping_lock_id;
-  uint64_t ping_addr;
+    uint16_t ping_row;
+    uint16_t ping_col;
+    uint16_t ping_lock_id;
+    uint64_t ping_addr;
 
-  uint16_t pong_row;
-  uint16_t pong_col;
-  uint16_t pong_lock_id;
-  uint64_t pong_addr;
+    uint16_t pong_row;
+    uint16_t pong_col;
+    uint16_t pong_lock_id;
+    uint64_t pong_addr;
 
-  bool is_plrtp;
-  bool is_input;
-  bool is_async;
-  bool is_connected;
-  bool require_lock;
+    bool is_plrtp;
+    bool is_input;
+    bool is_async;
+    bool is_connected;
+    bool require_lock;
 };
-struct buffer_type {
-  uint16_t row;
-  uint16_t col;
-  uint16_t ch_num;
-  uint16_t lock_id;
-  uint16_t bd_num;
-  bool s2mm;
+struct buffer_type
+{
+    uint16_t row;
+    uint16_t col;
+    uint16_t ch_num;
+    uint16_t lock_id;
+    uint16_t bd_num;
+    bool s2mm;
 };
 
-class plController {
- public:
-  /* Constructor
-  */
-  plController(const std::string& xclbin_path);
-  plController(const std::string& xclbin_path, const std::string& dma_info_path);
-  /* De-constructor
-   */
-  ~plController();
+class plController
+{
+  public:
+    /* Constructor
+    */
+    plController(const std::string& xclbin_path);
+    plController(const std::string& xclbin_path,
+                 const std::string& dma_info_path);
+    /* De-constructor
+     */
+    ~plController();
 
-  void enqueue_set_aie_iteration(const std::string& graphName, int num_iter);
+    void enqueue_set_aie_iteration(const std::string& graphName, int num_iter);
 
-  void enqueue_enable_aie_cores();
+    void enqueue_enable_aie_cores();
 
-  void enqueue_disable_aie_cores();
+    void enqueue_disable_aie_cores();
 
-  void enqueue_sync(uint32_t pld);
+    void enqueue_sync(uint32_t pld);
 
-  void enqueue_loop_begin(int trip_count);
-  void enqueue_loop_end();
+    void enqueue_loop_begin(int trip_count);
+    void enqueue_loop_end();
 
-  void enqueue_set_and_enqueue_dma_bd(const std::string& portName, int idx, int dma_bd_value);
+    void enqueue_set_and_enqueue_dma_bd(const std::string& portName, int idx,
+                                        int dma_bd_value);
 
-  void enqueue_sleep(unsigned int num_cycles);
+    void enqueue_sleep(unsigned int num_cycles);
 
-  void enqueue_halt();
+    void enqueue_halt();
 
-  void enqueue_update_aie_rtp(const std::string& rtpPort, int rtpVal);
+    void enqueue_update_aie_rtp(const std::string& rtpPort, int rtpVal);
 
-  /* return local metadata buffer size, user use allocate device buffer based on
-   * this size.
-   */
-  unsigned int get_metadata_size() const { return metadata.size(); };
+    /* return local metadata buffer size, user use allocate device buffer based
+     * on
+     * this size.
+     */
+    unsigned int get_metadata_size() const { return metadata.size(); };
 
-  /* return local microcode buffer size, user use allocate device buffer based
-   * on this size.
-   */
-  unsigned int get_microcode_size() const { return opcodeBuffer.size(); };
+    /* return local microcode buffer size, user use allocate device buffer based
+     * on this size.
+     */
+    unsigned int get_microcode_size() const { return opcodeBuffer.size(); };
 
-  /* copy local buffer to device buffer
-   */
-  void copy_to_device_buff(uint32_t* dst_op) const
-  {
-    memcpy(dst_op, opcodeBuffer.data(), opcodeBuffer.size() * sizeof(uint32_t));
-  }
+    /* copy local buffer to device buffer
+     */
+    void copy_to_device_buff(uint32_t* dst_op) const
+    {
+        memcpy(dst_op, opcodeBuffer.data(),
+               opcodeBuffer.size() * sizeof(uint32_t));
+    }
 
- private:
-  int read_elf_to_mem(std::string file_name, std::vector<uint32_t> m_buff);
-  // re-use this code from "core/edge/common/aie_parser.cpp"
-  void get_rtp();
+  private:
+    int read_elf_to_mem(std::string file_name, std::vector<uint32_t> m_buff);
+    // re-use this code from "core/edge/common/aie_parser.cpp"
+    void get_rtp();
 
-  std::vector<tile_type> get_tiles(const std::string& graph_name);
+    std::vector<tile_type> get_tiles(const std::string& graph_name);
 
-  std::vector<buffer_type> get_buffers(const std::string& port_name);
+    std::vector<buffer_type> get_buffers(const std::string& port_name);
 
-  std::vector<char> m_axlf;
-  std::unordered_map<std::string, rtp_type> rtps;
-  std::vector<uint32_t> opcodeBuffer;
-  std::vector<uint32_t> metadata;
-  uint32_t outputSize;
+    std::vector<char> m_axlf;
+    std::unordered_map<std::string, rtp_type> rtps;
+    std::vector<uint32_t> opcodeBuffer;
+    std::vector<uint32_t> metadata;
+    uint32_t outputSize;
 
-  std::string dma_info_path;
-  std::string aie_info_path;
-  bool ping_pong;
+    std::string dma_info_path;
+    std::string aie_info_path;
+    bool ping_pong;
 };
 
 } // end of namespace plctrl
