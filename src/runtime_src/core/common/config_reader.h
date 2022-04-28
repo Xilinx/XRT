c7adc8a864a228eea80ae216e224efe2c6da95e1
Rajkumar Rampelli <rajkumar@xilinx.com>
2022-04-28 17:25:50 +0530

    Set timeout value to 120 sec
    Signed-off-by: Rajkumar Rampelli <rajkumar@xilinx.com>

--- src/runtime_src/core/common/config_reader.h
+++ src/runtime_src/core/common/config_reader.h
@@ -832,11 +832,11 @@ get_flag_sw_emu_kernel_debug()
 }
 
 // This flag is added to exit device offline status check loop forcibly.
-// By default, device offline status loop runs for 60 seconds.
+// By default, device offline status loop runs for 120 seconds.
 inline unsigned int
 get_device_offline_timer()
 {
-  static unsigned int value = detail::get_uint_value("Runtime.dev_offline_timer", 60);
+  static unsigned int value = detail::get_uint_value("Runtime.dev_offline_timer", 120);
   return value;
 }
 
