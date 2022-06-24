168a9cee5231e4fff260d019b24d164c1e3c1279
Anurag Dubey <hackwa@users.noreply.github.com>
2022-06-24 09:36:14 -0600

    Add AIE trace buffer reuse switch (#6803)

--- src/runtime_src/xdp/profile/device/aie_trace/aie_trace_offload.h
+++ src/runtime_src/xdp/profile/device/aie_trace/aie_trace_offload.h
@@ -132,9 +132,10 @@ private:
 
     //Circular Buffer Tracking
     bool mEnCircularBuf;
+    bool mCircularBufOverwrite;
     // 1000 mb of trace per second
     // Very high bandwidth requirement
-    uint64_t circ_buf_min_rate_plio = TS2MM_DEF_BUF_SIZE * 1000;
+    uint64_t circ_buf_min_rate_plio = AIE_CIR_BUF_MIN_RATE_PLIO;
     uint64_t circ_buf_cur_rate_plio;
 
 private:
