1e323e059eed82191adebe6654982e8fc519edde
Daniel Benusovich <dbenusov@xilinx.com>
2022-07-18 12:36:33 -0700

    CR-1129020 Remove xclbin names from tests where possible. Update test init function to simplify interface

--- src/runtime_src/core/tools/xbutil2/SubCmdValidate.cpp
+++ src/runtime_src/core/tools/xbutil2/SubCmdValidate.cpp
@@ -1306,7 +1306,7 @@ psIopsTest(const std::shared_ptr<xrt_core::device>& _dev, boost::property_tree::
 * helper function to initialize test info
 */
 static boost::property_tree::ptree
-create_init_test(const std::string& name, const std::string& desc, const std::string& xclbin, bool is_explicit = false) {
+create_init_test(const std::string& name, const std::string& desc, const std::string& xclbin = "", bool is_explicit = false) {
   boost::property_tree::ptree _ptTest;
   _ptTest.put("name", name);
   _ptTest.put("description", desc);
@@ -1324,23 +1324,23 @@ struct TestCollection {
 * create test suite
 */
 static std::vector<TestCollection> testSuite = {
-  { create_init_test("aux-connection", "Check if auxiliary power is connected", ""), auxConnectionTest },
-  { create_init_test("pcie-link", "Check if PCIE link is active", ""), pcieLinkTest },
-  { create_init_test("sc-version", "Check if SC firmware is up-to-date", ""), scVersionTest },
-  { create_init_test("verify", "Run 'Hello World' PL kernel test", "verify.xclbin"), verifyKernelTest },
+  { create_init_test("aux-connection", "Check if auxiliary power is connected"), auxConnectionTest },
+  { create_init_test("pcie-link", "Check if PCIE link is active"), pcieLinkTest },
+  { create_init_test("sc-version", "Check if SC firmware is up-to-date"), scVersionTest },
+  { create_init_test("verify", "Run 'Hello World' PL kernel test"), verifyKernelTest },
   { create_init_test("dma", "Run dma test", "verify.xclbin"), dmaTest },
-  { create_init_test("iops", "Run scheduler performance measure test", "verify.xclbin"), iopsTest },
-  { create_init_test("mem-bw", "Run 'bandwidth kernel' and check the throughput", "bandwidth.xclbin"), bandwidthKernelTest },
+  { create_init_test("iops", "Run scheduler performance measure test", "verify.xclbin"), iopsTest }, // Host exec needs updates
+  { create_init_test("mem-bw", "Run 'bandwidth kernel' and check the throughput"), bandwidthKernelTest },
   { create_init_test("p2p", "Run P2P test", "bandwidth.xclbin"), p2pTest },
   { create_init_test("m2m", "Run M2M test", "bandwidth.xclbin"), m2mTest },
-  { create_init_test("hostmem-bw", "Run 'bandwidth kernel' when host memory is enabled", "bandwidth.xclbin"), hostMemBandwidthKernelTest },
+  { create_init_test("hostmem-bw", "Run 'bandwidth kernel' when host memory is enabled"), hostMemBandwidthKernelTest },
   { create_init_test("bist", "Run BIST test", "verify.xclbin", true), bistTest },
-  { create_init_test("vcu", "Run decoder test", "transcode.xclbin"), vcuKernelTest },
-  { create_init_test("aie", "Run PL controlled AIE test", "vck5000_pcie_pl_controller.xclbin.xclbin"), aiePlTest },
-  { create_init_test("ps-aie", "Run PS controlled AIE test", "ps_aie.xclbin"), psAieTest },
-  { create_init_test("ps-pl-verify", "Run PS controlled 'Hello World' PL kernel test", "ps_bandwidth.xclbin"), psBandwidthTest },
-  { create_init_test("ps-verify", "Run 'Hello World' PS kernel test", "ps_validate_bandwidth.xclbin"), psValidateTest },
-  { create_init_test("ps-iops", "Run IOPS PS test", "ps_validate_bandwidth.xclbin"), psIopsTest }
+  { create_init_test("vcu", "Run decoder test"), vcuKernelTest },
+  { create_init_test("aie", "Run PL controlled AIE test"), aiePlTest },
+  { create_init_test("ps-aie", "Run PS controlled AIE test"), psAieTest },
+  { create_init_test("ps-pl-verify", "Run PS controlled 'Hello World' PL kernel test"), psBandwidthTest },
+  { create_init_test("ps-verify", "Run 'Hello World' PS kernel test"), psValidateTest },
+  { create_init_test("ps-iops", "Run IOPS PS test", "ps_validate_bandwidth.xclbin"), psIopsTest } // Host exec needs updates
 };
 
 
