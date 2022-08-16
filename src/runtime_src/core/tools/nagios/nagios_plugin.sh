833b7f661ec116c33460a0d621ab71122bc74647
Daniel Benusovich <dbenusov@xilinx.com>
2022-08-16 13:42:28 -0700

    VITIS-5937 Update nagios script to function with Nagios

--- src/runtime_src/core/tools/nagios/nagios_plugin.sh
+++ src/runtime_src/core/tools/nagios/nagios_plugin.sh
@@ -1,22 +1,24 @@
+#!/bin/bash
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (C) 2022 Xilinx, Inc
 # Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
-#!/bin/bash
 
-# Source setup and ignore output
-source $XILINX_XRT/setup.sh > /dev/null
 # Generate the output JSON file. Ignore both error and standard output
-xbutil examine $@ -f JSON-plain -o /tmp/nagios_output.json &> /dev/null
+/opt/xilinx/xrt/bin/xbutil examine $@ -f JSON-plain -o /tmp/nagios_output.json &> /dev/null
 # Depending on command status return OK or FAILURE
+EXIT_CODE=0
 case $? in
   0)
     echo "STATUS: OK |"
+    EXIT_CODE=0
     ;;
   *)
     echo "STATUS: FAILURE |"
+    EXIT_CODE=1
     ;;
 esac
 # Output the generated JSON
 cat /tmp/nagios_output.json
 # Cleanup all extra files
 rm /tmp/nagios_output.json
+exit $EXIT_CODE
