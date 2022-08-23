33f02950650673bff43cc346f7d326ebc7fd25ee
sgundime <Subrahmanyam.Gundimeda@amd.com>
2022-08-23 14:36:24 +0530

    Added ERROR string in comment

--- src/runtime_src/core/pcie/emulation/hw_em/alveo_shim/shim.cxx
+++ src/runtime_src/core/pcie/emulation/hw_em/alveo_shim/shim.cxx
@@ -753,10 +753,13 @@ namespace xclhwemhal2 {
           if (mLogStream.is_open())
           {
             mLogStream << __func__ << " DISPLAY environment is not available so expect an exit from the application " << std::endl;
-            DEBUG_MSG_COUT(" DISPLAY environment is not available so expect an exit from the application ");
-            std::cerr << "DISPLAY environment is not available so expect an exit from the application";
+
+            std::string dMsg = "ERROR: [HW-EMU 26] DISPLAY environment is not available so expect an exit from the application";
+            logMessage(dMsg, 0);
+            //DEBUG_MSG_COUT(" DISPLAY environment is not available so expect an exit from the application ");
+            
           }
-          exit(0);
+          exit(EXIT_FAILURE);
         }
         
       }
