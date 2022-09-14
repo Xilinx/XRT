b3593d0d3d5fae1d2aeb4a978ee0164cb6b91b6e
Daniel Benusovich <dbenusov@xilinx.com>
2022-09-13 17:27:59 -0700

    Move base logic into seperate OO class
    Signed-off-by: Daniel Benusovich <dbenusov@xilinx.com>

---
+++ src/runtime_src/core/tools/xbmgmt2/OO_UpdateBase.h
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
+
+#ifndef __OO_UpdateBase_h_
+#define __OO_UpdateBase_h_
+
+#include "tools/common/OptionOptions.h"
+
+class OO_UpdateBase : public OptionOptions {
+ public:
+  virtual void execute( const SubCmdOptions &_options ) const;
+
+ public:
+  OO_UpdateBase( const std::string &_longName, bool _isHidden = false);
+
+ private:
+  std::string m_device;
+  std::string m_action;
+  std::string update;
+  std::vector<std::string> image;
+  std::string flashType;
+  bool m_help;
+};
+
+#endif
