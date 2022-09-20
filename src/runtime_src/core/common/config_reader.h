13483dc8bcb4a60b3037c06d27358c2c97e4b86b
Min Ma <min.ma@amd.com>
2022-09-20 12:04:46 -0700

    add internal debug flag in ini file
    Signed-off-by: Min Ma <min.ma@amd.com>

--- src/runtime_src/core/common/config_reader.h
+++ src/runtime_src/core/common/config_reader.h
@@ -545,6 +545,19 @@ get_ert_polling()
   return value;
 }
 
+/**
+ * Poll for XGQ command completion
+ */
+inline bool
+get_xgq_polling()
+{
+  /**
+   * xgq_polling flag will force KDS to poll XGQ commands regardless of interrupt config.
+   * This is added for XRT team interrupt debugging purpose and will not be documented.
+   */
+  static bool value = get_enable_flat() || detail::get_bool_value("Runtime.xgq_polling",false);
+  return value;
+}
 
 /**
  * Enable embedded scheduler CUDMA module
