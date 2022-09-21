f45cb4f23433083dd4739d005c2088652e3a8a0a
Daniel Benusovich <dbenusov@xilinx.com>
2022-09-21 13:32:03 -0700

    VITIS-6253 Add xclbin option option
    Signed-off-by: Daniel Benusovich <dbenusov@xilinx.com>

--- src/runtime_src/core/tools/xbmgmt2/OO_UpdateXclbin.cpp
+++ src/runtime_src/core/tools/xbmgmt2/OO_UpdateXclbin.cpp
@@ -6,14 +6,18 @@
 #include "OO_UpdateXclbin.h"
 
 // XRT - Include Files
+#include "core/common/error.h"
 #include "core/common/query_requests.h"
-#include "tools/common/XBUtilities.h"
+#include "tools/common/XBHelpMenusCore.h"
 #include "tools/common/XBUtilitiesCore.h"
+#include "tools/common/XBUtilities.h"
 namespace XBU = XBUtilities;
 
 // 3rd Party Library - Include Files
 #include <boost/format.hpp>
+#include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
+#include <boost/algorithm/string.hpp>
 namespace po = boost::program_options;
 
 // System - Include Files
