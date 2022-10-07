43926231f7183688add2dccfd391b36a1f000bea
Ch Vamshi Krishna <40261882+chvamshi-xilinx@users.noreply.github.com>
2022-10-07 10:42:02 +0530

    workaround for CR-1141322 (#7088) (#7092)
    Signed-off-by: Min Ma <min.ma@amd.com>
    
    Signed-off-by: Min Ma <min.ma@amd.com>
    Co-authored-by: Min Ma <min.ma@amd.com>
    (cherry picked from commit 763797fa01bfb44c35ed9e0223f66f0d243d5225)
    
    Co-authored-by: Min Ma <min.ma@xilinx.com>

--- src/runtime_src/core/common/drv/xrt_cu.c
+++ src/runtime_src/core/common/drv/xrt_cu.c
@@ -385,7 +385,7 @@ static inline int process_rq(struct xrt_cu *xcu)
 	xrt_cu_start(xcu);
 	if (xcu->thread) {
 		xcu->poll_count = 0;
-		if (xcu->interrupt_used)
+		if (!xcu->force_intr && xcu->interrupt_used)
 			xrt_cu_switch_to_poll(xcu);
 	}
 	set_xcmd_timestamp(xcmd, KDS_RUNNING);
@@ -551,6 +551,8 @@ int xrt_cu_intr_thread(void *data)
 	xcu->interrupt_used = 0;
 	xcu_info(xcu, "CU[%d] start", xcu->info.cu_idx);
 	mod_timer(&xcu->timer, jiffies + CU_TIMER);
+	if (xcu->force_intr)
+		xrt_cu_switch_to_interrupt(xcu);
 	while (!xcu->stop) {
 		/* Make sure to submit as many commands as possible.
 		 * This is why we call continue here. This is important to make
