196066eb452e15566878254b2c7758ba2d4cf07b
Max Zhen <max.zhen@amd.com>
2022-10-25 17:31:54 -0700

    Force xrt.h to be C compatible
    Signed-off-by: Max Zhen <max.zhen@amd.com>

---
+++ src/runtime_src/core/include/unittests/xrt_h_c_compilation/main.c
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2022, Advanced Micro Devices, Inc. All rights reserved.
+ */
+
+/*
+ * Test C compilation with xrt.h header file
+ */
+#include "xrt.h"
+
+int main()
+{
+  return 0;
+}
