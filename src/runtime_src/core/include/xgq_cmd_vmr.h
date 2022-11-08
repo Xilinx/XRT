a41879e71b966f31d967864eff6912a3bee8daaa
David Zhang <50243230+xdavidz@users.noreply.github.com>
2022-11-08 12:41:57 -0800

    system_dtb and plm_log are not being updated everytime (#7155)
    Signed-off-by: David Zhang <davidzha@xilinx.com>

--- src/runtime_src/core/include/xgq_cmd_vmr.h
+++ src/runtime_src/core/include/xgq_cmd_vmr.h
@@ -124,7 +124,7 @@ enum xgq_cmd_log_page_type {
 	XGQ_CMD_LOG_TASK_STATS  = 0x5,
 	XGQ_CMD_LOG_MEM_STATS	= 0x6,
 	XGQ_CMD_LOG_SYSTEM_DTB	= 0x7,
-	XGQ_CMD_LOG_PLM_SYNC	= 0x8,
+	XGQ_CMD_LOG_PLM_LOG	= 0x8,
 
 };
 
