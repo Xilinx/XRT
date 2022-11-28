fa351498b654d4de0bcb6784bc07c3b63edbab5e
Saifuddin <saifuddi@xilinx.com>
2022-11-28 15:53:34 +0530

    rebased with latest master
    Signed-off-by: Saifuddin <saifuddi@xilinx.com>

--- src/runtime_src/core/common/device.cpp
+++ src/runtime_src/core/common/device.cpp
@@ -254,9 +254,6 @@ void
 device::
 register_axlf(const axlf* top)
 {
-  // This lock is required when multiple xrt::hw_context are created from
-  // different threads
-  std::lock_guard<std::mutex> lk(device_lock);
   xrt::uuid xid{top->m_header.uuid};
 
   // Update xclbin caching from [slot, xclbin_uuid]+ data
