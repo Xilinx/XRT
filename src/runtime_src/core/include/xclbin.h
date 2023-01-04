c9e2b8481b5bd92e1c96d7ac248eeb4a17606f56
Fei Rui <36643163+xfreid@users.noreply.github.com>
2023-01-04 09:49:48 -0700

    add support for fixed ps kernel (#7244)
    * add support for fixed ps kernel
    
    Signed-off-by: xilinxfei <fei@xilinx.com>
    
    * add support for fixed ps kernel
    
    Signed-off-by: xilinxfei <fei@xilinx.com>
    
    * add support for fixed ps kernel
    
    Signed-off-by: xilinxfei <fei@xilinx.com>
    
    * add support for fixed ps kernel
    
    Signed-off-by: xilinxfei <fei@xilinx.com>
    
    * add support for fixed ps kernel
    
    Signed-off-by: xilinxfei <fei@xilinx.com>
    
    * add support for fixed ps kernel
    
    Signed-off-by: xilinxfei <fei@xilinx.com>
    
    * add support for fixed ps kernel
    
    Signed-off-by: xilinxfei <fei@xilinx.com>
    
    * add support for fixed ps kernel
    
    Signed-off-by: xilinxfei <fei@xilinx.com>
    
    * add support for fixed ps kernel
    
    Signed-off-by: xilinxfei <fei@xilinx.com>
    
    * add support for fixed ps kernel
    
    Signed-off-by: xilinxfei <fei@xilinx.com>
    
    Signed-off-by: xilinxfei <fei@xilinx.com>
    Co-authored-by: xilinxfei <fei@xilinx.com>

--- src/runtime_src/core/include/xclbin.h
+++ src/runtime_src/core/include/xclbin.h
@@ -329,6 +329,18 @@ extern "C" {
         FAST_ADAPTER = 5
     };
 
+    // m_subtype
+    enum PS_SUBTYPE {
+        ST_PS = 0,
+        ST_DPU = 1,
+    };
+
+    // m_functional
+    enum PS_FUNCTIONAL {
+        FC_DPU = 0,
+        FC_PREPOST = 1,
+    };
+
     #define IP_CONTROL_MASK  0xFF00
     #define IP_CONTROL_SHIFT 0x8
 
@@ -341,6 +353,19 @@ extern "C" {
                                  //         m_int_enable   : Bit  - 0x0000_0001;
                                  //         m_interrupt_id : Bits - 0x0000_00FE;
                                  //         m_ip_control   : Bits = 0x0000_FF00;
+                                 // properties is also used for ps kernel (i.e. -add-pskernel)
+       
+            // PS Kernels
+            // m_type: IP_PS_KERNEL
+            struct {
+               uint16_t m_subtype : 2;        // Bits - 0x0003 - PS_SUBTYPE enum values
+               uint16_t : 2;                  // Bits - 0x000C - Future use
+               uint16_t m_functional : 2;     // Bits - 0x0030 - PS_FUNCTIONAL enum values
+               uint16_t : 10;                 // Bits - 0xFFC0 - Future use
+               uint16_t m_kernel_id : 12;     // Bits - 0x0FFF
+               uint16_t : 4;                  // Bits - 0xF000 - Future use
+            } ps_kernel;
+
             struct {             // m_type: IP_MEM_*
                uint16_t m_index;
                uint8_t m_pc_index;
