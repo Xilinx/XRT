Xilinx/XRT

commit f8a701a63f9fbe004c323402e8c8d84c0ccdd790

64-bit BO handle in ishim on Linux (#7109)
* 64-bit BO handle in ishim

Signed-off-by: Max Zhen <max.zhen@amd.com>

src/runtime_src/core/common/api/xrt_bo.cpp
@@ -1,21 +1,10 @@
-/*
- * Copyright (C) 2020-2022, Xilinx Inc - All rights reserved
- * Xilinx Runtime (XRT) Experimental APIs
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may
- * not use this file except in compliance with the License. A copy of the
- * License is located at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2020-2022 Xilinx, Inc
+// Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
 
-// This file implements XRT xclbin APIs as declared in
+// Xilinx Runtime (XRT) Experimental APIs
+
+// This file implements XRT BO APIs as declared in
 // core/include/experimental/xrt_bo.h
 #define XCL_DRIVER_DLL_EXPORT  // exporting xrt_bo.h
 #define XRT_CORE_COMMON_SOURCE // in same dll as core_common
@@ -142,7 +131,7 @@ public:
   static constexpr uint64_t no_addr = std::numeric_limits<uint64_t>::max();
   static constexpr uint32_t no_group = std::numeric_limits<uint32_t>::max();
   static constexpr bo::flags no_flags = static_cast<bo::flags>(std::numeric_limits<uint32_t>::max());
-  static constexpr xclBufferHandle null_bo = XRT_NULL_BO;
+  static constexpr xrt_buffer_handle null_bo = XRT_INVALID_BUFFER_HANDLE;
   static constexpr xclBufferExportHandle null_export = XRT_NULL_BO_EXPORT;
 
 private:
@@ -167,20 +156,20 @@ protected:
   // deliberately made protected, this is a file-scoped controlled API
   std::shared_ptr<xrt_core::device> device;     // NOLINT device where bo is allocated
   std::vector<std::shared_ptr<bo_impl>> clones; // NOLINT local m2m clones if any
-  xclBufferHandle handle = null_bo;             // NOLINT driver bo handle
+  xrt_buffer_handle handle = null_bo;           // NOLINT driver bo handle
   size_t size = 0;                              // NOLINT size of buffer
   mutable uint64_t addr = no_addr;              // NOLINT bo device address
-  mutable uint32_t grpid = no_group;             // NOLINT memory group index
+  mutable uint32_t grpid = no_group;            // NOLINT memory group index
   mutable bo::flags flags = no_flags;           // NOLINT flags per bo properties
   mutable xclBufferExportHandle export_handle = null_export; // NOLINT export handle if exported
   bool free_bo;                                 // NOLINT should dtor free bo
 
 public:
   explicit bo_impl(size_t sz)
-    : handle(XRT_NULL_BO), size(sz), free_bo(false)
+    : handle(XRT_INVALID_BUFFER_HANDLE), size(sz), free_bo(false)
   {}
 
-  bo_impl(xclDeviceHandle dhdl, xclBufferHandle bhdl, size_t sz)
+  bo_impl(xclDeviceHandle dhdl, xrt_buffer_handle bhdl, size_t sz)
     : device(xrt_core::get_userpf_device(dhdl)), handle(bhdl), size(sz), free_bo(true)
   {}
 
@@ -201,7 +190,9 @@ public:
   }
 
   bo_impl(xclDeviceHandle dhdl, xcl_buffer_handle xhdl)
-    : device(xrt_core::get_userpf_device(dhdl)), handle(xhdl.bhdl), free_bo(false)
+    : device(xrt_core::get_userpf_device(dhdl))
+    , handle(to_xrt_buffer_handle(xhdl.bhdl))
+    , free_bo(false)
   {
     xclBOProperties prop{};
     device->get_bo_properties(handle, &prop);
@@ -242,7 +233,7 @@ public:
     clones.push_back(std::move(clone));
   }
 
-  xclBufferHandle
+  xrt_buffer_handle
   get_xcl_handle() const
   {
     return handle;
@@ -564,7 +555,7 @@ class buffer_ubuf : public bo_impl
   void* ubuf;
 
 public:
-  buffer_ubuf(xclDeviceHandle dhdl, xclBufferHandle bhdl, size_t sz, void* buf)
+  buffer_ubuf(xclDeviceHandle dhdl, xrt_buffer_handle bhdl, size_t sz, void* buf)
     : bo_impl(dhdl, bhdl, sz)
     , ubuf(buf)
   {}
@@ -586,7 +577,7 @@ class buffer_hbuf : public bo_impl
   xrt_core::aligned_ptr_type hbuf;
 
 public:
-  buffer_hbuf(xclDeviceHandle dhdl, xclBufferHandle bhdl, size_t sz, xrt_core::aligned_ptr_type&& b)
+  buffer_hbuf(xclDeviceHandle dhdl, xrt_buffer_handle bhdl, size_t sz, xrt_core::aligned_ptr_type&& b)
     : bo_impl(dhdl, bhdl, sz), hbuf(std::move(b))
   {}
 
@@ -606,7 +597,7 @@ class buffer_kbuf : public bo_impl
   void* hbuf;
 
 public:
-  buffer_kbuf(xclDeviceHandle dhdl, xclBufferHandle bhdl, size_t sz)
+  buffer_kbuf(xclDeviceHandle dhdl, xrt_buffer_handle bhdl, size_t sz)
     : bo_impl(dhdl, bhdl, sz), hbuf(device->map_bo(handle, true))
   {}
 
@@ -714,7 +705,7 @@ public:
 class buffer_dbuf : public bo_impl
 {
 public:
-  buffer_dbuf(xclDeviceHandle dhdl, xclBufferHandle bhdl, size_t sz)
+  buffer_dbuf(xclDeviceHandle dhdl, xrt_buffer_handle bhdl, size_t sz)
     : bo_impl(dhdl, bhdl, sz)
   {}
 
@@ -756,7 +747,7 @@ class buffer_nodma : public bo_impl
   }
 
 public:
-  buffer_nodma(xclDeviceHandle dhdl, xclBufferHandle hbuf, xclBufferHandle dbuf, size_t sz)
+  buffer_nodma(xclDeviceHandle dhdl, xrt_buffer_handle hbuf, xrt_buffer_handle dbuf, size_t sz)
     : bo_impl(sz), m_host_only(dhdl, hbuf, sz), m_device_only(dhdl, dbuf, sz)
   {
     device = xrt_core::get_userpf_device(dhdl);
@@ -854,8 +845,8 @@ public:
 class buffer_xbuf : public bo_impl
 {
 public:
-  buffer_xbuf(xclDeviceHandle dhdl, xclBufferHandle bhdl)
-    : bo_impl(dhdl, xcl_buffer_handle{bhdl})
+  buffer_xbuf(xclDeviceHandle dhdl, xcl_buffer_handle xhdl)
+    : bo_impl(dhdl, xhdl)
   {}
 
   void*
@@ -898,7 +889,7 @@ public:
 class buffer_clone : public bo_impl
 {
 public:
-  buffer_clone(xclDeviceHandle dhdl, const std::shared_ptr<bo_impl>& src, xclBufferHandle clone, size_t sz)
+  buffer_clone(xclDeviceHandle dhdl, const std::shared_ptr<bo_impl>& src, xrt_buffer_handle clone, size_t sz)
     : bo_impl(dhdl, clone, sz)
   {
     // copy src to clone
@@ -923,7 +914,7 @@ get_boh(xrtBufferHandle bhdl)
   return bo_cache.get_or_error(bhdl);
 }
 
-static xclBufferHandle
+static xrt_buffer_handle
 alloc_bo(xclDeviceHandle dhdl, void* userptr, size_t sz, xrtBufferFlags flags, xrtMemoryGroup grp)
 {
   auto device = xrt_core::get_userpf_device(dhdl);
@@ -931,7 +922,7 @@ alloc_bo(xclDeviceHandle dhdl, void* userptr, size_t sz, xrtBufferFlags flags, x
   return device->alloc_bo(userptr, sz, flags);
 }
 
-static xclBufferHandle
+static xrt_buffer_handle
 alloc_bo(xclDeviceHandle dhdl, size_t sz, xrtBufferFlags flags, xrtMemoryGroup grp)
 {
   auto device = xrt_core::get_userpf_device(dhdl);
@@ -1041,7 +1032,7 @@ alloc(xclDeviceHandle dhdl, size_t sz, xrtBufferFlags flags, xrtMemoryGroup grp)
 }
 
 static std::shared_ptr<xrt::bo_impl>
-alloc_xbuf(xclDeviceHandle dhdl, xclBufferHandle xhdl)
+alloc_xbuf(xclDeviceHandle dhdl, xcl_buffer_handle xhdl)
 {
   return std::make_shared<xrt::buffer_xbuf>(dhdl, xhdl);
 }
@@ -1171,7 +1162,8 @@ fill_copy_pkt(const xrt::bo& dst, const xrt::bo& src, size_t sz,
 #ifndef _WIN32
   const auto& dst_boh = dst.get_handle();
   const auto& src_boh = src.get_handle();
-  ert_fill_copybo_cmd(pkt, src_boh->get_xcl_handle(), dst_boh->get_xcl_handle(), src_offset, dst_offset, sz);
+  ert_fill_copybo_cmd(pkt, to_xclBufferHandle(src_boh->get_xcl_handle()),
+    to_xclBufferHandle(dst_boh->get_xcl_handle()), src_offset, dst_offset, sz);
 #else
   throw std::runtime_error("ert_fill_copybo_cmd not implemented on windows");
 #endif
@@ -1243,7 +1235,7 @@ bo(const bo& parent, size_t size, size_t offset)
 
 bo::
 bo(xclDeviceHandle dhdl, xcl_buffer_handle xhdl)
-  : handle(alloc_xbuf(dhdl, xhdl.bhdl))
+  : handle(alloc_xbuf(dhdl, xhdl))
 {}
 
 bo::
@@ -1492,7 +1484,7 @@ xrtBOAllocFromXcl(xrtDeviceHandle dhdl, xclBufferHandle xhdl)
 {
   try {
     return xdp::native::profiling_wrapper(__func__, [dhdl, xhdl] {
-      auto boh = alloc_xbuf(xrtDeviceToXclDevice(dhdl), xhdl);
+      auto boh = alloc_xbuf(xrtDeviceToXclDevice(dhdl), xcl_buffer_handle{xhdl});
       auto hdl = boh.get();
       bo_cache.add(hdl, std::move(boh));
       return hdl;

src/runtime_src/core/include/deprecated/xrt.h
@@ -67,9 +67,9 @@ typedef void * xclDeviceHandle;
 /*
  * typedef xclBufferHandle - opaque buffer handle
  *
- * A buffer handle of xclBufferHandle kind is obtained by allocating
- * buffer objects. The buffer handle is used by XRT APIs that operate
- * on on buffer objects.
+ * A buffer handle of xclBufferHandle kind is obtained by allocating buffer
+ * objects through HAL API. The buffer handle is used by XRT HAL APIs that
+ * operate on on buffer objects.
  */
 #ifdef _WIN32
 typedef void * xclBufferHandle;
@@ -79,6 +79,34 @@ typedef unsigned int xclBufferHandle;
 # define NULLBO	0xffffffff
 #endif
 #define XRT_NULL_BO NULLBO
+
+/*
+ * typedef xrt_buffer_handle - opaque buffer handle
+ *
+ * A buffer handle of xrt_buffer_handle kind is obtained by allocating buffer
+ * objects through ISHIM API. The buffer handle is used by XRT ISHIM APIs
+ * that operate on on buffer objects.
+ */
+typedef void * xrt_buffer_handle;
+#define XRT_INVALID_BUFFER_HANDLE	NULL
+
+static inline xclBufferHandle
+to_xclBufferHandle(xrt_buffer_handle hdl)
+{
+  return hdl == XRT_INVALID_BUFFER_HANDLE
+    ? XRT_NULL_BO
+#ifdef _WIN32
+    : hdl; // No cast needed, happen to be the same define as xclBufferHandle
+#else
+    : reinterpret_cast<uintptr_t>(hdl);
+#endif
+}
+
+static inline xrt_buffer_handle
+to_xrt_buffer_handle(xclBufferHandle hdl)
+{
+  return hdl == XRT_NULL_BO ? XRT_INVALID_BUFFER_HANDLE : reinterpret_cast<xrt_buffer_handle>(hdl);
+}
 
 /*
  * typedef xclBufferExportHandle

src/runtime_src/core/common/api/command.h
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (C) 2020-2022 Xilinx, Inc. All rights reserved.
+// Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
 #ifndef xrt_core_command_h_
 #define xrt_core_command_h_
 
@@ -54,7 +55,7 @@ public:
   /**
    * get_exec_bo() - get BO handle of command buffer
    */
-  virtual xclBufferHandle
+  virtual xrt_buffer_handle
   get_exec_bo() const = 0;
 
   /**

src/runtime_src/core/common/api/hw_queue.cpp
@@ -372,7 +372,7 @@ public:
   std::cv_status
   wait(size_t timeout_ms) override
   {
-    return m_device->wait_command(m_qhdl, XRT_NULL_BO, static_cast<int>(timeout_ms))
+    return m_device->wait_command(m_qhdl, XRT_INVALID_BUFFER_HANDLE, static_cast<int>(timeout_ms))
       ? std::cv_status::no_timeout
       : std::cv_status::timeout;
   }

src/runtime_src/core/common/api/kernel_int.h
@@ -1,19 +1,8 @@
-/*
- * Copyright (C) 2020, Xilinx Inc - All rights reserved
- * Xilinx Runtime (XRT) Experimental APIs
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may
- * not use this file except in compliance with the License. A copy of the
- * License is located at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2020 Xilinx, Inc
+// Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Xilinx Runtime (XRT) Experimental APIs
 
 #ifndef _XRT_COMMON_KERNEL_INT_H_
 #define _XRT_COMMON_KERNEL_INT_H_
@@ -37,8 +26,8 @@ namespace xrt_core { namespace kernel_int {
 void
 copy_bo_with_kdma(const std::shared_ptr<xrt_core::device>& core_device,
                   size_t sz,
-                  xclBufferHandle dst_bo, size_t dst_offset,
-                  xclBufferHandle src_bo, size_t src_offset);
+                  xrt_buffer_handle dst_bo, size_t dst_offset,
+                  xrt_buffer_handle src_bo, size_t src_offset);
 
 XRT_CORE_COMMON_EXPORT
 std::vector<const xclbin::kernel_argument*>

src/runtime_src/core/common/api/xrt_kernel.cpp
@@ -895,7 +895,7 @@ public:
     return m_device->get_core_device();
   }
 
-  xclBufferHandle
+  xrt_buffer_handle
   get_exec_bo() const override
   {
     return m_execbuf.first;
@@ -2884,8 +2884,8 @@ namespace xrt_core { namespace kernel_int {
 void
 copy_bo_with_kdma(const std::shared_ptr<xrt_core::device>& core_device,
                   size_t sz,
-                  xclBufferHandle dst_bo, size_t dst_offset,
-                  xclBufferHandle src_bo, size_t src_offset)
+                  xrt_buffer_handle dst_bo, size_t dst_offset,
+                  xrt_buffer_handle src_bo, size_t src_offset)
 {
 #ifndef _WIN32
   if (is_sw_emulation())
@@ -2898,7 +2898,8 @@ copy_bo_with_kdma(const std::shared_ptr<xrt_core::device>& core_device,
 
   // Get and fill the underlying packet
   auto pkt = cmd->get_ert_cmd<ert_start_copybo_cmd*>();
-  ert_fill_copybo_cmd(pkt, src_bo, dst_bo, src_offset, dst_offset, sz);
+  ert_fill_copybo_cmd(pkt, to_xclBufferHandle(src_bo), to_xclBufferHandle(dst_bo),
+    src_offset, dst_offset, sz);
 
   // Run the command and wait for completion
   cmd->run();

src/runtime_src/core/common/bo_cache.h
@@ -1,18 +1,6 @@
-/**
- * Copyright (C) 2019 Xilinx, Inc
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may
- * not use this file except in compliance with the License. A copy of the
- * License is located at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2019 Xilinx, Inc
+// Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
 
 #ifndef core_common_bo_cache_h_
 #define core_common_bo_cache_h_
@@ -39,7 +27,7 @@ public:
   // Helper typedef for std::pair. Note the elements are const so that the
   // pair is immutable. The clients should not change the contents of cmd_bo.
   template <typename CommandType>
-  using cmd_bo = std::pair<const xclBufferHandle, CommandType *const>;
+  using cmd_bo = std::pair<const xrt_buffer_handle, CommandType *const>;
 private:
 
   // We are really allocating a page size as that is what xocl/zocl do. Note on

src/runtime_src/core/common/ishim.h
@@ -60,19 +60,19 @@ struct ishim
   virtual void
   close_context(const xrt::uuid& xclbin_uuid, unsigned int ip_index) = 0;
 
-  virtual xclBufferHandle
+  virtual xrt_buffer_handle
   alloc_bo(size_t size, unsigned int flags) = 0;
 
-  virtual xclBufferHandle
+  virtual xrt_buffer_handle
   alloc_bo(void* userptr, size_t size, unsigned int flags) = 0;
 
   virtual void
-  free_bo(xclBufferHandle boh) = 0;
+  free_bo(xrt_buffer_handle boh) = 0;
 
   virtual xclBufferExportHandle
-  export_bo(xclBufferHandle boh) const = 0;
+  export_bo(xrt_buffer_handle boh) const = 0;
 
-  virtual xclBufferHandle
+  virtual xrt_buffer_handle
   import_bo(xclBufferExportHandle ehdl) = 0;
 
   virtual void
@@ -80,24 +80,24 @@ struct ishim
 
   // Import an exported BO from another process identified by argument pid.
   // This function is only supported on systems with pidfd kernel support
-  virtual xclBufferHandle
+  virtual xrt_buffer_handle
   import_bo(pid_t, xclBufferExportHandle)
   { throw not_supported_error{__func__}; }
 
   virtual void
-  copy_bo(xclBufferHandle dst, xclBufferHandle src, size_t size, size_t dst_offset, size_t src_offset) = 0;
+  copy_bo(xrt_buffer_handle dst, xrt_buffer_handle src, size_t size, size_t dst_offset, size_t src_offset) = 0;
 
   virtual void
-  sync_bo(xclBufferHandle bo, xclBOSyncDirection dir, size_t size, size_t offset) = 0;
+  sync_bo(xrt_buffer_handle bo, xclBOSyncDirection dir, size_t size, size_t offset) = 0;
 
   virtual void*
-  map_bo(xclBufferHandle boh, bool write) = 0;
+  map_bo(xrt_buffer_handle boh, bool write) = 0;
 
   virtual void
-  unmap_bo(xclBufferHandle boh, void* addr) = 0;
+  unmap_bo(xrt_buffer_handle boh, void* addr) = 0;
 
   virtual void
-  get_bo_properties(xclBufferHandle boh, struct xclBOProperties *properties) const = 0;
+  get_bo_properties(xrt_buffer_handle boh, struct xclBOProperties *properties) const = 0;
 
   virtual void
   reg_read(uint32_t ipidx, uint32_t offset, uint32_t* data) const = 0;
@@ -118,7 +118,7 @@ struct ishim
   unmgd_pwrite(const void* buffer, size_t size, uint64_t offset) = 0;
 
   virtual void
-  exec_buf(xclBufferHandle boh) = 0;
+  exec_buf(xrt_buffer_handle boh) = 0;
 
   virtual int
   exec_wait(int timeout_ms) const = 0;
@@ -172,7 +172,7 @@ struct ishim
 
   // Submits command for execution through hw queue
   virtual void
-  submit_command(xcl_hwqueue_handle, xclBufferHandle /*cmdbo*/) const
+  submit_command(xcl_hwqueue_handle, xrt_buffer_handle /*cmdbo*/) const
   { throw not_supported_error{__func__}; }
 
   // Wait for command completion through hw queue
@@ -180,7 +180,7 @@ struct ishim
   // cmdbo completed.  If cmdbo is XRT_NULL_BO then function must
   // returns when some previously submitted command completes.
   virtual int
-  wait_command(xcl_hwqueue_handle, xclBufferHandle /*cmdbo*/, int /*timeout_ms*/) const
+  wait_command(xcl_hwqueue_handle, xrt_buffer_handle /*cmdbo*/, int /*timeout_ms*/) const
   { throw not_supported_error{__func__}; }
 
   // Registers an xclbin, but does not load it.
@@ -335,52 +335,52 @@ struct shim : public DeviceType
       throw system_error(ret, "failed to close ip context");
   }
 
-  xclBufferHandle
+  xrt_buffer_handle
   alloc_bo(size_t size, unsigned int flags) override
   {
     auto bo = xclAllocBO(DeviceType::get_device_handle(), size, 0, flags);
     if (bo == XRT_NULL_BO)
       throw std::bad_alloc();
 
-    return bo;
+    return to_xrt_buffer_handle(bo);
   }
 
-  xclBufferHandle
+  xrt_buffer_handle
   alloc_bo(void* userptr, size_t size, unsigned int flags) override
   {
     auto bo = xclAllocUserPtrBO(DeviceType::get_device_handle(), userptr, size, flags);
     if (bo == XRT_NULL_BO)
       throw std::bad_alloc();
 
-    return bo;
+    return to_xrt_buffer_handle(bo);
   }
 
   void
-  free_bo(xclBufferHandle bo) override
+  free_bo(xrt_buffer_handle bo) override
   {
-    xclFreeBO(DeviceType::get_device_handle(), bo);
+    xclFreeBO(DeviceType::get_device_handle(), to_xclBufferHandle(bo));
   }
 
   xclBufferExportHandle
-  export_bo(xclBufferHandle bo) const override
+  export_bo(xrt_buffer_handle bo) const override
   {
-    auto ehdl = xclExportBO(DeviceType::get_device_handle(), bo);
+    auto ehdl = xclExportBO(DeviceType::get_device_handle(), to_xclBufferHandle(bo));
     if (ehdl == XRT_NULL_BO_EXPORT)
-      throw system_error(EINVAL, "Unable to export BO");
+      throw system_error(EINVAL, "Unable to export BO: bad export BO handle");
     if (ehdl < 0) // system error code
       throw system_error(ENODEV, "Unable to export BO");
     return ehdl;
   }
 
-  xclBufferHandle
+  xrt_buffer_handle
   import_bo(xclBufferExportHandle ehdl) override
   {
     auto ihdl = xclImportBO(DeviceType::get_device_handle(), ehdl, 0);
     if (ihdl == XRT_NULL_BO)
-      throw system_error(EINVAL, "unable to import BO");
+      throw system_error(EINVAL, "unable to import BO: bad BO handle");
     if (ihdl < 0) // system error code
       throw system_error(ENODEV, "unable to import BO");
-    return ihdl;
+    return to_xrt_buffer_handle(ihdl);
   }
 
   void
@@ -391,38 +391,41 @@ struct shim : public DeviceType
   }
 
   void
-  copy_bo(xclBufferHandle dst, xclBufferHandle src, size_t size, size_t dst_offset, size_t src_offset) override
+  copy_bo(xrt_buffer_handle dst, xrt_buffer_handle src, size_t size, size_t dst_offset, size_t src_offset) override
   {
-    if (auto err = xclCopyBO(DeviceType::get_device_handle(), dst, src, size, dst_offset, src_offset))
+    auto err = xclCopyBO(DeviceType::get_device_handle(),
+      to_xclBufferHandle(dst), to_xclBufferHandle(src), size, dst_offset, src_offset);
+    if (err)
       throw system_error(err, "unable to copy BO");
   }
 
   void
-  sync_bo(xclBufferHandle bo, xclBOSyncDirection dir, size_t size, size_t offset) override
+  sync_bo(xrt_buffer_handle bo, xclBOSyncDirection dir, size_t size, size_t offset) override
   {
-    if (auto err = xclSyncBO(DeviceType::get_device_handle(), bo, dir, size, offset))
+    auto err = xclSyncBO(DeviceType::get_device_handle(), to_xclBufferHandle(bo), dir, size, offset);
+    if (err)
       throw system_error(err, "unable to sync BO");
   }
 
   void*
-  map_bo(xclBufferHandle bo, bool write) override
+  map_bo(xrt_buffer_handle bo, bool write) override
   {
-    if (auto mapped = xclMapBO(DeviceType::get_device_handle(), bo, write))
+    if (auto mapped = xclMapBO(DeviceType::get_device_handle(), to_xclBufferHandle(bo), write))
       return mapped;
     throw system_error(EINVAL, "could not map BO");
   }
 
   void
-  unmap_bo(xclBufferHandle bo, void* addr) override
+  unmap_bo(xrt_buffer_handle bo, void* addr) override
   {
-    if (auto ret = xclUnmapBO(DeviceType::get_device_handle(), bo, addr))
+    if (auto ret = xclUnmapBO(DeviceType::get_device_handle(), to_xclBufferHandle(bo), addr))
       throw system_error(ret, "failed to unmap BO");
   }
 
   void
-  get_bo_properties(xclBufferHandle bo, struct xclBOProperties *properties) const override
+  get_bo_properties(xrt_buffer_handle bo, struct xclBOProperties *properties) const override
   {
-    if (auto ret = xclGetBOProperties(DeviceType::get_device_handle(), bo, properties))
+    if (auto ret = xclGetBOProperties(DeviceType::get_device_handle(), to_xclBufferHandle(bo), properties))
       throw system_error(ret, "failed to get BO properties");
   }
 
@@ -476,9 +479,9 @@ struct shim : public DeviceType
   }
 
   void
-  exec_buf(xclBufferHandle bo) override
+  exec_buf(xrt_buffer_handle bo) override
   {
-    if (auto ret = xclExecBuf(DeviceType::get_device_handle(), bo))
+    if (auto ret = xclExecBuf(DeviceType::get_device_handle(), to_xclBufferHandle(bo)))
       throw system_error(ret, "failed to launch execution buffer");
   }
 

commit ad33e65379404b16ec30bd5ad4d73b5bb041de15

Replace XRT_INVALID_BUFFER_HANDLE with pointer value sentinel
Convert the sentinel XRT_INVALID_BUFFER_HANDLE to a pointer value.
This addresses a fallout from #7109 which did not handle
xclBufferHandles of value 0.

Remove constexpr using XRT_INVALID_BUFFER_HANDLE because constexpr
doesn't allow pointer values.

Changes tested with standard regression tests.

src/runtime_src/core/common/api/xrt_bo.cpp
@@ -182,7 +182,8 @@ public:
   static constexpr uint64_t no_addr = std::numeric_limits<uint64_t>::max();
   static constexpr uint32_t no_group = std::numeric_limits<uint32_t>::max();
   static constexpr bo::flags no_flags = static_cast<bo::flags>(std::numeric_limits<uint32_t>::max());
-  static constexpr xrt_buffer_handle null_bo = XRT_INVALID_BUFFER_HANDLE;
+  // ptr cannot be const expor
+  //static constexpr xrt_buffer_handle null_bo = XRT_INVALID_BUFFER_HANDLE;
   static constexpr xclBufferExportHandle null_export = XRT_NULL_BO_EXPORT;
 
 private:
@@ -200,7 +201,7 @@ protected:
   // deliberately made protected, this is a file-scoped controlled API
   device_type device;                           // NOLINT device where bo is allocated
   std::vector<std::shared_ptr<bo_impl>> clones; // NOLINT local m2m clones if any
-  xrt_buffer_handle handle = null_bo;           // NOLINT driver bo handle
+  xrt_buffer_handle handle = XRT_INVALID_BUFFER_HANDLE; // NOLINT driver bo handle
   size_t size = 0;                              // NOLINT size of buffer
   mutable uint64_t addr = no_addr;              // NOLINT bo device address
   mutable uint32_t grpid = no_group;            // NOLINT memory group index

src/runtime_src/core/include/deprecated/xrt.h
@@ -88,7 +88,7 @@ typedef unsigned int xclBufferHandle;
  * that operate on on buffer objects.
  */
 typedef void * xrt_buffer_handle;
-#define XRT_INVALID_BUFFER_HANDLE	NULL
+#define XRT_INVALID_BUFFER_HANDLE ((xrt_buffer_handle)(uintptr_t)-1)
 
 static inline xclBufferHandle
 to_xclBufferHandle(xrt_buffer_handle hdl)