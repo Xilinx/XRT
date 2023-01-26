de02a41f67d2425bdec3eef06139107b6e770c22
Nishant Mysore <nishraptor@gmail.com>
2023-01-26 10:45:00 -0800

    Added scheduling of aie2 trace kernel if the device is aie2
    Signed-off-by: Nishant Mysore <nishraptor@gmail.com>

--- src/runtime_src/xdp/profile/plugin/aie_trace_new/x86/aie_trace.cpp
+++ src/runtime_src/xdp/profile/plugin/aie_trace_new/x86/aie_trace.cpp
@@ -140,7 +140,11 @@ namespace xdp {
       auto device = xrt::device(spdevice);
     
       auto uuid = device.get_xclbin_uuid();
-      auto aie_trace_kernel = xrt::kernel(device, uuid.get(), "aie_trace_config");
+      xrt::kernel aie_trace_kernel;
+      if (metadata->getHardwareGen() == 1)
+        aie_trace_kernel = xrt::kernel(device, uuid.get(), "aie_trace_config");
+      else 
+        aie_trace_kernel = xrt::kernel(device, uuid.get(), "aie2_trace_config");
 
       //input bo  
       auto bo0 = xrt::bo(device, INPUT_SIZE, 2);
