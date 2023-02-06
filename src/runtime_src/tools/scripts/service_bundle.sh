3e738824ea57fe856c9b04eb1aa0a5634471a27c
David Zhang <davidzha@xilinx.com>
2023-02-06 14:38:01 -0800

    add core dump collection and more host info. add progress bar add bdf name checking add output directory option
    Signed-off-by: David Zhang <davidzha@xilinx.com>

--- src/runtime_src/tools/scripts/service_bundle.sh
+++ src/runtime_src/tools/scripts/service_bundle.sh
@@ -11,7 +11,7 @@ FULL_CMD=${BASH_SOURCE[0]}
 BASE_CMD=`basename "$0"`
 
 progress() {
-	echo "running: $func - progress: $(($1+1))/$2"
+	echo -n "running: $func - progress: $(($1+1))/$2"
 	#echo -ne "running: $func - progress: \b\b\b$(($1+1))/$(($2+1))\b\b\b"
 }
 
@@ -22,6 +22,7 @@ usage() {
     echo "  --device, -d        card device bdf"
     echo "  --all, -a           collect all, including large size system core dump"
     echo "  --verbose, -v       verbose output"
+    echo "Example: $BASE_CMD -d 9f"
     exit $1
 }
 
@@ -56,11 +57,33 @@ done
 #############################
 # Funcs start here
 #############################
+core_dumps()
+{
+	if [ -z $ALL_LOG ];then
+		echo -n " - skipped: -a to enable"
+		return 0
+	fi
+
+	mkdir -p $1/var/crash/
+	cp -r /var/crash/** $1/var/crash/
+	
+	mkdir -p $1/cores/
+	ls *.core > /dev/null 2>&1
+	if [ $? -eq 0 ];then
+		cp *.core $1/cores/
+	fi
+
+	ls /opt/xilinx/xrt/bin/*.core > /dev/null 2>&1
+	if [ $? -eq 0 ];then
+		cp /opt/xilinx/xrt/bin/*.core $1/cores/
+	fi
+}
+
 xbutil_output()
 {
 	xbutil examine -d $DEVICE_BDF:0 > $1/xbutil_examine.txt
 	xbutil examine -d $DEVICE_BDF:0 -r all > $1/xbutil_examine_all.txt
-	xbutil validate -d $DEVICE_BDF:0 --verbose> $1/xbutil_validate.txt
+	xbutil validate -d $DEVICE_BDF:0 -r quick --verbose> $1/xbutil_validate.txt
 }
 
 xbmgmt_output()
@@ -73,8 +96,21 @@ xbmgmt_output()
 
 debug_logs()
 {
+	mkdir -p $1/var/log/
+	cp -r /var/log/** $1/var/log/
+
+	for xclmgmt in `ls /sys/module/xclmgmt/parameters/`
+	do
+		cat /sys/module/xclmgmt/parameters/$xclmgmt > $1/xclmgmt_parameters_$xclmgmt.txt
+	done
+
+	for xocl in `ls /sys/module/xocl/parameters/`
+	do
+		cat /sys/module/xocl/parameters/$xocl > $1/xocl_parameters_$xocl.txt
+	done
+
 	dmesg -T > $1/dmesg_current.txt
-	cp -r /var/log/** $1/
+
 	cat /sys/kernel/debug/xclmgmt/trace > $1/sys_kernel_debug_xclmgmt.txt &
 	PID1=$!
 	cat /sys/kernel/debug/xocl/trace > $1/sys_kernel_debug_xocl.txt &
@@ -109,11 +145,33 @@ vmr_logs()
 	done
 }
 
+host_info()
+{
+	cat /proc/iomem > $1/proc_iomem.txt
+	cat /proc/interrupts > $1/proc_interrupts.txt
+
+	uname -a > $1/uname_a.txt
+	lsb_release -d > $1/lsb_release_d.txt
+	lscpu > $1/lscpu.txt
+	lsmem > $1/lsmem.txt
+	lspci > $1/lspci.txt
+	lspci -vvv > $1/lspci_vvv.txt
+	lsblk > $1/lsblk.txt
+	vmstat -s > $1/vmstat_s.txt
+
+	top -b > $1/top.txt &
+	PID3=$!
+	sleep 2
+	kill -s 2 $PID3
+}
+
 funcArray=(
-	vmr_logs
-	debug_logs
+	host_info
 	xbmgmt_output
 	xbutil_output
+	vmr_logs
+	debug_logs
+	core_dumps
 )
 
 #############################
@@ -123,23 +181,28 @@ funcArray=(
 if [ -z $DEVICE_BDF ];then
 	echo "Please select one device"
 	usage 0
-fi
-
-XRT_PATH=`which xbmgmt > /dev/null`
-if [ $? -ne 0 ];then
-	echo "XRT is not found, using default XRT"
-	source /opt/xilinx/xrt/setup.sh > /dev/null
-	XRT_PATH=`which xbmgmt > /dev/null`
 else
-	echo "using configured XRT"
+	if [[ "$DEVICE_BDF" == *":"* ]] || [[ "$DEVICE_BDF" == *"."* ]];then
+		echo "only support single bdf without : or . now"
+		usage 0
+	fi
+	echo "card: $DEVICE_BDF"
 fi
-echo "$XRT_PATH"
 
-if [ -z $PATH_TO_OUTPUT];then
+which xbmgmt > /dev/null
+if [ $? -ne 0 ];then
+	echo "XRT is not found, use default XRT"
+	source /opt/xilinx/xrt/setup.sh > /dev/null
+else
+	echo "use configured XRT"
+fi
+
+if [ -z $PATH_TO_OUTPUT ];then
 	PATH_TO_OUTPUT="."
 fi
 
-PATH_TO_OUTPUT=$PATH_TO_OUTPUT/xrt_bundle
+PATH_TO_BUNDLE=$PATH_TO_OUTPUT/xrt_bundle
+echo "dump into: $PATH_TO_BUNDLE"
 
 #a=${!funcArray[@]}
 total=${#funcArray[@]}
@@ -148,16 +211,26 @@ for func in "${funcArray[@]}"
 do
 	progress $index $total $func
 
-	if [ ! -d $PATH_TO_OUTPUT/$func ];then
-		mkdir -p "$PATH_TO_OUTPUT/$func"
+	if [ ! -d $PATH_TO_BUNDLE/$func ];then
+		mkdir -p "$PATH_TO_BUNDLE/$func"
 	fi
 
-	$func "$PATH_TO_OUTPUT/$func"
+	$func "$PATH_TO_BUNDLE/$func"
+	echo "."
 
 	((index++))
 done
 
-echo "$BASE_CMD finished"
+echo "packaging data..."
+FROMSIZE=`du -sk --apparent-size $PATH_TO_BUNDLE |cut -f 1`;
+CHECKPOINT=`echo ${FROMSIZE}/50 |bc`
+echo "Estimated: [==================================================]"
+echo -n "Progress:  [";
+tar --record-size=1K --checkpoint="${CHECKPOINT}" --checkpoint-action="ttyout=>" -czf $PATH_TO_OUTPUT/xrt_bundle.tar.gz --absolute-names $PATH_TO_BUNDLE
+echo "]"
+realpath $PATH_TO_OUTPUT/xrt_bundle.tar.gz
+du -sh $PATH_TO_OUTPUT/xrt_bundle.tar.gz|cut -f1
+
 if [ ! -z $VERBOSE ];then
-	tree $PATH_TO_OUTPUT
+	tree $PATH_TO_BUNDLE
 fi
