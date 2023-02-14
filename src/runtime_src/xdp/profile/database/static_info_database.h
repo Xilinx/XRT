beeb8a0151eea5dffe65d32d77c941c35b647321
Paul Schumacher <schuey@xilinx.com>
2023-02-14 10:56:47 -0700

    updated file
    Signed-off-by: Paul Schumacher <schuey@xilinx.com>

--- src/runtime_src/xdp/profile/database/static_info_database.h
+++ src/runtime_src/xdp/profile/database/static_info_database.h
@@ -1,6 +1,6 @@
 /**
  * Copyright (C) 2016-2021 Xilinx, Inc
- * Copyright (C) 2022 Advanced Micro Devices, Inc. - All rights reserved
+ * Copyright (C) 2022-2023 Advanced Micro Devices, Inc. - All rights reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License"). You may
  * not use this file except in compliance with the License. A copy of the
@@ -111,8 +111,6 @@ namespace xdp {
     bool resetDeviceInfo(uint64_t deviceId, const std::shared_ptr<xrt_core::device>& device);
 
     // Functions that create the overall structure of the Xclbin's PL region
-    bool initializeStructure(XclbinInfo*,
-                             const std::shared_ptr<xrt_core::device>&);
     void createComputeUnits(XclbinInfo*, const ip_layout*);
     void createMemories(XclbinInfo*, const mem_topology*);
     void createConnections(XclbinInfo*, const ip_layout*, const mem_topology*,
@@ -123,7 +121,6 @@ namespace xdp {
     void addPortInfo(XclbinInfo*, const char*, size_t);
 
     // Functions that initialize the structure of the debug/profiling IP
-    bool initializeProfileMonitors(DeviceInfo*, const std::shared_ptr<xrt_core::device>&);
     void initializeAM(DeviceInfo* devInfo, const std::string& name,
                       const struct debug_ip_data* debugIpData) ;
     void initializeAIM(DeviceInfo* devInfo, const std::string& name,
@@ -134,7 +131,15 @@ namespace xdp {
                        const struct debug_ip_data* debugIpData) ;
     void initializeTS2MM(DeviceInfo* devInfo,
                          const struct debug_ip_data* debugIpData) ;
-    double findClockRate(std::shared_ptr<xrt_core::device> device) ;
+
+    void setDeviceNameFromXclbin(uint64_t deviceId, xrt::xclbin xrtXclbin);
+    void setAIEClockRateMHz(uint64_t deviceId, xrt::xclbin xrtXclbin) ;
+    bool initializeStructure(XclbinInfo*, xrt::xclbin);
+    bool initializeProfileMonitors(DeviceInfo*, xrt::xclbin);
+    double findClockRate(xrt::xclbin);
+    DeviceInfo* updateDevice(uint64_t deviceId, xrt::xclbin xrtXclbin) ;
+
+    
 
   public:
     VPStaticDatabase(VPDatabase* d) ;
@@ -236,7 +241,6 @@ namespace xdp {
     XDP_EXPORT void deleteCurrentlyUsedDeviceInterface(uint64_t deviceId) ;
     XDP_EXPORT bool isDeviceReady(uint64_t deviceId) ;
     XDP_EXPORT double getClockRateMHz(uint64_t deviceId, bool PL = true) ;
-    XDP_EXPORT void setAIEClockRateMHz(std::shared_ptr<xrt_core::device> device, uint64_t deviceId) ;
     XDP_EXPORT void setDeviceName(uint64_t deviceId, const std::string& name) ; 
     XDP_EXPORT std::string getDeviceName(uint64_t deviceId) ;
     XDP_EXPORT void setDeviceIntf(uint64_t deviceId, DeviceIntf* devIntf) ;
@@ -261,6 +265,11 @@ namespace xdp {
     XDP_EXPORT Memory* getMemory(uint64_t deviceId, int32_t memId) ;
     // Reseting device information whenever a new xclbin is added
     XDP_EXPORT void updateDevice(uint64_t deviceId, void* devHandle) ;
+
+    // *********************************************************
+    // ***** Functions related to trace_processor tool *****
+    // ***** which creates events from raw PL trace    *****
+    XDP_EXPORT void updateDevice(uint64_t deviceId, const std::string& xclbinFile);
 
     // *********************************************************
     // ***** Functions related to AIE specific information *****
