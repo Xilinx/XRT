97482b1ad1e84c175fc9193251a2d56f83cb8e62
Ryan Chane <103678133+rchane@users.noreply.github.com>
2023-02-28 21:25:36 -0800

    CR-1135808 Update Scaling to Throttling in SubCmdDump (#7404)
    * CR-1135808 Improved output for cmc report
    
    * Adjustments to error message and output for SubCmdConfigure
    
    * CR-1135808 Update SubCmdDump to use renamed throttling key
    
    * CR-1135808 Fix formatting issues.

--- src/runtime_src/core/tools/xbmgmt2/SubCmdDump.cpp
+++ src/runtime_src/core/tools/xbmgmt2/SubCmdDump.cpp
@@ -91,15 +91,15 @@ config_dump(const std::shared_ptr<xrt_core::device>& _dev, const std::string m_o
     bool is_versal = xrt_core::device_query<xrt_core::query::is_versal>(_dev);
     if (is_versal) {
       try {
-        child.put("scaling_enabled", xrt_core::device_query<xrt_core::query::xgq_scaling_enabled>(_dev));
-        child.put("scaling_power_override", xrt_core::device_query<xrt_core::query::xgq_scaling_power_override>(_dev));
-        child.put("scaling_temp_override", xrt_core::device_query<xrt_core::query::xgq_scaling_temp_override>(_dev));
+        child.put("throttling_enabled", xrt_core::device_query<xrt_core::query::xgq_scaling_enabled>(_dev));
+        child.put("throttling_power_override", xrt_core::device_query<xrt_core::query::xgq_scaling_power_override>(_dev));
+        child.put("throttling_temp_override", xrt_core::device_query<xrt_core::query::xgq_scaling_temp_override>(_dev));
       } catch(const xrt_core::query::exception&) {}
     } else {
       try {
-        child.put("scaling_enabled", xrt_core::device_query<xrt_core::query::xmc_scaling_enabled>(_dev));
-        child.put("scaling_power_override", xrt_core::device_query<xrt_core::query::xmc_scaling_power_override>(_dev));
-        child.put("scaling_temp_override", xrt_core::device_query<xrt_core::query::xmc_scaling_temp_override>(_dev));
+        child.put("throttling_enabled", xrt_core::device_query<xrt_core::query::xmc_scaling_enabled>(_dev));
+        child.put("throttling_power_override", xrt_core::device_query<xrt_core::query::xmc_scaling_power_override>(_dev));
+        child.put("throttling_temp_override", xrt_core::device_query<xrt_core::query::xmc_scaling_temp_override>(_dev));
       } catch(const xrt_core::query::exception&) {}
     }
   }
