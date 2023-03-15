ab0f03275cbb99e4c645d413c3c753947085d89a
yvonneya <yvonneya@xilinx.com>
2023-03-15 12:45:59 -0700

    LWPAECGDEV-69: Fixed using xrt::error.to_string() triggers an exception
    Signed-off-by: yvonneya <yvonneya@xilinx.com>

--- src/runtime_src/core/common/api/xrt_error.cpp
+++ src/runtime_src/core/common/api/xrt_error.cpp
@@ -249,6 +249,9 @@ public:
   std::string
   to_string()
   {
+    if (!m_errcode)
+      return std::string("No async error was detected");
+
     auto fmt = boost::format
       ("%s\n"
        "Timestamp: %s")
