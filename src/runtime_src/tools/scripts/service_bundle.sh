37c1939d48e5807a8c74ad5b5fdc55e67ed8bfb7
Prapul Krishnamurthy <111782805+prapulkrishnamurthy@users.noreply.github.com>
2023-03-22 12:37:20 -0700

    CR-1153424 IOMMU Status dump (#7385)
    * Iommu log from dmesg impl
    
    Signed-off-by: prapulkrishnamurthy <prapulk@xilinx.com>
    
    * Improved changes as per Review
    
    Signed-off-by: prapulkrishnamurthy <prapulk@xilinx.com>
    
    * Rearrange Output Files Review Changes
    
    Signed-off-by: prapulkrishnamurthy <prapulk@xilinx.com>
    
    ---------
    
    Signed-off-by: prapulkrishnamurthy <prapulk@xilinx.com>

--- src/runtime_src/tools/scripts/service_bundle.sh
+++ src/runtime_src/tools/scripts/service_bundle.sh
@@ -156,6 +156,21 @@ host_info()
 {
 	cat /proc/iomem > $1/proc_iomem.txt
 	cat /proc/interrupts > $1/proc_interrupts.txt
+	cat /etc/default/grub > $1/etc_grub_cfg.txt
+	dmesg | grep iommu > $1/iommu_dump.txt
+
+	#########################IOMMU Groups########################################
+	rm -r $1/iommu_groups.txt 2> /dev/null
+	export devs=$(find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V)
+	for g in $devs; do
+		echo "IOMMU Group ${g##*/}:" >> $1/iommu_groups.txt
+		for d in $g/devices/*; do
+			VALUE="$(/sbin/lspci -nns ${d##*/})"
+			echo -e "\t$VALUE"
+			printf "	%s\n" "$VALUE" >> $1/iommu_groups.txt
+		done;
+	done;
+	##############################################################################
 
 	uname -a > $1/uname_a.txt
 	lsb_release -d > $1/lsb_release_d.txt
