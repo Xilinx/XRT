0e20a46ba63505feee9aa34c1e8758f2d0772cfe
Soren Soe <2106410+stsoe@users.noreply.github.com>
2023-03-30 10:43:35 -0700

    CR-1158565 update_access_mode: Operation not supported
    Amend #7470 with updates to all shims in XRT to support
    update_access_mode which is used for HLS mailbox feature.
    
    Signed-off-by: Soren Soe <2106410+stsoe@users.noreply.github.com>

--- src/runtime_src/core/pcie/emulation/hw_emu/alveo_shim/shim.h
+++ src/runtime_src/core/pcie/emulation/hw_emu/alveo_shim/shim.h
@@ -117,6 +117,12 @@ using addr_type = uint64_t;
         , m_mode(mode)
       {}
 
+      void
+      update_access_mode(access_mode mode) override
+      {
+        m_mode = mode;
+      }
+
       slot_id
       get_slotidx() const override
       {
