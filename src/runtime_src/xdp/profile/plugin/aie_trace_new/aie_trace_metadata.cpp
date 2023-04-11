30295291b309a4cdf89030bce91a61c181a635cd
Paul Schumacher <schuey@xilinx.com>
2023-04-11 08:47:26 -0600

    fix for channel numbers in MEM tiles
    Signed-off-by: Paul Schumacher <schuey@xilinx.com>

--- src/runtime_src/xdp/profile/plugin/aie_trace_new/aie_trace_metadata.cpp
+++ src/runtime_src/xdp/profile/plugin/aie_trace_new/aie_trace_metadata.cpp
@@ -581,11 +581,11 @@ namespace xdp {
       }
 
       // Grab channel numbers (if specified; MEM tiles only)
-      if (graphMetrics[i].size() == 5) {
+      if (graphMetrics[i].size() > 3) {
         try {
           for (auto &e : tiles) {
             configChannel0[e] = std::stoi(graphMetrics[i][3]);
-            configChannel1[e] = std::stoi(graphMetrics[i][4]);
+            configChannel1[e] = std::stoi(graphMetrics[i].back());
           }
         } catch (...) {
           std::stringstream msg;
@@ -638,11 +638,11 @@ namespace xdp {
       }
 
       // Grab channel numbers (if specified; MEM tiles only)
-      if (graphMetrics[i].size() == 5) {
+      if (graphMetrics[i].size() > 3) {
         try {
           for (auto &e : tiles) {
             configChannel0[e] = std::stoi(graphMetrics[i][3]);
-            configChannel1[e] = std::stoi(graphMetrics[i][4]);
+            configChannel1[e] = std::stoi(graphMetrics[i].back());
           }
         } catch (...) {
           std::stringstream msg;
@@ -685,11 +685,11 @@ namespace xdp {
       }
 
       // Grab channel numbers (if specified; MEM tiles only)
-      if (metrics[i].size() == 4) {
+      if (metrics[i].size() > 2) {
         try {
           for (auto &e : tiles) {
             configChannel0[e] = std::stoi(metrics[i][2]);
-            configChannel1[e] = std::stoi(metrics[i][3]);
+            configChannel1[e] = std::stoi(metrics[i].back());
           }
         } catch (...) {
           std::stringstream msg;
@@ -741,10 +741,10 @@ namespace xdp {
 
       uint8_t channel0 = 0;
       uint8_t channel1 = 1;
-      if (metrics[i].size() == 5) {
+      if (metrics[i].size() > 3) {
         try {
           channel0 = std::stoi(metrics[i][3]);
-          channel1 = std::stoi(metrics[i][4]);
+          channel1 = std::stoi(metrics[i].back());
         } catch (...) {
           std::stringstream msg;
           msg << "Channel specifications in tile_based_" << tileName
@@ -821,10 +821,10 @@ namespace xdp {
       configMetrics[tile] = metrics[i][1];
       
       // Grab channel numbers (if specified; MEM tiles only)
-      if (metrics[i].size() == 4) {
+      if (metrics[i].size() > 2) {
         try {
           configChannel0[tile] = std::stoi(metrics[i][2]);
-          configChannel1[tile] = std::stoi(metrics[i][3]);
+          configChannel1[tile] = std::stoi(metrics[i].back());
         } catch (...) {
           std::stringstream msg;
           msg << "Channel specifications in tile_based_" << tileName
