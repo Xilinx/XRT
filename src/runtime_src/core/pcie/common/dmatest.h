68c9c81c464d12fd3841351415b29c2ae2ab6713
Saif <saifuddi@amd.com>
2023-04-19 22:34:21 -0500

    Cherry-pick dma validation issue to 2023.1
    Signed-off-by: Saif <saifuddi@xilinx.com>

--- src/runtime_src/core/pcie/common/dmatest.h
+++ src/runtime_src/core/pcie/common/dmatest.h
@@ -44,6 +44,7 @@ namespace xcldev {
         using buffer_and_deleter = std::pair<xrt_buffer_handle, xrt_core::aligned_ptr_type>;
         std::vector<buffer_and_deleter> mBOList;
         std::shared_ptr<xrt_core::device> mHandle;
+	std::unique_ptr<xrt_core::hwctx_handle> mhwCtxHandle;
         size_t mSize;
         size_t mTotalSize;
         unsigned mFlags;
@@ -129,10 +130,15 @@ namespace xcldev {
             if (count > 0x40000)
                 count = 0x40000;
 
+	    mhwCtxHandle = mHandle->create_hw_context(mHandle->get_xclbin_uuid().get(), {}, xrt::hw_context::access_mode::shared);
+	    xcl_bo_flags xflags{mFlags};
+	    xflags.slot = static_cast<uint8_t>(mhwCtxHandle->get_slotidx());
+	    mFlags = xflags.flags;
+
             for (long long i = 0; i < count; i++) {
                 // This can throw and callers of DMARunner are supposed to catch this.
                 xrt_core::aligned_ptr_type buf = xrt_core::aligned_alloc(xrt_core::getpagesize(), mSize);
-                auto bo = mHandle->alloc_bo(buf.get(), mSize, mFlags);
+                auto bo = mhwCtxHandle->alloc_bo(buf.get(), mSize, mFlags);
                 if (bo == XRT_INVALID_BUFFER_HANDLE)
                     break;
                 std::memset(buf.get(), mPattern, mSize);
