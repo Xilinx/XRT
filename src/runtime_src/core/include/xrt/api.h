7a8c9df6aa368b1a81555333565dccbc3f9b1020
Bryan Lozano <b.lozano.havoc@gmail.com>
2023-05-09 17:07:21 -0700

    Create xrt/api.h
    Provide users with an aggregating header file.
    
    This will allow users who are less familiar with xrt code base to quickly bring all of the APIs into their compilation unit.

---
+++ src/runtime_src/core/include/xrt/api.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2020-2022 Xilinx, Inc.  All rights reserved.
+// Copyright (C) 2022-2023 Advanced Micro Devices, Inc. All rights reserved.
+#ifndef _XRT_API_H_
+#define _XRT_API_H_
+
+#include <xrt/xrt_aie.h>
+#include <xrt/xrt_bo.h>
+#include <xrt/xrt_device.h>
+#include <xrt/xrt_graph.h>
+#include <xrt/xrt_hw_context.h>
+#include <xrt/xrt_kernel.h>
+#include <xrt/xrt_uuid.h>
+
+#endif
