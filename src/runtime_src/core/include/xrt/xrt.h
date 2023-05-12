d30f113c10bc93db77d6dd17cdaba942f7eadc97
bryanloz <bryanloz@xilinx.com>
2023-05-12 10:19:23 -0600

    Add aggregating header file for xrt/ APIs (Nonexperimental)
    Signed-off-by: bryanloz <bryanloz@xilinx.com>

---
+++ src/runtime_src/core/include/xrt/xrt.h
@@ -0,0 +1,12 @@
+#ifndef _XRT_XRT_H_
+#define _XRT_XRT_H_
+
+#include <xrt/xrt_aie.h>
+#include <xrt/xrt_bo.h>
+#include <xrt/xrt_device.h>
+#include <xrt/xrt_graph.h>
+#include <xrt/xrt_hw_context.h>
+#include <xrt/xrt_kernel.h>
+#include <xrt/xrt_uuid.h>
+
+#endif // _XRT_XRT_H_
