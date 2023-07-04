1101f8d9b588a766e67ab871584aa6051438c5d5
Daniel Benusovich <94083344+dbenusov-xilinx@users.noreply.github.com>
2023-07-03 22:53:29 -0700

    VITIS-8985 Refactor hw context metadata field in query requests (#7610)
    * VITIS-8971 Update memory report backend to prepare for MCDM changes
    
    Signed-off-by: Daniel Benusovich <dbenusov@xilinx.com>
    
    * VITIS-8971 Update aie partition report to use metadata structure
    
    Signed-off-by: Daniel Benusovich <dbenusov@xilinx.com>
    
    ---------
    
    Signed-off-by: Daniel Benusovich <dbenusov@xilinx.com>

--- src/runtime_src/core/tools/common/ReportAiePartitions.cpp
+++ src/runtime_src/core/tools/common/ReportAiePartitions.cpp
@@ -24,8 +24,8 @@ populate_aie_partition(const xrt_core::device* device)
     auto partition = pt_map.emplace(std::make_tuple(entry.start_col, entry.num_cols), boost::property_tree::ptree());
 
     boost::property_tree::ptree pt_entry;
-    pt_entry.put("xclbin_uuid", entry.xclbin_uuid);
-    pt_entry.put("slot_id", entry.slot_id);
+    pt_entry.put("xclbin_uuid", entry.metadata.xclbin_uuid);
+    pt_entry.put("slot_id", entry.metadata.id);
     pt_entry.put("usage_count", entry.usage_count);
     pt_entry.put("migration_count", entry.migration_count);
     pt_entry.put("device_bo_sync_count", entry.bo_sync_count);
