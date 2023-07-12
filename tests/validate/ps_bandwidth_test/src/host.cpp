c1de376356c41406224b294a06f617dc340d2682
Daniel Benusovich <dbenusov@xilinx.com>
2023-07-12 11:05:46 -0700

    VITIS-6327 Update host executables to use new property tree command options. Add dependency vectors into host executables
    Signed-off-by: Daniel Benusovich <dbenusov@xilinx.com>

--- tests/validate/ps_bandwidth_test/src/host.cpp
+++ tests/validate/ps_bandwidth_test/src/host.cpp
@@ -13,73 +13,69 @@
 * License for the specific language governing permissions and limitations
 * under the License.
 */
+#include <boost/program_options.hpp>
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <vector>
 // XRT includes
 #include "experimental/xrt_system.h"
 #include "xrt/xrt_device.h"
 #include "xrt/xrt_kernel.h"
 #include "xrt/xrt_bo.h"
 
-static void printHelp() {
-    std::cout << "usage: %s <options>\n";
-    std::cout << "  -p <path>\n";
-    std::cout << "  -d <device> \n";
-    std::cout << "  -l <loop_iter_cnt> \n";
-    std::cout << "  -s <supported>\n";
-    std::cout << "  -h <help>\n";
+static int
+validate_binary_file(const std::string& binaryfile, bool print = false)
+{
+    std::ifstream infile(binaryfile);
+    if (!infile.good()) {
+        if (print)
+            std::cout << "\nNOT SUPPORTED" << std::endl;
+        return EOPNOTSUPP;
+    } else {
+        if (print)
+            std::cout << "\nSUPPORTED" << std::endl;
+        return EXIT_SUCCESS;
+    }
 }
 
 int main(int argc, char** argv) {
     std::string dev_id = "0";
     std::string test_path;
-    std::string iter_cnt = "10000";
-    std::string b_file = "/ps_bandwidth.xclbin";
-    bool flag_s = false;
+    std::string iter_cnt;
+    std::string b_file;
+    bool flag_s;
 
-    for (int i = 1; i < argc; i++) {
-        if ((strcmp(argv[i], "-p") == 0) || (strcmp(argv[i], "--path") == 0)) {
-            test_path = argv[i + 1];
-        } else if ((strcmp(argv[i], "-d") == 0) || (strcmp(argv[i], "--device") == 0)) {
-            dev_id = argv[i + 1];
-        } else if ((strcmp(argv[i], "-l") == 0) || (strcmp(argv[i], "--loop_iter_cnt") == 0)) {
-            iter_cnt = argv[i + 1];
-        } else if ((strcmp(argv[i], "-s") == 0) || (strcmp(argv[i], "--supported") == 0)) {
-            flag_s = true;
-        } else if ((strcmp(argv[i], "-h") == 0) || (strcmp(argv[i], "--help") == 0)) {
-            printHelp();
-            return 1;
-        }
-    }
+    boost::program_options::options_description options;
+    options.add_options()
+        ("help,h", "Print help messages")
+        ("xclbin,x", boost::program_options::value<decltype(b_file)>(&b_file)->implicit_value("/lib/firmware/xilinx/ps_kernels/ps_bandwidth.xclbin"), "Path to the xclbin file for the test")
+        ("path,p", boost::program_options::value<decltype(test_path)>(&test_path)->required(), "Path to the platform resources")
+        ("device,d", boost::program_options::value<decltype(dev_id)>(&dev_id)->required(), "The Bus:Device.Function (e.g., 0000:d8:00.0) device of interest")
+        ("supported,s", boost::program_options::bool_switch(&flag_s), "Print supported or not")
+        ("include,i" , boost::program_options::value<decltype(depedency_paths)>(&depedency_paths)->multitoken(), "Paths to xclbins required for this test")
+        ("loop_iter_cnt,l", boost::program_options::value<decltype(iter_cnt)>(&iter_cnt)->implicit_value("10000"), "The number of iterations the test will sample over")
+    ;
 
-    if (test_path.empty()) {
-        std::cout << "ERROR : please provide the platform test path to -p option\n";
-        return EXIT_FAILURE;
-    }
-
-    std::string binaryfile = test_path + b_file;
-    std::ifstream infile(binaryfile);
-    if (flag_s) {
-        if (!infile.good()) {
-            std::cout << "\nNOT SUPPORTED" << std::endl;
-            return EOPNOTSUPP;
-        } else {
-            std::cout << "\nSUPPORTED" << std::endl;
+    boost::program_options::variables_map vm;
+    try {
+        boost::program_options::store(boost::program_options::parse_command_line(argc, argv, options), vm);
+        if (vm.count("help")) {
+            std::cout << options << std::endl;
             return EXIT_SUCCESS;
         }
-    }
-
-    if (!infile.good()) {
-        std::cout << "\nNOT SUPPORTED" << std::endl;
-        return EOPNOTSUPP;
+        boost::program_options::notify(vm);
+    } catch (boost::program_options::error& e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        std::cout << options << std::endl;
+        return EXIT_FAILURE;
     }
 
     auto num_devices = xrt::system::enumerate_devices();
 
     auto device = xrt::device {dev_id};
 
-    auto uuid = device.load_xclbin(binaryfile);
+    auto uuid = device.load_xclbin(b_file);
     auto bandwidth_kernel = xrt::kernel(device, uuid, "bandwidth_kernel");
 
     auto max_throughput_bo = xrt::bo(device, 4096, bandwidth_kernel.group_id(1));
