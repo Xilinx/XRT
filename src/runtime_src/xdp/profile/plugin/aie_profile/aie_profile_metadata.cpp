933aefd4db5c08b8fc634860f5d77d69fa8d6541
Daniel Benusovich <94083344+dbenusov-xilinx@users.noreply.github.com>
2023-07-17 07:54:34 -0700

    Fix windows VS22 compilation warnings (#7627)

--- src/runtime_src/xdp/profile/plugin/aie_profile/aie_profile_metadata.cpp
+++ src/runtime_src/xdp/profile/plugin/aie_profile/aie_profile_metadata.cpp
@@ -742,8 +742,8 @@ namespace xdp {
       if (graphMetrics[i].size() > 3) {
         try {
           for (auto &e : tiles) {
-            configChannel0[e] = std::stoi(graphMetrics[i][3]);
-            configChannel1[e] = std::stoi(graphMetrics[i].back());
+            configChannel0[e] = static_cast<uint8_t>(std::stoi(graphMetrics[i][3]));
+            configChannel1[e] = static_cast<uint8_t>(std::stoi(graphMetrics[i].back()));
           }
         } catch (...) {
           std::stringstream msg;
@@ -791,8 +791,8 @@ namespace xdp {
       if (graphMetrics[i].size() > 3) {
         try {
           for (auto &e : tiles) {
-            configChannel0[e] = std::stoi(graphMetrics[i][3]);
-            configChannel1[e] = std::stoi(graphMetrics[i].back());
+            configChannel0[e] = static_cast<uint8_t>(std::stoi(graphMetrics[i][3]));
+            configChannel1[e] = static_cast<uint8_t>(std::stoi(graphMetrics[i].back()));
           }
         } catch (...) {
           std::stringstream msg;
