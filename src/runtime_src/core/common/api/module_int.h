d16182cb34b7df656cbe8812b5a1ed1ac177a821
Soren Soe <2106410+stsoe@users.noreply.github.com>
2023-08-08 08:05:12 -0700

    Vitis-8819 Elf parsing and patching (#7659)
    * Vitis-8819  Mine ELF for column control code
    
    Support multi-column control code in page sized chunks.
    
    Amend definition of ert_dpu_data to allow for multiple copies in an
    ERT packet.
    
    Add ELF parsing code to xrt::module implementation.
    
    Update xrt::run to amend ert_packet payload count with the number
    of ert_dpu_packets prepended to packet payload.
    
    Still to-do is to support argument patching of control code.
    
    Signed-off-by: Soren Soe <2106410+stsoe@users.noreply.github.com>
    
    * Vitis-8819  Mine ELF for dynamic relocation info and patch ctrlcode
    
    Parse ELF for dynamic relocation sections and create argument patcher
    objects for patching of control code when arguments are known.
    
    The parsing and creation of dynamic relocation data and the creation
    of argument address patchers is done at ELF load time.
    
    The xrt::module ELF loader extracts AIE column ctrlcode from the ELF
    file and creates the argument patchers from dynamic relocation
    sections.
    
    The xrt::module is used when construction an xrt::kernel object. The
    kernel's xrt::module serves as a template for xrt::run objects created
    from the kernel.  A run object copies the module control code into an
    xrt::bo buffer object.  The address of the buffer object is used by
    the argument patchers when the run object patches the control code
    with buffer objects and scalar argument values.
    
    The underlying assumption is that all column ctrlcode is stored in a
    contiguous buffer object at the offsets calculated by the xrt::module
    ELF loader.
    
    The code added in this pull request is work-in-progress and very
    specific to our current control code (ELF) definition.  As our usage
    evolve, the code will be refactored into shim specific implemenations.
    
    Signed-off-by: Soren Soe <2106410+stsoe@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Soren Soe <2106410+stsoe@users.noreply.github.com>

--- src/runtime_src/core/common/api/module_int.h
+++ src/runtime_src/core/common/api/module_int.h
@@ -16,8 +16,23 @@ namespace xrt_core::module_int {
 
 // Provide access to underlying xrt::bo representing the instruction
 // buffer
-xrt::bo
-get_instruction_buffer(const xrt::module& module, const std::string& nm);
+const std::vector<std::pair<uint64_t, uint64_t>>&
+get_ctrlcode_addr_and_size(const xrt::module& module);
+
+
+// Patch buffer object into control code at given argument
+void
+patch(const xrt::module&, const std::string& argnm, const xrt::bo& bo);
+
+// Patch scalar into control code at given argument
+void
+patch(const xrt::module&, const std::string& argnm, const void* value, size_t size);
+
+// Check that all arguments have been patched and sync the buffer
+// to device if necessary.  Throw if not all arguments have been
+// patched.
+void
+sync(const xrt::module&);
 
 } // xrt_core::module_int
 
