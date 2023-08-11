21fc36c140952d05261e55d2964026af1c63cb97
Daniel Benusovich <94083344+dbenusov-xilinx@users.noreply.github.com>
2023-08-10 21:45:41 -0700

    CR-1170456 Add NOT SUPPORTED string to all ps tests (#7663)
    Signed-off-by: Daniel Benusovich <dbenusov@xilinx.com>

--- tests/validate/ps_aie_test/src/host.cpp
+++ tests/validate/ps_aie_test/src/host.cpp
@@ -75,6 +75,7 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
+    std::cout << "\nNOT SUPPORTED" << std::endl;
     return EOPNOTSUPP;
 
     auto num_devices = xrt::system::enumerate_devices();
