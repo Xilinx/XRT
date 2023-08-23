c92f0f2cf738a4741a32f4b90bc540f1be15f75d
Garg <saumyag@amd.com>
2023-08-23 05:05:54 +0000

    fix review comment
    Signed-off-by: Garg <saumyag@amd.com>

--- src/runtime_src/core/tools/common/reports/ReportAie.cpp
+++ src/runtime_src/core/tools/common/reports/ReportAie.cpp
@@ -90,7 +90,7 @@ writeReport(const xrt_core::device* /*_pDevice*/,
   _output << "Aie\n";
   // validate and print aie metadata by checking schema_version node
   if (!_pt.get_child_optional("aie_metadata.schema_version")) {
-    _output << "  <AIE information unavailable>" << std::endl << std::endl;
+    _output << "  <AIE information is not available>" << std::endl << std::endl;
     return;
   }
 
