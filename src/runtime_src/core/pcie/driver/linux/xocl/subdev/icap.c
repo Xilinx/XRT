cc6573b696d5a6401bbd50e363bf74cdc563ce56
Manoj Takasi <mtakasi@xsjubn18vda.xilinx.com>
2023-09-06 21:56:48 +0530

    Return ENODATA Error if bitstream is not present
    Signed-off-by: Manoj Takasi <mtakasi@xsjubn18vda.xilinx.com>

--- src/runtime_src/core/pcie/driver/linux/xocl/subdev/icap.c
+++ src/runtime_src/core/pcie/driver/linux/xocl/subdev/icap.c
@@ -1111,6 +1111,10 @@ static int icap_download_hw(struct icap *icap, const struct axlf *axlf)
 		primaryFirmwareOffset = primaryHeader->m_sectionOffset;
 		primaryFirmwareLength = primaryHeader->m_sectionSize;
 	}
+        else{
+                ICAP_ERR(icap,"invalied xclbin failed to read bitstream");
+                err = -ENODATA;
+                goto done;
 
 	if ((primaryFirmwareOffset + primaryFirmwareLength) > length) {
 		ICAP_ERR(icap, "Invalid BITSTREAM size");
