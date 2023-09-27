b4c68d9c6e512997de0ceb4e7f3260c38ad80795
IshitaGhosh <ighosh.account@gmail.com>
2023-09-26 19:27:48 -0600

    Add gurad macros
    Signed-off-by: IshitaGhosh <ighosh.account@gmail.com>

--- src/runtime_src/core/common/xdp/aie_profile.h
+++ src/runtime_src/core/common/xdp/aie_profile.h
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (C) 2023 Advanced Micro Devices, Inc. - All rights reserved
 
+#ifndef CORE_COMMON_XDP_AIE_PROFILE_H
+#define CORE_COMMON_XDP_AIE_PROFILE_H
+
 // This file declares APIs for XDP AIE Profile Plugin which make 
 // connections between all xrt_coreutil level hooks to the plugin.
 
@@ -24,3 +27,4 @@ end_poll(void* handle);
 
 } // end namespace xrt_core::xdp::aie::profile
 
+#endif // CORE_COMMON_XDP_AIE_PROFILE_H
