b9eecbc58befefcd26c51c94be58efeeeb303f7a
Soren Soe <2106410+stsoe@users.noreply.github.com>
2023-12-18 17:33:38 -0800

    Skeleton directory and build infrastructure for HIP bindings
    Very preliminary infra for HIP bindings.
    Build on linux with `build.sh -hip [other options]`
    
    Signed-off-by: Soren Soe <2106410+stsoe@users.noreply.github.com>

---
+++ src/runtime_src/hip/core/device.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 Advanced Micro Device, Inc. All rights reserved.
+#ifndef xrthip_device_h
+#define xrthip_device_h
+
+namespace xrthip_core {
+
+class device
+{
+};
+  
+} // xrthip_core
+
+#endif
