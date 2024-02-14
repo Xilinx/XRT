cb1a2479857d1ea43dfac06a8b7a273f3d0c5267
IshitaGhosh <ighosh.account@gmail.com>
2024-02-13 19:02:18 -0800

    Use Hw Context create implementation
    Signed-off-by: IshitaGhosh <ighosh.account@gmail.com>

--- src/runtime_src/xdp/profile/device/xrtIP.cpp
+++ src/runtime_src/xdp/profile/device/xrtIP.cpp
@@ -15,6 +15,7 @@
  */
 
 #include "core/common/message.h"
+#include "core/common/api/hw_context_int.h"
 
 #include "xrtIP.h"
 
@@ -31,10 +32,12 @@ XrtIP
   : xdpDevice(handle)
   , fullname(fullname)
   , deadlockDiagnosis(std::string())
-  , index(-1)
+//  , index(-1)
 {
+  hwContext = xrt_core::hw_context_int::create_hw_context_from_implementation(xdpDevice->getRawDevice());
+  xrtIP = new xrt::ip(hwContext, fullname);
   size_t pos = fullname.find(':');
-  kernelName = fullname.substr(0, pos);
+  std::string kernelName = fullname.substr(0, pos);
 
   // Find register info for our kernel
   for (auto& pair : ip_metadata_section->kernel_infos) {
@@ -48,21 +51,52 @@ XrtIP
   if (regInfo.empty())
     return;
 
+#if 0
   // Try to enable register access
   uint32_t low  = regInfo.begin()->first;
   uint32_t high = regInfo.rbegin()->first + REGSIZE_BYTES;
   index = xdpDevice->initXrtIP(fullname.c_str(), low, (high - low));
+#endif
 }
 
+XrtIP::
+~XrtIP()
+{
+  delete xrtIP;
+}
+#if 0
 int XrtIP::
 read(uint32_t offset, uint32_t* data)
 {
-  return xdpDevice->readXrtIP(index, offset, data);
+//  return xdpDevice->readXrtIP(index, offset, data);
+  *data = xrtIP->read_register(offset);
 }
+#endif
 
 std::string& XrtIP::
 getDeadlockDiagnosis(bool print)
 {
+  if (regInfo.empty())
+    return deadlockDiagnosis;
+
+  // Query this IP
+  for (const auto& e: regInfo) {
+//    uint32_t regdata = 0;
+    auto offset = e.first;
+    auto& messages = e.second;
+    // HW_EMU ??
+    uint32_t kernelInstRegData = xrtIP->read_register(offset);
+    for (unsigned int i=0; i < num_bits_deadlock_diagnosis; i++) {
+      if ((kernelInstRegData >> i) & 0x1)
+        deadlockDiagnosis += messages[i] + "\n";
+    }
+  }
+
+  if (print)
+    xrt_core::message::send(xrt_core::message::severity_level::warning, "XRT", deadlockDiagnosis);
+  return deadlockDiagnosis;
+
+#if 0
   if (regInfo.empty() || !initialized())
     return deadlockDiagnosis;
 
@@ -81,6 +115,7 @@ getDeadlockDiagnosis(bool print)
   if (print)
     xrt_core::message::send(xrt_core::message::severity_level::warning, "XRT", deadlockDiagnosis);
   return deadlockDiagnosis;
+#endif
 }
 
 }   // namespace xdp
