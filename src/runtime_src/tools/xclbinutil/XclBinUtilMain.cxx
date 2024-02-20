6e270f356103981280d43cc0aec71cf7f9d7dfe1
Fei Rui <36643163+xfreid@users.noreply.github.com>
2024-02-20 10:36:52 -0700

    Update src/runtime_src/tools/xclbinutil/XclBinUtilMain.cxx
    Co-authored-by: Ronan Keryell <ronan.keryell@amd.com>

--- src/runtime_src/tools/xclbinutil/XclBinUtilMain.cxx
+++ src/runtime_src/tools/xclbinutil/XclBinUtilMain.cxx
@@ -56,7 +56,7 @@ void drcCheckFiles(const std::vector<std::string> & _inputFiles,
    std::set<std::string> normalizedInputFiles;
 
    for( auto file : _inputFiles) {
-     if ( !fs::exists(file)) {
+     if (!fs::exists(file)) {
        std::string errMsg = "ERROR: The following input file does not exist: " + file;
        throw std::runtime_error(errMsg);
      }
