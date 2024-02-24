d6e24634d420446426618fd635ada1530c48dbfb
Chiming Zhang <Chiming.Zhang@amd.com>
2024-02-23 22:06:32 -0800

    VITIS-11112 HIP Binding: Memory Management (#7929)
    * stubs for hip memory API. (#6)
    
    Signed-off-by: Chiming Zhang <Chiming.Zhang@amd.com>
    
    * Stubs for HIP memory APIs (#7)
    
    * stubs for hip memory API.
    
    Signed-off-by: Chiming Zhang <Chiming.Zhang@amd.com>
    
    * fix some code formatting.
    
    Signed-off-by: Chiming Zhang <Chiming.Zhang@amd.com>
    
    ---------
    
    Signed-off-by: Chiming Zhang <Chiming.Zhang@amd.com>
    
    ---------
    
    Signed-off-by: Chiming Zhang <Chiming.Zhang@amd.com>

---
+++ src/runtime_src/hip/core/memory.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 Advanced Micro Device, Inc. All rights reserved.
+#ifndef xrthip_memory_h
+#define xrthip_memory_h
+
+
+namespace xrt::core::hip {
+
+
+class memory
+{
+
+};
+  
+} // xrt::core::hip
+
+#endif
