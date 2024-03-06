6a2d687b55a367f2e3b22b3a3e98780e44b73dca
xilinxfei <fei@xilinx.com>
2024-03-05 17:36:51 -0700

    integrate aie-pdi-transform into xclbinutil
    Signed-off-by: xilinxfei <fei@xilinx.com>

---
+++ src/runtime_src/tools/xclbinutil/aie-pdi-transform/src/cdo-parsing.c
@@ -0,0 +1,43 @@
+/******************************************************************************
+* Copyright (c) 2021 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+
+/*****************************************************************************/
+/**
+*
+* @file transform-parsing.c
+* @addtogroup test CDO parsing
+* @{
+* @cond transform-parsing
+* This is the file which contains general commands.
+*
+* @note
+* @endcond
+*
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+#include <stdio.h>
+#include <string.h>
+#include "cdo_cmd.h"
+#include "cdo_common.h"
+/************************** Constant Definitions *****************************/
+
+/**************************** Type Definitions *******************************/
+
+/***************** Macros (Inline Functions) Definitions *********************/
+/************************** Variable Definitions *****************************/
+int test_cdo_main (void)
+{
+	int Ret = 0;
+	return Ret;
+}
+
+/**
+ * @}
+ * @endcond
+ */
+
+ /** @} */
