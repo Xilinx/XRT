b09819b7c7410ee7b156015eb2c5307f63a2f667
Vinod Pangul <146476973+vipangul@users.noreply.github.com>
2024-03-25 16:14:14 -0700

    remove macro usage for client code
    Signed-off-by: Vinod Pangul <146476973+vipangul@users.noreply.github.com>

--- src/runtime_src/xdp/profile/plugin/aie_trace/client/aie_trace.cpp
+++ src/runtime_src/xdp/profile/plugin/aie_trace/client/aie_trace.cpp
@@ -808,11 +808,9 @@ namespace xdp {
 
     // Get partition columns
     // NOTE: for now, assume a single partition
-    uint8_t startCol = 0;
-#ifdef XDP_CLIENT_BUILD
     auto partitionCols = xdp::aie::getPartitionStartColumnsClient(handle);
-    startCol = partitionCols.at(0);
-#endif
+    uint8_t startCol = partitionCols.at(0);
+    
     //Start recording the transaction
     XAie_StartTransaction(&aieDevInst, XAIE_TRANSACTION_DISABLE_AUTO_FLUSH);
 
