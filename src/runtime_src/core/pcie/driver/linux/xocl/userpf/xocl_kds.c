f7eb888350119cf67efe6f53e7f6b5578776d5c4
Rajkumar Rampelli <rampelli@amd.com>
2024-04-24 00:23:30 +0530

    fixing rw_shared configuration in xgq based flow

--- src/runtime_src/core/pcie/driver/linux/xocl/userpf/xocl_kds.c
+++ src/runtime_src/core/pcie/driver/linux/xocl/userpf/xocl_kds.c
@@ -2259,6 +2259,7 @@ static int xocl_kds_update_xgq(struct xocl_dev *xdev, int slot_hdl,
 		XDEV(xdev)->kds.ert_disable = true;
 		goto create_regular_cu;
 	}
+        XDEV(xdev)->kds.cu_mgmt.rw_shared = cfg.rw_shared;
 
 	// Soft Kernel Info
 	scu_info = kzalloc(MAX_CUS * sizeof(struct xrt_cu_info), GFP_KERNEL);
