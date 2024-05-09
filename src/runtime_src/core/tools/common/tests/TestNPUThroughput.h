d6aa3b123bd6371f2530ca2b22851c9d727758b5
AShivangi <shivangiagarwal53@gmail.com>
2024-05-08 23:43:55 -0700

    Separate throughput and latency
    Signed-off-by: AShivangi <shivangiagarwal53@gmail.com>

--- src/runtime_src/core/tools/common/tests/TestNPUThroughput.h
+++ src/runtime_src/core/tools/common/tests/TestNPUThroughput.h
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright (C) 2023 Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.
 
-#ifndef __TestIPU_h_
-#define __TestIPU_h_
+#ifndef __TestNPUThroughput_h_
+#define __TestNPUThroughput_h_
 
 #include "tools/common/TestRunner.h"
 #include "xrt/xrt_device.h"
 
-class TestIPU : public TestRunner {
+class TestNPUThroughput : public TestRunner {
   public:
     boost::property_tree::ptree run(std::shared_ptr<xrt_core::device> dev);
 
   public:
-    TestIPU();
+    TestNPUThroughput();
 };
 
 #endif
--- src/runtime_src/core/tools/common/tests/TestVerify.cpp
+++ src/runtime_src/core/tools/common/tests/TestVerify.cpp
@@ -4,7 +4,6 @@
 // ------ I N C L U D E   F I L E S -------------------------------------------
 // Local - Include Files
 #include "TestVerify.h"
-#include "TestIPU.h"
 #include "tools/common/XBUtilities.h"
 namespace XBU = XBUtilities;
 
@@ -12,7 +11,8 @@ namespace XBU = XBUtilities;
 #include "xrt/xrt_bo.h"
 #include "xrt/xrt_device.h"
 #include "xrt/xrt_kernel.h"
-#define LENGTH 64
+
+static constexpr size_t buffer_size = 64;
 
 // ----- C L A S S   M E T H O D S -------------------------------------------
 TestVerify::TestVerify()
@@ -25,28 +25,13 @@ boost::property_tree::ptree
 TestVerify::run(std::shared_ptr<xrt_core::device> dev)
 {
   boost::property_tree::ptree ptree;
-  switch (xrt_core::device_query<xrt_core::query::device_class>(dev)) {
-  case xrt_core::query::device_class::type::ryzen:
-    ptree = TestIPU{}.run(dev);
-    break;
-  case xrt_core::query::device_class::type::alveo:
-    ptree = get_test_header();
-    runTest(dev, ptree);
-    break;
-  }
-  return ptree;
-}
-
-void
-TestVerify::runTest(std::shared_ptr<xrt_core::device> dev, boost::property_tree::ptree& ptree)
-{
   xrt::device device(dev);
 
   const std::string test_path = findPlatformPath(dev, ptree);
   if (test_path.empty()) {
     logger(ptree, "Error", "Platform test path was not found.");
     ptree.put("status", test_token_failed);
-    return;
+    return ptree;
   }
 
   const std::string b_file = findXclbinPath(dev, ptree);
@@ -57,7 +42,7 @@ TestVerify::runTest(std::shared_ptr<xrt_core::device> dev, boost::property_tree:
   if (!logic_uuid.empty() && !std::filesystem::exists(b_file)) {
     logger(ptree, "Details", "Verify xclbin not available or shell partition is not programmed. Skipping validation.");
     ptree.put("status", test_token_skipped);
-    return;
+    return ptree;
   }
   auto xclbin_uuid = device.load_xclbin(b_file);
 
@@ -70,19 +55,19 @@ TestVerify::runTest(std::shared_ptr<xrt_core::device> dev, boost::property_tree:
     } catch (const std::exception&) {
       logger(ptree, "Error", "Kernel could not be found.");
       ptree.put("status", test_token_failed);
-      return;
+      return ptree;
     }
   }
 
   // Allocate the output buffer to hold the kernel output
-  auto output_buffer = xrt::bo(device, sizeof(char) * LENGTH, krnl.group_id(0));
+  auto output_buffer = xrt::bo(device, sizeof(char) * buffer_size, krnl.group_id(0));
 
   // Run the kernel and store its contents within the allocated output buffer
   auto run = krnl(output_buffer);
   run.wait();
 
   // Prepare local buffer
-  char received_data[LENGTH] = {};
+  char received_data[buffer_size] = {};
 
   // Acquire and read the buffer data
   output_buffer.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
@@ -96,4 +81,5 @@ TestVerify::runTest(std::shared_ptr<xrt_core::device> dev, boost::property_tree:
   }
 
   ptree.put("status", test_token_passed);
+  return ptree;
 }
