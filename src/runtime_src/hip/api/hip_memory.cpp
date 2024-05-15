98e114c00319a656f8d18e4baf7791571a213c71
Chiming Zhang <Chiming.Zhang@amd.com>
2024-05-15 09:13:30 -0700

    add hipMemsetD32Async().
    Signed-off-by: Chiming Zhang <Chiming.Zhang@amd.com>

--- src/runtime_src/hip/api/hip_memory.cpp
+++ src/runtime_src/hip/api/hip_memory.cpp
@@ -219,13 +219,22 @@ namespace xrt::core::hip
   static void
   hip_memcpy_host2device_async(hipDeviceptr_t dst, void* src, size_t size, hipStream_t stream)
   {
+<<<<<<< HEAD
     throw_invalid_value_if(!src, std::string("src is nullptr in ") + __func__);
+=======
+    throw_invalid_value_if(!src, "Invalid src pointer in hip_memcpy_host2device_async!");
+>>>>>>> 3e5fb173c (add hipMemsetD32Async().)
 
     auto hip_mem_info = memory_database::instance().get_hip_mem_from_addr(dst);
     auto hip_mem_dst = hip_mem_info.first;
     auto offset = hip_mem_info.second;
+<<<<<<< HEAD
     throw_invalid_value_if(!hip_mem_dst, "invalid destination handle " + to_hex(dst) + " in " + __func__);
     throw_invalid_value_if(offset + size > hip_mem_dst->get_size(), "dst: " + to_hex(dst) + " out of bound in " + __func__);
+=======
+    throw_invalid_value_if(!hip_mem_dst, "Invalid destination handle in hip_memcpy_host2device_async!");
+    throw_invalid_value_if(offset + size > hip_mem_dst->get_size(), "dst out of bound in hip_memcpy_host2device_async!");
+>>>>>>> 3e5fb173c (add hipMemsetD32Async().)
 
     auto hip_stream = get_stream(stream);
     throw_invalid_value_if(!hip_stream, "invalid stream handle " + to_hex(stream) + " in " + __func__);
@@ -237,13 +246,19 @@ namespace xrt::core::hip
   }
 
   // fill data to dst async.
+<<<<<<< HEAD
   template<typename T> static void
   hip_memset_async(void* dst, T value, size_t size, hipStream_t stream)
+=======
+  template<typename element_type> static void
+  hip_memset_async(void* dst, int value, size_t size, hipStream_t stream)
+>>>>>>> 3e5fb173c (add hipMemsetD32Async().)
   {
     auto hip_mem_info = memory_database::instance().get_hip_mem_from_addr(dst);
     auto hip_mem_dst = hip_mem_info.first;
     auto offset = hip_mem_info.second;
     assert(hip_mem_dst->get_type() != xrt::core::hip::memory_type::invalid);
+<<<<<<< HEAD
     throw_invalid_value_if(offset + size > hip_mem_dst->get_size(), "dst: " + to_hex(dst) + " out of bound in " + __func__);
 
     auto element_size = sizeof(T);
@@ -266,6 +281,24 @@ namespace xrt::core::hip
     auto s_hdl = hip_stream.get();
     auto cmd_hdl = insert_in_map(command_cache,
                                 std::make_shared<copy_buffer<T>>(hip_stream, XCL_BO_SYNC_BO_TO_DEVICE, hip_mem_dst, vec, size, offset));
+=======
+    throw_invalid_value_if(offset + size > hip_mem_dst->get_size(), "dst out of bound in hip_memset!");
+
+    auto element_size = sizeof(element_type);
+
+    throw_invalid_value_if((element_size != 1 && element_size != 2 && element_size != 4), "invalid element_type in hip_memset_async!");
+    throw_invalid_value_if(size % element_size != 0, "invalid size in hip_memset_async!");
+
+    auto element_count = size / element_size;
+    std::vector<element_type> host_src(element_count, value);
+
+    auto hip_stream = get_stream(stream);
+    throw_invalid_value_if(!hip_stream, "Invalid stream handle in hip_memset_async!");
+
+    auto s_hdl = hip_stream.get();
+    auto cmd_hdl = insert_in_map(command_cache,
+                                std::make_shared<copy_buffer>(hip_stream, XCL_BO_SYNC_BO_TO_DEVICE, hip_mem_dst, host_src.data(), size, offset));
+>>>>>>> 3e5fb173c (add hipMemsetD32Async().)
     s_hdl->enqueue(command_cache.get(cmd_hdl));
   }
 } // xrt::core::hip
@@ -382,14 +415,22 @@ hipMemsetD32Async(void* dst, int value, size_t count, hipStream_t stream)
 
 // Fills the memory area pointed to by dev with the constant integer value for specified number of times.
 hipError_t
+<<<<<<< HEAD
 hipMemsetD16Async(void* dst, unsigned short value, size_t count, hipStream_t stream)
+=======
+hipMemsetD16Async(void* dst, int value, size_t count, hipStream_t stream)
+>>>>>>> 3e5fb173c (add hipMemsetD32Async().)
 {
   return handle_hip_memory_error([&] { xrt::core::hip::hip_memset_async<std::uint16_t>(dst, value, count*sizeof(std::uint16_t), stream); });
 }
 
 // Fills the memory area pointed to by dev with the constant integer value for specified number of times.
 hipError_t
+<<<<<<< HEAD
 hipMemsetD8Async(void* dst, unsigned char value, size_t count, hipStream_t stream)
+=======
+hipMemsetD8Async(void* dst, int value, size_t count, hipStream_t stream)
+>>>>>>> 3e5fb173c (add hipMemsetD32Async().)
 {
   return handle_hip_memory_error([&] { xrt::core::hip::hip_memset_async<std::uint8_t>(dst, value, count*sizeof(std::uint8_t), stream); });
 }
