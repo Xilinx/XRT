766c3d51b8f75c2293aac18062daa3123828e854
Ryan Chane <rchane@xilinx.com>
2024-06-11 13:21:46 -0700

    Move electrical info to platform report, cleanup aiepartitions ptree, clarified power mode
    Signed-off-by: Ryan Chane <rchane@xilinx.com>

--- src/runtime_src/core/tools/xbutil2/xbutil.cpp
+++ src/runtime_src/core/tools/xbutil2/xbutil.cpp
@@ -54,7 +54,7 @@ R"(
 },{
   "aie": [{
     "examine": [{
-      "report": ["electrical", "host", "platform", "aie-partitions"]
+      "report": ["host", "platform", "aie-partitions"]
     }]
   },{
     "configure": [{
