1b1e91d98433b829512d75da0241005566e14d58
jyothees <Jyotheeswar.Ganne@amd.com>
2024-08-01 16:19:49 +0530

    layer wise windowed event trace start layer support
    Signed-off-by: jyothees <Jyotheeswar.Ganne@amd.com>

--- src/runtime_src/xdp/profile/database/static_info/aie_util.cpp
+++ src/runtime_src/xdp/profile/database/static_info/aie_util.cpp
@@ -397,4 +397,38 @@ namespace xdp::aie {
     return startCols;
   }
 
+  std::vector<uint8_t> getPartitionNumColumsClient(void* handle)
+  {
+    std::vector<uint8_t> numCols;
+
+    try {
+      xrt::hw_context context = xrt_core::hw_context_int::create_hw_context_from_implementation(handle);
+      auto device = xrt_core::hw_context_int::get_core_device(context);
+      if (device==nullptr)
+        return std::vector<uint8_t>{0};
+
+      auto infoVector = xrt_core::device_query<xrt_core::query::aie_partition_info>(device.get());
+      if (infoVector.empty()) {
+        xrt_core::message::send(severity_level::info, "XRT", "No AIE partition information found.");
+        return std::vector<uint8_t>{0};
+      }
+
+      for (auto& info : infoVector) {
+        auto numCol = static_cast<uint8_t>(info.num_cols);
+        auto startCol = static_cast<uint8_t>(info.start_col);
+        xrt_core::message::send(severity_level::info, "XRT",
+            "Partition shift of " + std::to_string(startCol) +
+            " was found, number of columns: " + std::to_string(info.num_cols));
+        numCols.push_back(numCol);
+      }
+    }
+    catch(...) {
+      // Query not available
+      xrt_core::message::send(severity_level::info, "XRT", "Unable to query AIE partition information.");
+      return std::vector<uint8_t>{0};
+    }
+
+    return numCols;
+  }
+
 } // namespace xdp::aie
