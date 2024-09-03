1833602e8b9be65c54febf31f774e0745ea5623d
Ch Vamshi Krishna <40261882+chvamshi-xilinx@users.noreply.github.com>
2024-09-04 01:40:15 +0530

    aie driver api name changes (#8389)
    * aie driver api name changes
    
    * fix
    
    ---------
    
    Co-authored-by: ch vamshi krishna <chvamshi@xilinx.com>

--- src/runtime_src/core/edge/ps_kernels/profiling/aie_profile_config/aie_profile_config.cpp
+++ src/runtime_src/core/edge/ps_kernels/profiling/aie_profile_config/aie_profile_config.cpp
@@ -200,7 +200,7 @@ namespace {
                                  XAIEGBL_MEM_DMABD4CTRL_VALBD_MASK, XAIEGBL_MEM_DMABD5CTRL_VALBD_MASK,
                                  XAIEGBL_MEM_DMABD6CTRL_VALBD_MASK, XAIEGBL_MEM_DMABD7CTRL_VALBD_MASK};
 
-    auto tileOffset = _XAie_GetTileAddr(aieDevInst, row, column);
+    auto tileOffset = XAie_GetTileAddr(aieDevInst, row, column);
     for (int bd = 0; bd < NUM_BDS; ++bd) {
       uint32_t regValue = 0;
       XAie_Read32(aieDevInst, tileOffset + offsets[bd], &regValue);
