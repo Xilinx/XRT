bb022acdb3b0b7353d1d802a6f51552448b35ed2
Akshay Tondak <Akshay.Tondak@amd.com>
2024-09-11 15:36:21 -0700

    Spatial sharing overhead V2 : Code cleanup and review comments handled Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>

--- src/runtime_src/core/tools/common/tests/TestSpatialSharingOvd.cpp
+++ src/runtime_src/core/tools/common/tests/TestSpatialSharingOvd.cpp
@@ -13,129 +13,109 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <functional>
 
 namespace XBU = XBUtilities;
 
 static constexpr size_t host_app = 1; //opcode
 static constexpr size_t buffer_size = 1024; // 1KB
 static constexpr int itr_count = 1000;
-std::mutex m_mutex;
-std::condition_variable m_condVar;
-uint32_t thread_ready = 0;
-bool stop = false;
 
-void thread_ready_to_run() {
-  std::unique_lock<std::mutex> lock(m_mutex);
-  thread_ready++;
-  m_condVar.wait(lock);
-  lock.unlock();
-}
-void wait_for_threads_ready(uint32_t thread_num) {
-  std::unique_lock<std::mutex> lock(m_mutex);
+void TestSpatialSharingOvd::wait_for_threads_ready(uint32_t thread_num, std::mutex& mut, std::condition_variable& cond_var, uint32_t& thread_ready) {
+  std::unique_lock<std::mutex> lock(mut);
   while (thread_ready != thread_num) {
-      lock.unlock();
-      std::this_thread::sleep_for(std::chrono::microseconds(10));
-      lock.lock();
+    lock.unlock();
+    std::this_thread::sleep_for(std::chrono::microseconds(10));
+    lock.lock();
   }
-  m_condVar.notify_all();
+  cond_var.notify_all();
   lock.unlock();
 }
 
-BO_set::BO_set(xrt::device* device_ptr, xrt::kernel* kernel) {
-    bo_instr = xrt::bo(*device_ptr, buffer_size, XCL_BO_FLAGS_CACHEABLE, kernel->group_id(5));
-    bo_ifm = xrt::bo(*device_ptr, buffer_size, XRT_BO_FLAGS_HOST_ONLY, kernel->group_id(1));
-    bo_param = xrt::bo(*device_ptr, buffer_size, XRT_BO_FLAGS_HOST_ONLY, kernel->group_id(2));
-    bo_ofm = xrt::bo(*device_ptr, buffer_size, XRT_BO_FLAGS_HOST_ONLY, kernel->group_id(3));
-    bo_inter = xrt::bo(*device_ptr, buffer_size, XRT_BO_FLAGS_HOST_ONLY, kernel->group_id(4));
-    bo_mc = xrt::bo(*device_ptr, buffer_size, XRT_BO_FLAGS_HOST_ONLY, kernel->group_id(7));
-
-    std::memset(bo_instr.map<char*>(), (uint8_t)0, buffer_size);
+BO_set::BO_set(xrt::device& device, xrt::kernel& kernel) {
+  bo_instr = xrt::bo(device, buffer_size, XCL_BO_FLAGS_CACHEABLE, kernel.group_id(5));
+  bo_ifm = xrt::bo(device, buffer_size, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(1));
+  bo_param = xrt::bo(device, buffer_size, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(2));
+  bo_ofm = xrt::bo(device, buffer_size, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(3));
+  bo_inter = xrt::bo(device, buffer_size, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(4));
+  bo_mc = xrt::bo(device, buffer_size, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(7));
+  std::memset(bo_instr.map<char*>(), (uint8_t)0, buffer_size);
 }
 
 void 
 BO_set::sync_bos_to_device() {
-    bo_instr.sync(XCL_BO_SYNC_BO_TO_DEVICE);
-    bo_ifm.sync(XCL_BO_SYNC_BO_TO_DEVICE);
-    bo_param.sync(XCL_BO_SYNC_BO_TO_DEVICE);
-    bo_mc.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_instr.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_ifm.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_param.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_mc.sync(XCL_BO_SYNC_BO_TO_DEVICE);
 }
 
 void 
-BO_set::set_kernel_args(xrt::run* run) {
-    uint64_t opcode = 1;
-    run->set_arg(0, opcode);
-    run->set_arg(1, bo_ifm);
-    run->set_arg(2, bo_param);
-    run->set_arg(3, bo_ofm);
-    run->set_arg(4, bo_inter);
-    run->set_arg(5, bo_instr);
-    run->set_arg(6, buffer_size/sizeof(int));
-    run->set_arg(7, bo_mc);
-}
-
-TestCase::TestCase(std::string& xclbin_str, std::string& kernel) {
-  unsigned int device_index = 0;
-  device = xrt::device(device_index);
-  xclbin = xrt::xclbin(xclbin_str);
-  device.register_xclbin(xclbin);
-  kernel_name = kernel;
-  hw_ctx = new xrt::hw_context(device, xclbin.get_uuid());
+BO_set::set_kernel_args(xrt::run& run) {
+  uint64_t opcode = 1;
+  run.set_arg(0, opcode);
+  run.set_arg(1, bo_ifm);
+  run.set_arg(2, bo_param);
+  run.set_arg(3, bo_ofm);
+  run.set_arg(4, bo_inter);
+  run.set_arg(5, bo_instr);
+  run.set_arg(6, buffer_size/sizeof(int));
+  run.set_arg(7, bo_mc);
 }
 
 void
-TestCase::run()
+TestCase::run(std::mutex& mut, std::condition_variable& cond_var, uint32_t& thread_ready)
 {
-  std::vector<xrt::kernel*> kernels;
-  std::vector<BO_set*> bo_set_list;
-  std::vector<xrt::run*> run_list;
+  std::vector<xrt::kernel> kernels;
+  std::vector<BO_set> bo_set_list;
+  std::vector<xrt::run> run_list;
   for (uint32_t j = 0; j < queue_len; j++) {
-    auto kernel = new xrt::kernel(*hw_ctx, kernel_name);
-    auto bos = new BO_set(&device, kernel);
-    bos->sync_bos_to_device();
-    auto run = new xrt::run(*kernel);
-    bos->set_kernel_args(run);
-    run->start();
-    run->wait2();
+    auto kernel = xrt::kernel(hw_ctx, kernel_name);
+    auto bos = BO_set(device, kernel);
+    bos.sync_bos_to_device();
+    auto run = xrt::run(kernel);
+    bos.set_kernel_args(run);
+    run.start();
+    run.wait2();
 
     kernels.push_back(kernel);
     bo_set_list.push_back(bos);
     run_list.push_back(run);
   }
-  thread_ready_to_run();
+  thread_ready_to_run(mut, cond_var, thread_ready);
 
   for (int i = 0; i < itr_count; i++) {
     for (uint32_t cnt = 0; cnt < queue_len; cnt++) {
       try{
-        run_list[cnt]->start();
+        run_list[cnt].start();
       } catch (const std::exception& ex) {
-        std::cout << "ERROR: Caught exception: " << ex.what() << '\n';
+        logger(ptree, "Error", ex.what());
+        ptree.put("status", test_token_failed);
+        return;
       }
     }
     for (uint32_t cnt = 0; cnt < queue_len; cnt++) {
       try{
-        run_list[cnt]->wait2();
+        run_list[cnt].wait2();
       } catch (const std::exception& ex) {
-        std::cout << "ERROR: Caught exception: " << ex.what() << '\n';
+        logger(ptree, "Error", ex.what());
+        ptree.put("status", test_token_failed);
+        return;
       }
     }
   }
 }
 
-TestSpatialSharingOvd::TestSpatialSharingOvd()
-  : TestRunner("spatial-sharing-overhead", "Run Spatial Sharing Overhead Test")
-{}
-
-void 
-runTestcase(TestCase* test)
-{
-  test->run();
+void TestCase::thread_ready_to_run(std::mutex& mut, std::condition_variable& cond_var, uint32_t& thread_ready) {
+  std::unique_lock<std::mutex> lock(mut);
+  thread_ready++;
+  cond_var.wait(lock);
+  lock.unlock();
 }
 
 boost::property_tree::ptree
 TestSpatialSharingOvd::run(std::shared_ptr<xrt_core::device> dev){
-  boost::property_tree::ptree ptree = get_test_header();
   ptree.erase("xclbin");
-
   const auto xclbin_name = xrt_core::device_query<xrt_core::query::xclbin_name>(dev, xrt_core::query::xclbin_name::type::validate);
   auto xclbin_path = findPlatformFile(xclbin_name, ptree);
   if (!std::filesystem::exists(xclbin_path))
@@ -175,34 +155,45 @@ TestSpatialSharingOvd::run(std::shared_ptr<xrt_core::device> dev){
   auto working_dev = xrt::device(dev);
   working_dev.register_xclbin(xclbin);
 
+  std::mutex mut;
+  std::condition_variable cond_var;
+  uint32_t thread_ready = 0;
 
-  /*Run 2*/
+  /*Run 1*/
   std::vector<std::thread> threads;
   std::vector<TestCase> testcases;
 
-  testcases.emplace_back(xclbin_path, kernelName);
-  testcases.emplace_back(xclbin_path, kernelName);
-  threads.emplace_back(&runTestcase, &testcases[0]);
-  threads.emplace_back(&runTestcase, &testcases[1]);
-  wait_for_threads_ready((uint32_t)threads.size());
+  testcases.emplace_back(xclbin, kernelName);
+  testcases.emplace_back(xclbin, kernelName);
+  auto runTestcase = [&](TestCase& test) {
+    try{
+      test.run(mut, cond_var, thread_ready);
+    } catch (const std::exception& ex) {
+      logger(ptree, "Error", ex.what());
+      ptree.put("status", test_token_failed);
+    }
+  };
+  threads.emplace_back(runTestcase, testcases[0]);
+  threads.emplace_back(runTestcase, testcases[1]);
+  wait_for_threads_ready((uint32_t)threads.size(), mut, cond_var, thread_ready);
   auto start = std::chrono::high_resolution_clock::now(); 
   for (uint32_t i = 0; i < threads.size(); i++) {
     threads[i].join();
   }
   auto end = std::chrono::high_resolution_clock::now(); 
   float latencyShared = std::chrono::duration_cast<std::chrono::duration<float>>(end-start).count();;
-  /* End of Run 2*/
+  /* End of Run 1*/
 
   thread_ready = 0;
-  /* Run 1*/
-  TestCase t(xclbin_path, kernelName);
-  std::thread thr(&runTestcase, &t);
-  wait_for_threads_ready(1);
+  /* Run 2*/
+  TestCase t(xclbin, kernelName);
+  std::thread thr(runTestcase, t);
+  wait_for_threads_ready(1, mut, cond_var, thread_ready);
   start = std::chrono::high_resolution_clock::now(); 
   thr.join();
   end = std::chrono::high_resolution_clock::now(); 
   float latencySingle =  std::chrono::duration_cast<std::chrono::duration<float>>(end-start).count(); 
-  /*End of run 1*/
+  /*End of run 2*/
 
   logger(ptree, "Details", boost::str(boost::format("LatencySingle: '%.1f' ms") % (latencySingle * 1000)));
   logger(ptree, "Details", boost::str(boost::format("LatencyShared: '%.1f' ms") % (latencyShared * 1000)));
--- src/runtime_src/core/tools/common/tests/TestSpatialSharingOvd.h
+++ src/runtime_src/core/tools/common/tests/TestSpatialSharingOvd.h
@@ -4,42 +4,52 @@
 #ifndef _TESTSPATIALSHARINGOVD_
 #define _TESTSPATIALSHARINGOVD_
 
+#include <memory>
 #include "tools/common/TestRunner.h"
 #include "xrt/xrt_device.h"
 #include "xrt/xrt_kernel.h"
 #include "xrt/xrt_bo.h"
 class BO_set {
-    xrt::bo bo_instr;
-    xrt::bo bo_ifm;
-    xrt::bo bo_param;
-    xrt::bo bo_ofm;
-    xrt::bo bo_inter;
-    xrt::bo bo_mc;
-    uint32_t instr_size;
+  xrt::bo bo_instr;
+  xrt::bo bo_ifm;
+  xrt::bo bo_param;
+  xrt::bo bo_ofm;
+  xrt::bo bo_inter;
+  xrt::bo bo_mc;
+  uint32_t instr_size;
+
 public:
-    BO_set(xrt::device* device_ptr, xrt::kernel* kernel);
-    void set_kernel_args(xrt::run* run);
-    void sync_bos_to_device();
-};
-
-class TestCase {
-    // test cases to run in run_test
-    public:
-    xrt::device device;
-    xrt::hw_context *hw_ctx;
-    xrt::xclbin xclbin;
-    std::string kernel_name;
-    uint32_t queue_len = 4;
-    std::string priority = "normal";
-
-    TestCase(std::string& xclbin_str, std::string& kernel);
-    void run();
+  BO_set(xrt::device& device_ptr, xrt::kernel& kernel);
+  void set_kernel_args(xrt::run& run);
+  void sync_bos_to_device();
 };
 
 class TestSpatialSharingOvd : public TestRunner {
-  public:
-    boost::property_tree::ptree run(std::shared_ptr<xrt_core::device> dev);
-    //void runTestcase(TestCase* test);
-    TestSpatialSharingOvd();
+  void wait_for_threads_ready(uint32_t, std::mutex&, std::condition_variable&, uint32_t&);
+public:
+  boost::property_tree::ptree ptree;   
+  boost::property_tree::ptree run(std::shared_ptr<xrt_core::device> dev);
+  TestSpatialSharingOvd()
+    : TestRunner("spatial-sharing-overhead", "Run Spatial Sharing Overhead Test"), ptree(get_test_header()){}
 };
+
+class TestCase : public TestSpatialSharingOvd {
+  xrt::device device;
+  xrt::xclbin xclbin;
+  std::string kernel_name;
+  xrt::hw_context hw_ctx;
+  uint32_t queue_len = 4;
+  void thread_ready_to_run(std::mutex&, std::condition_variable&, uint32_t&); 
+
+public:
+
+  TestCase(xrt::xclbin& xclbin, std::string& kernel)
+      : device(xrt::device(0)), xclbin(xclbin), kernel_name(kernel) 
+  {
+    device.register_xclbin(xclbin);
+    hw_ctx = xrt::hw_context(device, xclbin.get_uuid());
+  }
+  void run(std::mutex&, std::condition_variable&, uint32_t&);
+};
+
 #endif
