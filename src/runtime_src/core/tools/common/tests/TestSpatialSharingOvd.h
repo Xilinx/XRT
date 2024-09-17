2574b5b187a77f9603145a3221dd8ad46b8f420e
Akshay Tondak <Akshay.Tondak@amd.com>
2024-09-17 10:32:05 -0700

    Added Comment for control flow explanation
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>

--- src/runtime_src/core/tools/common/tests/TestSpatialSharingOvd.h
+++ src/runtime_src/core/tools/common/tests/TestSpatialSharingOvd.h
@@ -6,6 +6,23 @@
 
 #include "tools/common/TestRunner.h"
 
+/**
+*  @brief Test control flow: 
+*
+* Two threads are spawned to run two instances of testcases concurrently. There's one hardware context created on each thread so 
+* the 2 threads are doing spatial sharing. The threads are added to the threads vector and started using the runTestcase() lambda. 
+* The program then waits for both threads to signal they are ready using wait_for_threads_ready().
+* Once ready, the latency for running the test cases in parallel is measured by recording the start and 
+* end times around the join calls for each thread. After the first run, the thread_ready counter is reset, 
+* and a second run is performed with a single TestCase instance executed in a single thread without spatial sharing. 
+* The latency for this single-threaded run is similarly measured. 
+* Finally, the latencies for both runs are logged to assess the overhead of running the test cases in parallel versus sequentially.
+* 
+* @see runTestcase()
+* @see wait_for_threads_ready()
+* @see std::thread::join()
+*/
+
 // Class representing the TestSpatialSharingOvd test
 class TestSpatialSharingOvd : public TestRunner {
 public:
