425cd199b2a33d4d9c783f92b41d864300940f89
Akshay Tondak <Akshay.Tondak@amd.com>
2024-09-25 13:43:40 -0700

    Temporal Sharing Overhead test final for PR
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>

--- src/runtime_src/core/tools/common/tests/TestTemporalSharingOvd.cpp
+++ src/runtime_src/core/tools/common/tests/TestTemporalSharingOvd.cpp
@@ -56,10 +56,6 @@ TestTemporalSharingOvd::run(std::shared_ptr<xrt_core::device> dev) {
   auto working_dev = xrt::device(dev);
   working_dev.register_xclbin(xclbin);
 
-  std::mutex mut;
-  std::condition_variable cond_var;
-  int thread_ready = 0;
-
   // Run 1 
   std::vector<std::thread> threads;
   std::vector<TestCase> testcases;
@@ -68,10 +64,12 @@ TestTemporalSharingOvd::run(std::shared_ptr<xrt_core::device> dev) {
   testcases.emplace_back(xclbin, kernelName, working_dev);
   testcases.emplace_back(xclbin, kernelName, working_dev);
 
+  initializeTests(testcases); 
+
   // Lambda function to run a test case. This will be sent to individual thread to be run.
   auto runTestcase = [&](TestCase& test) {
     try {
-      test.run(mut, cond_var, thread_ready);
+      test.run();
     } catch (const std::exception& ex) {
       logger(ptree, "Error", ex.what());
       ptree.put("status", test_token_failed);
@@ -79,42 +77,36 @@ TestTemporalSharingOvd::run(std::shared_ptr<xrt_core::device> dev) {
     }
   };
 
+  // Measure the latency for running the test cases in parallel
+  auto start = std::chrono::high_resolution_clock::now(); 
   // Create two threads to run the test cases
   threads.emplace_back(runTestcase, std::ref(testcases[0]));
   threads.emplace_back(runTestcase, std::ref(testcases[1]));
 
-  // Wait for both threads to be ready to begin clocking
-  wait_for_threads_ready((uint32_t)threads.size(), mut, cond_var, thread_ready);
-
-  // Measure the latency for running the test cases in parallel
-  auto start = std::chrono::high_resolution_clock::now(); 
   for (uint32_t i = 0; i < threads.size(); i++) {
     threads[i].join();
   }
   auto end = std::chrono::high_resolution_clock::now(); 
   float latencySpatial = std::chrono::duration_cast<std::chrono::duration<float>>(end-start).count();
 
-  thread_ready = 0;
-
-  // Run 2 
   threads.clear();
   testcases.clear();
 
+  // Run 2 
   // Create three test cases and add them to the vector
   testcases.emplace_back(xclbin, kernelName, working_dev);
   testcases.emplace_back(xclbin, kernelName, working_dev);
   testcases.emplace_back(xclbin, kernelName, working_dev);
 
+  initializeTests(testcases); 
+
+  // Measure the latency for running the test cases in parallel
+  start = std::chrono::high_resolution_clock::now(); 
   // Create three threads to run the test cases
   threads.emplace_back(runTestcase, std::ref(testcases[0]));
   threads.emplace_back(runTestcase, std::ref(testcases[1]));
   threads.emplace_back(runTestcase, std::ref(testcases[2]));
 
-  // Wait for both threads to be ready to begin clocking
-  wait_for_threads_ready((uint32_t)threads.size(), mut, cond_var, thread_ready);
-
-  // Measure the latency for running the test cases in parallel
-  start = std::chrono::high_resolution_clock::now(); 
   for (uint32_t i = 0; i < threads.size(); i++) {
     threads[i].join();
   }
@@ -123,12 +115,25 @@ TestTemporalSharingOvd::run(std::shared_ptr<xrt_core::device> dev) {
   // End of Run 2 
 
   if(XBU::getVerbose()){
-    logger(ptree, "Details", boost::str(boost::format("LatencySpatial: '%.1f' ms") % (latencySpatial * 1000)));
-    logger(ptree, "Details", boost::str(boost::format("LatencyTemporal: '%.1f' ms") % (latencyTemporal * 1000)));
+    logger(ptree, "Details", boost::str(boost::format("Spatially shared contexts latency: '%.1f' ms") % (latencySpatial * 1000)));
+    logger(ptree, "Details", boost::str(boost::format("Temporally shared contexts latency: '%.1f' ms") % (latencyTemporal * 1000)));
   }
   logger(ptree, "Details", boost::str(boost::format("Overhead: '%.1f' ms") % ((latencyTemporal - latencySpatial) * 1000)));
 
   // Set the test status to passed
   ptree.put("status", test_token_passed);
   return ptree;
+}
+
+void
+TestTemporalSharingOvd::initializeTests(std::vector<TestCase>& testcases) {
+  for (int i = 0; i < testcases.size(); i++) {
+    try{
+      testcases[i].initialize();
+    } catch (const std::exception& ex) {
+      logger(ptree, "Error", ex.what());
+      ptree.put("status", test_token_failed);
+      return;
+    }
+  } 
 }
--- src/runtime_src/core/tools/xbutil2/SubCmdValidate.cpp
+++ src/runtime_src/core/tools/xbutil2/SubCmdValidate.cpp
@@ -43,6 +43,7 @@
 #include "tools/common/tests/TestCmdChainThroughput.h"
 #include "tools/common/tests/TestAIEReconfigOverhead.h"
 #include "tools/common/tests/TestSpatialSharingOvd.h"
+#include "tools/common/tests/TestTemporalSharingOvd.h"
 namespace XBU = XBUtilities;
 
 // 3rd Party Library - Include Files
@@ -116,7 +117,8 @@ std::vector<std::shared_ptr<TestRunner>> testSuite = {
   std::make_shared<TestCmdChainLatency>(),
   std::make_shared<TestCmdChainThroughput>(),
   std::make_shared<TestAIEReconfigOverhead>(),
-  std::make_shared<TestSpatialSharingOvd>()
+  std::make_shared<TestSpatialSharingOvd>(),
+  std::make_shared<TestTemporalSharingOvd>(),
 };
 
 /*
