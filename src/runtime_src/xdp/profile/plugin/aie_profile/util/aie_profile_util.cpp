8c04d6b082ddb934953e1cf18c74ea466f9886ca
Vinod Pangul <146476973+vipangul@users.noreply.github.com>
2024-09-25 22:18:22 -0700

    [XDP] Fix warnings for mcdm build  (#8458)
    * fix mcdm build error
    
    Signed-off-by: Vinod Pangul <146476973+vipangul@users.noreply.github.com>
    
    * handle more errors
    
    Signed-off-by: Vinod Pangul <146476973+vipangul@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Vinod Pangul <146476973+vipangul@users.noreply.github.com>

--- src/runtime_src/xdp/profile/plugin/aie_profile/util/aie_profile_util.cpp
+++ src/runtime_src/xdp/profile/plugin/aie_profile/util/aie_profile_util.cpp
@@ -449,17 +449,7 @@ namespace xdp::aie::profile {
     return bcPair;
   }
 
-  /****************************************************************************
-   * Get the stream width in bits for specified hw_gen
-   ***************************************************************************/
-  uint32_t getStreamWidth(uint8_t hw_gen)
-  {
-    uint32_t default_width = 32;
-    if (streamWidthMap.find(hw_gen) == streamWidthMap.end())
-      return default_width;
-    
-    return streamWidthMap.at(hw_gen);
-  }
+
 
   /****************************************************************************
    * Convert user specified bytes to beats for provided metric set
@@ -469,8 +459,8 @@ namespace xdp::aie::profile {
     if (metricSet != "start_to_bytes_transferred")
       return bytes;
 
-    uint32_t streamWidth = getStreamWidth(hw_gen);
-    uint32_t total_beats = std::ceil((static_cast<double>(bytes)*8) / streamWidth);
+    uint32_t streamWidth = aie::getStreamWidth(hw_gen);
+    uint32_t total_beats = static_cast<uint32_t>(std::ceil((static_cast<double>(bytes)*8) / streamWidth));
     return total_beats; 
   }
 
