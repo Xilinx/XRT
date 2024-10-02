720f95093bc70b8fdd17e56794a4fd463bd0a3cf
michuan <michuan@amd.com>
2024-10-02 15:14:26 -0400

    [Xbutil] Add support to in xbutil for the newly added device utilization queries
    [Why]
    Add support to in xbutil for the newly added device utilization queries
    
    [How]
    Add Tops section in platform report
    
    Jira Id: EDGEML-8265
    
    Signed-off-by: michuan <michuan@amd.com>

--- src/runtime_src/core/common/info_platform.cpp
+++ src/runtime_src/core/common/info_platform.cpp
@@ -360,16 +360,16 @@ add_tops_info(const xrt_core::device* device, ptree_type& pt)
     try {
         auto res_info = xrt_core::device_query<xq::xrt_resource_raw>(device);
         if (res_info.empty())
-        return;
+            return;
 
         for (auto& res: res_info) {
-            if(res.type == xrt_core::query::xrt_resource_raw::resource_type::ipu_tops_max) {
-                ptree_type pt_tops;
-                pt_tops.add("id", xq::xrt_resource_raw::get_name(res.type));
-                pt_tops.add("value", res.data_double);
-                pt_tops_array.push_back(std::make_pair("", pt_tops));
-                break;
-            }
+            if(res.type != xrt_core::query::xrt_resource_raw::resource_type::ipu_tops_max)
+                continue;
+
+            ptree_type pt_tops;
+            pt_tops.add("id", xq::xrt_resource_raw::get_name(res.type));
+            pt_tops.add("value", res.data_double);
+            pt_tops_array.push_back(std::make_pair("", pt_tops));
         }
         pt.put_child("tops", pt_tops_array);
     }
