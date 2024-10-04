f74ffd7db126b2c4b3cfea7a5ec2dd50da5a5c8b
Akshay Tondak <Akshay.Tondak@amd.com>
2024-10-04 11:46:18 -0700

    Adding missing file
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>

--- src/runtime_src/core/tools/common/tests/TestTCTOneColumn.cpp
+++ src/runtime_src/core/tools/common/tests/TestTCTOneColumn.cpp
@@ -3,6 +3,7 @@
 
 // ------ I N C L U D E   F I L E S -------------------------------------------
 // Local - Include Files
+#include "TestValidateUtilities.h"
 #include "TestTCTOneColumn.h"
 #include "TestValidateUtilities.h"
 #include "tools/common/XBUtilities.h"
