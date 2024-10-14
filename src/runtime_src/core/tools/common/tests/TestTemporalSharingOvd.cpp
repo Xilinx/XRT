efa944b76be8dc69033a06d713d0439391e4a1b1
Akshay Tondak <aktondak@amd.com>
2024-10-14 10:51:50 -0700

    Vitis 12999 : Temporal Sharing overhead benchmark test (#8516)
    * Spatial Sharing test update for low variance
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * Adding missing file
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * Temporal sharing v1
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * Temporal Sharing Overhead test final for PR
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * Update TestTemporalSharingOvd.h
    
    * Threshold Logic Addition
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * Threshold changes update
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * Update TestTemporalSharingOvd.cpp
    
    * Update to temporal sharing test after XrtSmiUtilities changes
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * Merge conflic fixes
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * Removing extra header inclusions
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * DCO Remediation Commit for Akshay Tondak <aktondak@amd.com>
    
    I, Akshay Tondak <aktondak@amd.com>, hereby add my Signed-off-by to this commit: 18c65928bb80cd5e8da9ca4cd94109b3b3d82c71
    I, Akshay Tondak <aktondak@amd.com>, hereby add my Signed-off-by to this commit: eebad9cdabd3bc393f16e35f215a701b8518668b
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * Fix for build fail
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * DCO Remediation Commit for Akshay Tondak <aktondak@amd.com>
    
    I, Akshay Tondak <aktondak@amd.com>, hereby add my Signed-off-by to this commit: 18c65928bb80cd5e8da9ca4cd94109b3b3d82c71
    I, Akshay Tondak <aktondak@amd.com>, hereby add my Signed-off-by to this commit: eebad9cdabd3bc393f16e35f215a701b8518668b
    
    Signed-off-by: Akshay Tondak <aktondak@amd.com>
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    ---------
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    Co-authored-by: Akshay Tondak <Akshay.Tondak@amd.com>

---
+++ src/runtime_src/core/tools/common/tests/TestTemporalSharingOvd.cpp
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.
+
+// ------ I N C L U D E   F I L E S -------------------------------------------
+
+// System - Include Files
+#include <thread>
+
+// Local - Include Files
+#include "tools/common/XBUtilities.h"
+#include "tools/common/tests/TestTemporalSharingOvd.h"
+#include "TestValidateUtilities.h"
+#include "xrt/xrt_bo.h"
+#include "xrt/xrt_device.h"
+#include "xrt/xrt_hw_context.h"
+#include "xrt/xrt_kernel.h"
+
+
+namespace XBU = XBUtilities;
+static constexpr size_t buffer_size = 1024; //1 KB
+
+boost::property_tree::ptree 
+TestTemporalSharingOvd::run(std::shared_ptr<xrt_core::device> dev) {
+  ptree.erase("xclbin");
+  try {
+    set_threshold(dev, ptree);
+    if(XBU::getVerbose())
+      logger(ptree, "Details", boost::str(boost::format("Threshold is %.1f ms") % get_threshold()));
+  }
+  catch (const std::runtime_error& ex) {
+    logger(ptree, "Details", ex.what());
+    ptree.put("status", test_token_skipped);
+    return ptree;
+  }
+  const auto xclbin_name = xrt_core::device_query<xrt_core::query::xclbin_name>(dev, xrt_core::query::xclbin_name::type::validate);
+  auto xclbin_path = findPlatformFile(xclbin_name, ptree);
+  if (!std::filesystem::exists(xclbin_path))
+    return ptree;
+
+  logger(ptree, "Xclbin", xclbin_path);
+
+  xrt::xclbin xclbin;
+  try{
+    xclbin = xrt::xclbin(xclbin_path);
+  }
+  catch(const std::runtime_error& ex) {
+    logger(ptree, "Error", ex.what());
+    ptree.put("status", test_token_failed);
+    return ptree;
+  }
+  auto xkernels = xclbin.get_kernels();
+
+  auto itr = std::find_if(xkernels.begin(), xkernels.end(), [](xrt::xclbin::kernel& k) {
+    auto name = k.get_name();
+    return name.rfind("DPU",0) == 0; // Starts with "DPU"
+  });
+
+  xrt::xclbin::kernel xkernel;
+  if (itr!=xkernels.end())
+    xkernel = *itr;
+  else {
+    logger(ptree, "Error", "No kernel with `DPU` found in the xclbin");
+    ptree.put("status", test_token_failed);
+    return ptree;
+  }
+  auto kernelName = xkernel.get_name();
+  if(XBU::getVerbose())
+    logger(ptree, "Details", boost::str(boost::format("Kernel name is '%s'") % kernelName));
+
+  auto working_dev = xrt::device(dev);
+  working_dev.register_xclbin(xclbin);
+
+  const auto seq_name = xrt_core::device_query<xrt_core::query::sequence_name>(dev, xrt_core::query::sequence_name::type::df_bandwidth);
+  auto dpu_instr = findPlatformFile(seq_name, ptree);
+  if (!std::filesystem::exists(dpu_instr))
+    return ptree;
+
+  logger(ptree, "DPU-Sequence", dpu_instr);
+
+  // Run 1 
+  std::vector<std::thread> threads;
+  std::vector<TestCase> testcases;
+
+  // Create two test cases and add them to the vector
+  TestParams params(xclbin, working_dev, kernelName, dpu_instr, 8, buffer_size, 10000);
+  testcases.emplace_back(params);
+  testcases.emplace_back(params);
+
+  initializeTests(testcases); 
+
+  // Lambda function to run a test case. This will be sent to individual thread to be run.
+  auto runTestcase = [&](TestCase& test) {
+    try {
+      test.run();
+    } catch (const std::exception& ex) {
+      logger(ptree, "Error", ex.what());
+      ptree.put("status", test_token_failed);
+      return;
+    }
+  };
+
+  // Measure the latency for running the test cases in parallel
+  auto start = std::chrono::high_resolution_clock::now(); 
+  // Create two threads to run the test cases
+  threads.emplace_back(runTestcase, std::ref(testcases[0]));
+  threads.emplace_back(runTestcase, std::ref(testcases[1]));
+
+  for (uint32_t i = 0; i < threads.size(); i++) {
+    threads[i].join();
+  }
+  auto end = std::chrono::high_resolution_clock::now(); 
+  float latencySpatial = std::chrono::duration_cast<std::chrono::duration<float>>(end-start).count();
+
+  threads.clear();
+  testcases.clear();
+
+  // Run 2 
+  // Create 4 test cases and add them to the vector
+  for (int i = 0; i < 4; i++)
+    testcases.emplace_back(params);
+
+  initializeTests(testcases); 
+
+  // Measure the latency for running the test cases in parallel
+  start = std::chrono::high_resolution_clock::now(); 
+  // Create four threads to run the test cases
+  for (unsigned int i = 0; i < testcases.size(); i++)
+    threads.emplace_back(runTestcase, std::ref(testcases[i]));
+
+  for (uint32_t i = 0; i < threads.size(); i++) {
+    threads[i].join();
+  }
+  end = std::chrono::high_resolution_clock::now(); 
+  float latencyTemporal = std::chrono::duration_cast<std::chrono::duration<float>>(end-start).count(); 
+  // End of Run 2 
+
+  if(XBU::getVerbose()){
+    logger(ptree, "Details", boost::str(boost::format("Spatially shared contexts latency: '%.1f' ms") % (latencySpatial * 1000)));
+    logger(ptree, "Details", boost::str(boost::format("Temporally shared contexts latency: '%.1f' ms") % (latencyTemporal * 1000)));
+  }
+  auto overhead = (latencyTemporal - latencySpatial); 
+  logger(ptree, "Details", boost::str(boost::format("Overhead: '%.1f' ms") % (overhead * 1000)));
+
+  // Set the test status to passed
+
+  //check if the value is in range
+  result_in_range(overhead, get_threshold(), ptree);
+  
+  ptree.put("status", test_token_passed);
+  return ptree;
+}
+
+void
+TestTemporalSharingOvd::initializeTests(std::vector<TestCase>& testcases) {
+  for (unsigned int i = 0; i < testcases.size(); i++) {
+    try{
+      testcases[i].initialize();
+    } catch (const std::exception& ex) {
+      logger(ptree, "Error", ex.what());
+      ptree.put("status", test_token_failed);
+      return;
+    }
+  } 
+}
