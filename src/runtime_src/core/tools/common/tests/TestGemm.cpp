f24a2343f4f1a2679a94e581dc6cbfa9283d81d3
AShivangi <shivangiagarwal53@gmail.com>
2024-10-16 08:55:10 -0700

    fixes
    Signed-off-by: AShivangi <shivangiagarwal53@gmail.com>

--- src/runtime_src/core/tools/common/tests/TestGemm.cpp
+++ src/runtime_src/core/tools/common/tests/TestGemm.cpp
@@ -38,17 +38,6 @@ TestGemm::run(std::shared_ptr<xrt_core::device> dev)
   boost::property_tree::ptree ptree = get_test_header();
   ptree.erase("xclbin");
 
-  try {
-    set_threshold(dev, ptree);
-    if(XBU::getVerbose())
-      logger(ptree, "Details", boost::str(boost::format("Threshold is %.1f TOPS") % get_threshold()));
-  }
-  catch (const std::runtime_error& ex) {
-    logger(ptree, "Details", ex.what());
-    ptree.put("status", test_token_skipped);
-    return ptree;
-  }
-
   const auto xclbin_name = xrt_core::device_query<xrt_core::query::xclbin_name>(dev, xrt_core::query::xclbin_name::type::gemm);
   auto xclbin_path = findPlatformFile(xclbin_name, ptree);
   if (!std::filesystem::exists(xclbin_path)){
@@ -180,10 +169,8 @@ TestGemm::run(std::shared_ptr<xrt_core::device> dev)
     logger(ptree, "Details", boost::str(boost::format("Average cycle count: %.1f") % (total_cycle_count/num_of_cores)));
   }
 
-  //check if the value is in range
-  result_in_range(TOPS, get_threshold(), ptree);
-
   logger(ptree, "Details", boost::str(boost::format("TOPS: %.1f") % TOPS));
+  ptree.put("status", test_token_passed);
 
   return ptree;
 }
