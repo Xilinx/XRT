33c2eb5744e542f59a8c46c20fca48ca23b16191
Soren Soe <2106410+stsoe@users.noreply.github.com>
2025-01-16 18:44:20 -0800

    Exclude Alveo content from NPU component (#8700)
    Ensure that Alveo specific content is built only for Alveo or legacy XRT
    components.  E.g., only build Alveo artifacts when XRT_ALVEO is defined.
    
    Signed-off-by: Soren Soe <2106410+stsoe@users.noreply.github.com>

--- src/CMake/version.cmake
+++ src/CMake/version.cmake
@@ -105,7 +105,7 @@ if (${XRT_NATIVE_BUILD} STREQUAL "yes")
 endif()
 
 # This is not required on MPSoC platform. To avoid yocto error, do NOT intall
-if ((${XRT_NATIVE_BUILD} STREQUAL "yes") AND (NOT WIN32))
+if (XRT_ALVEO AND (${XRT_NATIVE_BUILD} STREQUAL "yes") AND (NOT WIN32))
   # Copied over from dkms.cmake. TODO: cleanup
   set (XRT_DKMS_INSTALL_DIR "/usr/src/xrt-${XRT_VERSION_STRING}")
   install(FILES ${PROJECT_BINARY_DIR}/gen/version.h
