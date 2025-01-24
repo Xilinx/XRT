12f9f230d3d3133efc82e0167b675d9b50dca445
Akshay Tondak <aktondak@amd.com>
2025-01-24 13:23:00 -0800

    Adding missing files
    Signed-off-by: Akshay Tondak <aktondak@amd.com>

---
+++ src/runtime_src/core/common/smi.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+
+#include <string>
+
+namespace xrt_core::smi {
+
+std::string 
+get_smi_config();
+
+std::string
+get_default_smi_config();
+
+} // namespace xrt_core::smi
