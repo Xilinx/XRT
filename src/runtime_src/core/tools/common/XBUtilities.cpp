a1e39e6736ecb9f38d46c9f349d5ee14a72eadfc
AShivangi <124935354+AShivangi@users.noreply.github.com>
2025-02-07 00:07:37 +0530

    CR-1228185, CR-1228210 and CR-1228177 (#8747)
    * Bash CRs fixes
    
    Signed-off-by: AShivangi <shivangiagarwal53@gmail.com>
    
    * copyright update
    
    Signed-off-by: AShivangi <shivangiagarwal53@gmail.com>
    
    ---------
    
    Signed-off-by: AShivangi <shivangiagarwal53@gmail.com>

--- src/runtime_src/core/tools/common/XBUtilities.cpp
+++ src/runtime_src/core/tools/common/XBUtilities.cpp
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (C) 2019-2022 Xilinx, Inc
-// Copyright (C) 2022-2024 Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (C) 2022-2025 Advanced Micro Devices, Inc. All rights reserved.
 
 // ------ I N C L U D E   F I L E S -------------------------------------------
 #include "XBUtilities.h"
@@ -214,20 +214,24 @@ bdf2index(const std::string& bdfstr, bool _inUserDomain)
 
   std::reverse(std::begin(tokens), std::end(tokens));
 
-  //check if func was specified. func is optional
-  auto pos_of_func = tokens[0].find('.');
-  if (pos_of_func != std::string::npos) {
-    dev = static_cast<uint16_t>(std::stoi(std::string(tokens[0].substr(0, pos_of_func)), nullptr, radix));
-    func = static_cast<uint16_t>(std::stoi(std::string(tokens[0].substr(pos_of_func+1)), nullptr, radix));
+try {
+    //check if func was specified. func is optional
+    auto pos_of_func = tokens[0].find('.');
+    if (pos_of_func != std::string::npos) {
+      dev = static_cast<uint16_t>(std::stoi(std::string(tokens[0].substr(0, pos_of_func)), nullptr, radix));
+      func = static_cast<uint16_t>(std::stoi(std::string(tokens[0].substr(pos_of_func+1)), nullptr, radix));
+    }
+    else
+      dev = static_cast<uint16_t>(std::stoi(std::string(tokens[0]), nullptr, radix));
+
+    bus = static_cast<uint16_t>(std::stoi(std::string(tokens[1]), nullptr, radix));
+
+    // domain is not mandatory if it is "0000"
+    if(tokens.size() > 2)
+      domain = static_cast<uint16_t>(std::stoi(std::string(tokens[2]), nullptr, radix));
+  } catch (const std::invalid_argument&) {
+    throw std::runtime_error(boost::str(boost::format("Invalid BDF '%s'") % bdfstr) + XBUtilities::str_available_devs(_inUserDomain));
   }
-  else
-    dev = static_cast<uint16_t>(std::stoi(std::string(tokens[0]), nullptr, radix));
-
-  bus = static_cast<uint16_t>(std::stoi(std::string(tokens[1]), nullptr, radix));
-
-  // domain is not mandatory if it is "0000"
-  if(tokens.size() > 2)
-    domain = static_cast<uint16_t>(std::stoi(std::string(tokens[2]), nullptr, radix));
 
   // Iterate through the available devices to find a BDF match
   // This must not open any devices! Doing do would slow down the software
