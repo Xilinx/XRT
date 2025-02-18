6d580226e3137a988ab31019a86afafeeae12323
Ryan Chane <103678133+rchane@users.noreply.github.com>
2025-02-18 08:34:57 -0800

    Fix validate return status on exception (#8767)
    Signed-off-by: Ryan Chane <rchane@xilinx.com>

--- src/runtime_src/core/tools/xbutil2/SubCmdValidate.cpp
+++ src/runtime_src/core/tools/xbutil2/SubCmdValidate.cpp
@@ -318,6 +318,7 @@ run_test_suite_device( const std::shared_ptr<xrt_core::device>& device,
       ptTest = testPtr->get_test_header();
       XBValidateUtils::logger(ptTest, "Error", "The test timed out");
       ptTest.put("status", test_token_failed);
+      status = test_status::failed;
     }
     ptDeviceTestSuite.push_back( std::make_pair("", ptTest) );
 
