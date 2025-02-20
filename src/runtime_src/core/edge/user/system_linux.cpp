b2972ecaf5164216113644049ec4d0592e7c338c
rbramand <rbramand@amd.com>
2025-02-20 12:53:11 +0530

    Multi shim changes
    Signed-off-by: rbramand <rbramand@amd.com>

--- src/runtime_src/core/edge/user/system_linux.cpp
+++ src/runtime_src/core/edge/user/system_linux.cpp
@@ -16,17 +16,24 @@
  */
 
 // Local - Include files
+#include "shim.h"
 #include "system_linux.h"
 #include "device_linux.h"
 #include "core/common/time.h"
+
+#include "plugin/xdp/hal_profile.h"
 
 // 3rd Party Library - Include files
 #include <boost/property_tree/ini_parser.hpp>
 #include <boost/format.hpp>
 
 // System - Include files
+#include <fcntl.h>
+#include <filesystem>
 #include <fstream>
 #include <memory>
+#include <regex>
+#include <sys/ioctl.h>
 #include <thread>
 #include <unistd.h>
 
@@ -41,6 +48,25 @@
 #else
 #error "Unsupported platform"
   #define MACHINE_NODE_PATH ""
+#endif
+
+#ifdef EDGE_VE2
+#include "shim/device.h"
+
+// When EDGE_VE2 flag is set both zocl and VE2 shims are
+// combined.
+// Map for holding valid devices/shims of combined shim
+// [int, device type] -> [device index, shim/driver type]
+// As either of AIARM or ZOCL or both can be active together
+// we need mapping of device id to device type
+namespace {
+enum class dev_type : uint16_t
+{
+  aiarm = 0,
+  zocl = 1
+};
+static std::unordered_map<uint16_t, dev_type> dev_map;
+}
 #endif
 
 namespace {
@@ -122,8 +148,18 @@ std::shared_ptr<device>
 system_linux::
 get_userpf_device(device::handle_type handle, device::id_type id) const
 {
+#ifdef EDGE_VE2
+  auto type = dev_map[id];
+  if (type == dev_type::aiarm)
+    return std::shared_ptr<aiarm::device>(new aiarm::device(handle, id, true));
+  else if (type == dev_type::zocl)
+    return std::shared_ptr<device_linux>(new device_linux(handle, id, true));
+
+  throw std::runtime_error("get_userpf_device failed for device id : " + std::to_string(id));
+#else
   // deliberately not using std::make_shared (used with weak_ptr)
   return std::shared_ptr<device_linux>(new device_linux(handle, id, true));
+#endif
 }
 
 std::shared_ptr<device>
@@ -153,3 +189,202 @@ get_userpf_device(device::handle_type device_handle, device::id_type id)
 } // edge_linux
 
 } // xrt_core
+
+// Implementation of xclProbe and xclOpen used by system class.
+// By default xclProbe detects only zocl device but when flag
+// EDGE_VE2 is set shims of both traditional zocl and VE2
+// are combined and xclProbe should detect both kinds of devices
+#ifndef EDGE_VE2
+unsigned
+xclProbe()
+{
+  return xdp::hal::profiling_wrapper("xclProbe", [] {
+
+  const std::string zocl_drm_device = "/dev/dri/" + get_render_devname();
+  int fd;
+  if (std::filesystem::exists(zocl_drm_device)) {
+    fd = open(zocl_drm_device.c_str(), O_RDWR);
+    if (fd < 0)
+      return 0;
+  }
+  /*
+   * Zocl node is not present in some platforms static dtb, it gets loaded
+   * using overlay dtb, drm device node is not created until zocl is present
+   * So if enable_flat is set return 1 valid device
+   */
+  else if (xrt_core::config::get_enable_flat())
+    return 1;
+
+  std::vector<char> name(128,0);
+  std::vector<char> desc(512,0);
+  std::vector<char> date(128,0);
+  drm_version version;
+  std::memset(&version, 0, sizeof(version));
+  version.name = name.data();
+  version.name_len = 128;
+  version.desc = desc.data();
+  version.desc_len = 512;
+  version.date = date.data();
+  version.date_len = 128;
+
+  int result = ioctl(fd, DRM_IOCTL_VERSION, &version);
+  if (result) {
+    close(fd);
+    return 0;
+  }
+
+  result = std::strncmp(version.name, "zocl", 4);
+  close(fd);
+  return (result == 0) ? 1 : 0;
+  });
+}
+
+xclDeviceHandle
+xclOpen(unsigned deviceIndex, const char*, xclVerbosityLevel)
+{
+  return xdp::hal::profiling_wrapper("xclOpen",
+  [deviceIndex] {
+
+  try {
+    //std::cout << "xclOpen called" << std::endl;
+    if (deviceIndex >= xclProbe()) {
+      xrt_core::message::send(xrt_core::message::severity_level::info, "XRT",
+                       std::string("Cannot find index " + std::to_string(deviceIndex) + " \n"));
+      return static_cast<xclDeviceHandle>(nullptr);
+    }
+
+    auto handle = new ZYNQ::shim(deviceIndex);
+    bool checkDrmFD = xrt_core::config::get_enable_flat() ? false : true;
+    if (!ZYNQ::shim::handleCheck(handle, checkDrmFD)) {
+      delete handle;
+      handle = XRT_NULL_HANDLE;
+    }
+    return static_cast<xclDeviceHandle>(handle);
+  }
+  catch (const xrt_core::error& ex) {
+    xrt_core::send_exception_message(ex.what());
+  }
+  catch (const std::exception& ex) {
+    xrt_core::send_exception_message(ex.what());
+  }
+
+  return static_cast<xclDeviceHandle>(XRT_NULL_HANDLE);
+
+  }) ;
+}
+#else
+// xclProbe and xclOpen definitions for combined shim
+unsigned
+xclProbe()
+{
+  static const std::string render_dev_sym_dir{"/dev/dri/by-path/"};
+  std::string aiarm_render_devname;
+  std::string zocl_render_devname;
+
+  // On Edge AIE platforms 'telluride_drm' is the name of aiarm node in device tree
+  // On Edge platforms 'zyxclmm_drm' is the name of zocl node in device tree
+  // A symlink to render devices are created based on these node names
+  try {
+    static const std::regex aiarm_filter{"platform.*telluride_drm-render"};
+    static const std::regex zocl_filter{"platform.*zyxclmm_drm-render"};
+    if (!std::filesystem::exists(render_dev_sym_dir)) {
+      std::string msg{"DRM device path - " + render_dev_sym_dir +
+                      "doesn't exist, cannot detect devices"};
+      xrt_core::message::send(xrt_core::message::severity_level::error, "XRT", msg);
+      return 0;
+    }
+    std::filesystem::directory_iterator end_itr;
+    for (std::filesystem::directory_iterator itr{render_dev_sym_dir}; itr != end_itr; ++itr) {
+      if (std::regex_match(itr->path().filename().string(), aiarm_filter)) {
+        aiarm_render_devname = std::filesystem::read_symlink(itr->path()).filename().string();
+      }
+      if (std::regex_match(itr->path().filename().string(), zocl_filter)) {
+        zocl_render_devname = std::filesystem::read_symlink(itr->path()).filename().string();
+      }
+    }
+  }
+  catch (const std::exception& e) {
+    xrt_core::message::send(xrt_core::message::severity_level::error, "XRT",
+                            std::string{"Unable to read renderD* path of devices"} + e.what());
+    return 0;
+  }
+
+  auto check_validity = [](const std::string& drm_dev_name, const std::string& ver_name) {
+    if (!std::filesystem::exists(drm_dev_name))
+      return false;
+
+    int fd = open(drm_dev_name.c_str(), O_RDWR);
+    if (fd < 0)
+      return false;
+
+    std::vector<char> name(128,0);
+    std::vector<char> desc(512,0);
+    std::vector<char> date(128,0);
+    drm_version version;
+    std::memset(&version, 0, sizeof(version));
+    version.name = name.data();
+    version.name_len = 128;
+    version.desc = desc.data();
+    version.desc_len = 512;
+    version.date = date.data();
+    version.date_len = 128;
+    int result = ioctl(fd, DRM_IOCTL_VERSION, &version);
+    if (result) {
+      close(fd);
+      return false;
+    }
+    result = std::strncmp(version.name, ver_name.c_str(), ver_name.length());
+    close(fd);
+    return (result == 0) ? true : false;
+  };
+
+  unsigned device_count = 0;
+  if (check_validity("/dev/dri/" + aiarm_render_devname, "AIARM")) {
+    dev_map[device_count++] = dev_type::aiarm;
+  }
+
+  if (check_validity("/dev/dri/" + zocl_render_devname, "zocl")) {
+    dev_map[device_count++] = dev_type::zocl;
+  }
+
+  return device_count;
+}
+
+xclDeviceHandle
+xclOpen(unsigned deviceIndex, const char*, xclVerbosityLevel)
+{
+  try {
+    if (deviceIndex >= xclProbe()) {
+      xrt_core::message::send(xrt_core::message::severity_level::info, "XRT",
+                              "Cannot find index " + std::to_string(deviceIndex) + " \n");
+      return static_cast<xclDeviceHandle>(nullptr);
+    }
+
+    auto type = dev_map[deviceIndex];
+
+    if (type == dev_type::aiarm) {
+      auto handle = new aiarm::shim(deviceIndex);
+      if (!aiarm::shim::handleCheck(handle)) {
+        delete handle;
+        handle = XRT_NULL_HANDLE;
+      }
+      return static_cast<xclDeviceHandle>(handle);
+    }
+    else if (type == dev_type::zocl) {
+      auto handle = new ZYNQ::shim(deviceIndex);
+      if (!ZYNQ::shim::handleCheck(handle)) {
+        delete handle;
+        handle = XRT_NULL_HANDLE;
+      }
+      return static_cast<xclDeviceHandle>(handle);
+    }
+  }
+  catch (const xrt_core::error& ex) {
+    xrt_core::send_exception_message(ex.what());
+  }
+  catch (const std::exception& ex) {
+    xrt_core::send_exception_message(ex.what());
+  }
+  return static_cast<xclDeviceHandle>(XRT_NULL_HANDLE);
+}
+#endif
