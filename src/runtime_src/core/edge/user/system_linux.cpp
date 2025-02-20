0a41fadc34cfcc70f1da39ada814122220a6a22e
rbramand <rbramand@amd.com>
2025-02-21 00:04:41 +0530

    Refactor code
    Signed-off-by: rbramand <rbramand@amd.com>

--- src/runtime_src/core/edge/user/system_linux.cpp
+++ src/runtime_src/core/edge/user/system_linux.cpp
@@ -52,10 +52,11 @@
 
 #ifdef EDGE_VE2
 #include "shim/device.h"
+#endif
 
-// When EDGE_VE2 flag is set both zocl and VE2 shims are
-// combined.
-// Map for holding valid devices/shims of combined shim
+// This system class enumerates both Traditional Edge devices(Zocl driver)
+// and Edge VE2 devices(AIARM driver).
+// Map for holding valid devices of combined shim
 // [int, device type] -> [device index, shim/driver type]
 // As either of AIARM or ZOCL or both can be active together
 // we need mapping of device id to device type
@@ -66,223 +67,25 @@ enum class dev_type : uint16_t
   zocl = 1
 };
 static std::unordered_map<uint16_t, dev_type> dev_map;
-}
-#endif
 
-namespace {
-
-// Singleton registers with base class xrt_core::system
-// during static global initialization.  If statically
-// linking with libxrt_core, then explicit initialiation
-// is required
-static xrt_core::system_linux*
-singleton_instance()
+static unsigned int
+enumerate_devices()
 {
-  static xrt_core::system_linux singleton;
-  return &singleton;
-}
-
-// Dynamic linking automatically constructs the singleton
-struct X
-{
-  X() { singleton_instance(); }
-} x;
-
-static boost::property_tree::ptree
-driver_version(const std::string& driver)
-{
-  boost::property_tree::ptree _pt;
-  std::string ver("unknown");
-  std::string hash("unknown");
-  //dkms flow is not available for zocl
-  //so version.h file is not available at zocl build time
-#if defined(XRT_DRIVER_VERSION)
-  std::string zocl_driver_ver = XRT_DRIVER_VERSION;
-  std::stringstream ss(zocl_driver_ver);
-  getline(ss, ver, ',');
-  getline(ss, hash, ',');
-#endif
-
-  _pt.put("name", driver);
-  _pt.put("version", ver);
-  _pt.put("hash", hash);
-
-  return _pt;
-}
-
-}
-
-namespace xrt_core {
-
-void
-system_linux::
-get_driver_info(boost::property_tree::ptree &pt)
-{
-  boost::property_tree::ptree _ptDriverInfo;
-  _ptDriverInfo.push_back( std::make_pair("", driver_version("zocl") ));
-  pt.put_child("drivers", _ptDriverInfo);
-}
-
-std::pair<device::id_type, device::id_type>
-system_linux::
-get_total_devices(bool is_user) const
-{
-  device::id_type num = xclProbe();
-  return std::make_pair(num, num);
-}
-
-void
-system_linux::
-scan_devices(bool /*verbose*/, bool /*json*/) const
-{
-}
-
-std::shared_ptr<device>
-system_linux::
-get_userpf_device(device::id_type id) const
-{
-  return xrt_core::get_userpf_device(xclOpen(id, nullptr, XCL_QUIET));
-}
-
-std::shared_ptr<device>
-system_linux::
-get_userpf_device(device::handle_type handle, device::id_type id) const
-{
-#ifdef EDGE_VE2
-  auto type = dev_map[id];
-  if (type == dev_type::aiarm)
-    return std::shared_ptr<aiarm::device>(new aiarm::device(handle, id, true));
-  else if (type == dev_type::zocl)
-    return std::shared_ptr<device_linux>(new device_linux(handle, id, true));
-
-  throw std::runtime_error("get_userpf_device failed for device id : " + std::to_string(id));
-#else
-  // deliberately not using std::make_shared (used with weak_ptr)
-  return std::shared_ptr<device_linux>(new device_linux(handle, id, true));
-#endif
-}
-
-std::shared_ptr<device>
-system_linux::
-get_mgmtpf_device(device::id_type id) const
-{
-  // deliberately not using std::make_shared (used with weak_ptr)
-  return std::shared_ptr<device_linux>(new device_linux(nullptr, id, false));
-}
-
-void
-system_linux::
-program_plp(const device* dev, const std::vector<char> &buffer) const
-{
-  throw std::runtime_error("plp program is not supported");
-}
-
-namespace edge_linux {
-
-std::shared_ptr<device>
-get_userpf_device(device::handle_type device_handle, device::id_type id)
-{
-  singleton_instance(); // force loading if necessary
-  return xrt_core::get_userpf_device(device_handle, id);
-}
-
-} // edge_linux
-
-} // xrt_core
-
-// Implementation of xclProbe and xclOpen used by system class.
-// By default xclProbe detects only zocl device but when flag
-// EDGE_VE2 is set shims of both traditional zocl and VE2
-// are combined and xclProbe should detect both kinds of devices
-#ifndef EDGE_VE2
-unsigned
-xclProbe()
-{
-  return xdp::hal::profiling_wrapper("xclProbe", [] {
-
-  const std::string zocl_drm_device = "/dev/dri/" + get_render_devname();
-  int fd;
-  if (std::filesystem::exists(zocl_drm_device)) {
-    fd = open(zocl_drm_device.c_str(), O_RDWR);
-    if (fd < 0)
-      return 0;
-  }
   /*
    * Zocl node is not present in some platforms static dtb, it gets loaded
    * using overlay dtb, drm device node is not created until zocl is present
    * So if enable_flat is set return 1 valid device
+   * Valid for SOM kind of platforms
    */
-  else if (xrt_core::config::get_enable_flat())
+  if (xrt_core::config::get_enable_flat())
     return 1;
 
-  std::vector<char> name(128,0);
-  std::vector<char> desc(512,0);
-  std::vector<char> date(128,0);
-  drm_version version;
-  std::memset(&version, 0, sizeof(version));
-  version.name = name.data();
-  version.name_len = 128;
-  version.desc = desc.data();
-  version.desc_len = 512;
-  version.date = date.data();
-  version.date_len = 128;
-
-  int result = ioctl(fd, DRM_IOCTL_VERSION, &version);
-  if (result) {
-    close(fd);
-    return 0;
-  }
-
-  result = std::strncmp(version.name, "zocl", 4);
-  close(fd);
-  return (result == 0) ? 1 : 0;
-  });
-}
-
-xclDeviceHandle
-xclOpen(unsigned deviceIndex, const char*, xclVerbosityLevel)
-{
-  return xdp::hal::profiling_wrapper("xclOpen",
-  [deviceIndex] {
-
-  try {
-    //std::cout << "xclOpen called" << std::endl;
-    if (deviceIndex >= xclProbe()) {
-      xrt_core::message::send(xrt_core::message::severity_level::info, "XRT",
-                       std::string("Cannot find index " + std::to_string(deviceIndex) + " \n"));
-      return static_cast<xclDeviceHandle>(nullptr);
-    }
-
-    auto handle = new ZYNQ::shim(deviceIndex);
-    bool checkDrmFD = xrt_core::config::get_enable_flat() ? false : true;
-    if (!ZYNQ::shim::handleCheck(handle, checkDrmFD)) {
-      delete handle;
-      handle = XRT_NULL_HANDLE;
-    }
-    return static_cast<xclDeviceHandle>(handle);
-  }
-  catch (const xrt_core::error& ex) {
-    xrt_core::send_exception_message(ex.what());
-  }
-  catch (const std::exception& ex) {
-    xrt_core::send_exception_message(ex.what());
-  }
-
-  return static_cast<xclDeviceHandle>(XRT_NULL_HANDLE);
-
-  }) ;
-}
-#else
-// xclProbe and xclOpen definitions for combined shim
-unsigned
-xclProbe()
-{
   static const std::string render_dev_sym_dir{"/dev/dri/by-path/"};
   std::string aiarm_render_devname;
   std::string zocl_render_devname;
 
-  // On Edge AIE platforms 'telluride_drm' is the name of aiarm node in device tree
-  // On Edge platforms 'zyxclmm_drm' is the name of zocl node in device tree
+  // On Edge VE2 kind of devices 'telluride_drm' is the name of aiarm node in device tree
+  // On Edge traditional devices 'zyxclmm_drm' is the name of zocl node in device tree
   // A symlink to render devices are created based on these node names
   try {
     static const std::regex aiarm_filter{"platform.*telluride_drm-render"};
@@ -350,28 +153,30 @@ xclProbe()
   return device_count;
 }
 
-xclDeviceHandle
-xclOpen(unsigned deviceIndex, const char*, xclVerbosityLevel)
+static xclDeviceHandle
+get_device_handle(xrt_core::device::id_type id)
 {
   try {
-    if (deviceIndex >= xclProbe()) {
+    auto total_devices = enumerate_devices();
+    if (id >= total_devices) {
       xrt_core::message::send(xrt_core::message::severity_level::info, "XRT",
-                              "Cannot find index " + std::to_string(deviceIndex) + " \n");
+                              "Cannot find index " + std::to_string(id) + " \n");
       return static_cast<xclDeviceHandle>(nullptr);
     }
 
-    auto type = dev_map[deviceIndex];
-
+    auto type = dev_map[id];
+#ifdef EDGE_VE2
     if (type == dev_type::aiarm) {
-      auto handle = new aiarm::shim(deviceIndex);
+      auto handle = new aiarm::shim(id);
       if (!aiarm::shim::handleCheck(handle)) {
         delete handle;
         handle = XRT_NULL_HANDLE;
       }
       return static_cast<xclDeviceHandle>(handle);
     }
-    else if (type == dev_type::zocl) {
-      auto handle = new ZYNQ::shim(deviceIndex);
+#endif
+    if (type == dev_type::zocl) {
+      auto handle = new ZYNQ::shim(id);
       if (!ZYNQ::shim::handleCheck(handle)) {
         delete handle;
         handle = XRT_NULL_HANDLE;
@@ -387,4 +192,126 @@ xclOpen(unsigned deviceIndex, const char*, xclVerbosityLevel)
   }
   return static_cast<xclDeviceHandle>(XRT_NULL_HANDLE);
 }
+
+// Singleton registers with base class xrt_core::system
+// during static global initialization.  If statically
+// linking with libxrt_core, then explicit initialiation
+// is required
+static xrt_core::system_linux*
+singleton_instance()
+{
+  static xrt_core::system_linux singleton;
+  return &singleton;
+}
+
+// Dynamic linking automatically constructs the singleton
+struct X
+{
+  X() { singleton_instance(); }
+} x;
+
+static boost::property_tree::ptree
+driver_version(const std::string& driver)
+{
+  boost::property_tree::ptree _pt;
+  std::string ver("unknown");
+  std::string hash("unknown");
+  //dkms flow is not available for zocl
+  //so version.h file is not available at zocl build time
+#if defined(XRT_DRIVER_VERSION)
+  std::string zocl_driver_ver = XRT_DRIVER_VERSION;
+  std::stringstream ss(zocl_driver_ver);
+  getline(ss, ver, ',');
+  getline(ss, hash, ',');
 #endif
+
+  _pt.put("name", driver);
+  _pt.put("version", ver);
+  _pt.put("hash", hash);
+
+  return _pt;
+}
+
+}
+
+namespace xrt_core {
+
+void
+system_linux::
+get_driver_info(boost::property_tree::ptree &pt)
+{
+  boost::property_tree::ptree _ptDriverInfo;
+  _ptDriverInfo.push_back( std::make_pair("", driver_version("zocl") ));
+  pt.put_child("drivers", _ptDriverInfo);
+}
+
+std::pair<device::id_type, device::id_type>
+system_linux::
+get_total_devices(bool is_user) const
+{
+  device::id_type num = enumerate_devices();
+  return std::make_pair(num, num);
+}
+
+void
+system_linux::
+scan_devices(bool /*verbose*/, bool /*json*/) const
+{
+}
+
+std::shared_ptr<device>
+system_linux::
+get_userpf_device(device::id_type id) const
+{
+  auto handle = get_device_handle(id);
+  return xrt_core::get_userpf_device(handle);
+}
+
+std::shared_ptr<device>
+system_linux::
+get_userpf_device(device::handle_type handle, device::id_type id) const
+{
+  auto type = dev_map[id];
+
+#ifdef EDGE_VE2
+  if (type == dev_type::aiarm) {
+    // deliberately not using std::make_shared (used with weak_ptr)
+    return std::shared_ptr<aiarm::device>(new aiarm::device(handle, id, true));
+  }
+#endif
+
+  if (type == dev_type::zocl) {
+    // deliberately not using std::make_shared (used with weak_ptr)
+    return std::shared_ptr<device_linux>(new device_linux(handle, id, true));
+  }
+
+  throw std::runtime_error("get_userpf_device failed for device id : " + std::to_string(id));
+}
+
+std::shared_ptr<device>
+system_linux::
+get_mgmtpf_device(device::id_type id) const
+{
+  // deliberately not using std::make_shared (used with weak_ptr)
+  return std::shared_ptr<device_linux>(new device_linux(nullptr, id, false));
+}
+
+void
+system_linux::
+program_plp(const device* dev, const std::vector<char> &buffer) const
+{
+  throw std::runtime_error("plp program is not supported");
+}
+
+namespace edge_linux {
+
+std::shared_ptr<device>
+get_userpf_device(device::handle_type device_handle, device::id_type id)
+{
+  singleton_instance(); // force loading if necessary
+  return xrt_core::get_userpf_device(device_handle, id);
+}
+
+} // edge_linux
+
+} // xrt_core
