dcc8ff31da5ff1bb28b2706aacdcb71187ad321e
Soren Soe <2106410+stsoe@users.noreply.github.com>
2025-03-11 14:19:43 -0700

    Add missing include (#8812)
    Signed-off-by: Soren Soe <2106410+stsoe@users.noreply.github.com>

--- src/runtime_src/core/edge/user/system_linux.cpp
+++ src/runtime_src/core/edge/user/system_linux.cpp
@@ -1,24 +1,10 @@
-/**
- * Copyright (C) 2020 Xilinx, Inc
- * Copyright (C) 2022 Advanced Micro Devices, Inc. - All rights reserved
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may
- * not use this file except in compliance with the License. A copy of the
- * License is located at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
-// Local - Include files
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2020 Xilinx, Inc. All rights reserved.
+// Copyright (C) 2022-2025 Advanced Micro Devices, Inc. All rights reserved.
 #include "shim.h"
 #include "system_linux.h"
 #include "device_linux.h"
+#include "core/common/message.h"
 #include "core/common/time.h"
 
 #include "plugin/xdp/hal_profile.h"
