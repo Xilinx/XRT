22b705bbb3338fb5176958baa3f552f0e4b2c974
karthik dmg <karthdmg@amd.com>
2025-04-03 02:48:57 +0530

    Fix for handling CUs with undefined protocol
    Signed-off-by: karthik dmg <karthdmg@amd.com>

--- src/runtime_src/core/pcie/driver/linux/xocl/userpf/xocl_kds.c
+++ src/runtime_src/core/pcie/driver/linux/xocl/userpf/xocl_kds.c
@@ -2333,6 +2333,9 @@ static int xocl_kds_update_xgq(struct xocl_dev *xdev, int slot_hdl,
 		struct xgq_cmd_resp_query_cu resp;
 		void *xgq;
 
+		if (cu_info[i].protocol == CTRL_NONE)
+		continue;
+
 		ret = xocl_kds_xgq_query_cu(xdev, cu_info[i].cu_idx, 0, &resp);
 		if (ret)
 			goto create_regular_cu;
@@ -2546,6 +2549,9 @@ int xocl_kds_unregister_cus(struct xocl_dev *xdev, int slot_hdl)
 	for (i = 0; i < MAX_CUS; i++) {
 		xcu = cu_mgmt->xcus[i];
 		if (!xcu)
+			continue;
+
+		if (xcu->info.protocol == CTRL_NONE)
 			continue;
 
 		/* Unregister the CUs as per slot order */
