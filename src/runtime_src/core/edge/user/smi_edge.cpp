a11a3ef18dcac64f4b5963cdf8d6e57641f166f2
Akshay Tondak <aktondak@amd.com>
2025-04-09 14:55:57 -0700

    SMI improvements (#8861)
    * smi improvements
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * clang tidy and review comments handled
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * clang-tidy updates and build fix
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * build fix
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * default destructor
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * Adding some missing options
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    * Correcting incorrect query params
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    
    ---------
    
    Signed-off-by: Akshay Tondak <Akshay.Tondak@amd.com>
    Co-authored-by: Akshay Tondak <Akshay.Tondak@amd.com>

--- src/runtime_src/core/edge/user/smi_edge.cpp
+++ src/runtime_src/core/edge/user/smi_edge.cpp
@@ -4,9 +4,11 @@
 
 namespace shim_edge::smi {
 
-smi_edge::
-smi_edge() : smi_base(
-  {
+xrt_core::smi::subcommand
+create_validate_subcommand()
+{
+  std::vector<xrt_core::smi::basic_option> validate_test_desc = {
+    {"all", "All applicable validate tests will be executed (default)", "common"},
     {"aux-connection", "Check if auxiliary power is connected", "common"},
     {"dma", "Run dma test", "common"},
     {"hostmem-bw", "Run 'bandwidth kernel' when host memory is enabled", "common"},
@@ -14,10 +16,31 @@ smi_edge() : smi_base(
     {"mem-bw", "Run 'bandwidth kernel' and check the throughput", "common"},
     {"p2p", "Run P2P test", "common"},
     {"pcie-link", "Check if PCIE link is active", "common"},
+    {"quick", "Only the first 4 tests will be executed", "common"},
     {"sc-version","Check if SC firmware is up-to-date", "common"},
     {"verify", "Run 'Hello World' kernel test", "common"}
-  },
-  {
+  };
+
+   std::map<std::string, std::shared_ptr<xrt_core::smi::option>> validate_suboptions;
+  validate_suboptions.emplace("device", std::make_shared<xrt_core::smi::option>("device", "d", "The Bus:Device.Function (e.g., 0000:d8:00.0) device of interest", "common", "", "string"));
+  validate_suboptions.emplace("format", std::make_shared<xrt_core::smi::option>("format", "f", "Report output format. Valid values are:\n"
+                                "\tJSON        - Latest JSON schema\n"
+                                "\tJSON-2020.2 - JSON 2020.2 schema", "common", "JSON", "string"));
+  validate_suboptions.emplace("output", std::make_shared<xrt_core::smi::option>("output", "o", "Direct the output to the given file", "common", "", "string"));
+  validate_suboptions.emplace("help", std::make_shared<xrt_core::smi::option>("help", "h", "Help to use this sub-command", "common", "", "none"));
+  validate_suboptions.emplace("run", std::make_shared<xrt_core::smi::listable_description_option>("run", "r", "Run a subset of the test suite. Valid options are:\n",
+                              "common", "",  "array", validate_test_desc));
+  validate_suboptions.emplace("path", std::make_shared<xrt_core::smi::option>("path", "p", "Path to the directory containing validate xclbins", "hidden", "", "string"));
+  validate_suboptions.emplace("param", std::make_shared<xrt_core::smi::option>("param", "", "Extended parameter for a given test. Format: <test-name>:<key>:<value>", "hidden", "", "string"));
+  validate_suboptions.emplace("pmode", std::make_shared<xrt_core::smi::option>("pmode", "", "Specify which power mode to run the benchmarks in. Note: Some tests might be unavailable for some modes", "hidden", "", "string")); 
+
+  return {"validate", "Validates the given device by executing the platform's validate executable", "common", std::move(validate_suboptions)};
+}
+
+xrt_core::smi::subcommand
+create_examine_subcommand()
+{
+  std::vector<xrt_core::smi::basic_option> examine_report_desc = {
     {"aie", "AIE metadata in xclbin", "common"},
     {"aiemem", "AIE memory tile information", "common"},
     {"aieshim", "AIE shim tile status", "common"},
@@ -34,44 +57,52 @@ smi_edge() : smi_base(
     {"platform", "Platforms flashed on the device", "common"},
     {"qspi-status", "QSPI write protection status", "common"},
     {"thermal", "Thermal sensors present on the device", "common"}
-  },
-  {
-    {"device", "d", "The Bus:Device.Function (e.g., 0000:d8:00.0) device of interest", "common", "", "string"},
-    {"help", "h", "Help to use this sub-command", "common", "", "none"},
-    {"daemon", "", "Update the device daemon configuration", "hidden", "", "none"},
-    {"purge", "", "Remove the daemon configuration file", "hidden", "", "string"},
-    {"host", "", "IP or hostname for device peer", "hidden", "", "string"},
-    {"security", "", "Update the security level for the device", "hidden", "", "string"},
-    {"clk_throttle", "", "Enable/disable the device clock throttling", "hidden", "", "string"},
-    {"ct_threshold_power_override", "", "Update the power threshold in watts", "hidden", "", "string"},
-    {"ct_threshold_temp_override", "", "Update the temperature threshold in celsius", "hidden", "", "string"},
-    {"ct_reset", "", "Reset all throttling options", "hidden", "", "string"},
-    {"showx", "", "Display the device configuration settings", "hidden", "", "string"}
-  })
-{}
+  };
 
-// Create an instance of the derived class
-static shim_edge::smi::smi_edge smi_instance;
+  std::map<std::string, std::shared_ptr<xrt_core::smi::option>> examine_suboptions; 
+  examine_suboptions.emplace("device", std::make_shared<xrt_core::smi::option>("device", "d", "The Bus:Device.Function (e.g., 0000:d8:00.0) device of interest", "common", "", "string"));
+  examine_suboptions.emplace("format", std::make_shared<xrt_core::smi::option>("format", "f", "Report output format. Valid values are:\n"
+                                "\tJSON        - Latest JSON schema\n"
+                                "\tJSON-2020.2 - JSON 2020.2 schema", "common", "JSON", "string"));
+  examine_suboptions.emplace("output", std::make_shared<xrt_core::smi::option>("output", "o", "Direct the output to the given file", "common", "", "string"));
+  examine_suboptions.emplace("help", std::make_shared<xrt_core::smi::option>("help", "h", "Help to use this sub-command", "common", "", "none"));
+  examine_suboptions.emplace("report", std::make_shared<xrt_core::smi::listable_description_option>("report", "r", "The type of report to be produced. Reports currently available are:\n", "common", "", "array", examine_report_desc));
+  examine_suboptions.emplace("element", std::make_shared<xrt_core::smi::option>("element", "e", "Filters individual elements(s) from the report. Format: '/<key>/<key>/...'", "hidden", "", "array"));
+
+  return {"examine", "This command will 'examine' the state of the system/device and will generate a report of interest in a text or JSON format.", "common", std::move(examine_suboptions)};
+} // namespace shim_pcie::smi
+
+xrt_core::smi::subcommand
+create_configure_subcommand()
+{
+  std::map<std::string, std::shared_ptr<xrt_core::smi::option>> configure_suboptions;
+  configure_suboptions.emplace("device", std::make_shared<xrt_core::smi::option>("device", "d", "The Bus:Device.Function (e.g., 0000:d8:00.0) device of interest", "common", "", "string"));
+  configure_suboptions.emplace("help", std::make_shared<xrt_core::smi::option>("help", "h", "Help to use this sub-command", "common", "", "none"));
+  configure_suboptions.emplace("daemon", std::make_shared<xrt_core::smi::option>("daemon", "", "Update the device daemon configuration", "hidden", "", "none"));
+  configure_suboptions.emplace("purge", std::make_shared<xrt_core::smi::option>("purge", "", "Remove the daemon configuration file", "hidden", "", "string"));
+  configure_suboptions.emplace("host", std::make_shared<xrt_core::smi::option>("host", "", "IP or hostname for device peer", "hidden", "", "string"));
+  configure_suboptions.emplace("security", std::make_shared<xrt_core::smi::option>("security", "", "Update the security level for the device", "hidden", "", "string"));
+  configure_suboptions.emplace("clk_throttle", std::make_shared<xrt_core::smi::option>("clk_throttle", "", "Enable/disable the device clock throttling", "hidden", "", "string"));
+  configure_suboptions.emplace("ct_threshold_power_override", std::make_shared<xrt_core::smi::option>("ct_threshold_power_override", "", "Update the power threshold in watts", "hidden", "", "string"));
+  configure_suboptions.emplace("ct_threshold_temp_override", std::make_shared<xrt_core::smi::option>("ct_threshold_temp_override", "", "Update the temperature threshold in celsius", "hidden", "", "string"));
+  configure_suboptions.emplace("ct_reset", std::make_shared<xrt_core::smi::option>("ct_reset", "", "Reset all throttling options", "hidden", "", "string"));
+  configure_suboptions.emplace("showx", std::make_shared<xrt_core::smi::option>("showx", "", "Display the device configuration settings", "hidden", "", "string"));
+
+  return {"configure", "Device and host configuration", "common", std::move(configure_suboptions)};
+}
 
 std::string
 get_smi_config()
 {
-  // Call the get_smi_config method
-  return smi_instance.build_smi_config();
-}
+  // Get the singleton instance
+  auto smi_instance = xrt_core::smi::instance();
 
-const xrt_core::smi::tuple_vector&
-get_validate_tests()
-{
-  // Call the get_validate_tests method
-  return smi_instance.get_validate_tests();
-}
+  // Add subcommands
+  smi_instance->add_subcommand("validate", create_validate_subcommand());
+  smi_instance->add_subcommand("examine", create_examine_subcommand());
+  smi_instance->add_subcommand("configure", create_configure_subcommand());
 
-const xrt_core::smi::tuple_vector&
-get_examine_reports()
-{
-  // Call the get_examine_reports method
-  return smi_instance.get_examine_reports();
+  return smi_instance->build_smi_config();
 }
 
 } // namespace shim_edge::smi
