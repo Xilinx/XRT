dd325663ecc5c82a7cc793142d3f387e25326637
bisingha <bikash.singha@amd.com>
2025-04-10 19:25:14 +0530

    Fix for CR-1235776 and CR-1236948
    Signed-off-by: bisingha <bikash.singha@amd.com>

--- src/runtime_src/core/edge/drm/zocl/edge/zocl_aie.c
+++ src/runtime_src/core/edge/drm/zocl/edge/zocl_aie.c
@@ -120,6 +120,9 @@ zocl_aie_error_cb(void *arg)
 	struct aie_errors *errors;
 	int i;
 
+	DRM_WARN("%s: Received AIE_ERR callback, ignoring it\n", __func__);
+	return;
+
 	if (!slot) {
 		DRM_ERROR("%s: slot is not initialized\n", __func__);
 		return;
