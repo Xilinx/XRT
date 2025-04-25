68a2323de101cabb660637a65e06ddbd70119d7f
Snigdha Gupta <snigupta@xcosnigupta50x.xlnx.xilinx.com>
2025-04-25 14:17:54 -0600

    Fix minor bug
    Signed-off-by: Snigdha Gupta <snigupta@xcosnigupta50x.xlnx.xilinx.com>

--- src/runtime_src/xdp/profile/plugin/aie_status/aie_status_plugin.h
+++ src/runtime_src/xdp/profile/plugin/aie_status/aie_status_plugin.h
@@ -54,7 +54,7 @@ namespace xdp {
     void getTilesForStatus();
     void endPoll();
     std::string getCoreStatusString(uint32_t status);
-    uint64_t getDeviceIDFromHandle(void* handle);
+    uint64_t getDeviceIDFromHandle(void* handle, bool hw_context_flow);
     
     // Threads used by this plugin
     void pollDeadlock(uint64_t index, void* handle);
