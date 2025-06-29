47158cb78c01f8ceda165c84638bba1ddd6e376f
Ishita Ghosh <ishita.ghosh@amd.com>
2025-06-28 18:17:39 -0700

    New API to read MicroController info from AIE_TRACE_METADATA
    Signed-off-by: Ishita Ghosh <ishita.ghosh@amd.com>

--- src/runtime_src/xdp/profile/database/static_info/filetypes/aie_control_config_filetype.h
+++ src/runtime_src/xdp/profile/database/static_info/filetypes/aie_control_config_filetype.h
@@ -1,18 +1,5 @@
-/**
- * Copyright (C) 2022-2023 Advanced Micro Devices, Inc. - All rights reserved
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may
- * not use this file except in compliance with the License. A copy of the
- * License is located at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2022-2025 Advanced Micro Devices, Inc. All rights reserved
 
 #ifndef AIE_CONTROL_CONFIG_FILETYPE_DOT_H
 #define AIE_CONTROL_CONFIG_FILETYPE_DOT_H
@@ -106,6 +93,9 @@ class AIEControlConfigFiletype : public xdp::aie::BaseFiletypeImpl {
         getTiles(const std::string& graph_name,
                  module_type type, 
                  const std::string& kernel_name = "all") const override;
+
+        std::vector<UCInfo>
+        getActiveMicroControllers() const override;
 
     protected:
         std::string getMessage(std::string secName) const;
--- src/runtime_src/xdp/profile/database/static_info/filetypes/aie_trace_config_filetype.h
+++ src/runtime_src/xdp/profile/database/static_info/filetypes/aie_trace_config_filetype.h
@@ -1,18 +1,5 @@
-/**
- * Copyright (C) 2024 Advanced Micro Devices, Inc. - All rights reserved
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may
- * not use this file except in compliance with the License. A copy of the
- * License is located at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2022-2025 Advanced Micro Devices, Inc. All rights reserved
 
 #ifndef AIE_TRACE_CONFIG_FILETYPE_DOT_H
 #define AIE_TRACE_CONFIG_FILETYPE_DOT_H
@@ -51,6 +38,9 @@ class AIETraceConfigFiletype : public AIEControlConfigFiletype {
         getTiles(const std::string& graph_name,
                  module_type type, 
                  const std::string& kernel_name = "all") const override;
+
+        std::vector<UCInfo>
+        getActiveMicroControllers() const override;
 };
 
 } // namespace xdp::aie
