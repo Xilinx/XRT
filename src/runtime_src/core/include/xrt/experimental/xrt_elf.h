b3976d5c85025c91a7a1d8e3e1f633a372dd4720
Soren Soe <2106410+stsoe@users.noreply.github.com>
2025-07-03 21:07:40 -0700

    Amend xrt::elf with constructor from string literal (#9057)
    This resolves ambiguity between std::string and std::string_view
    contructor.
    
    Signed-off-by: Soren Soe <2106410+stsoe@users.noreply.github.com>

--- src/runtime_src/core/include/xrt/experimental/xrt_elf.h
+++ src/runtime_src/core/include/xrt/experimental/xrt_elf.h
@@ -33,6 +33,9 @@ class elf : public detail::pimpl<elf_impl>
 public:
   elf() = default;
 
+  /**
+   * elf() - Constructor from ELF file
+   */
   XRT_API_EXPORT
   explicit
   elf(const std::string& fnm);
@@ -49,6 +52,16 @@ public:
   XRT_API_EXPORT
   explicit
   elf(const std::string_view& data);
+
+  /**
+   * elf() - Constructor from ELF file
+   *
+   * Avoid ambiguity between std::string and std::string_view.
+   */
+  explicit
+  elf(const char* fnm)
+    : elf(std::string(fnm))
+  {}
 
   /**
    * elf() - Constructor from raw ELF data stream
