d9059ef411fa867e9c4a0dcff7c9c02b0c5f1ccf
Akshay Tondak <aktondak@amd.com>
2025-07-18 09:36:40 -0700

    [XRT-SMI]Run recipe migration  (#9073)
    * Run recipe migrations
    
    Signed-off-by: Akshay Tondak <aktondak@amd.com>
    
    * Update to get correct metrics
    
    Signed-off-by: Akshay Tondak <aktondak@amd.com>
    
    ---------
    
    Signed-off-by: Akshay Tondak <aktondak@amd.com>

--- src/runtime_src/core/tools/common/tests/TestAIEReconfigOverhead.cpp
+++ src/runtime_src/core/tools/common/tests/TestAIEReconfigOverhead.cpp
@@ -4,23 +4,15 @@
 #include "TestAIEReconfigOverhead.h"
 #include "TestValidateUtilities.h"
 #include "tools/common/XBUtilities.h"
-#include "xrt/xrt_bo.h"
 #include "xrt/xrt_device.h"
-#include "xrt/xrt_hw_context.h"
-#include "xrt/xrt_kernel.h"
+#include "core/common/runner/runner.h"
+#include "core/common/json/nlohmann/json.hpp"
 namespace XBU = XBUtilities;
+using json = nlohmann::json;
 
-// System - Include Files
-#include <fstream>
-#include <filesystem>
-#include <thread>
-#include <iostream>
-static constexpr size_t buffer_size_mb = 128;
-static constexpr size_t buffer_size = buffer_size_mb * 1024 * 1024; //128 MB
-static constexpr size_t word_count = buffer_size/16;
-static constexpr int itr_count = 1000;
-static constexpr size_t inter_size = 1024 * 1024;
-static constexpr unsigned int StartAddr = 32 * 1024 * 1024;
+static constexpr std::string_view recipe_file = "recipe_aie_reconfig.json";
+static constexpr std::string_view recipe_nop_file = "recipe_nop.json";
+static constexpr std::string_view profile_file = "profile.json";
 
 TestAIEReconfigOverhead::TestAIEReconfigOverhead()
   : TestRunner("aie-reconfig-overhead", "Run end-to-end array reconfiguration overhead through shim DMA")
@@ -29,127 +21,43 @@ TestAIEReconfigOverhead::TestAIEReconfigOverhead()
 boost::property_tree::ptree
 TestAIEReconfigOverhead::run(std::shared_ptr<xrt_core::device> dev)
 {
+
   boost::property_tree::ptree ptree = get_test_header();
-  ptree.erase("xclbin");
+  std::string repo_path = xrt_core::device_query<xrt_core::query::runner>(dev, xrt_core::query::runner::type::aie_reconfig_overhead);
+  repo_path = XBValidateUtils::findPlatformFile(repo_path, ptree);
+  std::string recipe = repo_path + std::string(recipe_file);
+  std::string recipe_noop = repo_path + std::string(recipe_nop_file);
+  std::string profile = repo_path + std::string(profile_file);
+  try
+  {
+    xrt_core::runner runner(xrt::device(dev), recipe, profile, std::filesystem::path(repo_path));
 
-  std::string xclbin_path = XBValidateUtils::get_xclbin_path(dev, xrt_core::query::xclbin_name::type::validate_elf, ptree);
+    //Run 1
+    runner.execute();
+    runner.wait();
+    auto report = json::parse(runner.get_report());
+    auto elapsed = report["cpu"]["elapsed"].get<double>();
 
-  if (!std::filesystem::exists(xclbin_path)){
-    XBValidateUtils::logger(ptree, "Details", "The test is not supported on this device.");
+    runner = xrt_core::runner(xrt::device(dev), recipe_noop, profile, std::filesystem::path(repo_path));
+
+    //Run 2
+    runner.execute();
+    runner.wait();
+    report = json::parse(runner.get_report());
+    auto elapsed_nop = report["cpu"]["elapsed"].get<double>();
+
+    auto iterations = report["cpu"]["iterations"].get<double>(); 
+    double overhead = (elapsed - elapsed_nop) / (iterations * 1000); //NOLINT conversion to ms 
+
+    XBValidateUtils::logger(ptree, "Details", boost::str(boost::format("Array reconfiguration overhead: %.1f ms") % overhead));
+    ptree.put("status", XBValidateUtils::test_token_passed);
     return ptree;
   }
-
-  xrt::xclbin xclbin;
-  try {
-    xclbin = xrt::xclbin(xclbin_path);
-  }
-  catch (const std::runtime_error& ex) {
-    XBValidateUtils::logger(ptree, "Error", ex.what());
+  catch(const std::exception& e)
+  {
+    XBValidateUtils::logger(ptree, "Error", e.what());
     ptree.put("status", XBValidateUtils::test_token_failed);
     return ptree;
   }
-
-  // Determine The DPU Kernel Name
-  auto kernelName = XBValidateUtils::get_kernel_name(xclbin, ptree);
-
-  auto working_dev = xrt::device(dev);
-  working_dev.register_xclbin(xclbin);
-
-
-  xrt::hw_context hwctx;
-  xrt::kernel kernel, kernel_no_op;
-
-  const auto elf_name = xrt_core::device_query<xrt_core::query::elf_name>(dev, xrt_core::query::elf_name::type::aie_reconfig_overhead);
-  auto elf_path = XBValidateUtils::findPlatformFile(elf_name, ptree);
-  
-  if (!std::filesystem::exists(elf_path))
-    return ptree;
-  
-  try {
-    hwctx = xrt::hw_context(working_dev, xclbin.get_uuid());
-    kernel = get_kernel(hwctx, kernelName, elf_path);
-    kernel_no_op = get_kernel(hwctx, kernelName); 
-  } 
-  catch (const std::exception& )
-  {
-    XBValidateUtils::logger (ptree, "Error", "Not enough columns available. Please make sure no other workload is running on the device.");
-    ptree.put("status", XBValidateUtils::test_token_failed);
-    return ptree;
-  }
-
-  //Create BOs
-  xrt::bo bo_ifm, bo_ofm, bo_inter, bo_instr, bo_instr_no_op;
-  
-  bo_ifm = xrt::ext::bo{working_dev, buffer_size};
-  bo_ofm = xrt::ext::bo{working_dev, buffer_size};
-  bo_inter = xrt::ext::bo{working_dev, inter_size};
-
-  // map input buffer
-  // Incremental byte pattern
-  auto ifm_mapped = bo_ifm.map<int*>();
-  for (size_t i = 0; i < word_count; i++)
-    ifm_mapped[i] = (int)(i % word_count);
-
-  //Sync BOs
-  bo_ifm.sync(XCL_BO_SYNC_BO_TO_DEVICE);
-
-  //Log
-  if(XBU::getVerbose()) { 
-    XBValidateUtils::logger(ptree, "Details", boost::str(boost::format("Buffer size: %f MB") % buffer_size_mb));
-    XBValidateUtils::logger(ptree, "Details", boost::str(boost::format("No. of iterations: %f") % itr_count));
-  }
-
-  auto start = std::chrono::high_resolution_clock::now();
-  for (int i = 0 ;i < itr_count ; i++){
-    try{
-      xrt::run run;
-      run = kernel_no_op(3, 0, 0, bo_ifm, 0, bo_ofm, bo_inter, 0);
-
-      // Wait for kernel to be done
-      run.wait2();
-    }
-    catch (const std::exception& ex)
-    {
-      XBValidateUtils::logger(ptree, "Error", ex.what());
-      ptree.put("status", XBValidateUtils::test_token_failed);
-      return ptree;
-    }
-    bo_ofm.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
-  }
-  auto end = std::chrono::high_resolution_clock::now();
-  float elapsedSecsNoOpAverage = std::chrono::duration_cast<std::chrono::duration<float>>(end-start).count();
-  elapsedSecsNoOpAverage /= itr_count;
-
-  start = std::chrono::high_resolution_clock::now();
-  for (int i = 0; i< itr_count; i++)
-  {
-    try{
-      xrt::run run;
-      run = kernel(3, 0, 0, bo_ifm, 0, bo_ofm, bo_inter, 0);
-      // Wait for kernel to be done
-      run.wait2();
-    }
-    catch (const std::exception& ex)
-    {
-      XBValidateUtils::logger(ptree, "Error", ex.what());
-      ptree.put("status", XBValidateUtils::test_token_failed);
-      return ptree;
-    }
-    bo_ofm.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
-    auto *ofm_mapped = bo_ofm.map<int8_t*>();
-    if(std::memcmp(ifm_mapped, ofm_mapped + StartAddr, word_count)){
-      XBValidateUtils::logger(ptree, "Error", "Value read back does not match reference for array reconfiguration instruction buffer");
-      ptree.put("status", XBValidateUtils::test_token_failed);
-      return ptree;
-    }
-  }
-
-  end = std::chrono::high_resolution_clock::now();
-  auto elapsedSecsAverage = std::chrono::duration_cast<std::chrono::duration<double>>(end-start).count();
-  elapsedSecsAverage /= itr_count;
-  double overhead = (elapsedSecsAverage - elapsedSecsNoOpAverage)*1000; //in ms
-
-  XBValidateUtils::logger(ptree, "Details", boost::str(boost::format("Array reconfiguration overhead: %.1f ms") % overhead));
-  ptree.put("status", XBValidateUtils::test_token_passed);
   return ptree;
 }
--- src/runtime_src/core/tools/common/tests/TestCmdChainThroughput.cpp
+++ src/runtime_src/core/tools/common/tests/TestCmdChainThroughput.cpp
@@ -6,22 +6,13 @@
 #include "TestCmdChainThroughput.h"
 #include "TestValidateUtilities.h"
 #include "tools/common/XBUtilities.h"
-#include "xrt/xrt_bo.h"
 #include "xrt/xrt_device.h"
-#include "xrt/xrt_hw_context.h"
-#include "xrt/xrt_kernel.h"
-#include "xrt/experimental/xrt_kernel.h"
+#include "core/common/runner/runner.h"
+#include "core/common/json/nlohmann/json.hpp"
 namespace XBU = XBUtilities;
 
-#include <filesystem>
-
-static constexpr size_t buffer_size = 20;
-
-// It is important to keep the iteration count fairly large
-// in order to hide the ramp-up time of clock scaling. 
-static constexpr int itr_count = 1000;
-static constexpr int run_count = 100;
-
+static constexpr std::string_view recipe_file = "recipe_cmd_chain_throughput.json";
+static constexpr std::string_view profile_file = "profile_cmd_chain_throughput.json";
 // ----- C L A S S   M E T H O D S -------------------------------------------
 TestCmdChainThroughput::TestCmdChainThroughput()
   : TestRunner("cmd-chain-throughput", "Run end-to-end throughput test using command chaining")
@@ -31,193 +22,28 @@ boost::property_tree::ptree
 TestCmdChainThroughput::run(std::shared_ptr<xrt_core::device> dev)
 {
   boost::property_tree::ptree ptree = get_test_header();
-  ptree.erase("xclbin");
+  std::string repo_path = xrt_core::device_query<xrt_core::query::runner>(dev, xrt_core::query::runner::type::cmd_chain_throughput);
+  repo_path = XBValidateUtils::findPlatformFile(repo_path, ptree);
+  std::string recipe = repo_path + std::string(recipe_file);
+  std::string profile = repo_path + std::string(profile_file);
+  try
+  {
+    xrt_core::runner runner(xrt::device(dev), recipe, profile, std::filesystem::path(repo_path));
+    runner.execute();
+    runner.wait();
 
-  // Check Whether Use ELF or DPU Sequence
-  auto elf = XBValidateUtils::get_elf();
-  std::string xclbin_path; 
-  
-  if (!elf) {
-    xclbin_path = XBValidateUtils::get_xclbin_path(dev, xrt_core::query::xclbin_name::type::validate, ptree);
-    if (XBU::getVerbose())
-      XBValidateUtils::logger(ptree, "Details", "Using DPU Sequence");
-  } else {
-    xclbin_path = XBValidateUtils::get_xclbin_path(dev, xrt_core::query::xclbin_name::type::validate_elf, ptree);
-    if (XBU::getVerbose())
-      XBValidateUtils::logger(ptree, "Details", "Using ELF");
-  }
+    auto report = nlohmann::json::parse(runner.get_report());
+    auto throughput = report["cpu"]["throughput"].get<double>();
 
-  if (!std::filesystem::exists(xclbin_path)){
-    XBValidateUtils::logger(ptree, "Details", "The test is not supported on this device.");
-    return ptree;
+    XBValidateUtils::logger(ptree, "Details", boost::str(boost::format("Average throughput: %.1f ops/s") % throughput));
+    ptree.put("status", XBValidateUtils::test_token_passed);
   }
-
-  xrt::xclbin xclbin;
-  try {
-    xclbin = xrt::xclbin(xclbin_path);
-  }
-  catch (const std::runtime_error& ex) {
+  catch (const std::exception& ex)
+  {
     XBValidateUtils::logger(ptree, "Error", ex.what());
     ptree.put("status", XBValidateUtils::test_token_failed);
     return ptree;
   }
 
-  // Determine The DPU Kernel Name
-  auto kernelName = XBValidateUtils::get_kernel_name(xclbin, ptree);
-
-  auto working_dev = xrt::device(dev);
-  working_dev.register_xclbin(xclbin);
-
-  xrt::hw_context hwctx;
-  xrt::kernel kernel;
-
-  if (!elf) { // DPU
-    try {
-      hwctx = xrt::hw_context(working_dev, xclbin.get_uuid());
-      kernel = xrt::kernel(hwctx, kernelName);
-    } 
-    catch (const std::exception& )
-    {
-      XBValidateUtils::logger (ptree, "Error", "Not enough columns available. Please make sure no other workload is running on the device.");
-      ptree.put("status", XBValidateUtils::test_token_failed);
-      return ptree;
-    }
-  }
-  else { // ELF
-    const auto elf_name = xrt_core::device_query<xrt_core::query::elf_name>(dev, xrt_core::query::elf_name::type::nop);
-    auto elf_path = XBValidateUtils::findPlatformFile(elf_name, ptree);
-
-    if (!std::filesystem::exists(elf_path))
-      return ptree;
-    
-    try {
-      hwctx = xrt::hw_context(working_dev, xclbin.get_uuid());
-      kernel = get_kernel(hwctx, kernelName, elf_path);
-    } 
-    catch (const std::exception& )
-    {
-      XBValidateUtils::logger (ptree, "Error", "Not enough columns available. Please make sure no other workload is running on the device.");
-      ptree.put("status", XBValidateUtils::test_token_failed);ptree.put("status", XBValidateUtils::test_token_failed);
-      return ptree;
-    }
-  }
-
-  // Find PS kernel instance as expected by KMD, but
-  // construct the xrt::kernel from the CU base name
-  std::string kernel_name;
-  xrt::xclbin::ip cu;
-  for (const auto& ip : xclbin.get_ips()) {
-    if (ip.get_type() != xrt::xclbin::ip::ip_type::ps)
-      continue;
-
-    cu = ip;
-    auto cu_name = cu.get_name();
-    kernel_name = cu_name.substr(0, cu_name.find(':'));
-    break;
-  }
-
-  // create specified number of runs and populate with arguments
-  std::vector<xrt::bo> global_args;
-  std::vector<xrt::run> runs;
-
-  if (!elf) {
-    for (int i=0; i < run_count; ++i) {
-      auto run = xrt::run(kernel);
-      for (const auto& arg : cu.get_args()) {
-        auto arg_idx = static_cast<int>(arg.get_index());
-        if (arg.get_host_type() == "uint64_t")
-          run.set_arg(arg_idx, static_cast<uint64_t>(XBValidateUtils::get_opcode()));
-        else if (arg.get_host_type() == "uint32_t")
-          run.set_arg(arg_idx, static_cast<uint32_t>(1));
-        else if (arg.get_host_type().find('*') != std::string::npos) {
-          xrt::bo bo;
-          if (arg.get_name() == "instruct")
-            bo = xrt::bo(hwctx, arg.get_size(), xrt::bo::flags::cacheable, kernel.group_id(arg_idx));
-          else 
-            bo = xrt::bo(working_dev, arg.get_size(), xrt::bo::flags::host_only, kernel.group_id(arg_idx));
-  
-        bo.sync(XCL_BO_SYNC_BO_TO_DEVICE);
-        global_args.push_back(bo);
-        run.set_arg(arg_idx, bo);
-        }
-      }
-      runs.push_back(std::move(run));
-    }
-  }
-  else {
-    for (int i=0; i < run_count; ++i) {
-      auto run = xrt::run(kernel);
-      for (const auto& arg : cu.get_args()) {
-        auto arg_idx = static_cast<int>(arg.get_index());
-        if (arg.get_host_type() == "uint64_t") // opcode
-        run.set_arg(arg_idx, static_cast<uint64_t>(XBValidateUtils::get_opcode()));
-        else if (arg.get_host_type() == "uint32_t") // nistruct
-          run.set_arg(arg_idx, static_cast<uint32_t>(0));
-        else if (arg.get_host_type().find('*') != std::string::npos) {
-          if (arg.get_name() == "instruct")
-            run.set_arg(arg_idx, 0);
-          else {
-            xrt::bo bo = xrt::ext::bo{working_dev, buffer_size};
-            bo.sync(XCL_BO_SYNC_BO_TO_DEVICE);
-            global_args.push_back(bo);
-            run.set_arg(arg_idx, bo);
-          }
-        }
-      }
-      runs.push_back(std::move(run));
-    }
-  }
-
-  // Log
-  if(XBU::getVerbose()) {
-    XBValidateUtils::logger(ptree, "Details", boost::str(boost::format("Instruction size: %f bytes") % buffer_size));
-    XBValidateUtils::logger(ptree, "Details", boost::str(boost::format("No. of commands: %f") % (itr_count*run_count)));
-  }
-
-  // For throughput, create two runlist and start them in parallel,
-  // then wait and start each one again. This ensures NPU is always
-  // busy.
-  xrt::runlist runlist1{hwctx};
-  xrt::runlist runlist2{hwctx};
-  for (int i = 0; i < run_count; ++i) {
-    if (i % 2 == 0)
-      runlist1.add(runs[i]);
-    else
-      runlist2.add(runs[i]);
-  }
-
-  auto start = std::chrono::high_resolution_clock::now();
-
-  // start the two runlists
-  runlist1.execute();
-  runlist2.execute();
-
-  // iterate wait and start
-  for (int i = 1; i < itr_count; ++i) {
-    try {
-      runlist1.wait();
-      runlist1.execute();
-      runlist2.wait();
-      runlist2.execute();
-    }
-    catch (const std::exception& ex) {
-      XBValidateUtils::logger(ptree, "Error", ex.what());
-      ptree.put("status", XBValidateUtils::test_token_failed);
-      return ptree;
-    }
-  }
-
-  // wait for last iteration to finish
-  runlist1.wait();
-  runlist2.wait();
-
-  auto end = std::chrono::high_resolution_clock::now();
-  auto elapsedSecs = std::chrono::duration_cast<std::chrono::duration<double>>(end-start).count();
-
-  // Compute the throughput
-  const double throughput = ((itr_count*run_count) / elapsedSecs);
-
-  XBValidateUtils::logger(ptree, "Details", boost::str(boost::format("Average throughput: %.1f ops") % throughput));
-  ptree.put("status", XBValidateUtils::test_token_passed);
   return ptree;
 }
