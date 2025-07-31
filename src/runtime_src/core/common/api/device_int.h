71ac959868da045dc54b0a4146d08d0b7a7eb542
Ch Vamshi Krishna <chvamshi@xcochvamshi40x.xlnx.xilinx.com>
2025-08-01 00:59:42 +0530

    reusing reset function for device reset purpose

--- src/runtime_src/core/common/api/device_int.h
+++ src/runtime_src/core/common/api/device_int.h
@@ -24,6 +24,10 @@ get_xcl_device_handle(xrtDeviceHandle dhdl);
 std::cv_status
 exec_wait(const xrt::device& device, const std::chrono::milliseconds& timeout_ms);
 
+// This API is used by xocl for resetting the shared pointer.
+void
+reset(const xrt::device& device);
+
 }} // device_int, xrt_core
 
 #endif
