63d23c84c82decde4f6170c87802b23c12479588
Paul Schumacher <schuey@xilinx.com>
2025-09-11 09:19:10 -0600

    added placeholder counter addresses
    Signed-off-by: Paul Schumacher <schuey@xilinx.com>

--- src/runtime_src/xdp/profile/plugin/aie_base/generations/npu3_registers.h
+++ src/runtime_src/xdp/profile/plugin/aie_base/generations/npu3_registers.h
@@ -6,7 +6,49 @@
 
 namespace npu3
 {
-  // TODO: Add NPU3-specific memory map here
+
+// TODO: Add NPU3-specific memory map when available
+
+// Core Counters
+const unsigned int cm_performance_counter0 = 0;
+const unsigned int cm_performance_counter1 = 0;
+const unsigned int cm_performance_counter2 = 0;
+const unsigned int cm_performance_counter3 = 0;
+const unsigned int cm_performance_counter4 = 0;
+const unsigned int cm_performance_counter5 = 0;
+const unsigned int cm_performance_counter6 = 0;
+const unsigned int cm_performance_counter7 = 0;
+const unsigned int cm_performance_counter8 = 0;
+const unsigned int cm_performance_counter9 = 0;
+const unsigned int cm_performance_counter10 = 0;
+const unsigned int cm_performance_counter11 = 0;
+// Memory Tile Counters
+const unsigned int mem_performance_counter0 = 0;
+const unsigned int mem_performance_counter1 = 0;
+const unsigned int mem_performance_counter2 = 0;
+const unsigned int mem_performance_counter3 = 0;
+const unsigned int mem_performance_counter4 = 0;
+const unsigned int mem_performance_counter5 = 0;
+const unsigned int mem_performance_counter6 = 0;
+const unsigned int mem_performance_counter7 = 0;
+const unsigned int mem_performance_counter8 = 0;
+const unsigned int mem_performance_counter9 = 0;
+const unsigned int mem_performance_counter10 = 0;
+const unsigned int mem_performance_counter11 = 0;
+// Interface Tile Counters
+const unsigned int shim_performance_counter0 = 0;
+const unsigned int shim_performance_counter1 = 0;
+const unsigned int shim_performance_counter2 = 0;
+const unsigned int shim_performance_counter3 = 0;
+const unsigned int shim_performance_counter4 = 0;
+const unsigned int shim_performance_counter5 = 0;
+const unsigned int shim_performance_counter6 = 0;
+const unsigned int shim_performance_counter7 = 0;
+const unsigned int shim_performance_counter8 = 0;
+const unsigned int shim_performance_counter9 = 0;
+const unsigned int shim_performance_counter10 = 0;
+const unsigned int shim_performance_counter11 = 0;
+
 } // namespace npu3
 
 #endif /* NPU3_REGISTERS_H_ */
