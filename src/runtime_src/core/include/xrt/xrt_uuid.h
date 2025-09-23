39dc9d0f87df468a488b5ff1ff18ba9646b56787
Pavel Shramov <shramov@mexmat.net>
2025-09-23 09:33:15 +0300

    Drop redundant assignment operator for xrt::uuid
    xrt::uuid already has non-explicit constructor from xuid_t that
    enables assignment from xuid_t.
    
    Removed operator is choosen by compiler in value assignment
    `uuid_variable = {};` that leads to call with null pointer and
    segmentation fault.
    
    Signed-off-by: Pavel Shramov <shramov@mexmat.net>
    
    Contribute to PR for CI approval
    
    CI is allowing validation for org members only.  Amend this
    PR with minor changes so as to get CI to accept and validate the PR.
    
    Signed-off-by: Soren Soe <2106410+stsoe@users.noreply.github.com>

--- src/runtime_src/core/include/xrt/xrt_uuid.h
+++ src/runtime_src/core/include/xrt/xrt_uuid.h
@@ -1,19 +1,6 @@
-/**
- * Copyright (C) 2016-2021 Xilinx, Inc
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may
- * not use this file except in compliance with the License. A copy of the
- * License is located at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2020-2022 Xilinx, Inc. All rights reserved.
+// Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
 #ifndef xrt_uuid_h_
 #define xrt_uuid_h_
 
@@ -113,21 +100,6 @@ public:
   {
     uuid source(rhs);
     std::swap(*this,source);
-    return *this;
-  }
-
-  /**
-   * operator=() - assignment
-   *
-   * @param val
-   *   Value to be assigned from
-   * @return 
-   *   Reference to this
-   */
-  uuid&
-  operator=(const xuid_t val)
-  {
-    uuid_copy(m_uuid,val);
     return *this;
   }
 
