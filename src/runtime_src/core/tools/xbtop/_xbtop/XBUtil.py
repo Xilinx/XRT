1eb0b54ff7aa9eed212187ee8d97f708aa2d5ef5
Pavel Shramov <shramov@mexmat.net>
2025-10-01 12:39:22 +0300

    Do not ignore Ctrl-C in xbtop
    Relax TTY mode from raw to cbreak so Ctrl-C can be used as normal.
    
    Signed-off-by: Pavel Shramov <shramov@mexmat.net>

--- src/runtime_src/core/tools/xbtop/_xbtop/XBUtil.py
+++ src/runtime_src/core/tools/xbtop/_xbtop/XBUtil.py
@@ -268,7 +268,7 @@ class _GetchUnix:
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
         try:
-            tty.setraw(sys.stdin.fileno())
+            tty.setcbreak(sys.stdin.fileno())
             ch = sys.stdin.read(1)
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
