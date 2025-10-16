Xilinx/XRT

commit cf0e33ccb485e19c280636f42a3445bd2ebc1b46

Update xrt-runner.exe filtering of profile executions (#9325)
- Fix iterations based on `--iterations <num>`
- Support new switch `[--mode <throughput|latency>]`

New updated profiles support multiple executions from same
profile.json, but this can lead to long run times and high
memory usage for scripted execution of multiple models.

In order to reduce memory footprint and execution time, the runner
executable is enhanced to obey `--iterations <num>` and to support a
new switch (`--mode`) that filters the profile to include only
requested executions modes.  The default behavior executes the full
profile.

```
xrt-runner --help
usage: xrt-runner.exe [options]
 [--recipe <recipe.json>] recipe file to run
 [--profile <profile.json>] execution profile
 [--iterations <number>] override all profile iterations
 [--script <script>] runner script, enables multi-threaded execution
 [--threads <number>] number of threads to use when running script (default: #jobs)
 [--dir <path>] directory containing artifacts (default: current dir)
 [--mode <latency|throughput>] execute only specified mode (default: all)
 [--progress] show progress
 [--asap] process jobs immediately (default: wait for all jobs to initialize)
 [--report [<file>]] output runner metrics to <file> or use stdout for no <file> or '-'

% xrt-runner.exe --recipe recipe.json --profile profile.json [--iterations <num>] [--dir <path>]
% xrt-runner.exe --script runner.json [--threads <num>] [--iterations <num>] [--dir <path>]

Note, [--threads <number>] overrides the default number, where default is the number of
jobs in the runner script.

Note, [--iterations <num>] overrides iterations in profile.json, but not in runner script.
If the runner script specifies iterations for a recipe/profile pair, then this value is
sticky for that recipe/profile pair.

Note, [--mode <latency|throughput>] filters execution sections in profile.json such

src/runtime_src/core/common/runner/main.cpp
@@ -70,9 +70,10 @@ using json = nlohmann::json;
 namespace sfs = std::filesystem;
 
 namespace {
-static bool g_progress = false;   // NOLINT
-static bool g_asap = false;       // NOLINT
-static uint32_t g_iterations = 0; // NOLINT
+static bool g_progress = false;    // NOLINT
+static bool g_asap = false;        // NOLINT
+static uint32_t g_iterations = 0;  // NOLINT
+static std::string g_mode = "all"; // NOLINT
 
 constexpr double
 to_mb(size_t bytes)
@@ -104,31 +105,57 @@ touchup_recipe(const std::string& recipe)
   return json.dump();
 }
 
+// Remove profile execution sections based on specified mode
+static void
+filter_mode(json& profile, const std::string& mode)
+{
+  if (mode == "all")
+    return;
+
+  auto& execs = profile["executions"];
+  execs.erase(std::remove_if(execs.begin(), execs.end(),
+                             [mode](const json& exec) {
+                               return exec["mode"] != mode;
+                             }),
+              execs.end());
+}
+
+// Touch up profile(s) with specified iterations
+static void
+touchup_iterations(json& profile, uint32_t iterations)
+{
+  if (iterations)
+    profile["execution"]["iterations"] = iterations;
+
+  for (auto& exec : profile["executions"])
+    exec["iterations"] = iterations;
+}
+
 // Touch up profiles(s)
 // Return parsed / modified json as a json string
 static std::string
-touchup_profile_mt(const std::string& profile, uint32_t iterations)
+touchup_profile_mt(const std::string& profile, const std::string& mode, uint32_t iterations)
 {
   // disable xrt::runner profile verbosity which is unsynchronized
   // with threaded execution
   std::ifstream istr(profile);
   auto json = json::parse(istr);
   json["execution"]["verbose"] = false;
-  
-  // maybe override this profile iterations
-  if (iterations)
-    json["execution"]["iterations"] = iterations;
+
+  filter_mode(json, mode);
+  touchup_iterations(json, iterations);
   
   return json.dump();
 }
 
 static std::string
-touchup_profile(const std::string& profile, uint32_t iterations)
+touchup_profile(const std::string& profile, const std::string& mode, uint32_t iterations)
 {
   std::ifstream istr(profile);
   auto json = json::parse(istr);
-  if (iterations)
-    json["execution"]["iterations"] = iterations;
+
+  filter_mode(json, mode);
+  touchup_iterations(json, iterations);
 
   return json.dump();
 }
@@ -413,7 +440,9 @@ struct script_runner
       sfs::path recipe = root / node["recipe"];
       auto recipe_json_string = touchup_recipe(recipe.string());
       sfs::path profile = root / node["profile"];
-      auto profile_json_string = touchup_profile_mt(profile.string(), node.value<uint32_t>("iterations", g_iterations));
+      auto iterations = node.value<uint32_t>("iterations", g_iterations);
+      auto mode = node.value<std::string>("mode", g_mode);
+      auto profile_json_string = touchup_profile_mt(profile.string(), mode, iterations);
       sfs::path dir = root / node["dir"];
       m_job_queue.add(job_type{device, std::move(id), recipe_json_string, profile_json_string, dir.string()});
     }
@@ -464,6 +493,7 @@ usage()
   std::cout << " [--script <script>] runner script, enables multi-threaded execution\n";
   std::cout << " [--threads <number>] number of threads to use when running script (default: #jobs)\n";
   std::cout << " [--dir <path>] directory containing artifacts (default: current dir)\n";
+  std::cout << " [--mode <latency|throughput>] execute only specified mode (default: all)\n";
   std::cout << " [--progress] show progress\n";
   std::cout << " [--asap] process jobs immediately (default: wait for all jobs to initialize)\n";
   std::cout << " [--report [<file>]] output runner metrics to <file> or use stdout for no <file> or '-'\n";
@@ -475,7 +505,10 @@ usage()
   std::cout << "jobs in the runner script.\n\n";
   std::cout << "Note, [--iterations <num>] overrides iterations in profile.json, but not in runner script.\n";
   std::cout << "If the runner script specifies iterations for a recipe/profile pair, then this value is\n";
-  std::cout << "sticky for that recipe/profile pair.\n";
+  std::cout << "sticky for that recipe/profile pair.\n\n";
+  std::cout << "Note, [--mode <latency|throughput>] filters execution sections in profile.json such\n";
+  std::cout << "only specified modes are executed. If the runner script specifies a mode for a recipe/profile\n";
+  std::cout << "pair, then this value is sticky for that recipe/profile pair.\n";
 }
 
 // Entry for parsing the runner script file
@@ -513,7 +546,7 @@ run_single(const std::string& recipe, const std::string& profile, const std::str
   xrt_core::systime st;
   xrt::device device{0};
   auto recipe_json_string = touchup_recipe(recipe);
-  auto profile_json_string = touchup_profile(profile, g_iterations);
+  auto profile_json_string = touchup_profile(profile, g_mode, g_iterations);
   xrt_core::runner runner {device, recipe_json_string, profile_json_string, dir};
   runner.execute();
   runner.wait();
@@ -591,6 +624,8 @@ run(int argc, char* argv[])
       profile = arg;
     else if (cur == "--dir" || cur == "-d")
       dir = arg;
+    else if (cur == "--mode" || cur == "-m")
+      g_mode = arg;
     else if (cur == "-f" || cur == "--script")
       script = arg;
     else if (cur == "-t" || cur == "--threads")

commit 091eefe323b790320424f649aaf2de2061593e42

Fix runner regression from #9325
The runner can override profile iterations but the override accidentally
defaulted the value to 0 causing a hang.

Two fixes in this PR.  (1) fix the accidental override, (2) throw
if profile.json specifies iterations less than `1`.

Signed-off-by: Soren Soe <2106410+stsoe@users.noreply.github.com>

src/runtime_src/core/common/runner/main.cpp
@@ -124,8 +124,10 @@ filter_mode(json& profile, const std::string& mode)
 static void
 touchup_iterations(json& profile, uint32_t iterations)
 {
-  if (iterations)
-    profile["execution"]["iterations"] = iterations;
+  if (!iterations)
+    return;
+
+  profile["execution"]["iterations"] = iterations;
 
   for (auto& exec : profile["executions"])
     exec["iterations"] = iterations;

src/runtime_src/core/common/runner/runner.cpp
@@ -2128,6 +2128,15 @@ class profile
       return 1;
     }
 
+    static size_t
+    get_iterations(const json& j)
+    {
+      if (auto value = j.value("iterations", 1))
+        return value;
+
+      throw profile_error("bad iterations value in profile, must be greater than 0");
+    }
+
     void
     execute_iteration(size_t iteration)
     {
@@ -2165,7 +2174,7 @@ class profile
       , m_depth(get_depth(m_mode, j))
       , m_recipe_runs(rr->num_runs())
       , m_executor{m_profile, rr, m_depth}
-      , m_iterations(j.value("iterations", 1))
+      , m_iterations{get_iterations(j)}
       , m_iteration(get_iteration_node(m_mode, j))
       , m_verbose(j.value("verbose", true))
       , m_validate(j.value("validate", false))