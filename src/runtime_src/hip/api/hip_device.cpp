bfaffb6bd5a092948fc86142a2d4b0a3753bcae4
Wendy Liang <wendy.liang@amd.com>
2025-10-15 17:06:27 -0700

    hip: remove memory pool APIs
    Remove memory pool related APIs
    
    Signed-off-by: Wendy Liang <wendy.liang@amd.com>

--- src/runtime_src/hip/api/hip_device.cpp
+++ src/runtime_src/hip/api/hip_device.cpp
@@ -6,7 +6,6 @@
 
 #include "hip/core/common.h"
 #include "hip/core/device.h"
-#include "hip/core/memory_pool.h"
 #include "hip/core/module.h"
 
 #include <cstring>
@@ -56,10 +55,6 @@ device_init()
       continue;
     auto dev = std::make_unique<xrt::core::hip::device>(i);
     device_cache.add(i, std::move(dev));
-    auto default_mem_pool = std::make_shared<xrt::core::hip::memory_pool>(device_cache.get_or_error(i), MAX_MEMORY_POOL_SIZE_NPU, MEMORY_POOL_BLOCK_SIZE_NPU);
-    memory_pool_db[i].push_front(default_mem_pool);
-    current_memory_pool_db[i] = default_mem_pool;
-    insert_in_map(mem_pool_cache, default_mem_pool);
   }
 }
 
