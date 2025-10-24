b35582706ddc99572793d604ab39a9536c71f0b4
Jason Villarreal <jvillar@xilinx.com>
2025-10-24 08:28:05 -0700

    Adding scope to function call
    Signed-off-by: Jason Villarreal <jvillar@xilinx.com>

--- src/runtime_src/core/common/xdp/profile.cpp
+++ src/runtime_src/core/common/xdp/profile.cpp
@@ -54,13 +54,14 @@ namespace {
       }
     }
   }
-
+  
   static bool is_hw_emulation()
   {
     static auto xem = std::getenv("XCL_EMULATION_MODE");
     static bool hwem = xem ? (std::strcmp(xem, "hw_emu") == 0) : false;
     return hwem;
   }
+  
 } // end anonymous namespace
 
 // This file makes the connections between all xrt_coreutil level hooks
@@ -675,7 +676,7 @@ update_device(void* handle, bool hw_context_flow)
            handle,
            hw_context_flow);
 
-  if (!is_hw_emulation()) {
+  if (!::is_hw_emulation()) {
     load_once_and_update(
              []() {
               return ((xrt_core::config::get_device_trace() != "off") ||
