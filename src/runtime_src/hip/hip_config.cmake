4ae60f03d20b2eb7d2031f4ee18346f32e48276c
Rahul Bramandlapalli <rbramand@amd.com>
2025-11-25 17:09:44 +0530

    Add HIP cross compilation support
    Signed-off-by: Rahul Bramandlapalli <rbramand@amd.com>

--- src/runtime_src/hip/hip_config.cmake
+++ src/runtime_src/hip/hip_config.cmake
@@ -1,19 +1,57 @@
 # SPDX-License-Identifier: Apache-2.0
-# Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (C) 2024 - 2025 Advanced Micro Devices, Inc. All rights reserved.
 
 message("-- Looking for HIP include files...")
-if (NOT WIN32)
-  # We should find HIP cmake either in standard cmake locations or in the /opt/rocm location
-  set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "/usr/lib/x86_64-linux-gnu/cmake/hip;/usr/lib/x86_64-linux-gnu/cmake/amd_comgr;/opt/rocm/lib/cmake/hip;/opt/rocm/lib/cmake/amd_comgr")
-  # hip-config itself requires these other directories to find its dependencies
-  set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};/usr/lib/x86_64-linux-gnu/cmake/hip;/usr/lib/x86_64-linux-gnu/cmake/amd_comgr;/usr/lib/x86_64-linux-gnu/cmake/hsa-runtime64;/opt/rocm/lib/cmake/hip;/opt/rocm/lib/cmake/amd_comgr;/opt/rocm/lib/cmake/hsa-runtime64")
-else ()
+if(NOT WIN32)
+  # When cross-compiling, prepend CMAKE_SYSROOT to absolute paths
+  # Otherwise use paths directly on the build machine
+  if (CMAKE_CROSSCOMPILING AND CMAKE_SYSROOT)
+    # We should find HIP cmake either in standard cmake locations or in the /opt/rocm location
+    list(APPEND CMAKE_MODULE_PATH
+      "${CMAKE_SYSROOT}/usr/lib/x86_64-linux-gnu/cmake/hip"
+      "${CMAKE_SYSROOT}/usr/lib/x86_64-linux-gnu/cmake/amd_comgr"
+      "${CMAKE_SYSROOT}/opt/rocm/lib/cmake/hip"
+      "${CMAKE_SYSROOT}/opt/rocm/lib/cmake/amd_comgr"
+    )
+    # hip-config itself requires these other directories to find its dependencies
+    list(APPEND CMAKE_PREFIX_PATH
+      "${CMAKE_SYSROOT}/usr/lib/x86_64-linux-gnu/cmake/hip"
+      "${CMAKE_SYSROOT}/usr/lib/x86_64-linux-gnu/cmake/amd_comgr"
+      "${CMAKE_SYSROOT}/usr/lib/x86_64-linux-gnu/cmake/hsa-runtime64"
+      "${CMAKE_SYSROOT}/opt/rocm/lib/cmake/hip"
+      "${CMAKE_SYSROOT}/opt/rocm/lib/cmake/amd_comgr"
+      "${CMAKE_SYSROOT}/opt/rocm/lib/cmake/hsa-runtime64"
+    )
+  else()
+    # We should find HIP cmake either in standard cmake locations or in the /opt/rocm location
+    list(APPEND CMAKE_MODULE_PATH
+      "/usr/lib/x86_64-linux-gnu/cmake/hip"
+      "/usr/lib/x86_64-linux-gnu/cmake/amd_comgr"
+      "/opt/rocm/lib/cmake/hip"
+      "/opt/rocm/lib/cmake/amd_comgr"
+    )
+    # hip-config itself requires these other directories to find its dependencies
+    list(APPEND CMAKE_PREFIX_PATH
+      "/usr/lib/x86_64-linux-gnu/cmake/hip"
+      "/usr/lib/x86_64-linux-gnu/cmake/amd_comgr"
+      "/usr/lib/x86_64-linux-gnu/cmake/hsa-runtime64"
+      "/opt/rocm/lib/cmake/hip"
+      "/opt/rocm/lib/cmake/amd_comgr"
+      "/opt/rocm/lib/cmake/hsa-runtime64"
+    )
+  endif()
+else()
   set(HIP_PLATFORM "amd")
   # HIP SDK installs hip files to C:/Program Files/AMD/ROCm in windows
   # Latest version installed (6.1, 5.7 or whatever available) will be picked
   # Users can set HIP_DIR to location of HIP installation or default path is used
-  set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} $ENV{HIP_DIR} "C:/Program Files/AMD/ROCm/6.2/lib/cmake/hip" "C:/Program Files/AMD/ROCm/6.1/lib/cmake/hip" "C:/Program Files/AMD/ROCm/5.7/lib/cmake/hip")
-endif ()
+  list(APPEND CMAKE_MODULE_PATH
+    $ENV{HIP_DIR}
+    "C:/Program Files/AMD/ROCm/6.2/lib/cmake/hip"
+    "C:/Program Files/AMD/ROCm/6.1/lib/cmake/hip"
+    "C:/Program Files/AMD/ROCm/5.7/lib/cmake/hip"
+  )
+endif()
 
 include(hip-config)
 message("-- Found at ${HIP_INCLUDE_DIR}")
