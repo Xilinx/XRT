24e924a4a0a5c1350ac887a1e7ea40810d746fd0
Rahul Bramandlapalli <rbramand@amd.com>
2025-11-26 11:43:19 +0530

    Consider multi arch paths
    Signed-off-by: Rahul Bramandlapalli <rbramand@amd.com>

--- src/runtime_src/hip/hip_config.cmake
+++ src/runtime_src/hip/hip_config.cmake
@@ -3,38 +3,78 @@
 
 message("-- Looking for HIP include files...")
 if(NOT WIN32)
+  # Determine library architecture for Debian/Ubuntu multiarch systems
+  # (x86_64-linux-gnu, aarch64-linux-gnu)
+  # CMAKE_LIBRARY_ARCHITECTURE is automatically set by CMake on Debian/Ubuntu
+  # For other distros or incomplete toolchains, provide a reasonable fallback
+  if(NOT CMAKE_LIBRARY_ARCHITECTURE)
+    if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64|arm64|ARM64")
+      set(CMAKE_LIBRARY_ARCHITECTURE "aarch64-linux-gnu")
+    elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "arm")
+      set(CMAKE_LIBRARY_ARCHITECTURE "arm-linux-gnueabihf")
+    else()
+      set(CMAKE_LIBRARY_ARCHITECTURE "x86_64-linux-gnu")
+    endif()
+  endif()
+
   # When cross-compiling, prepend CMAKE_SYSROOT to absolute paths
   # Otherwise use paths directly on the build machine
   if (CMAKE_CROSSCOMPILING AND CMAKE_SYSROOT)
-    # We should find HIP cmake either in standard cmake locations or in the /opt/rocm location
+    # Search paths for HIP cmake files:
+    # - Debian/Ubuntu use multiarch: /usr/lib/<arch>/cmake/
+    # - RHEL/CentOS/Fedora use: /usr/lib64/cmake/ or /usr/lib/cmake/
+    # - ROCm typically installs to: /opt/rocm/lib/cmake/
     list(APPEND CMAKE_MODULE_PATH
-      "${CMAKE_SYSROOT}/usr/lib/x86_64-linux-gnu/cmake/hip"
-      "${CMAKE_SYSROOT}/usr/lib/x86_64-linux-gnu/cmake/amd_comgr"
+      "${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/cmake/hip"
+      "${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/cmake/amd_comgr"
+      "${CMAKE_SYSROOT}/usr/lib64/cmake/hip"
+      "${CMAKE_SYSROOT}/usr/lib64/cmake/amd_comgr"
+      "${CMAKE_SYSROOT}/usr/lib/cmake/hip"
+      "${CMAKE_SYSROOT}/usr/lib/cmake/amd_comgr"
       "${CMAKE_SYSROOT}/opt/rocm/lib/cmake/hip"
       "${CMAKE_SYSROOT}/opt/rocm/lib/cmake/amd_comgr"
     )
     # hip-config itself requires these other directories to find its dependencies
     list(APPEND CMAKE_PREFIX_PATH
-      "${CMAKE_SYSROOT}/usr/lib/x86_64-linux-gnu/cmake/hip"
-      "${CMAKE_SYSROOT}/usr/lib/x86_64-linux-gnu/cmake/amd_comgr"
-      "${CMAKE_SYSROOT}/usr/lib/x86_64-linux-gnu/cmake/hsa-runtime64"
+      "${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/cmake/hip"
+      "${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/cmake/amd_comgr"
+      "${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/cmake/hsa-runtime64"
+      "${CMAKE_SYSROOT}/usr/lib64/cmake/hip"
+      "${CMAKE_SYSROOT}/usr/lib64/cmake/amd_comgr"
+      "${CMAKE_SYSROOT}/usr/lib64/cmake/hsa-runtime64"
+      "${CMAKE_SYSROOT}/usr/lib/cmake/hip"
+      "${CMAKE_SYSROOT}/usr/lib/cmake/amd_comgr"
+      "${CMAKE_SYSROOT}/usr/lib/cmake/hsa-runtime64"
       "${CMAKE_SYSROOT}/opt/rocm/lib/cmake/hip"
       "${CMAKE_SYSROOT}/opt/rocm/lib/cmake/amd_comgr"
       "${CMAKE_SYSROOT}/opt/rocm/lib/cmake/hsa-runtime64"
     )
   else()
-    # We should find HIP cmake either in standard cmake locations or in the /opt/rocm location
+    # Search paths for HIP cmake files:
+    # - Debian/Ubuntu use multiarch: /usr/lib/<arch>/cmake/
+    # - RHEL/CentOS/Fedora use: /usr/lib64/cmake/ or /usr/lib/cmake/
+    # - ROCm typically installs to: /opt/rocm/lib/cmake/
     list(APPEND CMAKE_MODULE_PATH
-      "/usr/lib/x86_64-linux-gnu/cmake/hip"
-      "/usr/lib/x86_64-linux-gnu/cmake/amd_comgr"
+      "/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/cmake/hip"
+      "/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/cmake/amd_comgr"
+      "/usr/lib64/cmake/hip"
+      "/usr/lib64/cmake/amd_comgr"
+      "/usr/lib/cmake/hip"
+      "/usr/lib/cmake/amd_comgr"
       "/opt/rocm/lib/cmake/hip"
       "/opt/rocm/lib/cmake/amd_comgr"
     )
     # hip-config itself requires these other directories to find its dependencies
     list(APPEND CMAKE_PREFIX_PATH
-      "/usr/lib/x86_64-linux-gnu/cmake/hip"
-      "/usr/lib/x86_64-linux-gnu/cmake/amd_comgr"
-      "/usr/lib/x86_64-linux-gnu/cmake/hsa-runtime64"
+      "/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/cmake/hip"
+      "/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/cmake/amd_comgr"
+      "/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/cmake/hsa-runtime64"
+      "/usr/lib64/cmake/hip"
+      "/usr/lib64/cmake/amd_comgr"
+      "/usr/lib64/cmake/hsa-runtime64"
+      "/usr/lib/cmake/hip"
+      "/usr/lib/cmake/amd_comgr"
+      "/usr/lib/cmake/hsa-runtime64"
       "/opt/rocm/lib/cmake/hip"
       "/opt/rocm/lib/cmake/amd_comgr"
       "/opt/rocm/lib/cmake/hsa-runtime64"
