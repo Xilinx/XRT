7c30a1b4f94a3bbe49826b22aa97b2d30decb693
Soren Soe <2106410+stsoe@users.noreply.github.com>
2025-12-08 13:58:23 -0800

    Redo how XRT computes repo paths on Linux
    Repo path(s) are used by XRT internally to locate data files used at
    runtime.  For example, xrt-smi looks for xclbin, elf, files, and more
    from the repo path.
    
    This PR affects Linux only and sets uses repo search path rooted
    
    1. XILINX_XRT if set
    - 1a `$XILINX_XRT` if set
    - 1b otherwise based on location of `xrt_coreutil.so`
    - 2b otherwise `XRT_INSTALL_PREFIX` (/usr, /opt/xilinx/xrt, ...)
    2. XDG_DATA_HOME
    - 2a `$XDG_DATA_HOME/xrt` if `$XDG_DATA_HOME` is set
    - 2b `$HOME/.local/share/xrt` if `$HOME` is set
    
    Signed-off-by: Soren Soe <2106410+stsoe@users.noreply.github.com>

--- src/runtime_src/core/common/detail/linux/xilinx_xrt.h
+++ src/runtime_src/core/common/detail/linux/xilinx_xrt.h
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (C) 2023-2025 Advanced Micro Devices, Inc. All rights reserved.
 
+#include "core/common/debug.h"
 #include <filesystem>
 #include <stdexcept>
 #include <string>
@@ -14,21 +15,45 @@ namespace xrt_core::detail {
 
 namespace sfs = std::filesystem;
 
+// Get XRT install path from DSO location or compile time constant
 sfs::path
 xilinx_xrt()
 {
-  // This returns CMAKE_INSTALL_PREFIX.  The internal default cmake
-  // install path is /opt/xilinx/xrt, for upstreaming most likely /usr.
-  return sfs::path(XRT_INSTALL_PREFIX);
+  Dl_info info {};
+  if (dladdr(reinterpret_cast<void*>(&xilinx_xrt), &info) == 0 || !info.dli_fname)
+    return sfs::path(XRT_INSTALL_PREFIX);
+
+  if (std::string(info.dli_fname).find("libxrt_coreutil") == std::string::npos)
+    return sfs::path(XRT_INSTALL_PREFIX);
+
+  // Relocatable path based on install location of this DSO
+  sfs::path so_path(sfs::canonical(info.dli_fname)); // /.../lib/libxrt_coreutil.so
+  return so_path.parent_path().parent_path();
 }
 
 std::vector<sfs::path>
 platform_repo_path()
 {
-  // If CMAKE_INSTALL_PREFIX is /usr, then /usr/amdxdna will be invalid,
-  // not sure if this matters or why the second path is even present?
-  // Regardless, default /opt/xilinx/xrt behavior remains unchanged.
-  return {sfs::path("/lib/firmware/amdnpu"), xilinx_xrt() / "amdxdna"};
+  std::vector<sfs::path> paths;
+  auto xrt = xrt_core::environment::xilinx_xrt();
+
+  // 1. Install path
+  // If xilinx_xrt is rooted at xrt, then share is subdir
+  // otherwise xrt is rooted at share
+  if (xrt.filename() == "xrt")
+    // rooted at xrt, share is subdir
+    paths.push_back(xrt / "share");
+  else
+    // rooted at non-xrt specific dir, append xrt/share
+    paths.push_back(xrt / "share/xrt");
+
+  // 2. XDG data path ($HOME/.local/share) is also a candidate
+  if (const char* xdg = std::getenv("XDG_DATA_HOME"); xdg && *xdg)
+    paths.push_back(sfs::path(xdg) / "xrt");
+  else if (const char* home = std::getenv("HOME"); home && *home)
+    paths.push_back(sfs::path(home) / ".local/share/xrt");
+
+  return paths;
 }
 
 } // xrt_core::detail
