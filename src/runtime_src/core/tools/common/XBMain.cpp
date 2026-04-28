7ad3d3e32b7b2d9274ded9a3e8706dcda0a8ac60
Ryan Chane <rchane@xilinx.com>
2026-04-28 13:05:58 -0700

    Fix defaulted logic
    Signed-off-by: Ryan Chane <rchane@xilinx.com>

--- src/runtime_src/core/tools/common/XBMain.cpp
+++ src/runtime_src/core/tools/common/XBMain.cpp
@@ -120,29 +120,35 @@ void  main_(int argc, char** argv,
   XBU::setAdvance( bAdvance );
   XBU::setForce( bForce );
 
-  // Was device specified? If not, try to use default device.
-  {
-    const auto& device_var = vm["device"];
-    if (device_var.defaulted()) {
-      boost::property_tree::ptree available_devices = XBU::get_available_devices(isUserDomain);
-      if (available_devices.empty())
-        throw std::runtime_error("No devices found.");
-      if (available_devices.size() > 1) {
-        std::cerr << "\nERROR: Multiple devices found. Please specify a single device using the --device option\n\n";
-        std::cerr << XBUtilities::str_available_devs(isUserDomain) << std::endl;
-        std::cout << std::endl;
-        throw xrt_core::error(std::errc::operation_canceled);
-      }
-      for (const auto& kd : available_devices) {
-        sDevice = kd.second.get<std::string>("bdf");
-        break;
-      }
-    }
-    else if (sDevice.empty() || boost::algorithm::iequals(sDevice, "default")) {
-      std::cerr << "\nERROR: Option --device (-d) requires a BDF.\n";
+  // Was device specified? If not, check for default device.
+  const auto& device_var = vm["device"];
+  if (device_var.defaulted() && boost::iequals(sDevice, "default")) {
+    sDevice.clear();
+    boost::property_tree::ptree available_devices = XBU::get_available_devices(isUserDomain);
+
+    // DRC: Are there any devices
+    if (available_devices.empty()) 
+      throw std::runtime_error("No devices found.");
+
+    // DRC: Are there multiple devices, if so then no default device can be found.
+    if (available_devices.size() > 1) {
+      std::cerr << "\nERROR: Multiple devices found. Please specify a single device using the --device option\n\n";
       std::cerr << XBUtilities::str_available_devs(isUserDomain) << std::endl;
+
+      std::cout << std::endl;
       throw xrt_core::error(std::errc::operation_canceled);
     }
+
+    // We have only 1 item in the array, get it
+    for (const auto &kd : available_devices) {
+      sDevice = kd.second.get<std::string>("bdf");
+      break; // Exit after the first item
+    }
+  }
+  else if (sDevice.empty() || boost::iequals(sDevice, "default")) {
+    std::cerr << "\nERROR: Option --device (-d) requires a BDF.\n";
+    std::cerr << XBUtilities::str_available_devs(isUserDomain) << std::endl;
+    throw xrt_core::error(std::errc::operation_canceled);
   }
 
   // If there is a device value, parse for valid subcommands for this device.
