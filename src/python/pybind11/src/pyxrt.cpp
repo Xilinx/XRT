93201b604f69f31691b8dc0124ed561c2445345b
andrej <an.roesti@gmail.com>
2026-05-14 09:40:22 -0600

    Expose xrt::run::get_ctrl_scratchpad_bo in Python PyXRT bindings
    Signed-off-by: andrej <an.roesti@gmail.com>

--- src/python/pybind11/src/pyxrt.cpp
+++ src/python/pybind11/src/pyxrt.cpp
@@ -445,7 +445,9 @@ PYBIND11_MODULE(pyxrt, m) {
                             return r.wait2(timeout);
                     }, "Wait for the specified milliseconds for the run to complete")
         .def("state", &xrt::run::state, "Return the current execution state of the run.")
-        .def("add_callback", &xrt::run::add_callback, "Register a callback to be invoked when the run reaches the requested state.");
+        .def("add_callback", &xrt::run::add_callback, "Register a callback to be invoked when the run reaches the requested state.")
+        .def("get_ctrl_scratchpad_bo", &xrt::run::get_ctrl_scratchpad_bo,
+             "Get the control scratchpad buffer object associated with this run");
 
     py::class_<xrt::kernel> pyker(m, "kernel", "Represents a set of instances matching a specified name");
 
