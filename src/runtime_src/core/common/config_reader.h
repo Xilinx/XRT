175da8f9a03a360890b284d11984fa6890826464
predutta <predutta@amd.com>
2026-08-19 18:00:57 -0700

    Add AIE dtrace INI getters for L2-L2 memtile metrics
    Signed-off-by: predutta <predutta@amd.com>

--- src/runtime_src/core/common/config_reader.h
+++ src/runtime_src/core/common/config_reader.h
@@ -1011,6 +1011,22 @@ get_aie_dtrace_settings_tile_based_aie_metrics()
   return value;
 }
 
+inline std::string
+get_aie_dtrace_settings_tile_based_memory_tile_metrics()
+{
+  static std::string value =
+      detail::get_string_value("AIE_dtrace_settings.tile_based_memory_tile_metrics", "");
+  return value;
+}
+
+inline std::string
+get_aie_dtrace_settings_l2_l2_design_points()
+{
+  static std::string value =
+      detail::get_string_value("AIE_dtrace_settings.l2_l2_design_points", "");
+  return value;
+}
+
 // AIE_trace_settings
 
 inline std::string
