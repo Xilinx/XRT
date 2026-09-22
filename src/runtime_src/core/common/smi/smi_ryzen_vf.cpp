c333df5d0d7cde433f213a1cc8db71306f7e4750
Akshay Tondak <aktondak@amd.com>
2026-09-22 13:23:47 -0700

    update list of commands for vf
    Signed-off-by: Akshay Tondak <aktondak@amd.com>

--- src/runtime_src/core/common/smi/smi_ryzen_vf.cpp
+++ src/runtime_src/core/common/smi/smi_ryzen_vf.cpp
@@ -13,6 +13,17 @@ config_gen_npu3_vf::
 config_gen_npu3_vf()
   : config_gen_npu3()
 {
+  // VF cannot run preemption-overhead (PF/classic only).
+  validate_test_desc = {
+    {"all", "All applicable validate tests will be executed (default)", "common"},
+    {"df-bw", "Run bandwidth test on data fabric", "hidden"},
+    {"shim-dma-bw", "Run 2xRead/1xWrite bandwidth test for SHIM DMA", "hidden"},
+    {"latency", "Run end-to-end latency test", "common"},
+    {"throughput", "Run end-to-end throughput test", "common"},
+    {"gemm", "Measure the TOPS value of GEMM INT8operations", "common"},
+    {"sanity", "Run a small model and validate sanity of the device", "hidden"}
+  };
+
   examine_report_desc = {
     {"aie-partitions", "AIE partition information", "common"},
     {"all", "All known reports are produced", "common"},
@@ -22,6 +33,18 @@ config_gen_npu3_vf()
     {"clocks", "Clock frequency information", "hidden"},
     {"debug", "Debug configuration settings for the device", "hidden"}
   };
+}
+
+subcommand
+config_gen_npu3_vf::create_configure_subcommand()
+{
+  // VF cannot configure firmware-log, event-trace, or auto-coredump (PF only).
+  std::map<std::string, std::shared_ptr<option>> configure_suboptions;
+  configure_suboptions.emplace("device", std::make_shared<option>("device", "d", "The Bus:Device.Function (e.g., 0000:d8:00.0) device of interest", "common", "", "string"));
+  configure_suboptions.emplace("help", std::make_shared<option>("help", "h", "Help to use this sub-command", "common", "", "none"));
+  configure_suboptions.emplace("pmode", std::make_shared<option>("pmode", "", "Modes: default, powersaver, balanced, performance, turbo", "common", "", "string", true));
+
+  return {"configure", "Device and host configuration", "common", std::move(configure_suboptions)};
 }
 
 } // namespace xrt_core::smi::ryzen
